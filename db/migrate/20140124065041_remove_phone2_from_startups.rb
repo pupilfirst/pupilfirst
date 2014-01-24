73b30d178b23e7260737cb94e2ae749de505ced1
goutham vel <gouthamvel@gmail.com>
2014-01-24 15:22:06 +0530

    remove migrations used for changes in phone field in startups. ps: mess-up with refactoring.

--- db/migrate/20140124065041_remove_phone2_from_startups.rb
+++
@@ -1,5 +0,0 @@
-class RemovePhone2FromStartups < ActiveRecord::Migration
-  def change
-  	remove_column :startups, :phone2
-  end
-end
--- db/migrate/20140124065402_change_phone1_to_phone_in_startups.rb
+++
@@ -1,5 +0,0 @@
-class ChangePhone1ToPhoneInStartups < ActiveRecord::Migration
-  def change
-    rename_column :startups, :phone1, :phone
-  end
-end

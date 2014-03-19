4764cb38fc6c5044043b8a0bb2fe9d93a35d7fe8
goutham vel <gouthamvel@gmail.com>
2014-03-19 17:04:40 +0530

    remove dotenv

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'dotenv'
-Dotenv.load
+if Rails.env.development?
+  require 'dotenv'
+  Dotenv.load
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

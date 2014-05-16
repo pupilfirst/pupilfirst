a75a212c226bdb9c385934a4a226b57b99db3b4b
Hari Gopal <hari@mobme.in>
2014-05-16 15:29:17 +0530

    Setting staging environment to log to STDOUT as well.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -44,6 +44,7 @@ Svapp::Application.configure do
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
+  config.logger = ::Logger.new STDOUT
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]

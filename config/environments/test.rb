47cd628307d4949a1475ade3053603f9068e73cb
Hari Gopal <hari@mobme.in>
2014-05-28 16:42:17 +0530

    Setting cache_classes to true for the test environment.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@ Svapp::Application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = false
+  config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that

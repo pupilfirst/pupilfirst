c58f23a48909ab74dd11360f1e9ba02d4a1a6b3c
Hari Gopal <hari@mobme.in>
2014-06-18 14:48:06 +0530

    Listing categories shouldn't require API token.

--- app/controllers/v1/categories_controller.rb
+++ app/controllers/v1/categories_controller.rb
@@ -1,4 +1,6 @@
 class V1::CategoriesController < V1::BaseController
+  skip_before_filter :require_token
+
   def index
     @categories = if(params[:category_type])
       Category.where(category_type: params[:category_type])

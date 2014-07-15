3af9fe1347a45c90bba86742ab7835adfee0626d
Hari Gopal <hari@mobme.in>
2014-07-15 16:48:17 +0530

    Updating seeds.rb to create admin user as superadmin.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# Create an admin user for the /admin interface. This user is a 'superadmin', who can do everything possible from the
+# ActiveAdmin interface.
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin_type: 'superadmin')

00844b54e30b1e98e75582b0ac88268f50438948
Hari Gopal <hari@mobme.in>
2014-09-04 15:09:42 +0530

    Redirecting logged in user to User show page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,8 @@ class WelcomeController < ApplicationController
   layout 'home'
 
   def index
-    if current_user and current_user.startup.present?
-      redirect_to current_user.startup
-    elsif current_user
-      redirect_to controller: :startups
+    if current_user
+      redirect_to current_user
     end
   end
 

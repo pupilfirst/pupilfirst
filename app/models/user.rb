74fdc8b656dbeee9d2718c9ebe3edffd271746ed
Hari Gopal <hari@mobme.in>
2014-09-12 17:03:39 +0530

    Disabling User registration and adding links to applications on Android and iOS stores.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, #:confirmable,
+  devise :invitable, :database_authenticatable, #:registerable, #:confirmable,
     :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :requests

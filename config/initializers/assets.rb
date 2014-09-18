157faf3ea2f408bdb9dfbe114f0ec3adad3cfa40
Hari Gopal <hari@mobme.in>
2014-09-18 16:11:06 +0530

    Removing deleted files from asset precompile list.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,4 @@ Rails.application.config.assets.precompile += %w(
   active_admin_custom.js
   active_admin_custom.css
   select2.png
-  bootstrap/glyphicons-halflings.png
-  bootstrap/glyphicons-halflings-white.png
 )

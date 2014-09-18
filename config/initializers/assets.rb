274c77df134589d0406bea6934a071767087accd
Hari Gopal <hari@mobme.in>
2014-09-18 15:59:35 +0530

    Adding halflings PNGs to precompile list.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,7 @@
-Rails.application.config.assets.precompile += %w( active_admin_custom.js active_admin_custom.css select2.png )
+Rails.application.config.assets.precompile += %w(
+  active_admin_custom.js
+  active_admin_custom.css
+  select2.png
+  bootstrap/glyphicons-halflings.png
+  bootstrap/glyphicons-halflings-white.png
+)

cf8cd835b9419e21ba7c2ff6a7a04133bc36dbf9
Hari Gopal <hari@mobme.in>
2014-09-22 14:59:14 +0530

    Better messages for frontend User#show.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,19 @@
 module UsersHelper
+  def startup_html(startup)
+    if startup.present?
+      link_to startup.try(:name), startup_url(startup)
+    else
+      '<em>Not member of a Startup yet</em>'.html_safe
+    end
+  end
+
+  def value_or_not_available(value)
+    if value
+      value
+    else
+      '<em>Not Available</em>'.html_safe
+    end
+  end
 
   def generate_sep_pdf(out_file, profile_pic, signature, data={})
     require "prawn"

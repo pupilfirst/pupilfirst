3f6ed0fa540bc47e83a3ccf2396c09eb2789d592
Hari Gopal <hari@mobme.in>
2014-09-25 15:15:03 +0530

    Wrapping up the welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
-  layout 'home'
-
   def index
     if current_user
       redirect_to current_user
     end
+
+    @full_width = true
   end
 
   def team

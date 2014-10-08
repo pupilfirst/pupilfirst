64de04c7e275640512f3eb498d74c88281b5f056
Hari Gopal <hari@mobme.in>
2014-10-08 17:19:03 +0530

    Setting constant values for Mentor model.

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -1,3 +1,31 @@
 class Mentor < ActiveRecord::Base
   belongs_to :user
+  accepts_nested_attributes_for :user
+
+  TIME_AVAILABILITY_MORNING_WEEKDAYS = 'morning_weekdays'
+  TIME_AVAILABILITY_EVENING_WEEKDAYS = 'evening_weekdays'
+  TIME_AVAILABILITY_MIDDAY_WEEKDAYS = 'midday_weekdays'
+  TIME_AVAILABILITY_MIDDAY_WEEKENDS = 'midday_weekends'
+
+  SKILL_PROGRAMMING = 'programming'
+  SKILL_SALES = 'sales'
+  SKILL_FINANCE = 'finance'
+
+  EXPERTISE_NOVICE = 'novice'
+  EXPERTISE_INTERMEDIATE = 'intermediate'
+  EXPERTISE_ADVANCED = 'advanced'
+  EXPERTISE_EXPERT = 'expert'
+
+  CTC_BELOW_3L = 150000
+  CTC_BETWEEN_3L_AND_6L = 450000
+  CTC_BETWEEN_6L_AND_12L = 900000
+  CTC_BETWEEN_12L_AND_36L = 2400000
+  CTC_BETWEEN_36L_AND_1CR = 6800000
+  CTC_ABOVE_1_CR = 10000000
+
+  DONATE_100 = 100
+  DONATE_75 = 75
+  DONATE_50 = 50
+  DONATE_25 = 25
+  DONATE_0 = 0
 end

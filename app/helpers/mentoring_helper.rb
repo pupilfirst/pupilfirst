d347fc824765dd17371972bbfc289a3d231f0c7d
Hari Gopal <hari@mobme.in>
2014-10-13 11:41:57 +0530

    Making company level selectable from list.

--- app/helpers/mentoring_helper.rb
+++ app/helpers/mentoring_helper.rb
@@ -1,2 +1,20 @@
 module MentoringHelper
+  def company_level_label(product_progress)
+    case product_progress
+      when Startup::PRODUCT_PROGRESS_IDEA
+        'Idea stage'
+      when Startup::PRODUCT_PROGRESS_MOCKUP
+        'Mockup stage'
+      when Startup::PRODUCT_PROGRESS_PROTOTYPE
+        'Prototyping'
+      when Startup::PRODUCT_PROGRESS_PRIVATE_BETA
+        'In Private Beta'
+      when Startup::PRODUCT_PROGRESS_PUBLIC_BETA
+        'In Public Beta'
+      when Startup::PRODUCT_PROGRESS_LAUNCHED
+        'Launched'
+      else
+        '<em>Not Known</em>'.html_safe
+    end
+  end
 end

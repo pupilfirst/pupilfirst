0489b22b9ea8e3ec7e68cb761d0bdeb641812c65
Hari Gopal <hari@mobme.in>
2014-10-28 18:21:29 +0530

    Better validation for StartupLink.

--- app/models/startup_link.rb
+++ app/models/startup_link.rb
@@ -1,8 +1,10 @@
 class StartupLink < ActiveRecord::Base
   belongs_to :startup
 
-  validates_length_of :name, maximum: 100
-  validates_length_of :url, maximum: 255
+  validates_length_of :name, within: 1..100
+  validates_length_of :url, within: 1..255
   validates :url, url: true
-  validates_length_of :description, maximum: 255, allow_nil: true
+  validates_length_of :description, maximum: 255
+
+  nilify_blanks
 end

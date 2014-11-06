7b85ac48153ae1e8527548de4c3880656aeae8f2
Hari Gopal <hari@mobme.in>
2014-11-06 16:49:23 +0530

    Adding mentor_skill to the list of acceptable category types.

--- app/models/category.rb
+++ app/models/category.rb
@@ -9,7 +9,7 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :startups, join_table: 'startups_categories'
   has_and_belongs_to_many :users
 
-  TYPES = %w(event news startup user) unless defined?(TYPES)
+  TYPES = %w(event news startup user mentor_skill) unless defined?(TYPES)
 
   # Before a category is destroyed, make sure that entries in association tables are removed.
   before_destroy do

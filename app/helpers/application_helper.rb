6f8ad5c9f70d3d1c95e795fac3afad155f2938eb
Hari Gopal <hari@mobme.in>
2014-12-02 16:05:05 +0530

    Displaying mentor info, if available on user profile page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,25 @@
 module ApplicationHelper
-
   def fmt_time(t)
     raise ArgumentError.new("Should be a instance of Time/Date/DateTime. #{t.class} given.") unless t.is_a?(Time) or t.is_a?(Date) or t.is_a?(DateTime)
     t.strftime("%F %R%z")
+  end
+
+  def company_level_html(product_progress)
+    case product_progress
+      when Startup::PRODUCT_PROGRESS_IDEA
+        'Idea stage'
+      when Startup::PRODUCT_PROGRESS_MOCKUP
+        'Mockup stage'
+      when Startup::PRODUCT_PROGRESS_PROTOTYPE
+        'Prototyping'
+      when Startup::PRODUCT_PROGRESS_PRIVATE_BETA
+        'In Private Beta'
+      when Startup::PRODUCT_PROGRESS_PUBLIC_BETA
+        'In Public Beta'
+      when Startup::PRODUCT_PROGRESS_LAUNCHED
+        'Launched'
+      else
+        '<em>Not Known</em>'.html_safe
+    end
   end
 end
--- app/helpers/mentoring_helper.rb
+++ app/helpers/mentoring_helper.rb
@@ -1,23 +1,4 @@
 module MentoringHelper
-  def company_level_label(product_progress)
-    case product_progress
-      when Startup::PRODUCT_PROGRESS_IDEA
-        'Idea stage'
-      when Startup::PRODUCT_PROGRESS_MOCKUP
-        'Mockup stage'
-      when Startup::PRODUCT_PROGRESS_PROTOTYPE
-        'Prototyping'
-      when Startup::PRODUCT_PROGRESS_PRIVATE_BETA
-        'In Private Beta'
-      when Startup::PRODUCT_PROGRESS_PUBLIC_BETA
-        'In Public Beta'
-      when Startup::PRODUCT_PROGRESS_LAUNCHED
-        'Launched'
-      else
-        '<em>Not Known</em>'.html_safe
-    end
-  end
-
   def cost_to_company_collection
     {
       'Less than 3 lakh' => Mentor::CTC_BELOW_3L,

e047432aef224678ada2f42ca528e949de27d8e8
Hari Gopal <hari@mobme.in>
2014-12-03 12:16:23 +0530

    Chaning the site title.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -7,7 +7,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Svapp"
+  config.site_title = 'Startup Village'
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.

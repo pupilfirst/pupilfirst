dc5b1dd3f430dbe45c024c9118702a9b2ebbb36c
Hari Gopal <hari@mobme.in>
2015-01-09 15:30:56 +0530

    Minor changes.

--- app/controllers/startup_jobs_controller.rb
+++ app/controllers/startup_jobs_controller.rb
@@ -1,18 +1,18 @@
 class StartupJobsController < ApplicationController
   before_filter :authenticate_user!, except: [:show, :list_all, :index]
   before_filter :restrict_to_startup_founders, only: [:new, :create, :repost]
-  
+
   def new
-    @startup = Startup.find(params[:startup_id])
+    @startup = Startup.find params[:startup_id]
     @startup_job = @startup.startup_jobs.new
     @agreement_live = @startup.is_agreement_live?
 
     unless @agreement_live
       flash[:alert] = 'Your do have an active agreement with Startup Village. Please enter into agreement with SV to use this feature.'
-      redirect_to startup_startup_jobs_path(@startup, @startup_job)
+      redirect_to startup_startup_jobs_path @startup, @startup_job
     end
   end
-  
+
   def create
     @startup = Startup.find params[:startup_id]
     @startup_jobs = @startup.startup_jobs.new startup_job_params
@@ -26,15 +26,14 @@ class StartupJobsController < ApplicationController
   end
 
   def show
-    @startup = Startup.find(params[:startup_id])
-    @startup_job = @startup.startup_jobs.find(params[:id])
+    @startup = Startup.find params[:startup_id]
+    @startup_job = @startup.startup_jobs.find params[:id]
   end
 
   def index
-    @startup = Startup.find(params[:startup_id])
+    @startup = Startup.find params[:startup_id]
     @startup_jobs = @startup.startup_jobs.all
     @startup_founder = disallow_unauthenticated_repost
-    
   end
 
   def list_all
@@ -47,19 +46,23 @@ class StartupJobsController < ApplicationController
     @startup_job.expires_on = StartupJob::EXPIRY_DURATION
     @startup_job.save!
 
-    redirect_to startup_startup_jobs_path(@startup,@startup_job)
+    redirect_to startup_startup_jobs_path(@startup, @startup_job)
   end
 
   def destroy
-    @startup = Startup.find(params[:startup_id])
-    @startup_job = @startup.startup_jobs.find(params[:startup_job_id])
-    @startup_job.destroy
+    @startup = Startup.find params[:startup_id]
+    @startup_job = @startup.startup_jobs.find params[:startup_job_id]
+    @startup_job.destroy!
+
     redirect_to startup_startup_jobs_path(@startup, @startup_job)
   end
 
   private
+
   def startup_job_params
-    params.require(:startup_job).permit(:title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff)
+    params.require(:startup_job).permit(
+      :title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff
+    )
   end
 
 
@@ -67,10 +70,9 @@ class StartupJobsController < ApplicationController
     if current_user.startup.try(:id) != params[:startup_id].to_i || !current_user.is_founder?
       raise_not_found
     end
-  end 
+  end
 
   def disallow_unauthenticated_repost
     (current_user.try(:startup).try(:id) == params[:startup_id].to_i) && (current_user.try(:is_founder?))
   end
-
 end

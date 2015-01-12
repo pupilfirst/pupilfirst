10a525d4a962f3fcb7512ace2be890dfa725c677
Vishnu Vijayakumar <vishnu.vijayakumar@mobme.in>
2015-01-12 12:40:15 +0530

    Refactoring 'new job' entries should not exceed minimum value than it's maximum value

--- app/models/startup_job.rb
+++ app/models/startup_job.rb
@@ -3,11 +3,14 @@ class StartupJob < ActiveRecord::Base
 
   belongs_to :startup
 
-  validates :title, presence: true
-  validates :salary_min, presence: true
-  validates_presence_of :equity_min, if: :equity_max
+  validates :title, :salary_min, presence: true, if: :startup_id
+  validates_numericality_of :salary_min, less_than: :salary_max, if: :salary_max
+  validates_presence_of :equity_min, if: :equity_max 
   validates_presence_of :equity_max, if: :equity_min
-  validates_presence_of :equity_cliff, :equity_vest, if: :equity_min
+  validates_numericality_of :equity_max, greater_than: :equity_min, if: :equity_min
+  validates_presence_of :equity_cliff, if: :equity_vest && :equity_min
+  validates_numericality_of :equity_cliff, greater_than: :equity_vest, if: :equity_vest
+  validates_presence_of :equity_vest, if: :equity_cliff, less_than: :equity_cliff
   validates :description, length: { maximum: 500 }
 
   # TODO: Minimum values should not be greater than maximum values.

1336d1e8bd4c10046fff43ec8eb720accc394210
Vishnu Vijayakumar <vishnu.vijayakumar@mobme.in>
2015-01-12 12:30:02 +0530

    Added 'Startup_id' to 'permitted params' of activeadmin

--- app/admin/startup_job.rb
+++ app/admin/startup_job.rb
@@ -2,5 +2,5 @@ ActiveAdmin.register StartupJob do
   controller do
     newrelic_ignore
   end
-  permit_params :title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff, :expires_on
+  permit_params :title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff, :expires_on, :startup_id
 end

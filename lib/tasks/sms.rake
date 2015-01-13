eff21b39c4e1bee5b51cce9e559b8ac5b5567b1c
Sonu Oommen <sonu.oommen@mobme.in>
2015-01-13 15:24:49 +0530

    sms task, test pending

--- lib/tasks/sms.rake
+++ lib/tasks/sms.rake
@@ -17,8 +17,8 @@ namespace :sms do
       "On Campus: #{startups_at_visakhapatnam.physically_incubated.count}\n" +
       "Incubated startups (cumulative): #{startups_at_visakhapatnam.agreement_signed.count}"
 
-    msisdns_total = APP_CONFIG[:sms_statistics_all] + APP_CONFIG[:sms_statistics_total]
-    msisdns_visakhapatnam = APP_CONFIG[:sms_statistics_all] + APP_CONFIG[:sms_statistics_visakhapatnam]
+    msisdns_total =  DbConfig.where(key: 'sms_statistics_all' || 'sms_statistics_total').pluck(:value).first.split(",")
+    msisdns_visakhapatnam = DbConfig.where(key: 'sms_statistics_all' || 'sms_statistics_visakhapatnam').pluck(:value).first.split(",")
 
     msisdns_total.each do |msisdn|
       RestClient.post(APP_CONFIG[:sms_provider_url], text: statistics_total, msisdn: msisdn)
--- spec/lib/tasks/sms_rake_spec.rb
+++ spec/lib/tasks/sms_rake_spec.rb
@@ -5,23 +5,24 @@ describe 'sms:statistics' do
 
   # Let's override environment variable values...
   before(:all) do
-    APP_CONFIG[:sms_statistics_all] = %w(919876543210)
-    APP_CONFIG[:sms_statistics_total] = %w(917876543210)
-    APP_CONFIG[:sms_statistics_visakhapatnam] = %w(918976543210)
+    # APP_CONFIG[:sms_statistics_all] = %w(919876543210)
+    # APP_CONFIG[:sms_statistics_total] = %w(917876543210)
+    # APP_CONFIG[:sms_statistics_visakhapatnam] = %w(918976543210)
     APP_CONFIG[:sms_provider_url] = 'https://mobme.in'
   end
 
-  # ...and return them to originals after the tests are run.
+  # # ...and return them to originals after the tests are run.
   after(:all) do
-    APP_CONFIG[:sms_statistics_all] = ENV['SMS_STATISTICS_ALL'].split(',')
-    APP_CONFIG[:sms_statistics_total] = ENV['SMS_STATISTICS_TOTAL'].split(',')
-    APP_CONFIG[:sms_statistics_visakhapatnam] = ENV['SMS_STATISTICS_VISAKHAPATNAM'].split(',')
+    # APP_CONFIG[:sms_statistics_all] = ENV['SMS_STATISTICS_ALL'].split(',')
+    # APP_CONFIG[:sms_statistics_total] = ENV['SMS_STATISTICS_TOTAL'].split(',')
+    # APP_CONFIG[:sms_statistics_visakhapatnam] = ENV['SMS_STATISTICS_VISAKHAPATNAM'].split(',')
     APP_CONFIG[:sms_provider_url] = ENV['SMS_PROVIDER_URL']
   end
 
-  it 'includes environment' do
-    expect(subject.prerequisites).to include('environment')
-  end
+  # it 'includes environment' do
+  #   expect(subject.prerequisites).to include('environment')
+  # end
+  binding.pry
 
   it 'does something' do
     total_statistics = "Total statistics\nTotal incubation requests: 5281\nIncubated startups: 0\nCommunity: 0\nStudent entrepreneurs: 0\nOn Campus: 0\nIncubated startups (cumulative): 849\n"

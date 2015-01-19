9b9079634b0e9f2963b6dffd4b02a5d2b13b5123
Hari Gopal <hari@mobme.in>
2015-01-19 12:41:16 +0530

    Removing now useless APP_CONFIG keys

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,9 +1,6 @@
 APP_CONFIG = {
   sms_provider_url: ENV['SMS_PROVIDER_URL'],
   login_secret: ENV['LOGIN_SECRET'],
-  # sms_statistics_all: ENV['SMS_STATISTICS_ALL'].try(:split, ','),
-  # sms_statistics_total: ENV['SMS_STATISTICS_TOTAL'].try(:split, ','),
-  # sms_statistics_visakhapatnam: ENV['SMS_STATISTICS_VISAKHAPATNAM'].try(:split, ','),
   api_version: ENV['API_VERSION'],
   google_analytics_tracking_id: ENV['GOOGLE_ANALYTICS_TRACKING_ID']
 }.with_indifferent_access

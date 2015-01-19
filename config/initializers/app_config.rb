7c5681ef52bf90140c08e8949a978961890346a8
Hari Gopal <hari@mobme.in>
2015-01-19 12:41:40 +0530

    Prepping for requiring API application token

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -2,5 +2,6 @@ APP_CONFIG = {
   sms_provider_url: ENV['SMS_PROVIDER_URL'],
   login_secret: ENV['LOGIN_SECRET'],
   api_version: ENV['API_VERSION'],
-  google_analytics_tracking_id: ENV['GOOGLE_ANALYTICS_TRACKING_ID']
+  google_analytics_tracking_id: ENV['GOOGLE_ANALYTICS_TRACKING_ID'],
+  application_tokens: ENV['APPLICATION_TOKENS'].try(:split, ',')
 }.with_indifferent_access

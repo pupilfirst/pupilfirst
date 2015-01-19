e085df4de667cd12a0d6b312ff2df874e242a0d3
Sonu Oommen <sonu.oommen@mobme.in>
2015-01-19 20:45:14 +0530

    deleted events/index.html.erb and old migration without posters_name

--- db/migrate/20150113134354_add_approved_posters_email_and_phone_number_to_event.rb
+++
@@ -1,7 +0,0 @@
-class AddApprovedPostersEmailAndPhoneNumberToEvent < ActiveRecord::Migration
-  def change
-    add_column :events, :approved, :boolean
-    add_column :events, :posters_email, :string
-    add_column :events, :posters_phone_number, :integer
-  end
-end

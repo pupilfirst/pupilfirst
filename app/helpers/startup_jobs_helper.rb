3f587b1524d53677b1ca16806ab796af3e11cc21
Hari Gopal <hari@mobme.in>
2015-02-06 16:15:26 +0530

    Return false after logger warn.

--- app/helpers/startup_jobs_helper.rb
+++ app/helpers/startup_jobs_helper.rb
@@ -4,6 +4,7 @@ module StartupJobsHelper
       !job.expired? || job.can_be_modified_by?(current_user)
     else
       logger.warn "Skipping display of job from missing startup with ID #{job.startup_id}"
+      false
     end
   end
 end

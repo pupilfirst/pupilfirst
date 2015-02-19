6d41c323f7ad190cc2bc692baaddd2e28e4d9a4a
Vishnu Vijayakumar <vishnu.vijayakumar@mobme.in>
2015-02-19 10:54:51 +0530

    Added additional startup job's fields to permit params

--- app/admin/startup_job.rb
+++ app/admin/startup_job.rb
@@ -2,5 +2,5 @@ ActiveAdmin.register StartupJob do
   controller do
     newrelic_ignore
   end
-  permit_params :title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff, :expires_on, :startup_id
+  permit_params :title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff, :expires_on, :startup_id, :skills, :experience, :qualification, :contact_number, :contact_name
 end

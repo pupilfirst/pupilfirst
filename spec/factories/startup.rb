ecb025a62a34c95e28d161924f4c571bd8daa86d
Hari Gopal <hari@mobme.in>
2015-02-23 18:46:06 +0530

    Adding an incubated startup factory.

--- spec/factories/startup.rb
+++ spec/factories/startup.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :startup do |f|
-    f.name { Faker::Lorem.characters(20) }
+    f.name { Faker::Lorem.words(rand(3) + 1).join ' ' }
     f.logo { fixture_file_upload(Rails.root.join(*%w[ spec fixtures files example.jpg ]), 'image/jpg') }
     f.pitch { Faker::Lorem.words(6).join(' ') }
     f.address { Faker::Lorem.words(6).join(' ') }
@@ -15,6 +15,13 @@ FactoryGirl.define do
       startup.founders << create(:founder, startup: startup)
       startup.founders << create(:founder, startup: startup)
       startup.categories = [create(:startup_category)]
+    end
+
+    factory :incubated_startup do
+      approval_status Startup::APPROVAL_STATUS_APPROVED
+      agreement_first_signed_at { 18.months.ago }
+      agreement_last_signed_at { 6.months.ago }
+      agreement_ends_at { 6.months.from_now }
     end
   end
 

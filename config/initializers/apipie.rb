48480d08212c880a5f658390413ae471f9e9aeef
Abdul Jaleel <ajaleelp@gmail.com>
2015-02-24 12:04:11 +0530

    config controllers_matcher in apipie initializer

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,5 +3,5 @@ Apipie.configure do |config|
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
   # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/*.rb"
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/v1/*.rb"
 end

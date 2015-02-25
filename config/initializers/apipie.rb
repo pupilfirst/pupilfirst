ddd6b237387b22971153d611f28b52dad17f19d8
Abdul Jaleel <ajaleelp@gmail.com>
2015-02-25 13:26:46 +0530

    Add basic authentication to apipie

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -4,4 +4,9 @@ Apipie.configure do |config|
   config.doc_base_url            = "/apipie"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/v1/*.rb"
+    config.authenticate = Proc.new do
+     authenticate_or_request_with_http_basic do |username, password|
+       username == "admin" && password == "password"
+    end
+  end
 end

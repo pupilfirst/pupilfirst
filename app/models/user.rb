47f6213a7516f2e0a3f919163f49c4e11d379199
Vishnu Vijayakumar <vishnu.vijayakumar@mobme.in>
2015-02-26 11:40:10 +0530

    Included the field description in spec/models/startup_job_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :confirmable, #:registerable, 
+  devise :invitable, :database_authenticatable, :confirmable, #:registerable,
     :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   has_many :requests

3b4a38df457ed13da9e8e63205ef9037b21506a2
Abdul Jaleel <ajaleelp@gmail.com>
2015-03-03 17:18:22 +0530

    Create startup within mentor_meeting factory

--- spec/factories/mentor_meeting.rb
+++ spec/factories/mentor_meeting.rb
@@ -1,10 +1,15 @@
 FactoryGirl.define do
   factory :mentor_meeting do
-    user { create :founder }
+    user { create :user_with_out_password }
     mentor
     purpose { Faker::Lorem.words(5).join ' ' }
     duration { MentorMeeting.valid_durations.sample }
     suggested_meeting_at { 3.days.from_now }
     suggested_meeting_time Mentor::AVAILABILITY_TIME_MORNING
+
+    after :build do |meeting|
+      startup = create :startup
+      startup.founders << meeting.user
+    end
   end
 end

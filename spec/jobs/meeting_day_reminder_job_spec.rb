ed423d7bae78a64a52df494bdf8225933a3051b9
Abdul Jaleel <ajaleelp@gmail.com>
2015-03-03 17:19:11 +0530

    Add spec for meetind_day_reminder_job

--- spec/jobs/meeting_day_reminder_job_spec.rb
+++ spec/jobs/meeting_day_reminder_job_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe MeetingDayReminderJob, :type => :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.perform' do
+  	context 'when a meeting is scheduled for today' do
+  		let!(:meeting) { create :mentor_meeting, meeting_at: Time.now}
+  		it 'sends user and mentor reminder emails' do
+        message_delivery = instance_double(ActionMailer::MessageDelivery)
+        expect(MentoringMailer).to receive(:meeting_today_user).with(meeting).and_return(message_delivery)
+        expect(message_delivery).to receive(:deliver_later)
+        MeetingDayReminderJob.perform_now
+      end
+    end
+  end
 end

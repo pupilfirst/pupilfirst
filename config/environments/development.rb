d701ac83973a3c7499f3d7911407ee555b450bd6
Hari Gopal <hari@mobme.in>
2015-03-03 16:10:15 +0530

    Setting URL options for roadie for all environments.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
@@ -54,6 +54,9 @@ Rails.application.configure do
   Bullet.console = true
   Bullet.rails_logger = true
   Bullet.add_footer = true
+
+  # Roadie asset host.
+  config.roadie.url_options = { host: 'localhost', scheme: 'http', port: 3000 }
 end
 
 

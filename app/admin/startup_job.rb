8416236e17ecd937c2038c6fcda49409e3e5ba86
Hari Gopal <hari@mobme.in>
2015-03-03 11:52:53 +0530

    Reducing content on adming startup jobs index page.

--- app/admin/startup_job.rb
+++ app/admin/startup_job.rb
@@ -2,5 +2,18 @@ ActiveAdmin.register StartupJob do
   controller do
     newrelic_ignore
   end
-  permit_params :title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff, :expires_on, :startup_id, :skills, :experience, :qualification, :contact_number, :contact_name
+
+  index do
+    selectable_column
+    actions
+    column :title
+    column :contact_name
+    column :contact_email
+    column :contact_number
+    column :created_at
+    column :expires_on
+  end
+
+  permit_params :title, :description, :salary_max, :salary_min, :equity_max, :equity_min, :equity_vest, :equity_cliff,
+    :expires_on, :startup_id, :skills, :experience, :qualification, :contact_number, :contact_name
 end

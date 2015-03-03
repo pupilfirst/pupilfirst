c063588994dc6dd631e16286a9d947b576991fe8
Hari Gopal <hari@mobme.in>
2015-03-03 15:04:28 +0530

    Cleaning up the jobs show page.

--- app/helpers/startup_jobs_helper.rb
+++ app/helpers/startup_jobs_helper.rb
@@ -7,4 +7,26 @@ module StartupJobsHelper
       false
     end
   end
+
+  def value_or_not_available(value)
+    if value.blank?
+      '<em>Not Available</em>'.html_safe
+    else
+      value
+    end
+  end
+
+  def equity_html(job)
+    equity = "#{job.equity_min}%"
+
+    if job.equity_max.present?
+      equity += " &ndash; #{job.equity_max}%"
+    end
+
+    if job.equity_vest.present? && job.equity_cliff.present?
+      equity += " (#{job.equity_vest} year vest with #{job.equity_cliff} year cliff)"
+    end
+
+    equity.html_safe
+  end
 end

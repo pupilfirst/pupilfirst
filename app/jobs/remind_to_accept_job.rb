cd5648a4c1b264e9b1dfee621844ae45ffb75d77
Abdul Jaleel <ajaleelp@gmail.com>
2015-03-04 16:54:40 +0530

    Correct error in remind_to_accept_job

--- app/jobs/remind_to_accept_job.rb
+++ app/jobs/remind_to_accept_job.rb
@@ -1,10 +1,10 @@
 class RemindToAcceptJob < ActiveJob::Base
 
   def perform
-    MentorMeeting.requested.where('meeting_at < ?', 2.days.from_now).each do |meet|
+    MentorMeeting.requested.where('suggested_meeting_at < ?', 2.days.from_now).each do |meet|
       MentoringMailer.remind_mentor_to_accept(meet).deliver_later
     end
-    MentorMeeting.rescheduled.where('meeting_at < ?', 2.days.from_now).each do |meet|
+    MentorMeeting.rescheduled.where('suggested_meeting_at < ?', 2.days.from_now).each do |meet|
       MentoringMailer.remind_user_to_accept(meet).deliver_later
     end
   end

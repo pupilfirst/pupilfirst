f51d9bea4d2a34e925d8d9150cec41e6c2c6847c
Abdul Jaleel <ajaleelp@gmail.com>
2015-03-04 16:34:04 +0530

    Seperate tests for startup and mentor in meeting_day_reminder_job_spec

--- spec/jobs/meeting_day_reminder_job_spec.rb
+++ spec/jobs/meeting_day_reminder_job_spec.rb
@@ -4,12 +4,20 @@ RSpec.describe MeetingDayReminderJob, :type => :job do
   describe '.perform' do
   	context 'when a meeting is scheduled for today' do
   		let!(:meeting) { create :mentor_meeting, meeting_at: Time.now}
-  		it 'sends user and mentor reminder emails' do
+  		it 'sends user reminder email' do
         message_delivery = instance_double(ActionMailer::MessageDelivery)
         expect(MentoringMailer).to receive(:meeting_today_user).with(meeting).and_return(message_delivery)
         expect(message_delivery).to receive(:deliver_later)
         MeetingDayReminderJob.perform_now
       end
+
+      it 'sends mentor reminder email' do
+        message_delivery = instance_double(ActionMailer::MessageDelivery)
+        expect(MentoringMailer).to receive(:meeting_today_mentor).with(meeting).and_return(message_delivery)
+        expect(message_delivery).to receive(:deliver_later)
+        MeetingDayReminderJob.perform_now
+      end
+
     end
   end
 end

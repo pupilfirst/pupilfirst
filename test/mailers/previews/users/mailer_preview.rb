7cc6ef62b3a249604f76280381441d62697e2bf2
sonuoommen <sonuoommen26@gmail.com>
2015-03-05 18:47:07 +0530

    content_for layout for reset_password email

--- test/mailers/previews/users/mailer_preview.rb
+++ test/mailers/previews/users/mailer_preview.rb
@@ -1,8 +1,6 @@
 class Users::MailerPreview < ActionMailer::Preview
   # helper :application # gives access to all helpers defined within `application_helper`.
   # include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`.
-  add_template_helper(Devise::Controllers::UrlHelpers)
-
   # hit http://localhost:3000/rails/mailers/users/mailer/confirmation_instructions
   def confirmation_instructions
     Devise::Mailer.confirmation_instructions(User.first, {})
@@ -17,4 +15,9 @@ class Users::MailerPreview < ActionMailer::Preview
   def unlock_instructions
     Devise::Mailer.unlock_instructions(User.first, {})
   end
+
+  def invitation_instructions
+    Devise::Mailer.reset_password_instructions(User.first, {})
+  end
+
 end

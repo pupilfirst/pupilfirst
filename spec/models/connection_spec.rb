623aeebfeefedafa3397481def3e94829a284ae6
Hari Gopal <hari@mobme.in>
2015-03-13 16:15:36 +0530

    Correcting spec.

--- spec/models/connection_spec.rb
+++ spec/models/connection_spec.rb
@@ -11,7 +11,7 @@ describe Connection do
     let(:contact) { create :user_as_contact }
 
     it 'send notification to the user for whom connection is created' do
-      expect(UserPushNotifyJob).to receive(:perform).with(user.id, :create_connection, I18n.t('notifications.create_connection', fullname: contact.fullname),
+      expect(UserPushNotifyJob).to receive(:perform_later).with(user.id, 'create_connection', I18n.t('notifications.create_connection', fullname: contact.fullname),
         contact.attributes.slice('fullname', 'phone', 'email', 'company', 'designation'))
 
       create :connection, user: user, contact: contact, direction: Connection::DIRECTION_SV_TO_USER

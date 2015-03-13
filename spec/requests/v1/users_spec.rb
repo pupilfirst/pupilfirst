824f6e0554bba9f20d2a73ffe5c4a70b2b3f9b45
Hari Gopal <hari@mobme.in>
2015-03-13 16:16:18 +0530

    Remove pending marker.

--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -6,8 +6,6 @@ describe V1::UsersController do
   describe 'GET on user' do
     context 'fetches details of user when id is provided' do
       it 'returns http success with details' do
-        pending 'why?'
-
         user = create(:user_with_out_password)
         get "/api/users/#{user.id}", {}, version_header
         expect(response).to be_success

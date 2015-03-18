66f6444a58574dfefb5e77776e0fd49389f9c223
Abdul Jaleel <ajaleelp@gmail.com>
2015-03-18 12:54:04 +0530

    fix few mailers to deliver later

--- app/controllers/mentoring_controller.rb
+++ app/controllers/mentoring_controller.rb
@@ -83,7 +83,7 @@ class MentoringController < ApplicationController
       render 'new_step4' and return
     end
     flash[:notice] = 'You have successfully registered as a mentor!'
-    MentoringMailer.mentor_verification_ongoing(current_user).deliver
+    MentoringMailer.mentor_verification_ongoing(current_user).deliver_later
     redirect_to mentoring_url
   end
 

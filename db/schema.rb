3095de2336b66651012d954bacc5d08f80a04107
sonuoommen <sonuoommen26@gmail.com>
2015-03-19 13:05:13 +0530

    removed company_id from mentor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313114533) do
+ActiveRecord::Schema.define(version: 20150319071047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -213,11 +213,8 @@ ActiveRecord::Schema.define(version: 20150313114533) do
     t.string   "availability"
     t.string   "company_level"
     t.datetime "verified_at"
-    t.integer  "company_id"
     t.string   "company"
   end
-
-  add_index "mentors", ["company_id"], name: "index_mentors_on_company_id", using: :btree
 
   create_table "names", force: :cascade do |t|
     t.string   "first_name"

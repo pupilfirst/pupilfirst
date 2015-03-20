47762fc3f1a018193b61c142aea1a5d359461e35
Hari Gopal <hari@mobme.in>
2015-03-20 13:10:05 +0530

    Fixing typo.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Startup Vilage <no-reply@svlabs.in>'
+  config.mailer_sender = 'Startup Village <no-reply@svlabs.in>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'

7746f05e032fa775f64b2049b1de10e006f619bb
Hari Gopal <hari@mobme.in>
2015-03-23 11:21:59 +0530

    Adding email and its verification status to mentor CSV export.

--- app/admin/mentor.rb
+++ app/admin/mentor.rb
@@ -9,6 +9,8 @@ ActiveAdmin.register Mentor do
     column :name
     column(:phone_number) { |mentor| mentor.user.phone }
     column(:phone_verified) { |mentor| mentor.user.phone_verified }
+    column(:email) { |mentor| mentor.user.email }
+    column(:email_verified) { |mentor| mentor.user.confirmed_at.present? }
     column :verified_at
     column(:availability) { |mentor| availability_as_string mentor.availability }
     column :company

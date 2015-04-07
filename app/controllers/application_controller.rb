61578cb3330e503d651f65d6374f8c7e48eae72e
Abdul Jaleel <ajaleelp@gmail.com>
2015-04-07 15:41:10 +0530

    include redirect upon signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,13 +26,18 @@ class ApplicationController < ActionController::Base
     referer ? referer : super
   end
 
+  def after_sign_up_path_for(resource)
+    referer = session.delete :referer
+    referer ? referer : super
+  end
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:accept_invitation).concat [:avatar, :twitter_url, :linkedin_url, :username]
     # TODO
     # Unpermitted parameters: salutation, fullname, born_on(1i), born_on(2i), born_on(3i), is_student, college, course, semester
-    devise_parameter_sanitizer.for(:sign_up).concat [:salutation, :fullname, :born_on, :is_student, :college_id, :course, :semester]
+    devise_parameter_sanitizer.for(:sign_up).concat [:fullname]
     devise_parameter_sanitizer.for(:accept_invitation).concat [:salutation, :fullname, :email, :born_on, :is_student, :college_id, :course, :semester, :startup, :accept_startup]
   end
 

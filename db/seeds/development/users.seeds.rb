c7e532384cfc17f4ce2aa185b9e90f2be0157948
Hari Gopal <hari@mobme.in>
2015-04-20 14:26:48 +0530

    Confirming seeded users.

--- db/seeds/development/users.seeds.rb
+++ db/seeds/development/users.seeds.rb
@@ -1,7 +1,7 @@
 require_relative 'helper'
 
 # Just a user.
-User.create!(email: 'someone@mobme.in', fullname: 'Some One', password: 'password', password_confirmation: 'password')
+User.create!(email: 'someone@mobme.in', fullname: 'Some One', password: 'password', password_confirmation: 'password', confirmed_at: Time.now)
 
 # A user is to be a mentor.
-User.create!(email: 'mentor@svlabs.in', fullname: 'Mentor User', password: 'password', password_confirmation: 'password', title: 'Chief Mentor')
+User.create!(email: 'mentor@svlabs.in', fullname: 'Mentor User', password: 'password', password_confirmation: 'password', title: 'Chief Mentor', confirmed_at: Time.now)

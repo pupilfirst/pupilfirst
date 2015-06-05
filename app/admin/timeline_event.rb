595ced38d437470bfbaeafb393247c86a3f19ad1
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-05 15:43:47 +0530

    Add permit_params for admin/timeline_event.rb

--- app/admin/timeline_event.rb
+++ app/admin/timeline_event.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register TimelineEvent do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :title, :description, :iteration, :type, :image, :links
   #
   # or
   #

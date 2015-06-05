f6fe39d47be8d134c5ae368754a69131a6408b06
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-05 15:40:39 +0530

    Add TimelineEvent to ActiveAdmin

---
+++ app/admin/timeline_event.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register TimelineEvent do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+
+end

a754d86378c4985c102a5139aad044b5dc7eaae7
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-05 19:27:39 +0530

    Include presence validation for TimelineEvent

--- app/models/timeline_event.rb
+++ app/models/timeline_event.rb
@@ -2,4 +2,5 @@ class TimelineEvent < ActiveRecord::Base
   belongs_to :startup
   mount_uploader :image, TimelineImageUploader
   serialize :links
+  validates_presence_of :title, :type, :eventdate, :startup_id, :iteration
 end

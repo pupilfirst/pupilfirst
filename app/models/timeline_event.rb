f52e247785c46fbcc4950bb7f77e1b1ea47fc0ad
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-05 19:44:12 +0530

    Validate inclusion of  TimelineEvent type

--- app/models/timeline_event.rb
+++ app/models/timeline_event.rb
@@ -3,4 +3,11 @@ class TimelineEvent < ActiveRecord::Base
   mount_uploader :image, TimelineImageUploader
   serialize :links
   validates_presence_of :title, :type, :eventdate, :startup_id, :iteration
+
+  def self.valid_types
+    ["a","c","e"]
+  end
+  validates_inclusion_of :type, in: valid_types
+
+
 end

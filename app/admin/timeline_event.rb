756f0f472f56e9bdd435406f365251f8c08d1b2e
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-18 19:25:18 +0530

    Update activeadmin form for TimelineEvent

--- app/admin/timeline_event.rb
+++ app/admin/timeline_event.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register TimelineEvent do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :title, :description, :iteration, :type, :image, :links, :event_on
+  permit_params :title, :description, :iteration, :event_type, :image, :links, :event_on
   #
   # or
   #
@@ -12,4 +12,18 @@ ActiveAdmin.register TimelineEvent do
   #   permitted << :other if resource.something?
   #   permitted
   # end
+
+  form do |f|
+    f.inputs 'Event Details' do
+      f.input :title
+      f.input :event_type, collection: TimelineEvent.valid_types, include_blank: false
+      f.input :description
+      f.input :iteration
+      f.input :image
+      f.input :links
+      f.input :event_on, as: :datepicker
+    end
+    f.actions
+  end
+
 end

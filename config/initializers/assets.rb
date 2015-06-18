803e15a7cea311c311a34fdf3743624bcf29f1b2
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-18 15:31:26 +0530

    Configure TimelineStyle.css to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.precompile += %w(
   demo/owl-carousel/owl.carousel.js
   demo/ct-carousel/masonry.pkgd.min.js
   demo/ct-carousel/jquery.flexslider-min.js
+  demo/TimelineStyle.css
 )

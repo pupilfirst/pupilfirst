75879e22ac33bc332c536d9846a76d046faf8ee8
Hari Gopal <hari@mobme.in>
2015-06-23 14:21:45 +0530

    Creating a generic non-fixed navbar demo layout and using it for login page.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
   # GET /resource/sign_in
+  layout 'demo_generic_inner'
+
   def new
     # Store referer in session if it's sent.
     session[:referer] = params[:referer] if params[:referer].present?

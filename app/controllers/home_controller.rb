43646f224f4e5677bea833e83d0e41780e8fdc1a
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-25 19:30:12 +0530

    Add link to continue reading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,7 @@ class HomeController < ApplicationController
   def get_latest_blogs
     wp = Rubypress::Client.new(:host => "startatsv.com",:username => "admin",:password => "Startup1#")
     filter_hash = {orderby: "post_date",order: "DESC",post_type: "post",post_status: "publish",number: 4}
-    fields_array = ["post_title","post_content","post_thumbnail"]
+    fields_array = ["post_title","post_content","post_thumbnail","link"]
     wp.getPosts(filter: filter_hash,fields: fields_array)
   end
 

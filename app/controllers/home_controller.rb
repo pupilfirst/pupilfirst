e875772a343929f824a1bf43168a2d67496ab922
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-25 19:17:59 +0530

    Pull most recent blog posts and display @ home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,10 @@ class HomeController < ApplicationController
   private
 
   def get_latest_blogs
-    [1,2]
+    wp = Rubypress::Client.new(:host => "startatsv.com",:username => "admin",:password => "Startup1#")
+    filter_hash = {orderby: "post_date",order: "DESC",post_type: "post",post_status: "publish",number: 4}
+    fields_array = ["post_title","post_content","post_thumbnail"]
+    wp.getPosts(filter: filter_hash,fields: fields_array)
   end
 
 end

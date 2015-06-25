9ba67b4d1852c54d5eba92396ec4789922254960
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-25 18:30:10 +0530

    Add stub method to pull blog posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,14 @@ class HomeController < ApplicationController
   layout 'demo'
 
   def index
-
+    @blogs = get_latest_blogs
   end
+
+  private
+
+  def get_latest_blogs
+    [1,2]
+  end
+
 end
+

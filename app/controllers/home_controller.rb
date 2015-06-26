8521305ec42085300c61d55caf6ed2fe3196dcc0
Abdul Jaleel <ajaleelp@gmail.com>
2015-06-26 13:56:08 +0530

    Move wordpress credentials to env

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   private
 
   def get_latest_blogs
-    wp = Rubypress::Client.new(:host => "startatsv.com",:username => "admin",:password => "Startup1#")
+    wp = Rubypress::Client.new(:host => "startatsv.com",:username => ENV['WP_USERNAME'],:password => ENV['WP_PASSWORD'])
     filter_hash = {orderby: "post_date",order: "DESC",post_type: "post",post_status: "publish",number: 4}
     fields_array = ["post_title","post_content","post_thumbnail","link"]
     wp.getPosts(filter: filter_hash,fields: fields_array)

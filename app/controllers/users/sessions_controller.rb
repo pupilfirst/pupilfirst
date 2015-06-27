d21f324c0b06154ad03761d2f76953cc5b04bdb1
Hari Gopal <hari@mobme.in>
2015-06-27 15:27:55 +0530

    Sessions controller does not need custom layout anymore.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
   # GET /resource/sign_in
-  layout 'demo_generic_inner'
-
   def new
     # Store referer in session if it's sent.
     session[:referer] = params[:referer] if params[:referer].present?

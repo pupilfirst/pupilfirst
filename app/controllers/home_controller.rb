f905f0c2bc35622f8b599fba23148e7c483cfc5d
Hari Gopal <hari@mobme.in>
2015-06-29 16:24:17 +0530

    Do not indefinitely attept retry on contacting blog.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,11 +8,15 @@ class HomeController < ApplicationController
   private
 
   def get_latest_blogs
-    Rails.cache.fetch("latest_blogs",expires_in: 24.hours) do
-      wp = Rubypress::Client.new(:host => "startatsv.com",:username => ENV['WP_USERNAME'],:password => ENV['WP_PASSWORD'],:retry_timeouts => true)
-      filter_hash = {orderby: "post_date",order: "DESC",post_type: "post",post_status: "publish",number: 4}
-      fields_array = ["post_title","post_content","post_thumbnail","link"]
-      wp.getPosts(filter: filter_hash,fields: fields_array)
+    begin
+      Rails.cache.fetch("latest_blogs", expires_in: 24.hours) do
+        wp = Rubypress::Client.new(:host => 'startatsv.com', :username => ENV['WP_USERNAME'], :password => ENV['WP_PASSWORD'])
+        filter_hash = { orderby: 'post_date', order: 'DESC', post_type: 'post', post_status: 'publish', number: 4 }
+        fields_array = %w(post_title post_content post_thumbnail link)
+        wp.getPosts(filter: filter_hash, fields: fields_array)
+      end
+    rescue Net::ReadTimeout
+      []
     end
   end
 end

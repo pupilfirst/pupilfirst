27a980deb2e051b0e37e619ed2cab48cc112d942
Abdul Jaleel <ajaleelp@gmail.com>
2015-07-02 17:53:52 +0530

    Remove react config from application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,6 @@ module Svapp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'New Delhi'
 
-    # Enable ReactJS addons.
-    config.react.addons = true
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de

5ee1317914726ea73276f6c706ec9adebec53a87
Abdul Jaleel <ajaleelp@gmail.com>
2015-07-14 14:43:32 +0530

    Remove magnific popup related assets

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -36,19 +36,6 @@ var giveWhiteBackgroundToTopNav = function() {
   });
 };
 
-var popupStartupTimeline = function() {
-  $('.inline-popups').magnificPopup({
-    delegate: 'a',
-    removalDelay: 500, //delay removal by X to allow out-animation
-    callbacks: {
-      beforeOpen: function () {
-        this.st.mainClass = this.st.el.attr('data-effect');
-      }
-    },
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-  });
-};
-
 var startupsShowcaseSlide = function() {
   var dataFromView = $("#startups-showcase-data");
 

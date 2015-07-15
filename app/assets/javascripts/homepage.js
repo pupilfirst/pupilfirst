c17802408ebb616ed9e0d2035fcc61e07c5a3f3f
Hari Gopal <hari@mobme.in>
2015-07-15 14:26:09 +0530

    Loading blog posts client-side.

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,10 +1,49 @@
 //= require jquery.flexslider
 
+function stripHTML(dirtyString) {
+  var container = document.createElement('div');
+  container.innerHTML = dirtyString;
+  return container.textContent || container.innerText;
+}
+
+var loadBlogPosts = function() {
+  $.get("http://startatsv.com?json=get_recent_posts&count=4", function(data) {
+    var blogPostsContainer = $("#blog-posts-container");
+    var postScaffold = $("#post-scaffold");
+
+    $.each(data.posts, function(postIndex, post) {
+      var postClone = postScaffold.clone();
+
+      // Make it a regular post.
+      postClone.removeAttr('id').addClass('post');
+
+      // Add post title, image, content and link.
+      postClone.find('.blog-post-image').attr('src', post.thumbnail_images.full.url);
+      postClone.find('.blog-post-title').html(post.title_plain);
+
+      // Reduce length of post content if title is long.
+      var contentLength = post.title_plain.length > 30 ? (200 - Math.round((post.title_plain.length - 30) * 1.5)) : 200;
+      postClone.find('.blog-post-content').html(stripHTML(post.content).substring(0, contentLength) + "...");
+
+      postClone.find('.blog-post-link').attr('href', post.url);
+
+      // Unhide the post, and append it to list of posts.
+      postClone.removeClass('hide');
+      postClone.appendTo(blogPostsContainer);
+    });
+  }, "jsonp").fail(function() {
+    // Show the failed text.
+    $("#blog-posts-loading-failed").removeClass('hide');
+  }).always(function () {
+    // Hide the loading text.
+    $("#blog-posts-loading").addClass('hide');
+  });
+};
+
 var navigateStagesAndLearning = function() {
   var val1=$(".progtrckr li.active .stage_name").attr("val");
 
   $("#stage_"+val1).fadeIn();
-
   $(".progtrckr li").on('click', function()
   {
     $(".progtrckr li").removeClass("active");
@@ -163,7 +202,7 @@ $(document).ready(startupsShowcaseSlide);
 $(document).ready(storiesSlide);
 $(document).ready(mediaShowcaseSlide);
 $(document).ready(prepareTestimonials);
-
+$(document).ready(loadBlogPosts);
 
 $(document).ready(function() {
   [].slice.call(document.querySelectorAll('.carousel-indicators > ol')).forEach(function(nav) {
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,24 +2,6 @@ class HomeController < ApplicationController
   layout 'homepage'
 
   def index
-    @blogs = get_latest_blogs
     @featured_startups = Startup.where(featured: true)
-  end
-
-  private
-
-  def get_latest_blogs
-    begin
-      Rails.cache.fetch("latest_blogs", expires_in: 24.hours) do
-        wp = Rubypress::Client.new(:host => 'startatsv.com', :username => ENV['WP_USERNAME'], :password => ENV['WP_PASSWORD'])
-        filter_hash = { orderby: 'post_date', order: 'DESC', post_type: 'post', post_status: 'publish', number: 4 }
-        fields_array = %w(post_title post_content post_thumbnail link)
-        wp.getPosts(filter: filter_hash, fields: fields_array)
-      end
-    rescue Net::ReadTimeout
-      []
-    rescue XMLRPC::FaultException #For incorrect username & password
-      []
-    end
   end
 end

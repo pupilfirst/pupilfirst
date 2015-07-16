f3155a51f9c32b3d747a88a1305e1ec05a4fe880
Hari Gopal <hari@mobme.in>
2015-07-16 13:26:56 +0530

    Removing useless environment variables.

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,8 +1,5 @@
 APP_CONFIG = {
   sms_provider_url: ENV['SMS_PROVIDER_URL'],
-  login_secret: ENV['LOGIN_SECRET'],
-  api_version: ENV['API_VERSION'],
   google_analytics_tracking_id: ENV['GOOGLE_ANALYTICS_TRACKING_ID'],
-  application_tokens: ENV['APPLICATION_TOKENS'].try(:split, ','),
   easyrtc_socket_url: ENV['EASYRTC_SOCKET_URL']
 }.with_indifferent_access

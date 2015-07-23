4c546f02d8633b1cf0e3db3843e8c9942d498c06
Hari Gopal <hari@mobme.in>
2015-07-23 16:18:59 +0530

    Major reorganization of asset requires.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,26 +1,12 @@
 Rails.application.config.assets.precompile += %w(
-  easyrtc_video.js
   active_admin_custom.css
-  select2.png
   mails.css
-  pnotify.css
-  timeline.css
-  owl-carousel/owl.carousel.css
-  general_style.css
-  icon/component.css
-  icon/modernizr.custom.js
   homepage.js
   homepage.css
+  timeline.css
   timeline.js
-  owl-carousel/owl.carousel.js
-  ct-carousel/masonry.pkgd.min.js
   startups_index.css
   startups_index.js
-  showcase.css
   faculty.css
-  homepage_header.css
   foundation.css
-  navbar.css
-  stages.css
-  above_the_fold.js
 )

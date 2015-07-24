171abcffb020b4162e32925ca1d7a0e63023239a
Abdul Jaleel <ajaleelp@gmail.com>
2015-07-24 11:48:31 +0530

    Comment out 'Url jumping' script which breaks bootstrap accordion

--- app/assets/javascripts/startups/index.js
+++ app/assets/javascripts/startups/index.js
@@ -14,20 +14,20 @@ if ($(document).scrollTop() > 450) {
 }
 
 /*Url jumping*/
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
+// $(function() {
+//   $('a[href*=#]:not([href=#])').click(function() {
+//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//       if (target.length) {
+//         $('html,body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
 
 $(document).ready(function() {
   $('body').scrollspy(
@@ -37,22 +37,3 @@ $(document).ready(function() {
     }
   );
 });
-
-
-var giveWhiteBackgroundToTopNav = function() {
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 300)
-    {
-      $("#SiteLogoWhite").hide();
-      $("#SiteLogo").fadeIn();
-      $('nav').addClass('shrink');
-    } else {
-      $("#SiteLogo").hide();
-      $("#SiteLogoWhite").fadeIn();
-      $('nav').removeClass('shrink');
-    }
-  });
-};
-
-$(document).ready(giveWhiteBackgroundToTopNav);
-

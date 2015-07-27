c3248c79879656d0b5f7c4713074307e2a0ad34d
Abdul Jaleel <ajaleelp@gmail.com>
2015-07-27 14:47:05 +0530

    Add roll number to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719115505) do
+ActiveRecord::Schema.define(version: 20150727091522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -298,6 +298,7 @@ ActiveRecord::Schema.define(version: 20150719115505) do
     t.integer  "years_of_work_experience"
     t.integer  "year_of_graduation"
     t.integer  "college_id"
+    t.string   "roll_number"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree

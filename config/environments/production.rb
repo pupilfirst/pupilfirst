2db3a5e3cf0585e9bbee34d53e8eab5e56821258
Hari Gopal <hari@mobme.in>
2015-07-30 15:02:48 +0530

    Changing asset host to assets.sv.co and setting appropriate CSP attributes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = 'https://d1efte8jdj3b9e.cloudfront.net'
+  config.action_controller.asset_host = 'https://assets.sv.co'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.

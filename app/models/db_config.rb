dd58ad1fdc513f72b17d104bc3639e2781d484e7
Abdul Jaleel <ajaleelp@gmail.com>
2015-08-11 13:28:48 +0530

    Provide comments with sample entries for feature_active in db_config

--- app/models/db_config.rb
+++ app/models/db_config.rb
@@ -9,15 +9,16 @@ class DbConfig < ActiveRecord::Base
     feature_startups: '(dev) Toggle Startups'
   }
 
-  # To use feature flags, add a VAR with name 'feature_FEATURE_NAME' and store JSON value with key 'users', or 'active'.
-  # 'users' key should contain an array of allowed user e-mails, OR 'active' should be set to affect all users.
+  # {"email_regexes": ["\S*(@mobme.in|sv.co)$"], "emails": ["someone@sv.co"]}
+  #     OR
+  # {"active": true}
   def self.feature_active?(key, user=nil)
     feature = where(key: "feature_#{key}").first
 
     return false unless feature
 
     feature_value = begin
-      JSON.load(feature[:value]).with_indifferent_access
+      JSON.load(feature.value).with_indifferent_access
     rescue JSON::ParserError
       return false
     end

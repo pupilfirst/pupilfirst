113e963e2fa1cac5c9a05f1d2f02ff7d161c0e93
Hari Gopal <hari@mobme.in>
2015-08-11 15:21:25 +0530

    Making thumb smaller. Making both thumb and mid square-cropped.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -36,11 +36,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fill => [50, 50]
   end
 
   version :mid do
-    process :resize_to_fit => [200, 200]
+    process :resize_to_fill => [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.

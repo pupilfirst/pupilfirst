9fbc2aeb0b01ce6c65d584e5f909d132bc9704d0
Vishnu Gopal <g.vishnu@gmail.com>
2015-08-13 17:11:47 +0530

    Setting a URL to a public Google Drive folder (just like the RTIs)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -82,7 +82,7 @@ class HomeController < ApplicationController
   end
   
   def press_kit
-    @press_kit_url = ""
+    @press_kit_url = "https://drive.google.com/folderview?id=0B9--SdQuJvHpfjJiam1nTnJCNnVIYkY2NVFXWTQwbXNpWUFoQU1oc1RZSHJraG4yb2Y1cDA&usp=sharing"
   end
 
   private

e44edee46e90445065abecfaa32a1c5c580d4091
Abdul Jaleel <ajaleelp@gmail.com>
2015-08-17 12:40:18 +0530

    Add TimelineEventType to active admin

---
+++ app/admin/timeline_event_type.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register TimelineEventType do
+menu parent: 'Startups'
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :key, :title, :sample_text
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+  index do
+    selectable_column
+    column :key
+    column :title
+    actions
+  end
+
+
+end

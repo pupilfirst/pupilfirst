da6b5b63dfad14bfd0c5062a2e861f44571ffec4
Abdul Jaleel <ajaleelp@gmail.com>
2015-08-18 11:51:17 +0530

    Mount BadgeUploader onto badge coloumn of TimelineEventType

--- app/models/timeline_event_type.rb
+++ app/models/timeline_event_type.rb
@@ -2,6 +2,8 @@ class TimelineEventType < ActiveRecord::Base
   validates_presence_of :key, :title
   validates_uniqueness_of :key
 
+  mount_uploader :badge, BadgeUploader
+
   def sample
     sample_text.present? ? sample_text : 'What\'s been happening?'
   end

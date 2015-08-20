e3eaac2453a983d33dbcce05c29a27aba6d581fd
Hari Gopal <hari@mobme.in>
2015-08-20 11:44:58 +0530

    Removing dupe raise_not_found.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,6 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-  
-  def raise_not_found
-    raise ActionController::RoutingError.new('Not Found')
-  end
 
   def feature_active?(feature)
     (Rails.env == "development") ||

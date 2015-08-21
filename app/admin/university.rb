a978604f72a5fa4823b677050e3cf15b879562a7
Abdul Jaleel <ajaleelp@gmail.com>
2015-08-21 14:52:21 +0530

    Add activeadmin interface for University Model

---
+++ app/admin/university.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register University do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+
+end

cc7508941c2cb736e3ecfa6f6d48cff69106d4bd
Abdul Jaleel <ajaleelp@gmail.com>
2015-08-24 15:54:45 +0530

    Add scopes to TimelineEventType for retrieving end_iteration and end_of_stage entries

--- app/models/timeline_event_type.rb
+++ app/models/timeline_event_type.rb
@@ -4,6 +4,11 @@ class TimelineEventType < ActiveRecord::Base
   validates_uniqueness_of :key
 
   TYPE_END_ITERATION = 'end_iteration'
+  TYPE_END_IDEA_STG = 'end_of_idea_stage'
+  TYPE_END_PROTOTYPE_STG = 'end_of_prototype_stage'
+  TYPE_END_CUSTOMER_STG = 'end_of_customer_stage'
+  TYPE_END_EFFICIENCY_STG = 'end_of_efficiency_stage'
+  TYPE_END_SCALE_STG = 'end_of_scale_stage'
 
   mount_uploader :badge, BadgeUploader
 
@@ -20,4 +25,8 @@ class TimelineEventType < ActiveRecord::Base
   def end_iteration?
     key == TYPE_END_ITERATION
   end
+
+  scope :end_iteration, -> {where(key: TYPE_END_ITERATION)}
+  scope :end_of_stage, -> {where(key: [TYPE_END_IDEA_STG, TYPE_END_PROTOTYPE_STG, TYPE_END_CUSTOMER_STG, TYPE_END_EFFICIENCY_STG, TYPE_END_SCALE_STG])}
+
 end

21b01fc9d0d18940d1ab23e125dd467d0897a498
Abdul Jaleel <ajaleelp@gmail.com>
2015-08-25 14:15:40 +0530

    Define a to_label method for universities

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,4 +1,8 @@
 class University < ActiveRecord::Base
   has_many :users
   validates_uniqueness_of :name, presence: true
+
+  def to_label
+  "#{name} #{location.present? ? " ["+location+"]" : ""}"
+  end
 end

a4710c12aa2b830c31a12598da3d7100dad73830
Hari Gopal <hari@mobme.in>
2015-08-28 01:01:46 +0530

    Minor fix for an edge-case handler.

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -26,7 +26,7 @@ class StartupsController < ApplicationController
   # POST /startups/team_leader_consent
   def team_leader_consent
     if current_user.startup.present? || !current_user.phone?
-      redirect_to action: 'new'
+      redirect_to new_user_startup_path(current_user)
     else
       Startup.new_incubation!(current_user)
       redirect_to incubation_path(id: :user_profile)

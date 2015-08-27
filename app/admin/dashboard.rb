b85542839f1af60ec3c8e5477f09a3d44b0881a1
Hari Gopal <hari@mobme.in>
2015-08-27 18:10:03 +0530

    Emptying the dashboard.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,29 +1,9 @@
-ActiveAdmin.register_page "Dashboard" do
-  page_action :users_count_total do
-    render json: Statistic.chartkick_parameter_by_date(Statistic::PARAMETER_COUNT_USERS, from: params[:from], to: params[:to])
-  end
+ActiveAdmin.register_page 'Dashboard' do
+  menu priority: 1
 
-  page_action :users_count_student_entrepreneurs do
-    render json: Statistic.chartkick_parameter_by_date(Statistic::PARAMETER_COUNT_USERS_STUDENT_ENTREPRENEURS, from: params[:from], to: params[:to])
-  end
-
-  page_action :startups_count_total do
-    render json: Statistic.chartkick_parameter_by_date(Statistic::PARAMETER_COUNT_STARTUPS, from: params[:from], to: params[:to])
-  end
-
-  page_action :startups_count_unready do
-    render json: Statistic.chartkick_parameter_by_date(Statistic::PARAMETER_COUNT_STARTUPS_UNREADY, from: params[:from], to: params[:to])
-  end
-
-  page_action :startups_count_current_split do
-    render json: Startup.current_startups_split
-  end
-
-  menu :priority => 1, :label => proc{ I18n.t('active_admin.dashboard') }
-
-  content :title => proc{ I18n.t('active_admin.dashboard') } do
+  content do
     div do
-      render 'statistics'
+      para 'Hey. Use top menu-s to navigate!'
     end
   end
 end

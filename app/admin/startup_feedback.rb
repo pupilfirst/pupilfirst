cf4850ff30db3b52fdc472bee6d9c811e64bc73e
Abdul Jaleel <ajaleelp@gmail.com>
2015-09-01 10:11:17 +0530

    Add a send_email attr_accessor to mark if email needs to be sent

--- app/admin/startup_feedback.rb
+++ app/admin/startup_feedback.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register StartupFeedback do
 
 menu parent: 'Startups'
-permit_params :feedback, :reference_url, :startup_id
+permit_params :feedback, :reference_url, :startup_id, :send_email
 
 index do
   selectable_column
@@ -9,6 +9,16 @@ index do
   column :feedback
   column :reference_url
   actions
+end
+
+form do |f|
+    f.inputs 'Event Details' do
+      f.input :startup
+      f.input :feedback
+      f.input :reference_url
+      f.input :send_email, :as => :boolean, :label => "Send as email to founders"
+    end
+    f.actions
 end
 
 end
--- app/models/startup_feedback.rb
+++ app/models/startup_feedback.rb
@@ -1,3 +1,10 @@
 class StartupFeedback < ActiveRecord::Base
   belongs_to :startup
+  attr_accessor :send_email
+
+  before_save do
+    if send_email == '1'
+      # send email here
+    end
+  end
 end

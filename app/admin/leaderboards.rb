75c2a9c77cbe191f2685461c63b7dedd9c6272f8
Hari Gopal <hari@mobme.in>
2015-09-02 16:35:45 +0530

    Moving filter into a sidebar.

--- app/admin/leaderboards.rb
+++ app/admin/leaderboards.rb
@@ -1,6 +1,10 @@
 ActiveAdmin.register_page 'Leaderboards' do
   menu parent: 'Startups'
 
+  sidebar :filter_by_date do
+    render 'admin/dashboard/karma_points_filter'
+  end
+
   content do
     render 'admin/dashboard/karma_points'
   end

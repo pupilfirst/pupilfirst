ae00352f53d718e3ccc33895689348a4722f9e73
Hari Gopal <hari@mobme.in>
2015-09-03 19:58:35 +0530

    Customizing AA KarmaPoint#index.

--- app/admin/karma_point.rb
+++ app/admin/karma_point.rb
@@ -3,6 +3,32 @@ ActiveAdmin.register KarmaPoint do
 
   permit_params :user_id, :points, :activity_type, :created_at
 
+  controller do
+    def scoped_collection
+      super.includes :user
+    end
+  end
+
+  index do
+    selectable_column
+
+    column :user do |karma_point|
+      span do
+        link_to karma_point.user.fullname, karma_point.user
+      end
+
+      span do
+        " (#{link_to karma_point.startup.name, karma_point.startup})".html_safe
+      end
+    end
+
+    column :points
+    column :activity_type
+    column :created_at
+
+    actions
+  end
+
   form do |f|
     f.inputs 'Extra' do
       f.input :user, collection: User.founders,
--- app/models/karma_point.rb
+++ app/models/karma_point.rb
@@ -1,3 +1,4 @@
 class KarmaPoint < ActiveRecord::Base
   belongs_to :user
+  delegate :startup, to: :user
 end

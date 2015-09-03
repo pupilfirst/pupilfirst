13e2852d9f73a83ca29a6998af757479e28ba263
Hari Gopal <hari@mobme.in>
2015-09-03 15:58:02 +0530

    Minor optimization.

--- app/admin/startup_feedback.rb
+++ app/admin/startup_feedback.rb
@@ -1,55 +1,58 @@
 ActiveAdmin.register StartupFeedback do
+  menu parent: 'Startups'
+  permit_params :feedback, :reference_url, :startup_id, :send_email
 
-menu parent: 'Startups'
-permit_params :feedback, :reference_url, :startup_id, :send_email
-
-index do
-  selectable_column
-  column :startup
-
-  column :feedback do |startup_feedback|
-    pre class: 'startup-feedback' do
-      startup_feedback.feedback
+  controller do
+    def scoped_collection
+      super.includes :startup
     end
   end
 
-  column :reference_url
+  index do
+    selectable_column
+    column :startup
 
-  column :send_at do |startup_feedback|
-    if startup_feedback.send_at.present?
-      startup_feedback.send_at
-    else
-      link_to('Email Now!', email_feedback_admin_startup_feedback_path(startup_feedback), method: :put, data: { confirm: 'Are you sure you want to email this feedback to the founders?' })
+    column :feedback do |startup_feedback|
+      pre class: 'startup-feedback' do
+        startup_feedback.feedback
+      end
     end
-  end
 
-  actions
-end
+    column :reference_url
 
-show do
-  attributes_table do
-    row :startup
-    row :feedback
-    row :reference_url
-    row :send_at do |startup_feedback|
+    column :send_at do |startup_feedback|
       if startup_feedback.send_at.present?
         startup_feedback.send_at
       else
         link_to('Email Now!', email_feedback_admin_startup_feedback_path(startup_feedback), method: :put, data: { confirm: 'Are you sure you want to email this feedback to the founders?' })
       end
     end
+
+    actions
   end
-end
 
-form partial: 'admin/startup_feedbacks/form'
+  show do
+    attributes_table do
+      row :startup
+      row :feedback
+      row :reference_url
+      row :send_at do |startup_feedback|
+        if startup_feedback.send_at.present?
+          startup_feedback.send_at
+        else
+          link_to('Email Now!', email_feedback_admin_startup_feedback_path(startup_feedback), method: :put, data: { confirm: 'Are you sure you want to email this feedback to the founders?' })
+        end
+      end
+    end
+  end
 
-member_action :email_feedback, method: :put do
-  startup_feedback = StartupFeedback.find params[:id]
-  startup_feedback.update(send_at: Time.now)
-  StartupMailer.feedback_as_email(startup_feedback,current_admin_user).deliver_later
-  flash[:alert] = 'Your feedback has been sent to the startup founders!'
-  redirect_to action: :index
-end
+  form partial: 'admin/startup_feedbacks/form'
 
-
+  member_action :email_feedback, method: :put do
+    startup_feedback = StartupFeedback.find params[:id]
+    startup_feedback.update(send_at: Time.now)
+    StartupMailer.feedback_as_email(startup_feedback, current_admin_user).deliver_later
+    flash[:alert] = 'Your feedback has been sent to the startup founders!'
+    redirect_to action: :index
+  end
 end

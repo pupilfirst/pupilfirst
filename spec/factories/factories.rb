4dc595a362cf8dc1b0b814c2ea08a104ce817ebb
Hari Gopal <hari@mobme.in>
2015-09-04 12:08:26 +0530

    Default factory shouldn't add more data than absolutely necessary.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -14,43 +14,14 @@ FactoryGirl.define do
 
   factory :user do
     fullname { Faker::Name.name }
-    salutation { %w(Mr Miss Mrs).sample }
     email { Faker::Internet.email }
-    born_on { Date.current.to_s }
-    gender { %w(male female).sample }
-    title { Faker::Lorem.characters(9) }
-    communication_address { Faker::Address.secondary_address }
-    district { Faker::Address.city }
-    state { Faker::Address.state }
-    pin { (rand(899999) + 100000).to_s }
-    avatar { fixture_file_upload(Rails.root.join(*%w[ spec fixtures files example.jpg ]), 'image/jpg') }
-    twitter_url { 'http://' + Faker::Internet.domain_name }
-    linkedin_url { 'http://' + Faker::Internet.domain_name }
-    college {create(:college)}
+
     factory :user_with_out_password do
       skip_password true
 
       factory :founder do
         is_founder true
       end
-
-      factory :user_with_facebook do
-        after(:create) do |user, evaluator|
-          create_list(:facebook_social_id, 1, user: user)
-        end
-      end
-
-      factory :user_as_contact, aliases: [:contact] do
-        is_contact true
-        sequence(:phone) { |n| "#{9876543210 + n}" }
-        company { "#{Faker::Name.last_name} Ltd." }
-        invitation_token { Faker::Lorem.characters 10 }
-      end
-    end
-
-    factory :user_with_password do
-      password "user_password"
-      password_confirmation "user_password"
     end
   end
 

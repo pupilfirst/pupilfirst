ea2fddc77abaf61a4a55c5d4d453219c94e979e7
Abdul Jaleel <ajaleelp@gmail.com>
2015-09-16 16:48:32 +0530

    Modify feedback email if feedback is about a timeline entry

--- app/models/startup_feedback.rb
+++ app/models/startup_feedback.rb
@@ -1,4 +1,16 @@
 class StartupFeedback < ActiveRecord::Base
   belongs_to :startup
   attr_accessor :send_email
+
+  REGEX_TIMELINE_EVENT_URL = /startups\/.*event-(?<event_id>[\d]+)/
+
+  def for_timeline_event?
+    self.reference_url.present? && self.reference_url.match(REGEX_TIMELINE_EVENT_URL).present? ? true : false
+  end
+
+  def timeline_event
+    self.reference_url.present? && self.reference_url.match(REGEX_TIMELINE_EVENT_URL).present? ? TimelineEvent.find(self.reference_url.match(REGEX_TIMELINE_EVENT_URL)[:event_id]) : nil
+  end
+
+
 end

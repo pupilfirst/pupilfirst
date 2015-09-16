b03b825b73fa33eb23c9583d8cd4dd8ba70b7897
Hari Gopal <hari@mobme.in>
2015-09-16 19:31:13 +0530

    RuboCop StartupFeedback.

--- app/models/startup_feedback.rb
+++ app/models/startup_feedback.rb
@@ -2,15 +2,18 @@ class StartupFeedback < ActiveRecord::Base
   belongs_to :startup
   attr_accessor :send_email
 
-  REGEX_TIMELINE_EVENT_URL = /startups\/.*event-(?<event_id>[\d]+)/
+  REGEX_TIMELINE_EVENT_URL = %r{startups/.*event-(?<event_id>[\d]+)}
 
   def for_timeline_event?
-    self.reference_url.present? && self.reference_url.match(REGEX_TIMELINE_EVENT_URL).present? ? true : false
+    if reference_url.present? && reference_url.match(REGEX_TIMELINE_EVENT_URL).present?
+      true
+    else
+      false
+    end
   end
 
   def timeline_event
-    self.reference_url.present? && self.reference_url.match(REGEX_TIMELINE_EVENT_URL).present? ? TimelineEvent.find(self.reference_url.match(REGEX_TIMELINE_EVENT_URL)[:event_id]) : nil
+    return unless reference_url.present? && reference_url.match(REGEX_TIMELINE_EVENT_URL).present?
+    TimelineEvent.find(reference_url.match(REGEX_TIMELINE_EVENT_URL)[:event_id])
   end
-
-
 end

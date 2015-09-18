14c029d14e8feee07955c1d527e73f4376346203
Hari Gopal <hari@mobme.in>
2015-09-18 20:57:40 +0530

    Remove sendgrid-specific code.

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ end
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-ActionMailer::Base.register_interceptor(SendGridSmtpApi::MailInterceptor)
 
 module Svapp
   class Application < Rails::Application

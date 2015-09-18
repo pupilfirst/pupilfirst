cde607d6bc7f1d44d3ca21ffed3c52f8c58420ef
Hari Gopal <hari@mobme.in>
2015-09-18 20:55:47 +0530

    Switch to using Mandrill (transactional email).

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,11 +97,12 @@ Rails.application.configure do
 end
 
 ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
   port: '587',
-  authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  address: 'smtp.mandrillapp.com',
+  user_name: ENV['MANDRILL_USERNAME'],
+  password: ENV['MANDRILL_APIKEY'],
   domain: 'heroku.com',
-  enable_starttls_auto: true
+  authentication: :plain
 }
+
+ActionMailer::Base.delivery_method = :smtp

ea7e576d9c85ed9bab47fb219c6419a2c254a9a4
Hari Gopal <hari@mobme.in>
2015-09-21 00:45:45 +0530

    Lita is threaded, so let's wrap handler code in a with_connection block.

--- lib/lita/handlers/backup.rb
+++ lib/lita/handlers/backup.rb
@@ -2,18 +2,20 @@ module Lita
   module Handlers
     class Backup < Handler
       on :unhandled_message do |payload|
-        message = payload[:message]
+        ActiveRecord::Base.connection_pool.with_connection do
+          message = payload[:message]
 
-        next if message.private_message?
+          next if message.private_message?
 
-        message_author_slack_username = message.user.mention_name
-        message_author = ::User.find_by slack_username: message_author_slack_username
+          message_author_slack_username = message.user.mention_name
+          message_author = ::User.find_by slack_username: message_author_slack_username
 
-        # TODO: Channel name should be accessible directly from message.room_object.name, but it isn't. Fix when possible.
-        # See: https://github.com/kenjij/lita-slack/issues/44
-        channel = Lita::Room.find_by_id(message.room_object.id).name
+          # TODO: Channel name should be accessible directly from message.room_object.name, but it isn't. Fix when possible.
+          # See: https://github.com/kenjij/lita-slack/issues/44
+          channel = Lita::Room.find_by_id(message.room_object.id).name
 
-        PublicSlackMessage.create! body: message.body, slack_username: message.user.mention_name, user: message_author, channel: channel
+          PublicSlackMessage.create! body: message.body, slack_username: message.user.mention_name, user: message_author, channel: channel
+        end
       end
 
       Lita.register_handler(self)

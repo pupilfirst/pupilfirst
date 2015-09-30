f4253a899b767d05b4989c009a59511392b2b8ee
Hari Gopal <hari@mobme.in>
2015-09-30 19:27:42 +0530

    Spec Faculty.valid_categories.

--- spec/models/faculty_spec.rb
+++ spec/models/faculty_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Faculty, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.valid_categories' do
+    it 'returns valid categories' do
+      categories = Faculty.valid_categories
+      expect(categories.count).to eq(3)
+      expect(categories - [Faculty::CATEGORY_TEAM, Faculty::CATEGORY_ADVISORY_BOARD, Faculty::CATEGORY_VISITING_FACULTY]).to be_empty
+    end
+  end
 end

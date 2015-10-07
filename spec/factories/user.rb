9dc52acfd0b08805fd7c6bf989252813eb2c8c2c
Hari Gopal <hari@mobme.in>
2015-10-07 21:54:58 +0530

    Try setting India as locale for Faker.

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :user do
     first_name { Faker::Name.first_name }
-    # Fakers last_names do not agree with our validation rules, hence re-using first_name
-    last_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
 
     factory :user_with_out_password do

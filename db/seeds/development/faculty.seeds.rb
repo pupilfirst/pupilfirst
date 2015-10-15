b874833dcdf612f143047fe0fd485bc69b8c4525
Hari Gopal <hari@mobme.in>
2015-10-15 14:05:54 +0530

    Seed faculty for development. [ci skip]

---
+++ db/seeds/development/faculty.seeds.rb
@@ -0,0 +1,37 @@
+require_relative 'helper'
+
+Faculty.create!(
+  name: 'Mickey Mouse',
+  title: 'CEO',
+  key_skills: Faker::Lorem.words(3).join(', '),
+  linkedin_url: 'https://linkedin.com',
+  category: 'team',
+  available_for_connect: true,
+  availability: Faker::Lorem.words(4).join(' '),
+  image: Rails.root.join('spec/support/uploads/faculty/mickey_mouse.jpg').open,
+  sort_index: 1
+)
+
+Faculty.create!(
+  name: 'Minne Mouse',
+  title: 'CTO',
+  key_skills: Faker::Lorem.words(3).join(', '),
+  linkedin_url: 'https://linkedin.com',
+  category: 'team',
+  available_for_connect: true,
+  availability: Faker::Lorem.words(4).join(' '),
+  image: Rails.root.join('spec/support/uploads/faculty/minnie_mouse.jpg').open,
+  sort_index: 2
+)
+
+Faculty.create!(
+  name: 'Donald Duck',
+  title: 'CFO',
+  key_skills: Faker::Lorem.words(3).join(', '),
+  linkedin_url: 'https://linkedin.com',
+  category: 'team',
+  available_for_connect: false,
+  availability: Faker::Lorem.words(4).join(' '),
+  image: Rails.root.join('spec/support/uploads/faculty/donald_duck.jpg').open,
+  sort_index: 3
+)

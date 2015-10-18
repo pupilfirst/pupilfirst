706a80266de9555474099e568e2e78ecf1f2b428
Abdul Jaleel <ajaleelp@gmail.com>
2015-10-18 20:17:18 +0530

    Reply to activity pings privately

--- lib/lita/handlers/activity.rb
+++ lib/lita/handlers/activity.rb
@@ -5,7 +5,7 @@ module Lita
 
       def greet(response)
         count = PublicSlackMessage.active_last_hour
-        response.reply("There were " + count.to_s + " active users in the last hour!")
+        response.reply_privately("There were " + count.to_s + " active users in the last hour!")
       end
     end
 

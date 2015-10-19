9cc05ac32b838ee6676364e9796acac6a76f8976
Abdul Jaleel <ajaleelp@gmail.com>
2015-10-19 16:17:45 +0530

    Restrict activity pings to Team SV, reply to public

--- lib/lita/handlers/activity.rb
+++ lib/lita/handlers/activity.rb
@@ -1,14 +1,14 @@
 module Lita
   module Handlers
     class Activity < Handler
-      route(/\Aactivity\?\z/, :activity, command: true)
+      route(/\Aactivity\?\z/, :activity, command: true, restrict_to: :sv_co_team)
 
       def activity(response)
         active_users = PublicSlackMessage.users_active_last_hour.batched
         user_names = active_users.present? ? "(@" + active_users.map(&:slack_username).join(', @') + ")" : ""
         active_startups = Startup.find active_users.select(:startup).distinct.pluck(:startup_id)
         startup_names = active_startups.present? ? "(" + active_startups.map(&:product_name).join(', ') + ")" : ""
-        response.reply_privately(
+        response.reply(
           "Here's activity for the last hour:",
           "batch founders active: " + active_users.count.to_s + user_names,
           "batch startups active: " + active_startups.count.to_s + startup_names)

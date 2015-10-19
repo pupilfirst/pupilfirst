1d7515857cabefccb6687785f53f2696ae1978a7
Abdul Jaleel <ajaleelp@gmail.com>
2015-10-19 16:21:57 +0530

    Also include total active users in vocalist's reply

--- lib/lita/handlers/activity.rb
+++ lib/lita/handlers/activity.rb
@@ -4,14 +4,16 @@ module Lita
       route(/\Aactivity\?\z/, :activity, command: true, restrict_to: :sv_co_team)
 
       def activity(response)
-        active_users = PublicSlackMessage.users_active_last_hour.batched
-        user_names = active_users.present? ? "(@" + active_users.map(&:slack_username).join(', @') + ")" : ""
-        active_startups = Startup.find active_users.select(:startup).distinct.pluck(:startup_id)
+        active_users = PublicSlackMessage.users_active_last_hour
+        active_batched_users = active_users.batched
+        user_names = active_batched_users.present? ? "(@" + active_batched_users.map(&:slack_username).join(', @') + ")" : ""
+        active_startups = Startup.find active_batched_users.select(:startup).distinct.pluck(:startup_id)
         startup_names = active_startups.present? ? "(" + active_startups.map(&:product_name).join(', ') + ")" : ""
         response.reply(
           "Here's activity for the last hour:",
-          "batch founders active: " + active_users.count.to_s + user_names,
-          "batch startups active: " + active_startups.count.to_s + startup_names)
+          "Total active users: " + active_users.count.to_s,
+          "Batch founders active: " + active_batched_users.count.to_s + user_names,
+          "Batch startups active: " + active_startups.count.to_s + startup_names)
       end
     end
 

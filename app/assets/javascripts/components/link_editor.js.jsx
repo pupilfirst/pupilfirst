12782ff7c7a7edf458f8f8c1b53543fea17bb1a8
Abdul Jaleel <ajaleelp@gmail.com>
2015-10-30 02:20:50 +0530

    Only show LinkList component if links present

--- app/assets/javascripts/components/link_editor.js.jsx
+++ app/assets/javascripts/components/link_editor.js.jsx
@@ -3,13 +3,24 @@ var LinkEditor = React.createClass({
     linksJSON: React.PropTypes.string
   },
   render: function() {
+    var links = JSON.parse(this.props.linksJSON);
     return (
       <div>
         <h4>Current Links</h4>
         <div className="row">
-          <div className="col-sm-offset-2 col-sm-10">
-            <LinkList linksJSON={ this.props.linksJSON }></LinkList>
-          </div>
+          { links && links.length > 0 ?
+          (
+            <div className="col-sm-offset-2 col-sm-10">
+              <LinkList linksJSON={ this.props.linksJSON }></LinkList>
+            </div>
+          )
+          :
+          (
+            <div className="col-sm-offset-2 col-sm-10">
+              <p>No links added!</p>
+            </div>
+          )
+          }
         </div>
       </div>
     );

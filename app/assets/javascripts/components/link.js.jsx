b818bcdbd48ce2b38f93c348e0c17f2bf235cdac
Abdul Jaleel <ajaleelp@gmail.com>
2015-10-29 18:04:54 +0530

    Add a link component

---
+++ app/assets/javascripts/components/link.js.jsx
@@ -0,0 +1,23 @@
+var Link = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string,
+    url: React.PropTypes.string,
+    private: React.PropTypes.bool
+  },
+
+  render: function() {
+    return (
+      <li className="list-group-item">
+        <a href={ this.props.url }>
+          <i className={ this.props.private ? 'fa fa-user-secret' : 'fa fa-globe'}></i>
+          &nbsp;{ this.props.title }
+        </a>
+        <div className="pull-right">
+          <a className="margin-right-10" href='#'>Edit</a>
+          <a href='#'>Delete</a>
+        </div>
+        <p className="grey-text margin-bottom-0">{ this.props.url }</p>
+      </li>
+          );
+        }
+});

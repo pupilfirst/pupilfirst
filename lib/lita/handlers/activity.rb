42b81107e80e60d2d65f5697db18dc63b69e533b
Hari Gopal <hari@mobme.in>
2015-11-20 15:54:22 +0530

    Refactor Lita activity handler.
    Wrapping execution in AR with_connection in attempt to fix DB
    connection fail issue.

--- lib/lita/handlers/activity.rb
+++ lib/lita/handlers/activity.rb
@@ -4,16 +4,36 @@ module Lita
       route(/\Aactivity\?\z/, :activity, command: true, restrict_to: :sv_co_team)
 
       def activity(response)
-        active_users = PublicSlackMessage.users_active_last_hour
-        active_batched_users = active_users.batched
-        user_names = active_batched_users.present? ? "(@" + active_batched_users.map(&:slack_username).join(', @') + ")" : ""
-        active_startups = Startup.find active_batched_users.select(:startup).distinct.pluck(:startup_id)
-        startup_names = active_startups.present? ? "(" + active_startups.map(&:product_name).join(', ') + ")" : ""
-        response.reply(
-          "Here's activity for the last hour:",
-          "Total active users: " + active_users.count.to_s,
-          "Batch founders active: " + active_batched_users.count.to_s + user_names,
-          "Batch startups active: " + active_startups.count.to_s + startup_names)
+        ActiveRecord::Base.connection_pool.with_connection do
+          response.reply(
+            "Here's activity for the last hour:",
+            'Total active users: ' + active_users.count.to_s,
+            'Batch founders active: ' + active_batched_users.count.to_s + user_names,
+            'Batch startups active: ' + active_startups.count.to_s + startup_names
+          )
+        end
+      end
+
+      private
+
+      def active_users
+        PublicSlackMessage.users_active_last_hour
+      end
+
+      def active_batched_users
+        active_users.batched
+      end
+
+      def user_names
+        active_batched_users.present? ? '(@' + active_batched_users.map(&:slack_username).join(', @') + ')' : ''
+      end
+
+      def active_startups
+        Startup.find active_batched_users.select(:startup).distinct.pluck(:startup_id)
+      end
+
+      def startup_names
+        active_startups.present? ? '(' + active_startups.map(&:product_name).join(', ') + ')' : ''
       end
     end
 

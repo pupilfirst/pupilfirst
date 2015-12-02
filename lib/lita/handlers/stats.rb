db58b979896c607490c52a1f344fde5bc70f4bd4
ajaleelp <ajaleelp@gmail.com>
2015-12-02 14:28:35 +0530

    Add a Stats handler for vocalist
    Responds to leaderboard? with the latest published leaderboard

---
+++ lib/lita/handlers/stats.rb
@@ -0,0 +1,34 @@
+module Lita
+  module Handlers
+    class Stats < Handler
+      route(/\Aleaderboard\?\z/, :leaderboard, command: true)
+
+      def leaderboard(response)
+        ActiveRecord::Base.connection_pool.with_connection do
+          response.reply(
+            "Last Published Leaderboard:",
+            ranked_list_of_startups
+          )
+        end
+      end
+
+      private
+
+      def self.ranked_list_of_startups
+        rank_list = ''
+        ranked_startups = Startup.leaderboard_of_batch Batch.first
+        ranked_startups.each do |startup_id, rank|
+          rank_list += "#{rank}.#{Startup.find(startup_id).product_name} "
+        end
+        unranked_startups = Startup.without_karma_and_rank_for_batch Batch.first
+        unranked_startups[0].each do |startup|
+          rank_list += "#{ranked_startups.length+1}.#{startup.product_name} "
+        end
+        rank_list
+      end
+
+    end
+
+    Lita.register_handler(Stats)
+  end
+end

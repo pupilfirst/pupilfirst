53e93f0e4f369d1b0e0cadd58668800758374a7a
ajaleelp <ajaleelp@gmail.com>
2015-12-03 23:45:40 +0530

    WIP: Hande pre-loading and submitting new slots using the slot grid

--- app/assets/javascripts/faculty/weekly_slots.coffee
+++ app/assets/javascripts/faculty/weekly_slots.coffee
@@ -4,5 +4,39 @@ slotsClickHandler = ->
 
     # Mark (or unmark) selected class.
     slot.toggleClass('selected')
+    slotValue = [slot.data('day'),slot.data('time')]
+
+    if $('#list_of_slots').val().length > 0
+      current_slots = JSON.parse($('#list_of_slots').val())
+    else
+      current_slots = []
+
+    if slot.hasClass('selected')
+      current_slots.push(slotValue)
+    else
+      index = findSlot(current_slots, slotValue)
+
+      if index > -1
+        current_slots.splice(index, 1);
+
+    $('#list_of_slots').val(JSON.stringify(current_slots))
+    console.log $('#list_of_slots').val()
+
+findSlot = (list, slotValue) ->
+  i = 0
+  while i < list.length
+    if String(list[i]) == String(slotValue)
+      return i
+    i++
+  -1
+
+markPresentSlots = ->
+  if $('#list_of_slots').val().length > 0
+    current_slots = JSON.parse($('#list_of_slots').val())
+    console.log current_slots
+
+
 
 $(document).on 'page:change', slotsClickHandler
+$(document).on 'page:change', findSlot
+$(document).on 'page:change', markPresentSlots
--- app/controllers/faculty_controller.rb
+++ app/controllers/faculty_controller.rb
@@ -25,5 +25,47 @@ class FacultyController < ApplicationController
     raise_not_found unless params[:token]
     @faculty = Faculty.find_by token: params[:token]
     raise_not_found unless @faculty
+
+    @slot_list = create_slot_list_for @faculty
+  end
+
+  # POST /faculty/:id/save_weekly_slots(token)
+  def save_weekly_slots
+    raise_not_found unless params[:token]
+
+    @faculty = Faculty.find_by token: params[:token]
+    raise_not_found unless @faculty
+
+    list_of_slots = JSON.parse(params[:list_of_slots])
+    save_slots_in_list list_of_slots, @faculty
+
+    flash.now[:success] = "Your slots have been saved succesfully!"
+    redirect_to :back
+  end
+
+  private
+
+  def save_slots_in_list(list, faculty)
+    start_date = 7.days.from_now.beginning_of_week.to_date
+    list.each do |slot|
+      date = start_date + slot[0] - 1 # index of dates start at 1
+      hour = slot[1].to_i
+      minute = (((slot[1].to_f) - hour) * 60).to_s.delete('.')[0..1]
+
+      connect_slot = faculty.connect_slots.find_or_initialize_by(
+        slot_at: Time.parse("#{date} #{hour.to_s.rjust(2, '0')}:#{minute}:00 +0530")
+      )
+      connect_slot.save!
+    end
+  end
+
+  def create_slot_list_for(faculty)
+    slots = faculty.connect_slots.next_week
+    list = slots.map do |slot|
+      day = (slot.slot_at.to_date - 7.days.from_now.beginning_of_week.to_date).to_i + 1
+      time = slot.slot_at.hour + slot.slot_at.min.to_f / 60
+      "[#{day},#{time}]"
+    end.join(',')
+    "[#{list}]"
   end
 end

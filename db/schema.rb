0c97e7262f232d2ca5be308820ecc47c05607a20
ajaleelp <ajaleelp@gmail.com>
2015-12-07 13:23:39 +0530

    Add a self_service flag to Faculty

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203110044) do
+ActiveRecord::Schema.define(version: 20151207073229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20151203110044) do
     t.datetime "updated_at",   null: false
     t.string   "email"
     t.string   "token"
+    t.boolean  "self_service"
   end
 
   add_index "faculty", ["category"], name: "index_faculty_on_category", using: :btree

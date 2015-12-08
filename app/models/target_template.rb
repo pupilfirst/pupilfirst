91c9cc0427638f27a8e64abdc5aad460b54d0493
ajaleelp <ajaleelp@gmail.com>
2015-12-08 20:32:12 +0530

    Minor fix - use the new Batch.current within TargetTemplate

--- app/models/target_template.rb
+++ app/models/target_template.rb
@@ -1,12 +1,5 @@
 class TargetTemplate < ActiveRecord::Base
-  def due_date(batch: current_batch)
+  def due_date(batch: Batch.current)
     (batch.start_date + days_from_start).to_date
-  end
-
-  # TODO: Rewrite this once batch is a model.
-  def current_batch
-    OpenStruct.new(
-      start_date: Date.parse('2015-08-17')
-    )
   end
 end

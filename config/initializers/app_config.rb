aceefe36f7fe081896b08c824fe85fb6bf5826b2
ajaleelp <ajaleelp@gmail.com>
2015-12-18 12:43:01 +0530

    Stub requests to slack in user_spec
    Also use APP_CONFIG to access ENV variables in code

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,5 +1,6 @@
 APP_CONFIG = {
   sms_provider_url: ENV['SMS_PROVIDER_URL'],
   google_analytics_tracking_id: ENV['GOOGLE_ANALYTICS_TRACKING_ID'],
-  easyrtc_socket_url: ENV['EASYRTC_SOCKET_URL']
+  easyrtc_socket_url: ENV['EASYRTC_SOCKET_URL'],
+  vocalist_api_token: ENV['VOCALIST_API_TOKEN']
 }.with_indifferent_access

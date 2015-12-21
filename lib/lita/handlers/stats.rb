67bcbb9262f0badc6efa8339957467d00b1891bc
ajaleelp <ajaleelp@gmail.com>
2015-12-21 12:49:55 +0530

    use the chat.postMessage slack API method to send leaderboard with links

--- lib/lita/handlers/stats.rb
+++ lib/lita/handlers/stats.rb
@@ -5,10 +5,14 @@ module Lita
 
       def leaderboard(response)
         ActiveRecord::Base.connection_pool.with_connection do
-          response.reply(
-            "Last Published Leaderboard:",
-            ranked_list_of_startups
-          )
+          response.reply('Please wait while I fetch the leaderboard for you :simple_smile:')
+          begin
+            message = "Here is the latest published leaderboard:%0A#{ranked_list_of_startups}"
+            RestClient.get "https://slack.com/api/chat.postMessage?token=#{APP_CONFIG[:slack_token]}
+              &channel=#{response.message.source.room}&text=#{message}&as_user=true"
+          rescue
+            response.reply(':confused: Oops! Something seems wrong. Please try again later!')
+          end
         end
       end
 
@@ -16,11 +20,11 @@ module Lita
         rank_list = ''
         ranked_startups = Startup.leaderboard_of_batch Batch.current
         ranked_startups.each do |startup_id, rank|
-          rank_list += "#{rank}. #{Startup.find(startup_id).product_name}\n"
+          rank_list += "#{rank}. <#{Rails.application.routes.url_helpers.startup_url(Startup.find(startup_id))}|#{Startup.find(startup_id).product_name}>%0A"
         end
         unranked_startups = Startup.without_karma_and_rank_for_batch Batch.current
         unranked_startups[0].each do |startup|
-          rank_list += "#{ranked_startups.length + 1}. #{startup.product_name}\n"
+          rank_list += "#{ranked_startups.length + 1}. <#{Rails.application.routes.url_helpers.startup_url(startup)}|#{startup.product_name}>%0A"
         end
         rank_list
       end

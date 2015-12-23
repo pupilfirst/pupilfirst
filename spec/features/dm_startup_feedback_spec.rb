10085459bf608d9f52b739b5139122df7e8a522a
ajaleelp <ajaleelp@gmail.com>
2015-12-23 16:39:12 +0530

    Complete spec for DMing startup feedback

--- spec/features/dm_startup_feedback_spec.rb
+++ spec/features/dm_startup_feedback_spec.rb
@@ -7,6 +7,15 @@ feature 'DM Startup Feedback' do
   let!(:faculty) { create :faculty }
   let!(:startup_feedback) { create :startup_feedback, faculty: faculty, startup: startup }
 
+  let!(:founder_1_request) do
+    stub_request(:get, 'https://slack.com/api/chat.postMessage?as_user=true&channel=DABCDEF'\
+    "&text=#{startup_feedback.as_slack_message}&token=xxxxxx&unfurl_links=false")
+  end
+  let!(:founder_2_request) do
+    stub_request(:get, 'https://slack.com/api/chat.postMessage?as_user=true&channel=D123456'\
+    "&text=#{startup_feedback.as_slack_message}&token=xxxxxx&unfurl_links=false")
+  end
+
   before :all do
     APP_CONFIG[:slack_token] = 'xxxxxx'
   end
@@ -27,6 +36,11 @@ feature 'DM Startup Feedback' do
       .to_return(body: '{"ok":true,"members":[{"id":"UABCDEF","name":"founder1"},{"id":"U123456","name":"founder2"}]}')
     stub_request(:get, "https://slack.com/api/im.list?token=xxxxxx")
       .to_return(body: '{"ok":true,"ims":[{"id":"D123456","user":"U123456"},{"id":"DABCDEF","user":"UABCDEF"}]}')
+
+    # add slack info for founders
+    startup.founders.first.update!(slack_username: 'founder1')
+    startup.founders.second.update!(slack_username: 'founder2')
+    startup.reload
   end
 
   context 'Admin visits startup feedback index page in AA' do
@@ -37,18 +51,6 @@ feature 'DM Startup Feedback' do
     end
 
     scenario 'Admin sends DM to all founders from the index page' do
-      # add slack info for founders
-      startup.founders.first.update!(slack_username: 'founder1')
-      startup.founders.second.update!(slack_username: 'founder2')
-      startup.reload
-      expect(startup.founders.where.not(slack_user_id: nil).count).to eq(2)
-
-      founder_1_request = stub_request(:get, 'https://slack.com/api/chat.postMessage?as_user=true&channel=DABCDEF'\
-        "&text=#{startup_feedback.as_slack_message}&token=xxxxxx&unfurl_links=false")
-
-      founder_2_request = stub_request(:get, 'https://slack.com/api/chat.postMessage?as_user=true&channel=D123456'\
-        "&text=#{startup_feedback.as_slack_message}&token=xxxxxx&unfurl_links=false")
-
       visit admin_startup_feedback_index_path
       expect(page).to have_text(startup_feedback.feedback)
       click_on 'DM on Slack Now!'
@@ -56,6 +58,26 @@ feature 'DM Startup Feedback' do
       expect(founder_1_request).to have_been_made.once
       expect(founder_2_request).to have_been_made.once
       expect(page).to have_text(startup_feedback.feedback)
+    end
+
+    scenario 'Admin sends DM to all founders from the show page' do
+      visit admin_startup_feedback_path(startup_feedback)
+      expect(page).to have_text("Startup feedback ##{startup_feedback.id}")
+      click_on 'Send DM to all founders.'
+
+      expect(founder_1_request).to have_been_made.once
+      expect(founder_2_request).to have_been_made.once
+      expect(page).to have_text("Startup feedback ##{startup_feedback.id}")
+    end
+
+    scenario 'Admin sends DM to a selected founder from the show page' do
+      visit admin_startup_feedback_path(startup_feedback)
+      expect(page).to have_text("Startup feedback ##{startup_feedback.id}")
+      click_on 'Send DM'
+
+      expect(founder_1_request).to have_been_made.once
+      expect(founder_2_request).not_to have_been_made
+      expect(page).to have_text("Startup feedback ##{startup_feedback.id}")
     end
   end
 end

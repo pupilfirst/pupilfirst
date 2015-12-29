9aec52d01db457fd62a73e895536ae6f59a00cab
Hari Gopal <hari@mobme.in>
2015-12-29 14:22:55 +0530

    Spec TargetTemplate.

---
+++ spec/factories/target_template.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :target_template do
+    days_from_start { [30, 60, 90, 180].sample }
+    role { Target.valid_roles.sample }
+    title { Faker::Lorem.words(4).join(' ') }
+    description { Faker::Lorem.paragraph(3, 0, 3) }
+    completion_instructions { Faker::Lorem.words(10).join(' ') }
+  end
+end
--- spec/models/target_template_spec.rb
+++ spec/models/target_template_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe TargetTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { create :target_template }
+
+  describe '#due_date' do
+    context 'when batch is supplied' do
+      it 'returns due date, as per batch' do
+        batch = create :batch, start_date: 1.year.ago, end_date: 6.months.ago
+        expect(subject.due_date(batch: batch)).to eq(batch.start_date + subject.days_from_start)
+      end
+    end
+
+    context 'when batch is not supplied' do
+      let!(:batch) { create :batch, start_date: 3.months.ago, end_date: 3.months.from_now }
+
+      it 'returns due date, as per current batch' do
+        expect(subject.due_date).to eq(batch.start_date + subject.days_from_start)
+      end
+    end
+  end
 end

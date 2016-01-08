5df97dd498a3edb646e027bc9a402e3ba177f862
Vinu T V <vinu.tv@mobme.in>
2016-01-08 14:24:03 +0530

    Added dynamic data to founder profile

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -3,4 +3,16 @@ module HomeHelper
     leading_startup_ids = Startup.leaderboard_toppers_for_batch Batch.current, count: event_count
     leading_startup_ids.map { |startup_id| Startup.find(startup_id).showcase_timeline_event }.compact
   end
+
+  def activity_class_for_count(count)
+    if count == 0
+      'activity-blank'
+    elsif count <= 5
+      'activity-low'
+    elsif count <= 10
+      'activity-medium'
+    else
+      'activity-high'
+    end
+  end
 end

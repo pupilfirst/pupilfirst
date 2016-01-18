5b8edac67e768b6a8628be4f809e8d190e7f5ed6
Hari Gopal <hari@mobme.in>
2016-01-18 19:53:08 +0530

    FactoryGirl: Ensure batch names don't repeat.

--- spec/factories/batch.rb
+++ spec/factories/batch.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :batch do
-    name { Faker::Lorem.word }
+    sequence(:name) { |n| "#{Faker::Lorem.word} #{n}" }
+    sequence(:batch_number) { |n| n + 1 }
     description { Faker::Lorem.words(10).join ' ' }
     start_date { 1.month.ago }
     end_date { 5.months.from_now }
-    sequence(:batch_number) { |n| n + 1 }
   end
 end

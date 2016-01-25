2ada9c6cbdb91ab1caa25198cef91b25a26b4f91
Hari Gopal <hari@mobme.in>
2016-01-26 01:14:13 +0530

    Make Rubocop happy.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       provider: 'AWS', # required
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # required
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # required
-      region: ENV['S3_REGION'],                          # optional, defaults to 'us-east-1'
+      region: ENV['S3_REGION'], # optional, defaults to 'us-east-1'
       host: 's3.amazonaws.com', # optional, defaults to nil
       # endpoint: 'https://s3.example.com:8080/'           # optional, defaults to nil
       path_style: true

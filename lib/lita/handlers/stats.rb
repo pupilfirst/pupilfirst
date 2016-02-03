8a1801397743a44569285e298f59f77f7ad78b81
ajaleelp <ajaleelp@gmail.com>
2016-02-03 16:10:36 +0530

    Vocalist: Reply with batch-wise leaderboards of current batches

--- lib/lita/handlers/stats.rb
+++ lib/lita/handlers/stats.rb
@@ -5,36 +5,45 @@ module Lita
 
       def leaderboard(response)
         ActiveRecord::Base.connection_pool.with_connection do
-          response.reply('Please wait while I fetch the leaderboard for you :simple_smile:')
+          response.reply('Please wait while I fetch the leaderboards of current batches for you :simple_smile:')
           begin
-            message = "Here is the <#{Rails.application.routes.url_helpers.about_leaderboard_url}|latest published leaderboard>: \n#{ranked_list_of_startups}"
-
             if response.message.source.private_message
               # respond directly to the user if private message
               slack_username = response.message.source.user.metadata['mention_name']
               user = ActiveRecord::Base::User.find_by(slack_username: slack_username)
-              PublicSlackTalk.post_message message: message, user: user
+              PublicSlackTalk.post_message message: leaderboard_response_message, user: user
             else
               # reply to the source channel if not a private message
               channel = response.message.source.room
-              PublicSlackTalk.post_message message: message, channel: channel
+              PublicSlackTalk.post_message message: leaderboard_response_message, channel: channel
             end
-            # RestClient.get "https://slack.com/api/chat.postMessage?"\
-            # "token=#{APP_CONFIG[:slack_token]}&channel=#{response.message.source.room}"\
-            # "&text=#{message}&as_user=true"
           rescue
             response.reply(':confused: Oops! Something seems wrong. Please try again later!')
           end
         end
       end
 
-      def ranked_list_of_startups
+      # construct the leaderboard response to be send
+      def leaderboard_response_message
+        return '_There appears to be no live batches on SV.CO now !_' unless Batch.live.present?
+
+        # Build response for all live batches
+        response = ''
+        Batch.live.each do |batch|
+          response += "*<#{Rails.application.routes.url_helpers.about_leaderboard_url}\
+          |Latest published leaderboard for Batch #{batch.batch_number} (#{batch.name})>:* \n#{ranked_list_for_batch batch}"
+        end
+
+        response
+      end
+
+      def ranked_list_for_batch(batch)
         rank_list = ''
-        ranked_startups = Startup.leaderboard_of_batch Batch.current
+        ranked_startups = Startup.leaderboard_of_batch batch
         ranked_startups.each do |startup_id, rank|
           rank_list += "#{rank}. <#{Rails.application.routes.url_helpers.startup_url(Startup.find(startup_id))}|#{Startup.find(startup_id).product_name}>\n"
         end
-        unranked_startups = Startup.without_karma_and_rank_for_batch Batch.current
+        unranked_startups = Startup.without_karma_and_rank_for_batch batch
         unranked_startups[0].each do |startup|
           rank_list += "#{ranked_startups.length + 1}. <#{Rails.application.routes.url_helpers.startup_url(startup)}|#{startup.product_name}>\n"
         end

1ab1d41683c69de129e4365dd8acfbd9862b8ec6
ajaleelp <ajaleelp@gmail.com>
2016-02-05 15:52:30 +0530

    Modify backup handler to save timestamp and parent_messag information

--- lib/lita/handlers/backup.rb
+++ lib/lita/handlers/backup.rb
@@ -4,8 +4,8 @@ module Lita
       on :unhandled_message do |payload|
         ActiveRecord::Base.connection_pool.with_connection do
           message = payload[:message]
-          binding.pry
 
+          # do not backup private messages
           next if message.private_message?
 
           message_author_slack_username = message.user.mention_name
@@ -13,13 +13,18 @@ module Lita
 
           # TODO: Channel name should be accessible directly from message.room_object.name, but it isn't. Fix when possible.
           # See: https://github.com/kenjij/lita-slack/issues/44
+          # Alert: As mentioned in link above, the following approach to extract channel will fail for private messages.
           channel = Lita::Room.find_by_id(message.room_object.id).try(:name)
 
           # if reaction, fetch message reacted to
-          parent_message = fetch_parent_message(message) if message.is_a? Reaction
+          parent_message = fetch_parent_message(message) if message.is_a? Lita::Adapters::Slack::Reaction
+
+          # TODO: Probably deal with removal of reactions from the db too
+          # do not create entries for reactions removed
+          next if parent_message.present? && message.event == "removed"
 
           PublicSlackMessage.create! body: message.body, slack_username: message.user.mention_name,
-            user: message_author, channel: channel, parent_message: parent_message, timestamp:
+            user: message_author, channel: channel, parent_message: parent_message, timestamp: message.extensions[:slack][:timestamp]
         end
       end
 

738897483e49e108111015559c157529ced12097
Hari Gopal <hari@mobme.in>
2016-02-15 18:01:27 +0530

    We don't have mentors anymore.

--- db/seeds/development/founders.seeds.rb
+++ db/seeds/development/founders.seeds.rb
@@ -2,6 +2,3 @@ require_relative 'helper'
 
 # Just a user.
 Founder.create!(email: 'someone@mobme.in', first_name: 'Some', last_name: 'One', password: 'password', password_confirmation: 'password', confirmed_at: Time.now, born_on: 20.years.ago, gender: 'male')
-
-# A user is to be a mentor.
-Founder.create!(email: 'mentor@sv.co', first_name: 'Mentor', last_name: 'User', password: 'password', password_confirmation: 'password', confirmed_at: Time.now, born_on: 20.years.ago, gender: 'male')

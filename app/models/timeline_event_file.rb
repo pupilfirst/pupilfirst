b60a81a4b7d950ccb50b829bec0b66bf6e29b898
Hari Gopal <hari@mobme.in>
2016-02-15 12:02:49 +0530

    Allow timeline event files to be visible even if its event is private. Also spec the visible_to? method.

--- app/models/timeline_event_file.rb
+++ app/models/timeline_event_file.rb
@@ -5,14 +5,11 @@ class TimelineEventFile < ActiveRecord::Base
 
   # Used to determine whether file can be downloaded by visitor.
   def visible_to?(user)
-    # Owner of event will always have visibility.
-    return true if user == timeline_event.user
-
-    # Only owner has visibility to private event (and attachments).
-    return false if timeline_event.private?
-
     # A public file should be visible.
     return true unless private?
+
+    # Owner of event will always have visibility.
+    return true if user == timeline_event.user
 
     # If private, check if visitor is a founder in linked startup.
     user.present? && timeline_event.startup.founders.include?(user)

156c5b76ea636091aa6252a7273cead5f25ed8f5
Hari Gopal <hari@mobme.in>
2016-02-16 16:45:44 +0530

    Update founder seed.

--- db/seeds/development/founders.seeds.rb
+++ db/seeds/development/founders.seeds.rb
@@ -1,4 +1,13 @@
 require_relative 'helper'
 
-# Just a user.
-Founder.create!(email: 'someone@mobme.in', first_name: 'Some', last_name: 'One', password: 'password', password_confirmation: 'password', confirmed_at: Time.now, born_on: 20.years.ago, gender: 'male')
+# 'The' founder.
+Founder.create!(
+  email: 'someone@sv.co',
+  first_name: 'Some',
+  last_name: 'One',
+  password: 'password',
+  password_confirmation: 'password',
+  confirmed_at: Time.now,
+  born_on: 20.years.ago,
+  gender: Founder::GENDER_MALE
+)

edcef9157e1ef76bc8552dd104496f90774a5c8f
ajaleelp <ajaleelp@gmail.com>
2016-02-25 16:54:15 +0530

    Few corrections in seed files

--- db/seeds/development/colleges.seeds.rb
+++
@@ -1,9 +0,0 @@
-colleges = [
-  ['International School of Photonics, Cochin', 'CUSAT'],
-  ['Indian Institute of Space Science and Technology Thiruvananthapuram', 'IIST'],
-  ['Co-Operative Arts and Science College, Pazhayangadi', 'Kannur University']
-]
-
-colleges.each do |college|
-  College.create! name: college[0], university: college[1]
-end

30b7ccd507ad2433eed2a66d8227954d5986d8af
Hari Gopal <hari@mobme.in>
2016-03-02 12:56:53 +0530

    AA: Display all taggings on Tag#show.

--- app/admin/tag.rb
+++ app/admin/tag.rb
@@ -15,4 +15,20 @@ ActiveAdmin.register ActsAsTaggableOn::Tag, as: 'Tags' do
 
     actions
   end
+
+  show do |tag|
+    attributes_table do
+      row :id
+      row :name
+      row :taggings_count
+    end
+
+    panel 'Taggings' do
+      table_for tag.taggings do
+        column :taggable
+        column :taggable_type
+        column :created_at
+      end
+    end
+  end
 end

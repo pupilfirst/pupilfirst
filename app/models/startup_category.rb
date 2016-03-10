fc01f73e96c26f1f96d709f33126cb72970d7231
Hari Gopal <hari@mobme.in>
2016-03-10 14:21:16 +0530

    Startup category requires a unique non-nil name.

--- app/models/startup_category.rb
+++ app/models/startup_category.rb
@@ -1,5 +1,8 @@
 class StartupCategory < ActiveRecord::Base
   has_and_belongs_to_many :startups
+
+  validates :name, uniqueness: true, presence: true
+
   # Before a category is destroyed, make sure that entries in association tables are removed.
   before_destroy do
     startups.clear

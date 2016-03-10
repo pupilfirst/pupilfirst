e6a84168ade3f73345628e3550996acee89c8593
ajaleelp <ajaleelp@gmail.com>
2016-03-10 17:35:43 +0530

    Allow admin to mark faculty as inactive

--- app/admin/faculty.rb
+++ app/admin/faculty.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Faculty do
-  permit_params :name, :email, :title, :key_skills, :linkedin_url, :category, :image, :sort_index, :self_service, :current_commitment, :founder_id
+  permit_params :name, :email, :title, :key_skills, :linkedin_url, :category, :image, :sort_index, :self_service, :current_commitment, :founder_id, :inactive
 
   config.sort_order = 'sort_index_asc'
 
@@ -32,6 +32,7 @@ ActiveAdmin.register Faculty do
       f.input :key_skills
       f.input :linkedin_url
       f.input :sort_index
+      f.input :inactive
       f.input :self_service
       f.input :current_commitment
       f.input :founder

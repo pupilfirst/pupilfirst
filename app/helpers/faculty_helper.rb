2c3abddccb9621701babbe3cdd4b936a222d3516
Hari Gopal <hari@mobme.in>
2016-03-28 12:13:26 +0530

    Render faculty rating stars dynamically.

--- app/helpers/faculty_helper.rb
+++ app/helpers/faculty_helper.rb
@@ -30,4 +30,18 @@ module FacultyHelper
   def active_tab(name)
     @active_tab == name ? 'active' : nil
   end
+
+  def faculty_rating_stars(rating)
+    # Round rating to nearest .5, or .0.
+    rating = ((rating * 2).round / 2.0)
+
+    # Add full stars.
+    stars_html = (1..rating).map { |_r| '<i class="fa fa-star"></i>' }
+
+    # Add half star, if any.
+    stars_html << '<i class="fa fa-star-half-o"></i>' if rating.to_i != rating
+
+    # Return as HTML.
+    stars_html.join("\n").html_safe
+  end
 end

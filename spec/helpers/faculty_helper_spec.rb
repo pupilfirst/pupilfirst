e8f7c56b09175733a97dc6ff44cb51e8c93a3057
Hari Gopal <hari@mobme.in>
2016-04-06 13:00:38 +0530

    Remove empty ConnectRequestHelper and spec [skip ci].

--- spec/helpers/faculty_helper_spec.rb
+++ spec/helpers/faculty_helper_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the FacultyHelper. For example:
-#
-# describe FacultyHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe FacultyHelper, type: :helper do
   describe '#faculty_image_path' do
     it 'returns image file path for faculty' do

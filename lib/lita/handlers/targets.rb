0e086350e63bd116536e4956820b13af0d8b9878
Hari Gopal <hari@mobme.in>
2016-04-15 14:26:27 +0530

    Make Rubocop happy by refactoring.

--- lib/lita/handlers/targets.rb
+++ lib/lita/handlers/targets.rb
@@ -64,21 +64,31 @@ module Lita
 
         optional = []
 
-        optional << "*Completion Instructions:* #{chosen_target.completion_instructions}" if chosen_target.completion_instructions.present?
+        add_optional_completion_instructions(optional)
+        add_optional_resource_url(optional)
+        add_optional_rubric(optional)
 
+        @optional_target_data = (optional.join("\n") + "\n") if optional.present?
+      end
+
+      def add_optional_completion_instructions(optional)
+        optional << "*Completion Instructions:* #{chosen_target.completion_instructions}" if chosen_target.completion_instructions.present?
+      end
+
+      def add_optional_resource_url(optional)
         if chosen_target.resource_url.present?
           shortened_url = Shortener::ShortenedUrl.generate chosen_target.resource_url
           url_with_host = "https://sv.co/#{shortened_url.unique_key}"
           optional << "*Linked Resource:* <#{url_with_host}|#{url_with_host}>"
         end
+      end
 
+      def add_optional_rubric(optional)
         if chosen_target.rubric.present?
           shortened_url = Shortener::ShortenedUrl.generate(chosen_target.rubric_url, expires_at: 10.minutes.from_now)
           url_with_host = "https://sv.co/#{shortened_url.unique_key}"
           optional << "*Rubric:* <#{url_with_host}|#{chosen_target.rubric_filename}> _(link expires in 10 minutes)_"
         end
-
-        @optional_target_data = (optional.join("\n") + "\n") if optional.present?
       end
 
       def reply_with_targets_info

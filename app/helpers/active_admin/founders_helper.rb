98dbae1fc0c066951ce69ff94b9820180d4a62df
Hari Gopal <hari@sv.co>
2016-04-25 14:30:57 +0530

    Display startup tokens dropdown only if they are available.

--- app/helpers/active_admin/founders_helper.rb
+++ app/helpers/active_admin/founders_helper.rb
@@ -1,5 +1,9 @@
 module ActiveAdmin
   module FoundersHelper
+    def startup_tokens_available?
+      Founder.where.not(startup_token: nil).present?
+    end
+
     def startup_token_collection
       (Founder.distinct.pluck(:startup_token) - [nil]).map do |token|
         ["Team lead by #{Founder.lead_of(token).email} (Invited on #{token})", token]

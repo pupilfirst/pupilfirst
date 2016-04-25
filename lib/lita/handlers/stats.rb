774715852faf5fa217679c65042b0af3707d506a
Jaleel <jaleel@sv.co>
2016-04-25 12:53:53 +0530

    Simplify stats handler - response.reply is good enough

--- lib/lita/handlers/stats.rb
+++ lib/lita/handlers/stats.rb
@@ -5,8 +5,7 @@ module Lita
         /\Astate of SV.CO for batch *(\d*) *\?\z/,
         :state_of_batch,
         command: true,
-        restrict_to: :sv_co_team,
-        help: { 'state of SV.CO for batch N' => I18n.t('slack.help.state_of_svco') }
+        help: { 'state of SV.CO for batch N?' => I18n.t('slack.help.state_of_svco') }
       )
 
       def state_of_batch(response)
@@ -17,33 +16,16 @@ module Lita
           @batch_requested = @response.match_data[1].present? ? ::Batch.find_by_batch_number(@response.match_data[1].to_i) : nil
 
           # respond with error if batch number missing
-          unless @batch_requested.present?
-            send_batch_missing_message
-            return
-          end
-
-          @response.message.source.private_message ? send_stats_privately : send_stats_to_channel
+          @batch_requested.present? ? reply_with_state_of_batch : send_batch_missing_message
         end
       end
 
       def send_batch_missing_message
-        @response.reply('Please specify a batch number! eg: `state of SV.CO for batch 1?`')
+        @response.reply 'Please specify a batch number! eg: `state of SV.CO for batch 1?`'
       end
 
-      def send_stats_privately
-        slack_username = @response.message.source.user.metadata['mention_name']
-        founder = ::Founder.find_by(slack_username: slack_username)
-
-        if founder
-          PublicSlackTalk.post_message message: batch_state_message, founder: founder
-        else
-          reply_using_api_post_message channel: @response.message.source.room, message: batch_state_message
-        end
-      end
-
-      def send_stats_to_channel
-        channel = @response.message.source.room
-        PublicSlackTalk.post_message message: batch_state_message, channel: channel
+      def reply_with_state_of_batch
+        @response.reply batch_state_message
       end
 
       def batch_state_message
@@ -89,11 +71,6 @@ module Lita
       def list_of_names(startups)
         return '' unless startups.present?
         '(' + startups.map { |startup| "<#{Rails.application.routes.url_helpers.startup_url(startup)}|#{startup.product_name}>" }.join(', ') + ')'
-      end
-
-      def reply_using_api_post_message(channel:, message:)
-        RestClient.get "https://slack.com/api/chat.postMessage?token=#{APP_CONFIG[:slack_token]}&channel=#{channel}"\
-        "&text=#{CGI.escape message}&as_user=true"
       end
     end
 

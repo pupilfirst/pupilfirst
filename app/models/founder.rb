b20f144b33a882eb76c6e6b55b15950d812c4793
Hari Gopal <hari@sv.co>
2016-04-26 12:39:59 +0530

    Profile completion instructions shouldn't check whether startup has been created.

--- app/models/founder.rb
+++ app/models/founder.rb
@@ -365,20 +365,18 @@ class Founder < ActiveRecord::Base
   # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
   def profile_completion_percentage
     score = 20 # a default score given for required fields during registration
-    score += 20 if startup&.approved? # has an approved startup
-    score += 10 if slack_user_id.present? # has a valid slack account associated
+    score += 15 if slack_user_id.present? # has a valid slack account associated
     score += 10 if skype_id.present?
-    score += 10 if social_url_present? # has atleast 1 social media links
+    score += 15 if social_url_present? # has atleast 1 social media links
     score += 5 if communication_address.present?
-    score += 5 if about.present?
+    score += 10 if about.present?
     score += 10 if identification_proof.present?
-    score += 10 if resume_url.present? # has uploaded resume
+    score += 15 if resume_url.present? # has uploaded resume
     score
   end
 
   # Return the 'next-applicable' profile completion instruction as a string
   def profile_completion_instruction
-    return 'Complete your startup registraton!' unless startup&.approved?
     return 'Join the SV.CO Public Slack and update your slack username!' unless slack_user_id.present?
     return 'Update your Skype Id' unless skype_id.present?
     return 'Provide at-least one of your social profiles!' unless social_url_present?

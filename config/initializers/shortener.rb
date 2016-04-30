726a6831160c8f6f67452820261b142da0b6460b
Jaleel <jaleel@sv.co>
2016-04-30 12:45:19 +0530

    add forbidden keys for shortener urls

--- config/initializers/shortener.rb
+++ config/initializers/shortener.rb
@@ -1,3 +1,5 @@
 # Configs for the 'shortener' gem : https://github.com/jpmcgrath/shortener
 
 Shortener.default_redirect = "/404" # an ugly hack to prevent the gem from messing up with 404s
+
+Shortener.forbidden_keys.concat %w(admin founder startups about faculty library talent apply transparency timeline ahoy)

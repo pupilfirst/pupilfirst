940abf424ae1edc234956fa01bcaeefb872b1a8b
Jaleel <jaleel@sv.co>
2016-05-04 16:50:43 +0530

    Major refactoring

--- app/helpers/active_admin/targets_overview_helper.rb
+++ app/helpers/active_admin/targets_overview_helper.rb
@@ -1,49 +1,38 @@
 module ActiveAdmin
   module TargetsOverviewHelper
-    def assignees(target_template, status_scope)
+    def assignees_list(target_template, status:)
       if target_template.founder_role?
-        founders = Founder.find_by_batch(batch_selected)
-          .joins(:targets)
-          .where(targets: { target_template_id: target_template.id })
-          .merge(Target.send(status_scope))
-          .distinct
-
-        assignees_list = founders.map{ |f| link_to(f.fullname, admin_founder_path(f)) }.join(', ')
+        assignees(target_template, status).distinct.map { |f| link_to(f.fullname, admin_founder_path(f)) }
       else
-        startups = Startup
-          .joins(:targets)
-          .where(batch: batch_selected, targets: { target_template_id: target_template.id })
-          .merge(Target.send(status_scope))
-          .distinct
-
-        assignees_list = startups.map{ |s| link_to(s.product_name, admin_startup_path(s)) }.join(', ')
-      end
-
-      assignees_list.html_safe
+        assignees(target_template, status).distinct.map { |s| link_to(s.product_name, admin_startup_path(s)) }
+      end.join(', ').html_safe
     end
 
-    def assignees_with_extension(target_template)
-      if target_template.founder_role?
-        founders = Founder.find_by_batch(batch_selected)
+    def extended_scope?(status)
+      status.to_sym == :extended
+    end
+
+    def effective_scope(status)
+      extended_scope?(status) ? 'pending' : status
+    end
+
+    # returns founders or startups (as applicable) which have targets built from the specified template
+    def assignees(target_template, status)
+      assignees = if target_template.founder_role?
+        Founder.find_by_batch(batch_selected)
           .joins(:targets)
           .where(targets: { target_template_id: target_template.id })
-          .where('targets.due_date > ?', target_template.due_date )
-          .merge(Target.pending)
-          .distinct
-
-        assignees_list = founders.map{ |f| link_to(f.fullname, admin_founder_path(f)) }.join(', ')
+          .merge(Target.send(effective_scope(status)))
       else
-        startups = Startup
+        Startup
           .joins(:targets)
           .where(batch: batch_selected, targets: { target_template_id: target_template.id })
-          .where('targets.due_date > ?', target_template.due_date )
-          .merge(Target.pending)
-          .distinct
-
-        assignees_list = startups.map{ |s| link_to(s.product_name, admin_startup_path(s)) }.join(', ')
+          .merge(Target.send(effective_scope(status)))
       end
 
-      assignees_list.html_safe
+      # extended targets are those with due date greater than their parent template
+      assignees = assignees.where('targets.due_date > ?', target_template.due_date) if extended_scope?(status)
+      assignees
     end
   end
 end

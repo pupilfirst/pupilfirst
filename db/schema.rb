1df96ccb6d7b2610c054ea5379c5e6cdbab6b62d
Hari Gopal <hari@sv.co>
2016-05-16 13:44:31 +0530

    Remove links from GlossaryTerm.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513064505) do
+ActiveRecord::Schema.define(version: 20160516081338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -231,7 +231,6 @@ ActiveRecord::Schema.define(version: 20160513064505) do
   create_table "glossary_terms", force: :cascade do |t|
     t.string   "term"
     t.text     "definition"
-    t.text     "links"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

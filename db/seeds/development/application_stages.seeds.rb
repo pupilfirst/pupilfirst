f06a81d117525f886c31affc4bd30f70ce569ad7
Hari Gopal <hari@sv.co>
2016-05-16 16:01:01 +0530

    Seed application stages.

---
+++ db/seeds/development/application_stages.seeds.rb
@@ -0,0 +1,7 @@
+require_relative 'helper'
+
+ApplicationStage.create!(number: 1, name: 'Open')
+ApplicationStage.create!(number: 2, name: 'Test')
+ApplicationStage.create!(number: 3, name: 'Interview')
+ApplicationStage.create!(number: 4, name: 'Pre-selection')
+ApplicationStage.create!(number: 5, name: 'Closed')

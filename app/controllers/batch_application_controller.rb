c0d1d82e9ec0b158b0cf472ccb21b72e694ecc41
Hari Gopal <hari@sv.co>
2016-05-18 16:33:55 +0530

    Batch can be fetched by number, and correctly loading application stage number of batch.

--- app/controllers/batch_application_controller.rb
+++ app/controllers/batch_application_controller.rb
@@ -4,8 +4,8 @@ class BatchApplicationController < ApplicationController
   end
 
   def apply
-    @batch = Batch.find_by(name: params[:batch])
-    stage_number = batch&.application_stage&.number
+    @batch = Batch.find_by(name: params[:batch]) || Batch.find_by(batch_number: params[:batch])
+    stage_number = @batch&.application_stage&.number
     raise_not_found if stage_number.blank?
     render "batch_application/stage-#{stage_number}"
   end

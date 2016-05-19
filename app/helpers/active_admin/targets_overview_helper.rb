34b86bbae559edd44a255fc38a495a7c822e0573
Jaleel <jaleel@sv.co>
2016-05-19 14:26:06 +0530

    Target Overview: For completed targets, link the list to the respective events submitted

--- app/helpers/active_admin/targets_overview_helper.rb
+++ app/helpers/active_admin/targets_overview_helper.rb
@@ -2,10 +2,22 @@ module ActiveAdmin
   module TargetsOverviewHelper
     def assignees_list(target_template, status:)
       if target_template.founder_role?
-        assignees(target_template, status).distinct.map { |f| link_to(f.fullname, admin_founder_path(f)) }
+        assignees(target_template, status).distinct.map { |founder| link_to(founder.fullname, name_link(founder, target_template, status)) }
       else
-        assignees(target_template, status).distinct.map { |s| link_to(s.product_name, admin_startup_path(s)) }
+        assignees(target_template, status).distinct.map { |startup| link_to(startup.product_name, name_link(startup, target_template, status)) }
       end.join(', ').html_safe
+    end
+
+    def name_link(assignee, template, status)
+      status == :completed ? completion_event_path(assignee, template) : show_path(assignee)
+    end
+
+    def completion_event_path(assignee, template)
+      admin_target_path assignee.targets.completed.where(target_template: template).last
+    end
+
+    def show_path(assignee)
+      assignee.is_a?(Founder) ? admin_founder_path(assignee) : admin_startup_path(assignee)
     end
 
     def extended_scope?(status)

77eafb8c823ed5055b24dd8d8bef5b50a6da86bc
Jaleel <jaleel@sv.co>
2016-05-19 23:05:25 +0530

    Modify status and tooltip text for events with later improvements submitted

--- app/helpers/startups_helper.rb
+++ app/helpers/startups_helper.rb
@@ -61,4 +61,36 @@ module StartupsHelper
       startup.facebook_link.present? ||
       startup.twitter_link.present?
   end
+
+  def needs_improvement_tooltip_text(event)
+    if current_founder && @startup.founder?(current_founder)
+      needs_improvement_tooltip_for_founder(event)
+    else
+      needs_improvement_tooltip_for_public(event)
+    end
+  end
+
+  def needs_improvement_tooltip_for_founder(event)
+    if event.improved_timeline_event.present?
+      I18n.t('startup.show.timeline_cards.improved_later.tooltip_text.founder')
+    else
+      I18n.t('startup.show.timeline_cards.needs_imprvement.tooltip_text.founder')
+    end
+  end
+
+  def needs_improvement_tooltip_for_public(event)
+    if event.improved_timeline_event.present?
+      I18n.t('startup.show.timeline_cards.improved_later.tooltip_text.public')
+    else
+      I18n.t('startup.show.timeline_cards.needs_imprvement.tooltip_text.public')
+    end
+  end
+
+  def needs_improvement_status_text(event)
+    if event.improved_timeline_event.present?
+      I18n.t('startup.show.timeline_cards.improved_later.status_text')
+    else
+      I18n.t('startup.show.timeline_cards.needs_imprvement.status_text')
+    end
+  end
 end

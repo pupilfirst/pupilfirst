d0a2011416a85a9ff83d7108a1504aadb962ae7b
Hari Gopal <hari@sv.co>
2016-05-24 10:53:20 +0530

    AA: Set display_name for ApplicationSubmission.

--- app/models/application_submission.rb
+++ app/models/application_submission.rb
@@ -3,4 +3,8 @@ class ApplicationSubmission < ActiveRecord::Base
   belongs_to :batch_application
 
   serialize :submission_urls
+
+  def display_name
+    "#{batch_application.display_name} ##{id}"
+  end
 end

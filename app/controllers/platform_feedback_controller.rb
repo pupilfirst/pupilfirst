8b0ea14352e3ecfe2e17d0924d19f56a265a2540
Jaleel <jaleel@sv.co>
2016-05-24 15:20:46 +0530

    Send mail to help when a new feedback is submitted

--- app/controllers/platform_feedback_controller.rb
+++ app/controllers/platform_feedback_controller.rb
@@ -3,6 +3,7 @@ class PlatformFeedbackController < ApplicationController
     @platform_feedback = PlatformFeedback.new platform_feedback_params
 
     if @platform_feedback.save!
+      PlatformFeedbackMailer.new_platform_feedback(@platform_feedback).deliver_later
       flash[:success] = 'Thank You! Your feedback has been sent to the SV.CO team!'
     else
       flash[:error] = 'Something went wrong while saving your feedback! Please try again.'

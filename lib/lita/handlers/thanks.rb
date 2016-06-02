a6117df3682e475f9be29787d828add4452dd556
Jaleel <jaleel@sv.co>
2016-06-02 12:56:35 +0530

    Vocalist: Add a simple handler to respond to thanks

---
+++ lib/lita/handlers/thanks.rb
@@ -0,0 +1,19 @@
+# TODO: This handler should probably be renamed to 'miscellaneous.rb' or so and re-used for similar random cases
+
+module Lita
+  module Handlers
+    class Thanks < Handler
+      route(/\Athanks\s*\!*\s*\z|\Athank you\s*\!*\s*\z/i, :respond_to_thanks, command: true)
+
+      def respond_to_thanks(response)
+        response.reply random_response_to_thanks
+      end
+
+      def random_response_to_thanks
+        ["You are welcome :simple_smile:", "It was my pleasure ! :blush:", "Anytime :thumbsup:"].sample
+      end
+    end
+
+    Lita.register_handler(Thanks)
+  end
+end

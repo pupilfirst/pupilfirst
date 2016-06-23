7d4a943e80be9d471449a66999f6af44f4a9c9e5
Hari Gopal <hari@sv.co>
2016-06-23 14:57:08 +0530

    Display warning for new applicants if stage 2 ends within a week.

--- app/helpers/batch_applications_helper.rb
+++ app/helpers/batch_applications_helper.rb
@@ -8,4 +8,10 @@ module BatchApplicationsHelper
   def payment_button_message(batch_application)
     batch_application.payment.present? ? t('batch_application.stage_1.payment_retry') : t('batch_application.stage_1.payment_start')
   end
+
+  def applications_close_soon_message(batch)
+    deadline = batch.application_stage_deadline.strftime('%d %b, %l:%M %p (%z)')
+    delta = time_ago_in_words(batch.application_stage_deadline)
+    t('batch_application.general.applications_close_soon_html', batch_number: batch.batch_number, deadline: deadline, delta: delta)
+  end
 end

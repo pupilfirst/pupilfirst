3e10d6b6a8603069ec545ec05b28adae65a05185
Hari Gopal <hari@sv.co>
2016-06-23 13:59:39 +0530

    Customize payment button message depending on state.

--- app/helpers/batch_applications_helper.rb
+++ app/helpers/batch_applications_helper.rb
@@ -4,4 +4,8 @@ module BatchApplicationsHelper
   def next_stage_date
     current_stage.next.tentative_start_date(current_batch).strftime('%A, %b %e')
   end
+
+  def payment_button_message(batch_application)
+    batch_application.payment.present? ? t('batch_application.stage_1.payment_retry') : t('batch_application.stage_1.payment_start')
+  end
 end

1851e6c027455d495188c0cf633568cda6364b6a
Hari Gopal <hari@sv.co>
2016-06-28 15:27:49 +0530

    skip_before_action instead of skip_before_filter.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,5 +2,5 @@ class ErrorsController < ApplicationController
   include Gaffe::Errors
 
   # Render this page even if authenticity token checks fail.
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
 end

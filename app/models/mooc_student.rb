2db3073dbbdee459bb2c72496ea216e005063fd1
Jaleel <jaleel@sv.co>
2016-07-01 12:58:09 +0530

    Associate MoocStudent to User

--- app/models/mooc_student.rb
+++ app/models/mooc_student.rb
@@ -1,2 +1,3 @@
 class MoocStudent < ActiveRecord::Base
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160701072249) do
+ActiveRecord::Schema.define(version: 20160701072505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -337,6 +337,7 @@ ActiveRecord::Schema.define(version: 20160701072249) do
     t.string   "gender"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
 
   create_table "payments", force: :cascade do |t|

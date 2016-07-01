7bc353b30fcb7769ed10c450c9766c81bc45b3f6
Jaleel <jaleel@sv.co>
2016-07-01 16:34:31 +0530

    Setup a mailer to send login token

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+# Mails sent out to startups, as a whole.
+class UserMailer < ApplicationMailer
+  def send_login_token(user, url)
+    @user = user
+    @referer = url
+    mail(to: @user.email, subject: 'Log in to SV.CO')
+  end
+end

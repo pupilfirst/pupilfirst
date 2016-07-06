cc64cca52a26a30b18c7761dc7a06f845d048fa6
Jaleel <jaleel@sv.co>
2016-07-06 14:19:52 +0530

    Add some basic validation for MoocStudent

--- app/models/mooc_student.rb
+++ app/models/mooc_student.rb
@@ -1,3 +1,15 @@
 class MoocStudent < ActiveRecord::Base
   belongs_to :user
+
+  GENDER_MALE = -'male'
+  GENDER_FEMALE = -'female'
+  GENDER_OTHER = -'other'
+
+  def self.valid_gender_values
+    [GENDER_FEMALE, GENDER_MALE, GENDER_OTHER]
+  end
+
+  validates :gender, inclusion: { in: valid_gender_values }
+
+  validates_uniqueness_of :user_id
 end

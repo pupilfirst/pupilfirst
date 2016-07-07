edb46471e9e777960cf0af5ee4c8f36a0734e8c3
Hari Gopal <hari@sv.co>
2016-07-07 17:36:32 +0530

    Try setting GA's create event before all pagevide events.

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -3,9 +3,6 @@ if(/sv\.co/.test(window.location.hostname)) {
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  console.log('Google Analytics Debug: Create');
-  ga('create', 'UA-65573888-1', 'auto');
 } else {
   console.log('Detected development environment. Mocking function ga()...');
 
@@ -16,6 +13,8 @@ if(/sv\.co/.test(window.location.hostname)) {
 }
 
 $(document).on('page:change', function() {
+  console.log('Google Analytics Debug: Create');
+  ga('create', 'UA-65573888-1', 'auto');
   console.log('Google Analytics Debug: Page view');
   ga('send', 'pageview', window.location.pathname);
 });

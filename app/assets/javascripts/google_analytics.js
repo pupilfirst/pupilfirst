5eedaf3ecc165ab51ad1d8b0543a2e477a5158f2
Hari Gopal <hari@sv.co>
2016-07-07 17:19:49 +0530

    Attempt to debug Google Analytics failure in production.

--- app/assets/javascripts/google_analytics.js
+++ app/assets/javascripts/google_analytics.js
@@ -2,16 +2,20 @@ if(/sv\.co/.test(window.location.hostname)) {
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  console.log('Google Analytics Debug: Create');
   ga('create', 'UA-65573888-1', 'auto');
 } else {
+  console.log('Detected development environment. Mocking function ga()...');
+
   function ga() {
     var params = Array.prototype.slice.call(arguments, ga.length);
-    console.log('Google Analytics: ' + params);
+    console.log('Google Analytics Capture: ' + params);
   }
 }
 
 $(document).on('page:change', function() {
+  console.log('Google Analytics Debug: Page view');
   ga('send', 'pageview', window.location.pathname);
 });

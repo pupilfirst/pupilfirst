2ef6a4031c2b54840ad15e3cfab238b0d69187e3
Vishnu Gopal <g.vishnu@gmail.com>
2016-07-13 16:59:23 +0530

    Moving Postmark configuration to correct place.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,9 +94,10 @@ Rails.application.configure do
 
   # Do not display runtime information in production.
   config.middleware.delete(Rack::Runtime)
+  
+  # Postmark
+  config.action_mailer.delivery_method   = :postmark
+  config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
 end
-
-config.action_mailer.delivery_method   = :postmark
-config.action_mailer.postmark_settings = { api_token: ENV['POSTMARK_API_TOKEN'] }
 
 Rails.application.default_url_options = Svapp::Application.config.action_mailer.default_url_options

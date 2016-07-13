463029b91b4d003c9e053875c6252f311dd61e06
Hari Gopal <hari@sv.co>
2016-07-13 17:15:58 +0530

    Set new from address in devise initializer.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Startup Village <no-reply@sv.co>'
+  config.mailer_sender = 'SV.CO <help@sv.co>'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'DeviseMailer'

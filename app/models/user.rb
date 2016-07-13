b6ce240688a1119e82860b84bd2c9b8fed2bf927
Hari Gopal <hari@sv.co>
2016-07-13 12:12:23 +0530

    Fix validations on User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,7 @@ class User < ActiveRecord::Base
   has_secure_token :login_token
   after_create :regenerate_login_token
 
-  validates :email, uniqueness: true, format: { with: /@/, message: 'does not look like a valid address!' }
-
-  # Store unconfirmed phone number in a standardized form. Confirmed phone number will be copied from this field.
-  phony_normalize :phone, default_country_code: 'IN', add_plus: false
-
-  # Validate the unconfirmed phone number after it has been normalized.
-  validates_plausible_phone :phone, normalized_country_code: 'IN', allow_nil: true
+  validates :email, presence: true, uniqueness: true, format: { with: /@/, message: 'does not look like a valid address' }
+  validates :university_id, presence: true
+  validates :phone, format: { with: /\A[0-9]{10}\z/, message: 'should be a 10-digit number' }
 end

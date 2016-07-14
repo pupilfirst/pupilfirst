de3b39bb7f6f43bb9a2955e462d763ef7f56f6ad
Hari Gopal <hari@sv.co>
2016-07-14 17:49:25 +0530

    Update text in mailer and notification about batch application process's sign in link.

--- app/mailers/batch_applicant_mailer.rb
+++ app/mailers/batch_applicant_mailer.rb
@@ -2,7 +2,7 @@ class BatchApplicantMailer < ApplicationMailer
   def sign_in(email, token, batch)
     @token = token
     @batch = batch
-    mail(to: email, subject: 'One-time application link for SV.CO')
+    mail(to: email, subject: 'Apply to SV.CO')
   end
 
   def application_progress(batch, team_lead)

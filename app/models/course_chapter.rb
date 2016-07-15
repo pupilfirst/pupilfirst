8197aecc34bb7dc4d2b7e4faa51a3e58ca7c1cac
Jaleel <jaleel@sv.co>
2016-07-15 17:40:06 +0530

    Add validations and admin interface for CourseChapter

--- app/models/course_chapter.rb
+++ app/models/course_chapter.rb
@@ -1,5 +1,10 @@
 class CourseChapter < ActiveRecord::Base
   has_many :quiz_questions
 
-  validates_presence_of :name
+  validates_presence_of :name, :chapter_number, :sections_count
+  validates_uniqueness_of :chapter_number
+
+  def self.valid_chapter_numbers
+    CourseChapter.all.pluck(:chapter_number)
+  end
 end

eef1b3dcd411bad7409738753a457c7d125a29f3
Jaleel <jaleel@sv.co>
2016-07-18 13:49:21 +0530

    Revert "AA: Attempt adding questions directly from chapter page"
    This reverts commit b3ed46452b67cc894ea62716e76eea569fe91b45.

--- app/models/course_chapter.rb
+++ app/models/course_chapter.rb
@@ -1,6 +1,5 @@
 class CourseChapter < ActiveRecord::Base
   has_many :quiz_questions
-  accepts_nested_attributes_for :quiz_questions, allow_destroy: true
 
   validates_presence_of :name, :chapter_number, :sections_count
   validates_uniqueness_of :chapter_number

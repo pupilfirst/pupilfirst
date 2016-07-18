5528b48d1981e2431c6db27fff583f2347cac524
Jaleel <jaleel@sv.co>
2016-07-18 13:48:34 +0530

    AA: Attempt adding questions directly from chapter page

--- app/admin/course_chapter.rb
+++ app/admin/course_chapter.rb
@@ -3,7 +3,13 @@ ActiveAdmin.register CourseChapter do
   filter :name
   filter :chapter_number
 
-  permit_params :name, :chapter_number, :sections_count
+  permit_params do
+    permitted = :name, :chapter_number, :sections_count
+    permitted.append(quiz_questions_attributes: [:id, :question, :_destroy, answer_options_attributes: [:id, :value, :correct_answer, :_destroy]])
+    permitted
+  end
+
+  # permit_params :name, :chapter_number, :sections_count, quiz_questions_attributes: [:id, :question, answer_options_attributes: [:id, :value, :correct_answer, :_destroy], :_destroy]
 
   form do |f|
     f.semantic_errors(*f.object.errors.keys)
@@ -12,6 +18,15 @@ ActiveAdmin.register CourseChapter do
       f.input :name
       f.input :chapter_number
       f.input :sections_count
+      f.inputs 'Quiz Questions' do
+        f.has_many :quiz_questions, heading: false, allow_destroy: true, new_record: 'Add Question' do |question|
+          question.input :question
+          question.has_many :answer_options, heading: false, allow_destroy: true, new_record: 'Add Option' do |answer|
+            answer.input :value
+            answer.input :correct_answer
+          end
+        end
+      end
     end
 
     f.actions

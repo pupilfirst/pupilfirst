3a0825b3bbe4e1cd1dab491a77679e0f3ddc7b9f
Jaleel <jaleel@sv.co>
2016-07-20 13:00:12 +0530

    Fix presence validation for correct_answer

--- app/models/answer_option.rb
+++ app/models/answer_option.rb
@@ -1,5 +1,5 @@
 class AnswerOption < ActiveRecord::Base
   belongs_to :quiz_question
 
-  validates_presence_of :value, :quiz_question_id
+  validates_presence_of :value
 end
--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -4,11 +4,18 @@ class QuizQuestion < ActiveRecord::Base
   accepts_nested_attributes_for :answer_options, allow_destroy: true
 
   validates_presence_of :question, :course_chapter_id
-  # TODO: the following validation is busted! It seems to raise an error always.
-  validate :must_have_one_correct_answer
 
-  def must_have_one_correct_answer
-    errors.add :base, 'Must have an option marked as the correct answer' unless correct_answer.present?
+  validate :must_have_exactly_one_correct_answer
+
+  def must_have_exactly_one_correct_answer
+    errors.add :base, 'Must have exactly one correct answer' unless exactly_one_correct_answer?
+  end
+
+  def exactly_one_correct_answer?
+    # Answers might not be persisted yet. So we can't use the count short-hand as rubocop suggests
+    # rubocop: disable Performance/Count
+    answer_options.select { |o| o.correct_answer == true }.count == 1
+    # rubocop: enable Performance/Count
   end
 
   def correct_answer

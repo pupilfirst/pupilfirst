b620693d497c1fb7c4d75c54b13c9ee365a9f3c0
Vinu T V <vinu.tv@mobme.in>
2016-07-21 20:36:55 +0530

    Style founder login page

--- app/controllers/founders/sessions_controller.rb
+++ app/controllers/founders/sessions_controller.rb
@@ -1,5 +1,6 @@
 module Founders
   class SessionsController < Devise::SessionsController
+    layout 'application_v2', only: [:new]
     # GET /resource/sign_in
     def new
       # Store referer in session if it's sent.

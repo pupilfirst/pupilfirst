c3004d66935d84b0124f7bd2ebab609a679e9098
Vinu T V <vinu.tv@mobme.in>
2016-08-02 14:35:09 +0530

    Add application progress timeline to /apply stages

--- app/helpers/batch_applications_helper.rb
+++ app/helpers/batch_applications_helper.rb
@@ -23,16 +23,26 @@ module BatchApplicationsHelper
   # Used to determine the status of a stage in the progress bar. Returns one of :pending, :ongoing, :complete,
   # :expired, :rejected, or :not_applicable
   def stage_status(stage_number)
-    if applicant_stage_number == stage_number
-      applicant_status
+    if stage_number == applicant_stage_number
+      if applicant_stage_number == current_stage.number
+        applicant_status
+      elsif applicant_stage_number > current_stage.number
+        :pending
+      else
+        :expired
+      end
     elsif stage_number < applicant_stage_number
       :complete
-    elsif stage_number > applicant_stage_number
+    else
       if applicant_status.in? [:ongoing, :complete]
         :pending
       else
         :not_applicable
       end
     end
+  end
+
+  def pretty_stage_status(stage_number)
+    stage_status(stage_number).to_s.gsub('_', ' ').capitalize
   end
 end

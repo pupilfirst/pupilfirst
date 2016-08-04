49b6613dd801b76073bf3639241b647104abdf89
Jaleel <jaleel@sv.co>
2016-08-04 18:50:19 +0530

    Show the latest conversations in the Support Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register_page 'Dashboard' do
       initialize_intercom_client
       fetch_conversation_summary
       fetch_user_summary
-      fetch_latest_conversations
+      fetch_latest_conversations(5)
     end
 
     def initialize_intercom_client
@@ -30,7 +30,19 @@ ActiveAdmin.register_page 'Dashboard' do
       @active_user_count = user_counts.find { |h| h.key? 'Active' }['Active']
     end
 
-    def fetch_latest_conversations
+    def fetch_latest_conversations(n)
+      @conversations_to_display = []
+      conversations = @intercom.conversations.find(open: true, display_as: 'plaintext').conversations[0..n]
+
+      conversations.each do |conversation|
+        id = conversation['id']
+        user = @intercom.users.find(id: conversation['user']['id'])
+        user_name = user.name || (user.email.present? ? user.email : user.pseudonym)
+        body = conversation['conversation_message']['body']
+        @conversations_to_display << { id: id, name: user_name, body: body }
+      end
+
+      @conversations_to_display
     end
   end
 

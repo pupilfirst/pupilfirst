4168b6ed6cf0d691a47802d8313e51ae9abcb0e7
Jaleel <jaleel@sv.co>
2016-08-04 17:44:20 +0530

    WIP: Add a support card to the AA:dashboard to display intercom stats
    Uses the `intercom` gem.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,6 +1,37 @@
 ActiveAdmin.register_page 'Dashboard' do
   controller do
+    before_filter :pull_intercom_data
     skip_after_action :intercom_rails_auto_include
+
+    def pull_intercom_data
+      initialize_intercom_client
+      fetch_conversation_summary
+      fetch_user_summary
+      fetch_latest_conversations
+    end
+
+    def initialize_intercom_client
+      @intercom = Intercom::Client.new(app_id: ENV['INTERCOM_API_ID'], api_key: ENV['INTERCOM_API_KEY'])
+    end
+
+    def fetch_conversation_summary
+      # TODO: Rewrite using the more general 'Conversation App Count Object' once issue with gem fixed.
+      # refer: https://github.com/intercom/intercom-ruby/pull/238
+      counts_by_admin = @intercom.counts.for_type(type: 'conversation', count: 'admin').conversation['admin']
+
+      @open_conversations_count = counts_by_admin.inject(0) { |a, e| a + e['open'] }
+      @closed_conversations_count = counts_by_admin.inject(0) { |a, e| a + e['closed'] }
+    end
+
+    def fetch_user_summary
+      user_counts = @intercom.counts.for_type(type: 'user', count: 'segment').user['segment']
+
+      @new_user_count = user_counts.find { |h| h.key? 'New' }['New']
+      @active_user_count = user_counts.find { |h| h.key? 'Active' }['Active']
+    end
+
+    def fetch_latest_conversations
+    end
   end
 
   menu priority: 1

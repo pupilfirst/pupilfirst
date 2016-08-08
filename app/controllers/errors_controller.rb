d1a41aab48ee083800e356c2feb945a42875c8ea
Hari Gopal <hari@sv.co>
2016-08-08 16:38:57 +0530

    ErrorsController should always respond with HTML or JSON.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,4 +3,21 @@ class ErrorsController < ApplicationController
 
   # Render this page even if authenticity token checks fail.
   skip_before_action :verify_authenticity_token
+
+  # Only respond with HTML or JSON, regardless of what is asked for.
+  before_action :override_format
+
+  def show
+    respond_to do |format|
+      format.html { render "errors/#{@rescue_response}", status: @status_code }
+      format.json { render json: { error: @status_code }, status: @status_code }
+    end
+  end
+
+  private
+
+  # We want requests for images and all other general content (pdf, doc, etc) to be treated as html requests.
+  def override_format
+    request.format = 'html' unless params[:format] == 'json'
+  end
 end

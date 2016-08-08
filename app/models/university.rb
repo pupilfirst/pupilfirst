f24b8e6dd74f5aa5e23927ddc1cfa1d75b45a310
Jaleel <jaleel@sv.co>
2016-08-08 15:47:02 +0530

    Add 'Outside India' to list of states

--- app/models/university.rb
+++ app/models/university.rb
@@ -10,7 +10,7 @@ class University < ActiveRecord::Base
       'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Madhya Pradesh', 'Maharashtra',
       'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu',
       'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal', 'Andaman and Nicobar', 'Chandigarh',
-      'Dadra and Nagar Haveli', 'Daman and Diu', 'Lakshadweep', 'Delhi', 'Puducherry'
+      'Dadra and Nagar Haveli', 'Daman and Diu', 'Lakshadweep', 'Delhi', 'Puducherry', 'Outside India'
     ]
   end
 

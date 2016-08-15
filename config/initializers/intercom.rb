9957b0cdbb37a7037ed639f5340fa8a6ab38541b
Jaleel <jaleel@sv.co>
2016-08-15 16:43:04 +0530

    Enable secure mode for intercom conversations
    Note: Involves update of .env

--- config/initializers/intercom.rb
+++ config/initializers/intercom.rb
@@ -22,7 +22,7 @@ IntercomRails.config do |config|
   # This is required to enable secure mode, you can find it on your Setup
   # guide in the "Secure Mode" step.
   #
-  # config.api_secret = "..."
+  config.api_secret = Rails.application.secrets.intercom_secure_mode_secret_key
 
   # == Enabled Environments
   # Which environments is auto inclusion of the Javascript enabled for

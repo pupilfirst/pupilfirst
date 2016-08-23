da860de0919eb977d3dfb8448204624c83d64fb0
Jaleel <jaleel@sv.co>
2016-08-23 13:13:31 +0530

    Show graph for paid applicants, not all

--- app/helpers/active_admin/admissions_dashboard_helper.rb
+++ app/helpers/active_admin/admissions_dashboard_helper.rb
@@ -2,8 +2,8 @@ module ActiveAdmin
   module AdmissionsDashboardHelper
     def paid_applicants_by_reference
       named_references = BatchApplicant.reference_sources - ['Other (Please Specify)']
-      paid_applicants = BatchApplicant.where(reference: named_references).group(:reference).count
-      paid_applicants_others_count = BatchApplicant.where.not(reference: named_references).count
+      paid_applicants = BatchApplicant.conversion.where(reference: named_references).group(:reference).count
+      paid_applicants_others_count = BatchApplicant.conversion.where.not(reference: named_references).count
 
       paid_applicants["Other"] = paid_applicants_others_count if paid_applicants_others_count > 0
       paid_applicants.to_json

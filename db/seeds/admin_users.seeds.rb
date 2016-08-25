df0a08f4199d86ae89b6c69822e06cf71c647e1f
Hari Gopal <hari@sv.co>
2016-08-25 17:57:58 +0530

    Fix admin user seed. [skip ci]

--- db/seeds/admin_users.seeds.rb
+++ db/seeds/admin_users.seeds.rb
@@ -1,7 +1,7 @@
 # Create an admin user for the /admin interface. This user is a 'super-admin', who can do everything possible from the
 # ActiveAdmin interface.
 AdminUser.create!(
-  name: 'Development Admin',
+  fullname: 'Development Admin',
   email: 'admin@example.com',
   password: 'password',
   password_confirmation: 'password',

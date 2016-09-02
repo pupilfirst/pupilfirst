14a6eb8aba12f85fba39492b958005a403fec740
Jaleel <jaleel@sv.co>
2016-09-02 17:49:25 +0530

    Fix Typo

--- lib/intercom/intercom_client.rb
+++ lib/intercom/intercom_client.rb
@@ -1,6 +1,6 @@
 class IntercomClient
   def intercom_client
-    @intercom_client ||= Intercom::Client.new(app_id: ENV['INTERCOM_API_ID'], api_key: ENV['INTERCOM_API_KEY'])
+    @intercom_client ||= Intercom::Client.new(app_id: ENV['INTERCOM_APP_ID'], api_key: ENV['INTERCOM_API_KEY'])
   end
 
   # find a user given his email

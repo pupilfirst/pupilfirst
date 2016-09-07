986b79bf79c1d894640c04e25a8c566d825eabc4
Vinu T V <vinu.tv@mobme.in>
2016-09-07 15:25:25 +0530

    Change background image file path in app_config.rb

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -14,6 +14,6 @@ APP_CONFIG = {
 
 APP_CONSTANTS = {
   certificate_background_base64: Base64.strict_encode64(
-    open(File.expand_path(File.join(Rails.root, 'app', 'assets', 'images', 'apply', 'admission-process', 'coding-video-certificate.png'))).read
+    open(File.expand_path(File.join(Rails.root, 'app', 'assets', 'images', 'apply', 'batch_application', 'coding-video-certificate.png'))).read
   )
 }.with_indifferent_access

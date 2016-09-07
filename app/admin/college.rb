3e01353d05a875ed75e50fe0bcdda901b3c68ca1
Hari Gopal <hari@sv.co>
2016-09-07 18:24:19 +0530

    AA: Add interface for colleges, replacement_universities, and states

---
+++ app/admin/college.rb
@@ -0,0 +1,27 @@
+ActiveAdmin.register College do
+  include DisableIntercom
+
+  menu parent: 'Admissions'
+
+  permit_params :name, :also_known_as, :city, :state_id, :established_year, :website, :contact_numbers,
+    :replacement_university_id
+
+  filter :state
+  filter :state_id_null, label: 'State missing?', as: :boolean
+  filter :city
+  filter :city_null, label: 'City missing?', as: :boolean
+  filter :replacement_university
+  filter :name
+
+  index do
+    selectable_column
+
+    column :name
+    column :city
+    column :state
+    column :replacement_university
+    column :also_known_as
+
+    actions
+  end
+end
---
+++ app/admin/replacement_university.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register ReplacementUniversity do
+  include DisableIntercom
+
+  menu parent: 'Admissions'
+
+  permit_params :name, :state_id
+
+  index do
+    selectable_column
+
+    column :name
+
+    column :colleges do |university|
+      link_to university.colleges.count, admin_colleges_path(q: { replacement_university_id_eq: university.id })
+    end
+
+    column :state
+
+    actions
+  end
+end

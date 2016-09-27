b8947c27ae4af8490faf92462df932f3cdd7f926
Abdul Jaleel <jaleel@sv.co>
2016-09-28 00:21:00 +0530

    Further trim down AdmissionStatsService using a single for_states scope method

--- app/services/admission_stats_service.rb
+++ app/services/admission_stats_service.rb
@@ -15,161 +15,106 @@ class AdmissionStatsService
     @selected_batch_ids = batch_ids
 
     {
-      total_applications: total_applications_count,
-      total_applicants: total_applicants_count,
-      total_universities: total_universities_count,
-      total_states: total_location_count,
-      total_visits: unique_visits_count,
-      paid_applications: paid_applications_count,
-      paid_applications_today: paid_applications_today,
-      payment_initiated: payment_initiated_count,
-      payment_initiated_today: payment_initiated_today,
-      submitted_applications: submitted_applications_count,
-      submitted_applications_today: submitted_applications_today,
+      total_applications: total_applications,
+      total_applicants: total_applicants,
+      total_universities: total_universities,
+      total_states: total_locations,
+      total_visits: unique_visits,
+      paid_applications: paid_applications(:all),
+      paid_applications_today: paid_applications_today(:all),
+      payment_initiated: payment_initiated(:all),
+      payment_initiated_today: payment_initiated_today(:all),
+      submitted_applications: submitted_applications(:all),
+      submitted_applications_today: submitted_applications_today(:all),
       state_wise_stats: focused_states_stats.merge(other_states_stats)
     }
   end
 
   private
 
-  def selected_applications
-    BatchApplication.where(batch_id: selected_batch_ids)
-  end
-
-  def total_applications_count
-    selected_applications.count
-  end
-
-  def total_applicants_count
-    selected_applications.sum(:team_size) + BatchApplication.where(batch_id: selected_batch_ids, team_size: nil).count
-  end
-
-  def total_universities_count
-    # University.joins(:batch_applications).where(batch_applications: { batch: selected_batch_ids }).uniq.count
-    ReplacementUniversity.joins(:batch_applications).where(batch_applications: { batch: selected_batch_ids }).distinct.count
-  end
-
-  def total_location_count
-    # University.joins(:batch_applications).where(batch_applications: { batch: selected_batch_ids }).group(:location).count.count
-    State.joins(:batch_applications).where(batch_applications: { batch: selected_batch_ids }).distinct.count
-  end
-
-  def unique_visits_count
-    start_time = Time.parse 'August 1, 2016, 00:00:00+0530'
-    end_time = Time.now
-    Visit.where(started_at: start_time..end_time).count
-  end
-
-  def paid_applications_count
-    selected_applications.payment_complete.count
-  end
-
-  def paid_applications_today
-    selected_applications.paid_today.count
-  end
-
-  def payment_initiated_count
-    selected_applications.payment_initiated.count
-  end
-
-  def payment_initiated_today
-    selected_applications.payment_initiated_today.count
-  end
-
-  def submitted_applications_count
-    selected_applications.submitted_application.count
-  end
-
-  def submitted_applications_today
-    selected_applications.submitted_application.where('batch_applications.created_at > ?', Time.now.beginning_of_day).count
-  end
-
   def focused_states_stats
     State.focused_for_admissions.each_with_object({}) do |state, states|
       states[state.name.to_sym] = {
-        paid_applications: paid_applications_count_for(state),
-        paid_applications_today: paid_applications_today_for(state),
-        payment_initiated: payment_initiated_count_for(state),
-        payment_initiated_today: payment_initiated_today_for(state),
-        submitted_applications: submitted_applications_count_for(state),
-        submitted_applications_today: submitted_applications_today_for(state),
-        conversion_percentage: conversion_percentage_for(state)
+        paid_applications: paid_applications(state),
+        paid_applications_today: paid_applications_today(state),
+        payment_initiated: payment_initiated(state),
+        payment_initiated_today: payment_initiated_today(state),
+        submitted_applications: submitted_applications(state),
+        submitted_applications_today: submitted_applications_today(state),
+        conversion_percentage: conversion_percentage(state)
       }
     end
-  end
-
-  def paid_applications_count_for(state)
-    selected_applications.payment_complete.from_state(state).count
-  end
-
-  def paid_applications_today_for(state)
-    selected_applications.paid_today.from_state(state).count
-  end
-
-  def payment_initiated_count_for(state)
-    selected_applications.payment_initiated.from_state(state).count
-  end
-
-  def payment_initiated_today_for(state)
-    selected_applications.payment_initiated_today.from_state(state).count
-  end
-
-  def submitted_applications_count_for(state)
-    selected_applications.submitted_application.from_state(state).count
-  end
-
-  def submitted_applications_today_for(state)
-    selected_applications.submitted_application.where('batch_applications.created_at > ?', Time.now.beginning_of_day).from_state(state).count
-  end
-
-  def conversion_percentage_for(state)
-    total = selected_applications.from_state(state).count
-    return 0 unless total.positive?
-    (paid_applications_count_for(state).to_f / total) * 100
   end
 
   def other_states_stats
     {
       Others:
         {
-          paid_applications: paid_applications_count_for_others,
-          paid_applications_today: paid_applications_today_for_others,
-          payment_initiated: payment_initiated_count_for_others,
-          payment_initiated_today: payment_initiated_today_for_others,
-          submitted_applications: submitted_applications_count_for_others,
-          submitted_applications_today: submitted_applications_today_for_others,
-          conversion_percentage: conversion_percentage_for_others
+          paid_applications: paid_applications(:non_focused),
+          paid_applications_today: paid_applications_today(:non_focused),
+          payment_initiated: payment_initiated(:non_focused),
+          payment_initiated_today: payment_initiated_today(:non_focused),
+          submitted_applications: submitted_applications(:non_focused),
+          submitted_applications_today: submitted_applications_today(:non_focused),
+          conversion_percentage: conversion_percentage(:non_focused)
         }
     }
   end
 
-  def paid_applications_count_for_others
-    selected_applications.payment_complete.from_other_states.count
+  def selected_applications
+    BatchApplication.where(batch_id: selected_batch_ids)
   end
 
-  def paid_applications_today_for_others
-    selected_applications.paid_today.from_other_states.count
+  def total_applications
+    selected_applications.count
   end
 
-  def payment_initiated_count_for_others
-    selected_applications.payment_initiated.from_other_states.count
+  def total_applicants
+    selected_applications.sum(:team_size) + BatchApplication.where(batch_id: selected_batch_ids, team_size: nil).count
   end
 
-  def payment_initiated_today_for_others
-    selected_applications.payment_initiated_today.from_other_states.count
+  def total_universities
+    # University.joins(:batch_applications).where(batch_applications: { batch: selected_batch_ids }).uniq.count
+    ReplacementUniversity.joins(:batch_applications).where(batch_applications: { batch: selected_batch_ids }).distinct.count
   end
 
-  def submitted_applications_count_for_others
-    selected_applications.submitted_application.from_other_states.count
+  def total_locations
+    State.joins(:batch_applications).where(batch_applications: { batch: selected_batch_ids }).distinct.count
   end
 
-  def submitted_applications_today_for_others
-    selected_applications.submitted_application.where('batch_applications.created_at > ?', Time.now.beginning_of_day).from_other_states.count
+  def unique_visits
+    start_time = Time.parse 'August 1, 2016, 00:00:00+0530'
+    end_time = Time.now
+    Visit.where(started_at: start_time..end_time).count
   end
 
-  def conversion_percentage_for_others
-    total = selected_applications.from_other_states.count
+  def paid_applications(state_scope)
+    selected_applications.for_states(state_scope).payment_complete.count
+  end
+
+  def paid_applications_today(state_scope)
+    selected_applications.for_states(state_scope).paid_today.count
+  end
+
+  def payment_initiated(state_scope)
+    selected_applications.for_states(state_scope).payment_initiated.count
+  end
+
+  def payment_initiated_today(state_scope)
+    selected_applications.for_states(state_scope).payment_initiated_today.count
+  end
+
+  def submitted_applications(state_scope)
+    selected_applications.for_states(state_scope).submitted_application.count
+  end
+
+  def submitted_applications_today(state_scope)
+    selected_applications.for_states(state_scope).submitted_application.where('batch_applications.created_at > ?', Time.now.beginning_of_day).count
+  end
+
+  def conversion_percentage(state_scope)
+    total = selected_applications.for_states(state_scope).count
     return 0 unless total.positive?
-    (payment_completed_count_for_others.to_f / total) * 100
+    (paid_applications.for_states(state_scope).to_f / total) * 100
   end
 end

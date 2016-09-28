e7feb6e9d31b5d74c201856ec5dd331195d6be26
Abdul Jaleel <jaleel@sv.co>
2016-09-28 12:40:31 +0530

    Use the new Presenter for the admissions dashboard, remove stale helper

--- app/presenters/active_admin/admissions_dashboard_presenter.rb
+++ app/presenters/active_admin/admissions_dashboard_presenter.rb
@@ -1,9 +1,10 @@
 module ActiveAdmin
   class AdmissionsDashboardPresenter
-    attr_reader :stats
+    attr_reader :stats, :selected_batch_ids
 
     def initialize(batch_id)
       @stats = batch_id.present? ? AdmissionStatsService.load_stats(Batch.find(batch_id)) : AdmissionStatsService.load_overall_stats
+      @selected_batch_ids = batch_id.present? ? [batch_id] : Batch.all.pluck(:id)
     end
 
     # overall metrics in the hash returned by the AdmissionStatsService
@@ -35,6 +36,29 @@ module ActiveAdmin
           stats[m]
         end
       end
+    end
+
+    def paid_applicants_by_reference
+      named_references = BatchApplicant.reference_sources - ['Other (Please Specify)']
+      paid_applicants = BatchApplicant.for_batch_id_in(selected_batch_ids).conversion.where(reference: named_references).group(:reference).count
+      paid_applicants_others_count = BatchApplicant.for_batch_id_in(selected_batch_ids).conversion.where.not(reference: named_references).count
+
+      paid_applicants["Other"] = paid_applicants_others_count if paid_applicants_others_count.positive?
+      paid_applicants.to_json
+    end
+
+    def paid_applications_by_location
+      result = BatchApplication.joins(:college).select('colleges.state_id').where(batch_id: selected_batch_ids).payment_complete.group('colleges.state_id').count
+      result.map { |state_id, count| [State.find(state_id).name, count] }.to_h.to_json
+    end
+
+    def paid_applications_by_date
+      result = BatchApplication.where(batch_id: selected_batch_ids).payment_complete.joins(:payment).group_by_day('payments.paid_at').count.sort.to_h
+      result.map { |k, v| [k.strftime('%b %d'), v] }.to_h.to_json
+    end
+
+    def paid_applications_by_team_size
+      BatchApplication.where(batch_id: selected_batch_ids).payment_complete.group(:team_size).count.sort.to_h.to_json
     end
   end
 end

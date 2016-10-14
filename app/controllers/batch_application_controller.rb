9702ae2a972e7bb86dd1a1e1a271915de91ff109
Abdul Jaleel <jaleel@sv.co>
2016-10-14 15:30:33 +0530

    Get stage_3_expired page ready with certificate

--- app/controllers/batch_application_controller.rb
+++ app/controllers/batch_application_controller.rb
@@ -283,6 +283,10 @@ class BatchApplicationController < ApplicationController
     @batch_application = current_application.decorate
   end
 
+  def stage_3_expired
+    @batch_application = current_application.decorate
+  end
+
   def stage_4_submit
     # TODO: Server-side error handling for stage 4 inputs.
 
--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,5 +1,8 @@
 APP_CONSTANTS = {
-  certificate_background_base64: Base64.strict_encode64(
+  coding_video_certificate_background_base64: Base64.strict_encode64(
     open(File.expand_path(File.join(Rails.root, 'app', 'assets', 'images', 'apply', 'batch_application', 'coding-video-certificate.png'))).read
+  ),
+  interview_certificate_background_base64: Base64.strict_encode64(
+    open(File.expand_path(File.join(Rails.root, 'app', 'assets', 'images', 'apply', 'batch_application', 'interview-certificate.png'))).read
   )
 }.with_indifferent_access

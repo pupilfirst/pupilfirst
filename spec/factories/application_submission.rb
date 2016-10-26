8c46da253908e52362029b08ad83b014afaf4073
Hari Gopal <hari@sv.co>
2016-10-26 12:15:44 +0530

    Spec interview stage

--- spec/factories/application_submission.rb
+++ spec/factories/application_submission.rb
@@ -4,12 +4,22 @@ FactoryGirl.define do
     batch_application
 
     trait :stage_2_submission do
+      transient do
+        scored false
+      end
+
       application_stage { create :application_stage, number: 2 }
 
-      after(:create) do |application_submission|
-        create :application_submission_url, application_submission: application_submission, name: 'Live Website', url: "https://example.com/#{rand(1000)}"
-        create :application_submission_url, application_submission: application_submission, name: 'Code Submission', url: "https://github.com/user#{rand(1000)}/repository"
-        create :application_submission_url, application_submission: application_submission, name: 'Video Submission', url: "https://facebook.com/user#{rand(1000)}/video"
+      after(:create) do |application_submission, evaluator|
+        score, admin_user = if evaluator.scored
+          [rand(100), create(:admin_user)]
+        else
+          [nil, nil]
+        end
+
+        create :application_submission_url, application_submission: application_submission, name: 'Live Website', url: "https://example.com/#{rand(1000)}", score: score, admin_user: admin_user
+        create :application_submission_url, application_submission: application_submission, name: 'Code Submission', url: "https://github.com/user#{rand(1000)}/repository", score: score, admin_user: admin_user
+        create :application_submission_url, application_submission: application_submission, name: 'Video Submission', url: "https://facebook.com/user#{rand(1000)}/video", score: score, admin_user: admin_user
       end
     end
   end
---
+++ spec/features/batch_application/interview_stage_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+feature 'Application Interview Stage' do
+  include BatchApplicantSpecHelper
+
+  let(:batch) { create :batch, :in_stage_3 }
+  let(:batch_application) { create :batch_application, :stage_3, batch: batch }
+  let(:stage_3) { create :application_stage, number: 3 }
+
+  before do
+    sign_in_batch_applicant(batch_application.team_lead)
+  end
+
+  context 'when interview stage is ongoing' do
+    scenario 'user is shown ongoing state page' do
+      expect(page).to have_content("You've made it to the interviews!")
+    end
+
+    context 'when user has attended interview' do
+      before do
+        create(:application_submission, application_stage: stage_3, batch_application: batch_application)
+      end
+
+      scenario 'user is shown submitted state page' do
+        visit apply_continue_path
+        expect(page).to have_content('It was great meeting you at the interview.')
+      end
+    end
+  end
+
+  context 'when interview stage is over' do
+    before do
+      BatchStage.find_by(batch: batch, application_stage: stage_3).update!(ends_at: 1.day.ago)
+    end
+
+    context 'when applicant has not attended interview' do
+      scenario 'user is shown expired state page' do
+        visit apply_continue_path
+        expect(page).to have_content("It looks like your team didn't attend the interviews")
+      end
+    end
+
+    context 'when the applicant has attended interview' do
+      before do
+        create(:application_submission, application_stage: stage_3, batch_application: batch_application)
+      end
+
+      scenario 'user is shown submitted state page' do
+        visit apply_continue_path
+        expect(page).to have_content('It was great meeting you at the interview.')
+      end
+
+      context 'when the batch has progressed to next stage' do
+        let(:stage_4) { create(:application_stage, number: 4) }
+
+        before do
+          BatchStage.find_by(batch: batch, application_stage: stage_3).update!(ends_at: 1.day.ago)
+          create(:batch_stage, batch: batch, application_stage: stage_4, starts_at: 3.days.ago, ends_at: 4.days.from_now)
+        end
+
+        scenario 'user is shown rejected state page' do
+          visit apply_continue_path
+          expect(page).to have_content("We're sorry, but your team didn't make the cut during the interview process.")
+        end
+      end
+    end
+  end
+end

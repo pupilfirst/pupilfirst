78d9b4f85395f3c84c3a08f4c3e9f99edaab64ac
Abdul Jaleel <jaleel@sv.co>
2016-10-29 15:22:59 +0530

    Minor fix in after_sign_in_path_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    referer = params[:referer] || request.referer
+    referer = params[:referer]
 
     if referer
       referer

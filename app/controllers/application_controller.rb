ac3d03e1db86a670079c417b2d38d1e2e55af893
Abdul Jaleel <jaleel@sv.co>
2016-10-29 16:10:06 +0530

    Bring back storing referer in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    referer = params[:referer]
+    referer = params[:referer] || session[:referer]
 
     if referer
       referer

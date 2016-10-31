a11f336269cd78affd156a57d4a40afef221d15c
Vinu T V <vinu.tv@mobme.in>
2016-10-31 13:23:03 +0530

    Link gtu landing page to registration page

--- app/controllers/six_ways_controller.rb
+++ app/controllers/six_ways_controller.rb
@@ -1,7 +1,7 @@
 class SixWaysController < ApplicationController
   before_action :authorize_student, only: %w(completion_certificate)
   before_action :block_student, only: %w(student_details create_student)
-  before_action :gtu_variables, only: %w(gtu_index gtu_launch)
+  before_action :gtu_variables, only: %w(gtu_index)
 
   helper_method :quiz_score
 
@@ -22,10 +22,6 @@ class SixWaysController < ApplicationController
 
   # Landing page for GTU MOOC
   def gtu_index
-  end
-
-  # Temporary page for launch MOOC in GTU.
-  def gtu_launch
   end
 
   # GET /sixways/student_details

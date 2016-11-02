89e9f047b89b4961b3a0fbeaff9a7bb1a8ca7017
Hari Gopal <hari@sv.co>
2016-11-02 12:08:48 +0530

    User authentication spec specs and object, not a feature

--- spec/services/user_authentication_service_spec.rb
+++ spec/services/user_authentication_service_spec.rb
@@ -7,54 +7,69 @@ describe UserAuthenticationService do
 
   let!(:user) { create :user, email: 'valid_email@example.com' }
 
-  context 'invoked to mail login token' do
-    scenario 'user with given email does not exist' do
-      response = subject.mail_login_token('random@example.com', 'some_referer', true)
+  describe '.mail_login_token' do
+    context 'when a user with supplied email does not exist' do
+      it 'responds with error message' do
+        response = subject.mail_login_token('random@example.com', 'some_referer', true)
 
-      # it returns user not found error
-      expect(response[:success]).to be_falsey
-      expect(response[:message]).to eq('Could not find user with given email.')
+        # it returns user not found error
+        expect(response[:success]).to be_falsey
+        expect(response[:message]).to eq('Could not find user with given email.')
+      end
     end
 
-    scenario 'user with given email exists' do
-      old_token = user.login_token
-      response = subject.mail_login_token('valid_email@example.com', 'www.example.com', true)
+    context 'when user with supplied email exists' do
+      it 'generates new login token' do
+        expect do
+          subject.mail_login_token('valid_email@example.com', 'www.example.com', true)
+        end.to change { user.reload.login_token }
+      end
 
-      expect(response[:success]).to be_truthy
-      expect(response[:message]).to eq('Login token successfully emailed.')
+      it 'emails login link to user' do
+        subject.mail_login_token('valid_email@example.com', 'www.example.com', true)
 
-      # it should have generated a new token
-      user.reload
-      expect(user.login_token).to_not eq(old_token)
+        user.reload
 
-      # it successfully emails login link with token and referer
-      open_email('valid_email@example.com')
-      expect(current_email.subject).to eq('Log in to SV.CO')
-      expect(current_email.body).to include('http://localhost:3000/authenticate?')
-      expect(current_email.body).to include('referer=www.example.com')
-      expect(current_email.body).to include("token=#{user.login_token}")
+        open_email('valid_email@example.com')
+        expect(current_email.subject).to eq('Log in to SV.CO')
+        expect(current_email.body).to include('http://localhost:3000/authenticate?')
+        expect(current_email.body).to include('referer=www.example.com')
+        expect(current_email.body).to include("token=#{user.login_token}")
+      end
+
+      it 'responds with success message' do
+        response = subject.mail_login_token('valid_email@example.com', 'www.example.com', true)
+
+        expect(response[:success]).to be_truthy
+        expect(response[:message]).to eq('Login token successfully emailed.')
+      end
     end
   end
 
-  context 'invoked to authenticate a token' do
-    scenario 'token is invalid' do
-      response = subject.authenticate_token('some_token')
+  describe '.authenticate_token' do
+    context 'when token is invalid' do
+      it 'responds with error message' do
+        response = subject.authenticate_token('some_token')
 
-      # it returns authentication failure
-      expect(response[:success]).to be_falsey
-      expect(response[:message]).to eq('User authentication failed.')
+        # it returns authentication failure
+        expect(response[:success]).to be_falsey
+        expect(response[:message]).to eq('User authentication failed.')
+      end
     end
 
-    scenario 'token is valid' do
-      response = subject.authenticate_token(user.login_token)
+    context 'when token is valid' do
+      let!(:response) { subject.authenticate_token(user.login_token) }
 
-      # it returns authentication success
-      expect(response[:success]).to be_truthy
-      expect(response[:message]).to eq('User authenticated successfully.')
+      it 'responds with success message' do
+        # it returns authentication success
+        expect(response[:success]).to be_truthy
+        expect(response[:message]).to eq('User authenticated successfully.')
+      end
 
-      # it should have cleared the token
-      user.reload
-      expect(user.login_token).to eq(nil)
+      it 'clears user token' do
+        user.reload
+        expect(user.login_token).to eq(nil)
+      end
     end
   end
 end

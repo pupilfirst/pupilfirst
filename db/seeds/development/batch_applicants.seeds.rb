66aed066e92c398d0c4ec217fe864b0909f38367
Hari Gopal <hari@sv.co>
2016-11-16 15:14:16 +0530

    Seed co-applicants separately to avoid adding extra info

--- db/seeds/development/batch_applicants.seeds.rb
+++ db/seeds/development/batch_applicants.seeds.rb
@@ -19,12 +19,19 @@ after 'development:colleges' do
     { email: 'applicant+submitted@gmail.com' },
     { email: 'applicant+submitted+rejected@gmail.com' },
     { email: 'applicant+interview@gmail.com' },
-    { email: 'applicant+interview+rejected@gmail.com'},
-    { email: 'applicant+pre_selection@gmail.com'},
-    { email: 'coapplicant+pre_selection@gmail.com'}
+    { email: 'applicant+interview+rejected@gmail.com' },
+    { email: 'applicant+pre_selection@gmail.com' }
   ].map { |applicant| applicant.merge(applicant_defaults) }
 
   applicants.each do |applicant_attributes|
+    BatchApplicant.where(applicant_attributes).first_or_create!
+  end
+
+  coapplicants = [
+    { email: 'coapplicant+pre_selection@gmail.com' }
+  ].map { |applicant| applicant.merge(name: Faker::Name.name) }
+
+  coapplicants.each do |applicant_attributes|
     BatchApplicant.where(applicant_attributes).first_or_create!
   end
 end

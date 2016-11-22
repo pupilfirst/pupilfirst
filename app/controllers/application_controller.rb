0295c9d63e8089ab148678c65c2728d04b2dc4cb
Hari Gopal <hari@sv.co>
2016-11-22 13:28:23 +0530

    Add https://uploads.intercomcdn.com to CSP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -166,7 +166,7 @@ class ApplicationController < ActionController::Base
   def intercom_csp
     {
       script: 'https://widget.intercom.io https://js.intercomcdn.com',
-      connect: 'https://api-ping.intercom.io https://nexus-websocket-a.intercom.io https://nexus-websocket-b.intercom.io wss://nexus-websocket-a.intercom.io wss://nexus-websocket-b.intercom.io https://api-iam.intercom.io https://js.intercomcdn.com',
+      connect: 'https://api-ping.intercom.io https://nexus-websocket-a.intercom.io https://nexus-websocket-b.intercom.io wss://nexus-websocket-a.intercom.io wss://nexus-websocket-b.intercom.io https://api-iam.intercom.io https://js.intercomcdn.com https://uploads.intercomcdn.com',
       font: 'https://js.intercomcdn.com',
       image: 'https://js.intercomcdn.com https://static.intercomassets.com https://uploads.intercomcdn.com',
       media: 'https://js.intercomcdn.com'

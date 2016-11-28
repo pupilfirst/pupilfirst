71d61098aa85a4619d25382eaf58343d7c347f82
Abdul Jaleel <jaleel@sv.co>
2016-11-28 14:08:47 +0530

    Associate TargetGroup to ProgramWeek

--- app/models/program_week.rb
+++ app/models/program_week.rb
@@ -1,4 +1,6 @@
 class ProgramWeek < ApplicationRecord
+  has_many :target_groups
+
   validates_presence_of :name, :number
   # validates_uniqueness_of :number, scope: :batch_id
 end
--- app/models/target_group.rb
+++ app/models/target_group.rb
@@ -1,5 +1,6 @@
 class TargetGroup < ApplicationRecord
   has_many :targets
+  belongs_to :program_week
 
   validates_presence_of :name, :description
 end

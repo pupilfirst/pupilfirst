a0a214d7a3c36eace18a1ef25e34cde143a11f0a
Abdul Jaleel <jaleel@sv.co>
2016-11-29 12:54:25 +0530

    Add seeds for the new scheme of targets, target groups and program weeks

--- db/seeds/development/targets.seeds.rb
+++ db/seeds/development/targets.seeds.rb
@@ -1,23 +1,79 @@
 require_relative 'helper'
 
-after 'development:startups' do
+after 'development:target_groups' do
   puts 'Seeding targets'
 
-  avengers_startup = Startup.find_by(product_name: 'SuperHeroes')
+  batch = Batch.find_by(batch_number: 6)
+  week_1 = batch.program_weeks.find_by(number: 1)
+  week_1_group_1 = week_1.target_groups.find_by(number: 1)
+  week_1_group_2 = week_1.target_groups.find_by(number: 2)
+  week_1_group_3 = week_1.target_groups.find_by(number: 3)
+  week_2 = batch.program_weeks.find_by(number: 2)
+  week_2_group_1 = week_2.target_groups.find_by(number: 1)
+  week_2_group_2 = week_2.target_groups.find_by(number: 2)
 
-  targets_list = [
-    [avengers_startup, Faculty.first, 'founder', 'Upload Resume', 'Upload your latest resume!'],
-    [avengers_startup, Faculty.second, 'engineering', 'Select Tech Stack', 'Decide on the tech stack that works for you'],
-    [avengers_startup, Faculty.first, 'governance', 'Sign Agreements', 'Complete all your paperwork with SV.CO']
-  ]
+  Target.create!(
+    title: 'Add Info to your Startup Profile',
+    role: 'governance',
+    description: Faker::Lorem.words(10).join(' '),
+    target_type: Target::TYPE_TODO,
+    days_to_complete: 7,
+    target_group: week_1_group_1,
+    batch: batch
+  )
 
-  targets_list.each do |startup, assigner, role, title, description|
-    Target.create!(
-      assignee: startup,
-      assigner: assigner,
-      role: role,
-      title: title,
-      description: description
-    )
-  end
+  Target.create!(
+    title: 'Join Public Slack',
+    role: 'governance',
+    description: Faker::Lorem.words(10).join(' '),
+    completion_instructions: Faker::Lorem.words(30).join(' '),
+    target_type: Target::TYPE_TODO,
+    days_to_complete: 7,
+    target_group: week_1_group_1,
+    batch: batch
+  )
+
+  Target.create!(
+    title: 'Apply for a Passport',
+    role: Target::ROLE_FOUNDER,
+    description: Faker::Lorem.words(10).join(' '),
+    completion_instructions: Faker::Lorem.words(30).join(' '),
+    target_type: Target::TYPE_TODO,
+    days_to_complete: 60,
+    target_group: week_1_group_2,
+    batch: batch
+  )
+
+  Target.create!(
+    title: "Vishnu's talk: Bootcamp!",
+    role: Target::ROLE_FOUNDER,
+    description: Faker::Lorem.words(10).join(' '),
+    completion_instructions: Faker::Lorem.words(30).join(' '),
+    target_type: Target::TYPE_ATTEND,
+    days_to_complete: 7,
+    target_group: week_1_group_3,
+    batch: batch
+  )
+
+  Target.create!(
+    title: 'Execute Experiments',
+    role: 'engineering',
+    description: Faker::Lorem.words(10).join(' '),
+    completion_instructions: Faker::Lorem.words(30).join(' '),
+    target_type: Target::TYPE_TODO,
+    days_to_complete: 14,
+    target_group: week_2_group_1,
+    batch: batch
+  )
+
+  Target.create!(
+    title: 'Why Startups Fail/Succeed',
+    role: Target::ROLE_FOUNDER,
+    description: Faker::Lorem.words(10).join(' '),
+    completion_instructions: Faker::Lorem.words(30).join(' '),
+    target_type: Target::TYPE_LEARN,
+    days_to_complete: 7,
+    target_group: week_2_group_2,
+    batch: batch
+  )
 end

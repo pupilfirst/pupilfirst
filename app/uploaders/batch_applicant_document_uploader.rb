7ae76e1033535077b8673003969e6dbd554e15cb
Abdul Jaleel <jaleel@sv.co>
2016-12-02 14:15:44 +0530

    Remove thumbs for applicant documents, give links

--- app/uploaders/batch_applicant_document_uploader.rb
+++ app/uploaders/batch_applicant_document_uploader.rb
@@ -1,7 +1,4 @@
 class BatchApplicantDocumentUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-  include CarrierWave::BombShelter
-
   # Override the directory where uploaded files will be stored.
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -14,9 +11,5 @@ class BatchApplicantDocumentUploader < CarrierWave::Uploader::Base
 
   def fog_directory
     ENV['PRIVATE_S3_BUCKET_NAME']
-  end
-
-  version :thumb do
-    process resize_to_limit: [100, 100]
   end
 end

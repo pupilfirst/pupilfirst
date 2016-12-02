0ba1e92acd3516f766b258642ee2d47b3df5048a
Hari Gopal <hari@sv.co>
2016-12-02 12:07:43 +0530

    Make filename return 'existing file' in case of failure

--- app/models/concerns/private_filename_retrievable.rb
+++ app/models/concerns/private_filename_retrievable.rb
@@ -2,7 +2,9 @@
 module PrivateFilenameRetrievable
   extend ActiveSupport::Concern
 
+  # Carrierwave method .original_filename isn't reliably available on the sanitized file return. Reason for this is
+  # unknown - so this method call is set to return 'existing file' if that call fails.
   def filename(field)
-    public_send(field).sanitized_file.original_filename
+    public_send(field)&.sanitized_file&.original_filename || 'existing file'
   end
 end

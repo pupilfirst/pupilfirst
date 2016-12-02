25a34d71ef9d718fd681b4a0d2a7675141166ab7
Abdul Jaleel <jaleel@sv.co>
2016-12-02 12:38:45 +0530

    Allow admin to view/edit id and address proofs

--- app/uploaders/batch_applicant_document_uploader.rb
+++ app/uploaders/batch_applicant_document_uploader.rb
@@ -1,4 +1,8 @@
 class BatchApplicantDocumentUploader < CarrierWave::Uploader::Base
+  include CarrierWave::Backgrounder::Delay
+  include CarrierWave::MiniMagick
+  include CarrierWave::BombShelter
+
   # Override the directory where uploaded files will be stored.
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -11,5 +15,9 @@ class BatchApplicantDocumentUploader < CarrierWave::Uploader::Base
 
   def fog_directory
     ENV['PRIVATE_S3_BUCKET_NAME']
+  end
+
+  version :thumb do
+    process resize_to_limit: [100, 100]
   end
 end

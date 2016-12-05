a1d48bf1096560f7da3a1ce56f5a94703ae1bd81
Abdul Jaleel <jaleel@sv.co>
2016-12-05 18:26:43 +0530

    Fix a little stale code

--- app/helpers/active_admin/timeline_event_helper.rb
+++ app/helpers/active_admin/timeline_event_helper.rb
@@ -2,11 +2,9 @@ module ActiveAdmin
   module TimelineEventHelper
     def grouped_targets_for_linking(timeline_event)
       targets = if timeline_event.timeline_event_type.founder_event?
-        # Return incomplete founder targets
-        timeline_event.founder.targets
+        Target.founder
       else
-        # Return incomplete non-founder targets
-        timeline_event.startup.targets
+        Target.not_founder
       end
 
       # TODO: bring back below code after correcting the required scopes

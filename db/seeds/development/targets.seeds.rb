870b099f4e30afa392adbab75d3ca1aef2ed73a6
Mahesh Krishna Kumar <mahesh@sv.co>
2016-12-07 15:52:35 +0530

    Update seeds with change of column number to sort_index in TargetGroup

--- db/seeds/development/targets.seeds.rb
+++ db/seeds/development/targets.seeds.rb
@@ -5,12 +5,12 @@ after 'development:target_groups' do
 
   batch = Batch.find_by(batch_number: 6)
   week_1 = batch.program_weeks.find_by(number: 1)
-  week_1_group_1 = week_1.target_groups.find_by(number: 1)
-  week_1_group_2 = week_1.target_groups.find_by(number: 2)
-  week_1_group_3 = week_1.target_groups.find_by(number: 3)
+  week_1_group_1 = week_1.target_groups.find_by(sort_index: 1)
+  week_1_group_2 = week_1.target_groups.find_by(sort_index: 2)
+  week_1_group_3 = week_1.target_groups.find_by(sort_index: 3)
   week_2 = batch.program_weeks.find_by(number: 2)
-  week_2_group_1 = week_2.target_groups.find_by(number: 1)
-  week_2_group_2 = week_2.target_groups.find_by(number: 2)
+  week_2_group_1 = week_2.target_groups.find_by(sort_index: 1)
+  week_2_group_2 = week_2.target_groups.find_by(sort_index: 2)
 
   Target.create!(
     title: 'Add Info to your Startup Profile',

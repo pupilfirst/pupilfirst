a7c1ba77db1399d0c92729ba5a2785080893118a
Abdul Jaleel <jaleel@sv.co>
2016-12-10 18:49:04 +0530

    WIP: Fix the submission panel for each target

--- app/decorators/target_decorator.rb
+++ app/decorators/target_decorator.rb
@@ -9,6 +9,33 @@ class TargetDecorator < Draper::Decorator
     I18n.t("target.status.#{status(founder)}")
   end
 
+  def status_report_text(founder)
+    I18n.t("target.status_report.#{status(founder)}")
+  end
+
+  def status_hint_text(founder)
+    I18n.t("target.status_hint.#{status(founder)}", date: timeline_events&.last&.event_on&.strftime('%b %e'))
+  end
+
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def status_fa_icon(founder)
+    case status(founder)
+      when Targets::StatusService::STATUS_COMPLETE
+        'fa-thumbs-o-up'
+      when Targets::StatusService::STATUS_NEEDS_IMPROVEMENT
+        'fa-line-chart'
+      when Targets::StatusService::STATUS_EXPIRED
+        'fa-hourglass-end'
+      when Targets::StatusService::STATUS_NOT_ACCEPTED
+        'fa-thumbs-o-down'
+      when Targets::StatusService::STATUS_UNAVAILABLE
+        'fa-lock'
+      when Targets::StatusService::STATUS_SUBMITTED
+        'fa-hourglass-half'
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+
   ATTEND_SESSION_ICON = 'attend_session_icon.svg'.freeze
   TEAM_TODO_ICON = 'team_todo_icon.svg'.freeze
   PERSONAL_TODO_ICON = 'personal_todo_icon.svg'.freeze
@@ -31,5 +58,9 @@ class TargetDecorator < Draper::Decorator
 
   def completed_by?(founder)
     status(founder).in? [Targets::StatusService::STATUS_COMPLETE, Targets::StatusService::STATUS_NEEDS_IMPROVEMENT]
+  end
+
+  def pending_for?(founder)
+    status(founder) == Targets::StatusService::STATUS_PENDING
   end
 end

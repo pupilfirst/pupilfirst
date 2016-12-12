eca219eef88371d9d2c391621a0c17db6eb1d587
Mahesh Krishna Kumar <mahesh@sv.co>
2016-12-12 19:36:44 +0530

    Introduce horizontal split by weeks

--- app/admin/targets_overview.rb
+++ app/admin/targets_overview.rb
@@ -7,7 +7,16 @@ ActiveAdmin.register_page 'Targets Overview' do
     end
 
     def load_targets
-      @targets = params[:batch].present? ? Target.where(batch_id: params[:batch]) : Batch.last.targets
+      batch = params[:batch].present? ? Batch.find_by(id: params[:batch]) : Batch.last
+      @targets = []
+      batch.program_weeks.order(:number).each do |week|
+        week_targets = []
+        week.target_groups.each do |target_groups|
+          week_targets << target_groups.targets
+        end
+        week_targets.flatten!
+        @targets << week_targets
+      end
     end
   end
 

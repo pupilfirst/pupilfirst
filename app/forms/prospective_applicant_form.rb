69b8a66f13dc6eedffefb88a1ae900b1f2ede861
Abdul Jaleel <jaleel@sv.co>
2016-12-15 15:03:52 +0530

    Even prospective applicants must not enter bounced emails

--- app/forms/prospective_applicant_form.rb
+++ app/forms/prospective_applicant_form.rb
@@ -7,6 +7,16 @@ class ProspectiveApplicantForm < Reform::Form
   property :college_id, validates: { presence: true }
   property :college_text, validates: { length: { maximum: 250 } }
 
+  validate :email_must_not_be_bounced
+
+  def email_must_not_be_bounced
+    return if email.blank?
+
+    user = User.with_email(email).first
+    return if user.blank?
+    errors[:email] << 'previous mails to this address were bouncing! Supply a different one.' if user.email_bounced
+  end
+
   def save
     prospective_applicant = ProspectiveApplicant.with_email(email).first
     prospective_applicant = ProspectiveApplicant.new(email: email) if prospective_applicant.blank?

63dc4de31fdee76a123417dea7ea77b2831f74f7
Abdul Jaleel <jaleel@sv.co>
2016-12-22 16:32:32 +0530

    Timeline Builder: Add basic validation for links

--- app/assets/javascripts/components/timeline_builder/link_form.js.jsx
+++ app/assets/javascripts/components/timeline_builder/link_form.js.jsx
@@ -3,36 +3,87 @@ const TimelineBuilderLinkForm = React.createClass({
     addAttachmentCB: React.PropTypes.func
   },
 
+  getInitialState: function () {
+    return {
+      hasTitleError: false,
+      hasUrlError: false
+    }
+  },
+
   linkSubmit: function (event) {
     event.preventDefault();
 
-    // TODO: Add validations.
-
-    this.props.addAttachmentCB('link', {
-      title: $('.js-link-title').val(),
-      url: $('.js-link-url').val(),
-      visibility: $('.js-link-visibility').val()
-    });
-
-    setTimeout(this.clearForm, 500);
+    if (this.validate()) {
+      this.storeLink();
+      setTimeout(this.clearForm, 500);
+    }
   },
 
-  clearForm: function() {
+  clearForm: function () {
     $('.js-link-title').val('');
     $('.js-link-url').val('');
     $('.js-link-visibility').val('public');
   },
 
+  validate: function () {
+    let titleError = false;
+    let urlError = false;
+
+    if ($('.js-link-title').val().length == 0) {
+      titleError = true;
+    }
+
+    if ($('.js-link-url').val().length == 0) {
+      urlError = true;
+    }
+
+    if (titleError || urlError){
+      this.setState({hasTitleError: titleError, hasUrlError: urlError});
+    }
+
+    return !titleError && !urlError;
+  },
+
+  storeLink: function () {
+    this.props.addAttachmentCB('link', {
+      title: $('.js-link-title').val(),
+      url: $('.js-link-url').val(),
+      visibility: $('.js-link-visibility').val()
+    });
+  },
+
+  titleFormGroupClasses: function() {
+    return "form-group link-title-group" + (this.state.hasTitleError ? ' has-danger' : '');
+  },
+
+  urlFormGroupClasses: function() {
+    return "form-group link-url-group" + (this.state.hasUrlError ? ' has-danger' : '');
+  },
+
+  clearTitleError: function () {
+    this.setState({hasTitleError: false});
+  },
+
+  clearUrlError: function () {
+    this.setState({hasUrlError: false});
+  },
+
   render: function () {
     return (
       <form className="form-inline attachment-form">
-        <div className="form-group link-title-group">
+        <div className={ this.titleFormGroupClasses() }>
           <label className="sr-only" htmlFor="linkTitle">Link Title</label>
-          <input className="form-control link-title js-link-title" type="text" placeholder="Title"/>
+          <input className="form-control link-title js-link-title" type="text" placeholder="Title" onFocus={ this.clearTitleError }/>
+          { this.state.hasTitleError &&
+          <div className="form-control-feedback">Enter a valid title!</div>
+          }
         </div>
-        <div className="form-group link-url-group">
+        <div className={ this.urlFormGroupClasses() }>
           <label className="sr-only" htmlFor="linkUrl">URL</label>
-          <input className="form-control link-url js-link-url" type="text" placeholder="URL"/>
+          <input className="form-control link-url js-link-url" type="text" placeholder="URL" onFocus={ this.clearUrlError }/>
+          { this.state.hasUrlError &&
+          <div className="form-control-feedback">Enter a valid URL!</div>
+          }
           <small className="form-text text-muted hidden-xs-up">Please enter a full URL, starting with http(s).</small>
         </div>
         <div className="form-group visibility-option-group">

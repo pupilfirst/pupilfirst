7053c558076917aac03c133bd89a107e60c2afad
Abdul Jaleel <jaleel@sv.co>
2016-12-22 13:32:15 +0530

    Add spec for PerformanceService

--- spec/factories/karma_point.rb
+++ spec/factories/karma_point.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     founder { create :founder }
     points { [10, 20, 30, 40, 50].sample }
     activity_type { Faker::Lorem.words(10).join ' ' }
+
+    trait :for_last_week do
+      created_at { 7.days.ago }
+    end
   end
 end
---
+++ spec/services/startups/performance_service_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe Startups::PerformanceService do
+  subject { described_class.new }
+
+  let!(:batch) { create :batch, :with_startups, start_date: 12.days.ago }
+
+  SAMPLE_POINTS = [10, 200, 210, 210, 240, 280, 500, 1000].freeze
+
+  before do
+    # add karma points to some startups
+    SAMPLE_POINTS.each.with_index(1) do |points, index|
+      startup = batch.startups.order(:id).limit(index).last
+      create :karma_point, :for_last_week, startup: startup, points: points
+    end
+  end
+
+  describe '#leaderboard' do
+    it 'returns the leaderboard rank list for the batch' do
+      startup_ids = batch.startups.order(:id).pluck(:id)
+      expected_ranks = [8, 7, 5, 5, 4, 3, 2, 1, 9, 9]
+      expected_rank_list = startup_ids.each_with_index.map { |id, index| [id, expected_ranks[index]] }
+
+      expect(subject.leaderboard(batch).sort).to eq(expected_rank_list)
+    end
+  end
+
+  describe '#leaderboard_rank' do
+    it 'returns the leaderboard rank of the specified startup' do
+      expect(subject.leaderboard_rank(batch.startups.first)).to eq(8)
+    end
+  end
+
+  describe '#last_week_karma' do
+    it 'returns the karma points earned last week by the specified startup' do
+      expect(subject.last_week_karma(batch.startups.second)).to eq(200)
+      expect(subject.last_week_karma(batch.startups.last)).to eq(0)
+    end
+  end
+
+  describe '#relative_performance' do
+    it 'returns a relative measure of performance for the startup specified' do
+      startups = batch.startups.order(:id)
+
+      expect(subject.relative_performance(startups.first)).to eq(30)
+      expect(subject.relative_performance(startups.fifth)).to eq(50)
+      expect(subject.relative_performance(startups.limit(8).last)).to eq(90)
+    end
+  end
+end

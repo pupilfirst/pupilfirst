7ec90d4425461c28dd6cb61fc71cb7e978972a27
Hari Gopal <hari@sv.co>
2016-12-23 14:01:02 +0530

    Rewrite TimelineEventsController#create
    It now uses a new TimelineBuilderForm to handle validation of data
    accepted from the timeline builder, and creation of the timeline event.

---
+++ app/forms/timeline_builder_form.rb
@@ -0,0 +1,63 @@
+class TimelineBuilderForm < Reform::Form
+  property :description, validates: { presence: true, length: { maximum: TimelineEvent::MAX_DESCRIPTION_CHARACTERS } }
+  property :timeline_event_type_id, validates: { presence: true }
+  property :event_on, validates: { presence: true }
+  property :links
+  property :files, virtual: true
+  property :files_metadata, virtual: true
+  property :image
+
+  validate :timeline_event_type_should_exist
+  validate :files_should_have_metadata
+
+  def timeline_event_type_should_exist
+    return if timeline_event_type.present?
+    errors[:timeline_event_type_id] << 'is invalid'
+  end
+
+  def files_should_have_metadata
+    missing_metadata = files.keys.any? do |identifier|
+      parsed_files_metadata[identifier].blank?
+    end
+
+    errors[:files_metadata] << 'is incomplete' if missing_metadata
+  end
+
+  def timeline_event_type
+    @timeline_event_type ||= TimelineEventType.find(timeline_event_type_id)
+  end
+
+  def parsed_files_metadata
+    @parsed_metadata ||= JSON.parse(files_metadata)
+  end
+
+  def parsed_links
+    # Symbolize the keys in each hash to maintain compatibility with old code.
+    JSON.parse(links).map(&:symbolize_keys)
+  end
+
+  def save(founder)
+    TimelineEvent.transaction do
+      timeline_event = TimelineEvent.create!(
+        founder: founder,
+        startup: founder.startup,
+        description: description,
+        timeline_event_type: timeline_event_type,
+        event_on: Time.zone.parse(event_on),
+        links: parsed_links,
+        image: image
+      )
+
+      # Save timeline event files with metadata.
+      files.each do |identifier, file|
+        metadata = parsed_files_metadata[identifier]
+
+        timeline_event.timeline_event_files.create!(
+          file: file,
+          title: metadata['title'],
+          private: metadata['private']
+        )
+      end
+    end
+  end
+end

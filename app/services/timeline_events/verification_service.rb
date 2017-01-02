cec421c35301cb09f1a024d3c5d1276223da85c4
Abdul Jaleel <jaleel@sv.co>
2017-01-02 18:17:41 +0530

    Refactor the VerificationService a bit

--- app/services/timeline_events/verification_service.rb
+++ app/services/timeline_events/verification_service.rb
@@ -1,7 +1,5 @@
 module TimelineEvents
   class VerificationService
-    attr_reader :timeline_event, :target, :grade
-
     def initialize(timeline_event)
       @timeline_event = timeline_event
       @target = @timeline_event.target
@@ -11,7 +9,7 @@ module TimelineEvents
       @grade = grade
 
       TimelineEvent.transaction do
-        timeline_event.verify!
+        @timeline_event.verify!
         update_karma_points
       end
     end
@@ -21,46 +19,44 @@ module TimelineEvents
     def update_karma_points
       return unless points_for_target.present?
 
-      KarmaPoint.transaction do
-        remove_previous_points if target.present?
-        add_karma_points
-      end
+      remove_previous_points if @target.present?
+      add_karma_points
     end
 
     def remove_previous_points
-      KarmaPoint.where(source: target.timeline_events).delete_all
+      KarmaPoint.where(source: @target.timeline_events).delete_all
     end
 
     def add_karma_points
       KarmaPoint.create!(
-        source: timeline_event,
+        source: @timeline_event,
         founder: founder,
-        startup: timeline_event.startup,
-        activity_type: "Added a new Timeline event - #{timeline_event.title}",
+        startup: @timeline_event.startup,
+        activity_type: "Added a new Timeline event - #{@timeline_event.title}",
         points: applicable_points
       )
     end
 
     def founder
-      timeline_event.founder_event? ? timeline_event.founder : nil
+      @timeline_event.founder_event? ? @timeline_event.founder : nil
     end
 
     def applicable_points
-      return points_for_target unless grade.present?
+      return points_for_target unless @grade.present?
 
-      points_for_target * grade_multiplier(grade)
+      points_for_target * grade_multiplier
     end
 
-    def grade_multiplier(grade)
+    def grade_multiplier
       {
         TimelineEvent::GRADE_GOOD => 1,
         TimelineEvent::GRADE_GREAT => 1.5,
         TimelineEvent::GRADE_WOW => 2
-      }.with_indifferent_access[grade]
+      }.with_indifferent_access[@grade]
     end
 
     def points_for_target
-      @points_for_target ||= timeline_event&.target&.points_earnable
+      @points_for_target ||= @target&.points_earnable
     end
   end
 end

640383e9b90ea722b345f8acf4d6a964c147e1a4
Vishnu Gopal <vishnu@sv.co>
2017-01-05 14:32:07 +0530

    Bringing back navigation in the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @skip_container = true
     @sitewide_notice = true if %w(startupvillage.in registration).include?(params[:redirect_from])
-    @hide_nav_links = true
+    @hide_nav_links = false
     @instagram_images = Instagram.load_latest_images
 
     render layout: 'home'

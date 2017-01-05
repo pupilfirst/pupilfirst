532f6f7800f8b2822673c9979b094029421c2578
Hari Gopal <hari@sv.co>
2017-01-05 15:16:08 +0530

    ReplacementUniversity has many Founders

--- app/models/replacement_university.rb
+++ app/models/replacement_university.rb
@@ -4,5 +4,6 @@ class ReplacementUniversity < ApplicationRecord
   belongs_to :state
   has_many :colleges
   has_many :batch_applicants, through: :colleges
+  has_many :founders, through: :colleges
   has_many :batch_applications, through: :batch_applicants
 end

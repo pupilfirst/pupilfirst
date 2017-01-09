8bcc85f192dfb31db60af12c912657b20ccea7c6
Abdul Jaleel <jaleel@sv.co>
2017-01-09 15:50:31 +0530

    Handle founder rejecting publish permissions to facebook

--- app/controllers/founders/facebook_connect_controller.rb
+++ app/controllers/founders/facebook_connect_controller.rb
@@ -3,15 +3,23 @@ module Founders
     before_action :authenticate_founder!
 
     def connect
-      @facebook_client = Founders::FacebookService.new(current_founder)
-      redirect_to @facebook_client.oauth_url
+      facebook_client = Founders::FacebookService.new(current_founder)
+      redirect_to facebook_client.oauth_url
     end
 
+    # TODO: Probably refactor
     def connect_callback
-      if params[:error].present? || permission_not_granted?
-        set_flash_error
+      if params[:error].present?
+        flash[:error] = 'Something went wrong while connecting to Facebook. Please try again!'
       elsif params[:code].present?
-        set_access_token
+        facebook_client = Founders::FacebookService.new(current_founder)
+        token, expires = facebook_client.get_access_token_info(params[:code])
+        if facebook_client.permissions_granted?(token)
+          facebook_client.save_token_info!(token, expires)
+          flash[:success] = 'Facebook Connection Successful!'
+        else
+          flash[:error] = 'Publish rights are required. Please try again!'
+        end
       else
         raise 'Unexpected Facebook Response'
       end
@@ -23,23 +31,6 @@ module Founders
       Founders::FacebookService.new(current_founder).disconnect!
       flash[:success] = 'Facebook disconnected successfully!'
       redirect_to edit_founder_path
-    end
-
-    private
-
-    def set_flash_error
-      flash[:error] = "Something went wrong while connecting to Facebook (#{params[:error_description]}). Please try again!"
-    end
-
-    def set_access_token
-      @facebook_client = Founders::FacebookService.new(current_founder)
-      @facebook_client.save_access_token!(params[:code])
-      flash[:success] = 'Facebook Connection Successful!'
-    end
-
-    # TODO
-    def permission_not_granted?
-      false
     end
   end
 end

9dbe5d3c98df64549e9049b5d4670f27ac80cf6f
Hari Gopal <hari@sv.co>
2017-01-16 12:37:03 +0530

    Block bounced emails during applicant registration [skip ci]

--- app/forms/user_sign_in_form.rb
+++ app/forms/user_sign_in_form.rb
@@ -1,20 +1,15 @@
 class UserSignInForm < Reform::Form
+  include EmailBounceValidatable
+
   property :email, validates: { presence: true, length: { maximum: 250 }, email: true }
   property :referer
   property :shared_device
 
   validate :user_with_email_must_exist
-  validate :email_should_not_have_bounced
 
   def user_with_email_must_exist
     return if user.present?
     errors[:email] << 'Could not find user with this email'
-  end
-
-  def email_should_not_have_bounced
-    return unless user&.email_bounced?
-    errors[:base] << 'We cannot send a sign-in email to this address because a previous attempt failed. Please contact help@sv.co for more information.'
-    errors[:email] << 'is an address which bounced'
   end
 
   def save

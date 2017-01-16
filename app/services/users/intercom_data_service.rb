59f52c8b84d74127d840d1067a2297528b7fa393
Abdul Jaleel <jaleel@sv.co>
2017-01-16 17:05:41 +0530

    Config Intercom to use the User model, Setup a service to provide custom data

---
+++ app/services/users/intercom_data_service.rb
@@ -0,0 +1,29 @@
+module Users
+  class IntercomDataService
+    def initialize(user)
+      @user = user
+    end
+
+    def data
+      {
+        name: name,
+        startup: startup,
+        phone: phone
+      }
+    end
+
+    private
+
+    def name
+      @user.founder&.name || @user.batch_applicant&.name || @user.mooc_student&.name
+    end
+
+    def startup
+      @user.founder&.startup&.product_name
+    end
+
+    def phone
+      @user.founder&.phone || @user.batch_applicant&.phone || @user.mooc_student&.phone
+    end
+  end
+end

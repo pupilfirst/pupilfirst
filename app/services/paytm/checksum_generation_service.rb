6cf41b1b9c8cef41142c658efaa7ef35167e7247
Abdul Jaleel <jaleel@sv.co>
2017-01-18 17:33:11 +0530

    Fix some bugs uncovered from PayTM API documentation

--- app/services/paytm/checksum_generation_service.rb
+++ app/services/paytm/checksum_generation_service.rb
@@ -5,13 +5,14 @@ module Paytm
 
     def initialize(order_id:, customer_id:, amount:, phone:, email:)
       @params_list = {
+        "REQUEST_TYPE": 'DEFAULT',
         "MID": ENV.fetch('PAYTM_MERCHANT_ID'),
         "ORDER_ID": order_id,
         "CUST_ID": customer_id,
         "INDUSTRY_TYPE_ID": ENV.fetch('PAYTM_INDUSTRY_TYPE_ID'),
         "CHANNEL_ID": ENV.fetch('PAYTM_CHANNEL_ID'),
         "TXN_AMOUNT": amount,
-        "MSISDN": phone,
+        "MOBILE_NO": phone,
         "EMAIL": email,
         "WEBSITE": ENV.fetch('PAYTM_MERCHANT_WEBSITE_NAME')
       }

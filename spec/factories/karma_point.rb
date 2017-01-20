dfbb1f6dce4877d8e8bc8a3fdacae22ccd61e27e
Abdul Jaleel <jaleel@sv.co>
2017-01-20 12:20:54 +0530

    Ensure spec for PerformanceService accomodates leaderboard changes on a Monday

--- spec/factories/karma_point.rb
+++ spec/factories/karma_point.rb
@@ -3,9 +3,5 @@ FactoryGirl.define do
     founder { create :founder }
     points { [10, 20, 30, 40, 50].sample }
     activity_type { Faker::Lorem.words(10).join ' ' }
-
-    trait :for_last_week do
-      created_at { 7.days.ago }
-    end
   end
 end
--- spec/services/startups/performance_service_spec.rb
+++ spec/services/startups/performance_service_spec.rb
@@ -2,25 +2,48 @@ require 'rails_helper'
 
 describe Startups::PerformanceService do
   subject { described_class.new }
+  include ActiveSupport::Testing::TimeHelpers
 
   let!(:batch) { create :batch, :with_startups, start_date: 12.days.ago }
 
-  SAMPLE_POINTS = [10, 200, 210, 210, 240, 280, 500, 1000].freeze
+  POINTS_LAST_WEEK = [10, 200, 210, 210, 240, 280, 500, 1000].freeze
+  POINTS_TWO_WEEKS_BACK = [100, 200, 300, 400].freeze
 
   before do
-    # add karma points to some startups
-    SAMPLE_POINTS.each.with_index(1) do |points, index|
+    # add some karma points for last week
+    POINTS_LAST_WEEK.each.with_index(1) do |points, index|
       startup = batch.startups.order(:id).limit(index).last
-      create :karma_point, :for_last_week, startup: startup, points: points
+      create :karma_point, created_at: 10.days.ago, startup: startup, points: points
+    end
+
+    # add some karma points for two weeks back
+    POINTS_TWO_WEEKS_BACK.each.with_index(1) do |points, index|
+      startup = batch.startups.order(:id).limit(index).last
+      create :karma_point, created_at: 15.days.ago, startup: startup, points: points
     end
   end
 
   describe '#leaderboard' do
-    it 'returns the leaderboard rank list for the batch' do
-      expected_ranks = [8, 7, 5, 5, 4, 3, 2, 1, 9, 9]
-      expected_leaderboard = batch.startups.order(:id).each_with_index.map { |id, index| [id, expected_ranks[index], SAMPLE_POINTS[index] || 0] }
+    it 'returns the leaderboard rank list from last week when invoked after Monday 6 p.m' do
+      travel_to(Time.now.beginning_of_week + 20.hours) do
+        expected_ranks = [8, 7, 5, 5, 4, 3, 2, 1, 9, 9]
+        expected_leaderboard = batch.startups.order(:id).each_with_index.map do |startup, index|
+          [startup, expected_ranks[index], POINTS_LAST_WEEK[index] || 0]
+        end
 
-      expect(subject.leaderboard(batch).sort).to eq(expected_leaderboard)
+        expect(subject.leaderboard(batch).sort).to eq(expected_leaderboard)
+      end
+    end
+
+    it 'returns the leaderboard rank list from two weeks back when invoked before Monday 6 p.m' do
+      travel_to(Time.now.beginning_of_week + 15.hours) do
+        expected_ranks = [4, 3, 2, 1, 5, 5, 5, 5, 5, 5]
+        expected_leaderboard = batch.startups.order(:id).each_with_index.map do |startup, index|
+          [startup, expected_ranks[index], POINTS_TWO_WEEKS_BACK[index] || 0]
+        end
+
+        expect(subject.leaderboard(batch).sort).to eq(expected_leaderboard)
+      end
     end
   end
 

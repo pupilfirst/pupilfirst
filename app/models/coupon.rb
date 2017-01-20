a86e3644590a716ae69721b3a9df51b61d8a458a
Abdul Jaleel <jaleel@sv.co>
2017-01-20 17:00:07 +0530

    Add logic to check coupon validity

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -11,4 +11,15 @@ class Coupon < ApplicationRecord
   validates :code, uniqueness: true, presence: true
   validates :coupon_type, inclusion: { in: valid_coupon_types }
   validates :discount_percentage, presence: true, inclusion: { in: 0..100, message: 'must be between 0 and 100' }
+
+  def still_valid?
+    (expires_at.blank? || expires_at.future?) && redeems_left?
+  end
+
+  def redeems_left?
+    return true if redeem_limit.zero?
+
+    redeem_count = BatchApplication.where(coupon: self).count
+    redeem_count < redeem_limit
+  end
 end

55d22bca79edf00ca35452f31fc169e3f3008b90
Mahesh Krishna Kumar <mahesh@sv.co>
2017-01-31 18:05:33 +0530

    Change leaderboard rank icons

--- app/helpers/about/leaderboard_helper.rb
+++ app/helpers/about/leaderboard_helper.rb
@@ -2,11 +2,11 @@ module About
   module LeaderboardHelper
     def change_in_rank_icon(change_in_rank)
       if change_in_rank.negative?
-        'fa-chevron-down leaderboard__rank-icon red-text'
+        'fa-caret-down leaderboard__rank-icon red-text'
       elsif change_in_rank.positive?
-        'fa-chevron-up leaderboard__rank-icon green-text'
+        'fa-caret-up leaderboard__rank-icon green-text'
       else
-        'fa-arrows-h leaderboard__rank-icon grey-text'
+        'fa-circle leaderboard__rank-icon light-grey-text'
       end
     end
   end

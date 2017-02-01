2b6fe6107817c4183ec0def2f0af22c4127eb9d2
Vinu T V <vinu.tv@mobme.in>
2017-02-01 13:02:33 +0530

    Style leaderboard rank

--- app/helpers/about/leaderboard_helper.rb
+++ app/helpers/about/leaderboard_helper.rb
@@ -2,11 +2,11 @@ module About
   module LeaderboardHelper
     def change_in_rank_icon(change_in_rank)
       if change_in_rank.negative?
-        'fa-caret-down leaderboard__rank-icon red-text'
+        'fa-arrow-down leaderboard__rank-icon leaderboard__rank-icon--red'
       elsif change_in_rank.positive?
-        'fa-caret-up leaderboard__rank-icon green-text'
+        'fa-arrow-up leaderboard__rank-icon leaderboard__rank-icon--green'
       else
-        'fa-circle leaderboard__rank-icon light-grey-text'
+        'fa-dot-circle-o leaderboard__rank-icon leaderboard__rank-icon--grey'
       end
     end
   end

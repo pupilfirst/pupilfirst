07cb1a470dd2b176467389e837babbc306b56409
Abdul Jaleel <jaleel@sv.co>
2017-02-08 15:44:14 +0530

    Remove the stale intercom_user_update_service

--- app/decorators/batch_applicant_decorator.rb
+++ app/decorators/batch_applicant_decorator.rb
@@ -13,21 +13,6 @@ class BatchApplicantDecorator < Draper::Decorator
     batch_applications&.last&.batch&.name
   end
 
-  # TODO: Update the following to account for events after task submissions
-  def last_applicant_event
-    return nil unless batch_applications.any?
-
-    latest_application = batch_applications.last
-
-    return 'tasks_submitted' if latest_application.application_submissions.where(application_stage: ApplicationStage.find_by(name: 'Testing')).any?
-
-    return 'payment_complete' if latest_application.paid?
-
-    return 'payment_initiated' if latest_application.payment.present?
-
-    'submitted_application'
-  end
-
   def age
     return nil unless born_on.present?
     Date.today.year - born_on.year

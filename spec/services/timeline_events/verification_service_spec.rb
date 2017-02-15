bbab5094cf3eff4c81458c55433486f99f3bd208
Mahesh Krishna Kumar <mahesh@sv.co>
2017-02-15 22:37:40 +0530

    Remove resetting old karma points on new events

--- spec/services/timeline_events/verification_service_spec.rb
+++ spec/services/timeline_events/verification_service_spec.rb
@@ -4,15 +4,15 @@ describe TimelineEvents::VerificationService do
   subject { described_class.new(timeline_event) }
 
   let(:timeline_event) { create :timeline_event }
-  let(:karma_point) { create :karma_point, points: 50 }
+  let(:timeline_event_2) { create :timeline_event, verified_status: TimelineEvent::VERIFIED_STATUS_NEEDS_IMPROVEMENT }
+  let(:karma_point) { create :karma_point, points: 10 }
   let(:target) { create :target, points_earnable: 10 }
+  let(:startup) { create :startup }
+  let(:founder) { create :founder, startup: startup }
 
   before do
     # stub out vocalist notifications
     allow(TimelineEventVerificationNotificationJob).to receive(:perform_later).and_return(true)
-
-    # add an initial dummy karma point for testing
-    karma_point.update!(source: timeline_event)
   end
 
   describe '#update_status' do
@@ -40,7 +40,7 @@ describe TimelineEvents::VerificationService do
 
     context 'when the timeline event is associated with a target' do
       before do
-        timeline_event.update!(target: target)
+        timeline_event.update!(target: target, founder: founder, startup: startup)
       end
 
       context 'when asked to mark event as verified and graded as wow' do
@@ -60,6 +60,27 @@ describe TimelineEvents::VerificationService do
           timeline_event.reload
           expect(timeline_event.needs_improvement?).to eq(true)
           expect(timeline_event.karma_point.points).to eq(10)
+        end
+      end
+
+      context 'when asked to mark event as verified and the target had an earlier needs-improvement event' do
+        before do
+          timeline_event_2.update!(target: target, founder: founder, startup: startup)
+          karma_point.update!(source: timeline_event_2)
+        end
+        it 'marks the event as verified/wow and adds only the difference from previous awarded karma points' do
+          subject.update_status(TimelineEvent::VERIFIED_STATUS_VERIFIED, grade: TimelineEvent::GRADE_WOW)
+
+          timeline_event.reload
+          expect(timeline_event.verified?).to eq(true)
+          expect(timeline_event.karma_point.points).to eq(10)
+        end
+        it 'marks the event as verified/good and no new karma point is created' do
+          subject.update_status(TimelineEvent::VERIFIED_STATUS_VERIFIED, grade: TimelineEvent::GRADE_GOOD)
+
+          timeline_event.reload
+          expect(timeline_event.verified?).to eq(true)
+          expect(timeline_event.karma_point).to eq(nil)
         end
       end
     end

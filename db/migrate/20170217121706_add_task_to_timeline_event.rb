a930cc5a59028b42ee79edc53f935a9a93cb832d
Abdul Jaleel <jaleel@sv.co>
2017-02-18 15:08:04 +0530

    Prevent unnecessary migrations

--- db/migrate/20170217121706_add_task_to_timeline_event.rb
+++
@@ -1,6 +0,0 @@
-class AddTaskToTimelineEvent < ActiveRecord::Migration[5.0]
-  def change
-    add_column :timeline_events, :task_type, :string
-    rename_column :timeline_events, :target_id, :task_id
-  end
-end
--- db/migrate/20170218091349_restore_target_to_timeline_event.rb
+++
@@ -1,6 +0,0 @@
-class RestoreTargetToTimelineEvent < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :timeline_events, :task_id, :target_id
-    remove_column :timeline_events, :task_type
-  end
-end

a861e003b1bcf6482c04a4c3193c22510a3947fe
Hari Gopal <hari@sv.co>
2017-02-28 17:17:31 +0530

    Remove relationship from visit to founder

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -2,7 +2,6 @@ class Visit < ApplicationRecord
   has_many :ahoy_events, class_name: 'Ahoy::Event'
   belongs_to :user, polymorphic: true
   belongs_to :logged_in_user, -> { where(visits: { user_type: 'User' }) }, class_name: 'User', foreign_key: 'user_id'
-  belongs_to :founder, -> { where(visits: { user_type: 'Founder' }) }, foreign_key: 'user_id'
 
   scope :user_visits, -> { joins(:logged_in_user) }
   scope :last_week, -> { where('started_at > ?', 1.week.ago.beginning_of_day) }

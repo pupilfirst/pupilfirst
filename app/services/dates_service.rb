183a476ec2552ad6476425ef6387dff02e4d4dcd
Hari Gopal <hari@sv.co>
2017-03-01 13:05:59 +0530

    Refactor DatesService [skip ci]
    ... to expose week_start(time) and week_end(time).

--- app/services/dates_service.rb
+++ app/services/dates_service.rb
@@ -1,27 +1,41 @@
 class DatesService
-  # Starts on the week before last's Monday 6 PM IST.
-  def self.last_week_start_date
-    if monday? && before_evening?
-      8.days.ago.beginning_of_week
-    else
-      7.days.ago.beginning_of_week
-    end.in_time_zone('Asia/Calcutta') + 18.hours
-  end
+  class << self
+    # Start date(time) for 'last' week.
+    def last_week_start_date
+      week_start(last_week)
+    end
 
-  # Ends on last week's Monday 6 PM IST.
-  def self.last_week_end_date
-    if monday? && before_evening?
-      8.days.ago.end_of_week
-    else
-      7.days.ago.end_of_week
-    end.in_time_zone('Asia/Calcutta') + 18.hours
-  end
+    # End date(time) for 'last' week.
+    def last_week_end_date
+      week_end(last_week)
+    end
 
-  def self.monday?
-    Time.zone.now.wday == 1
-  end
+    # Weeks start at Monday 6 PM IST.
+    def week_start(time)
+      time.beginning_of_week.in_time_zone('Asia/Calcutta') + 18.hours
+    end
 
-  def self.before_evening?
-    Time.zone.now.hour < 18
+    # Weeks end at Monday 5:59:59 PM IST.
+    def week_end(time)
+      time.end_of_week.in_time_zone('Asia/Calcutta') + 18.hours
+    end
+
+    private
+
+    def last_week
+      if monday? && before_evening?
+        8.days.ago
+      else
+        7.days.ago
+      end
+    end
+
+    def monday?
+      Time.zone.now.wday == 1
+    end
+
+    def before_evening?
+      Time.zone.now.hour < 18
+    end
   end
 end

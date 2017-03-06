7804e741ac420381b4987878e6698dde26ec0175
Hari Gopal <hari@sv.co>
2017-03-06 10:15:53 +0530

    Style update to how leaderboard is displayed by Vocalist

--- lib/lita/handlers/leaderboard.rb
+++ lib/lita/handlers/leaderboard.rb
@@ -60,7 +60,8 @@ module Lita
           else
             ':rank_nochange:'
           end
-          rank_list += "*#{format('%02d', rank)}.* #{indicator}#{format('%+03d', change_in_rank)} - <#{Rails.application.routes.url_helpers.startup_url(startup)}|#{startup.product_name}>\n"
+
+          rank_list += "*#{format('%02d', rank)}.* #{indicator}`#{change_in_rank.to_s.rjust(3)}` - <#{Rails.application.routes.url_helpers.startup_url(startup)}|#{startup.product_name}>\n"
         end
         rank_list
       end

992d504cc5cf2073c9da034415ae2190923deadb
Abdul Jaleel <jaleel@sv.co>
2017-03-17 13:13:53 +0530

    Minor Refactoring
    Prevents admin dashboard from crashing when intercom does not respond with new/active user counts.

--- lib/intercom/intercom_client.rb
+++ lib/intercom/intercom_client.rb
@@ -115,14 +115,12 @@ class IntercomClient
 
   # total number of new users
   def new_users_count
-    return 0 unless user_count_by_segment.present?
-    user_count_by_segment.find { |h| h.key? 'New' }['New']
+    user_count_by_segment&.find { |h| h.key? 'New' }&.dig('New') || 0
   end
 
   # total number of active users
   def active_users_count
-    return 0 unless user_count_by_segment.present?
-    user_count_by_segment.find { |h| h.key? 'Active' }['Active']
+    user_count_by_segment&.find { |h| h.key? 'Active' }&.dig('Active') || 0
   end
 
   # fetch the latest n open conversations

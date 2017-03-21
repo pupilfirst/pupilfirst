3ee4f1f70df1aca8e199dbb36bf3582f2cf9dacb
Mahesh Krishna Kumar <maheshk@Maheshs-MacBook-Pro.local>
2017-03-21 14:14:31 +0530

    Create list for each Intercom segment

--- app/services/intercom/inactive_user_deletion_service.rb
+++ app/services/intercom/inactive_user_deletion_service.rb
@@ -3,6 +3,15 @@ module Intercom
   class InactiveUserDeletionService
     include Loggable
 
+    # Map Intercom segments to SendinBlue list names
+    SEGMENTS_TO_LIST = {
+      'Stale Paid Applicants' => 'Paid Applicants',
+      'Stale Payment Initiated' => 'Payment Initiated',
+      'Stale Conversing Users' => 'Conversing Users',
+      'Stale Applicants' => 'Applicants',
+      'Stale Leads' => 'Stale Leads'
+    }.freeze
+
     def initialize(mock: false)
       @mock = mock
       @intercom_client = Intercom::Client.new(token: Rails.application.secrets.intercom_access_token)
@@ -10,22 +19,25 @@ module Intercom
     end
 
     def execute
-      log 'Uploading contacts to SendinBlue...'
-      upload_contacts_to_sendinblue
-
-      log 'Deleting inactive users from Intercom...'
-      delete_inactive_users
-
-      log 'Deleting inactive leads from Intercom...'
-      delete_inactive_leads
+      intercom_segments.each do |segment|
+        log "Uploading contacts from Intercom segment: #{segment} to SendinBlue..."
+        upload_to_sendinblue(segment)
+        log "Deleting users from Intercom segment: #{segment}"
+        delete_from_intercom(segment)
+      end
     end
 
     private
 
-    def upload_contacts_to_sendinblue
-      return if contacts_to_upload.blank?
+    def intercom_segments
+      SEGMENTS_TO_LIST.keys
+    end
 
-      contacts_to_upload.each do |contact|
+    def upload_to_sendinblue(segment)
+      sendinblue_contacts = contacts_to_upload(segment)
+      return if sendinblue_contacts.blank?
+
+      sendinblue_contacts.each do |contact|
         if @mock
           log "@sendinblue_client.create_update_user({ email: '#{contact[:email]}', ...})"
         else
@@ -35,17 +47,17 @@ module Intercom
     end
 
     # Extract user information from intercom stale users/leads and create a contact list in the format required by SendinBlue.
-    def contacts_to_upload
-      stale_contacts = stale_users + stale_leads
-      log "There are #{stale_contacts.count} contacts to upload..."
+    def contacts_to_upload(segment)
+      segment_users = intercom_segment_users(segment)
+      log "There are #{segment_users.count} contacts to upload..."
 
-      stale_contacts.map do |stale_user|
-        next if stale_user.email.blank?
+      segment_users.map do |intercom_user|
+        next if intercom_user.email.blank?
 
         {
-          email: stale_user.email,
-          listid: ENV.fetch('SENDINBLUE_IMPORT_LIST_ID'),
-          attributes: sendinblue_attributes(stale_user)
+          email: intercom_user.email,
+          listid: [list_id(SEGMENTS_TO_LIST[segment])],
+          attributes: sendinblue_attributes(intercom_user)
         }
       end - [nil]
     end
@@ -60,34 +72,34 @@ module Intercom
       attributes
     end
 
-    def delete_inactive_users
-      return if stale_users.blank?
+    def delete_from_intercom(segment)
+      segment_users = intercom_segment_users(segment)
+      return if segment_users.blank?
       if @mock
-        log "@intercom_client.users.submit_bulk_job(delete_items: [#{@stale_users.count} users])"
+        log "@intercom_client.users.submit_bulk_job(delete_items: [#{segment_users.count} users])"
       else
-        @intercom_client.users.submit_bulk_job(delete_items: @stale_users)
+        @intercom_client.users.submit_bulk_job(delete_items: segment_users)
       end
     end
 
-    def delete_inactive_leads
-      return if stale_leads.blank?
-      if @mock
-        log "@intercom_client.users.submit_bulk_job(delete_items: [#{@stale_leads.count} users])"
-      else
-        @intercom_client.users.submit_bulk_job(delete_items: @stale_leads)
-      end
-    end
-
-    def stale_users
-      @stale_users ||= @intercom_client.users.find_all(segment_id: segment_id('Stale Users')).to_a
-    end
-
-    def stale_leads
-      @stale_leads ||= @intercom_client.users.find_all(segment_id: segment_id('Stale Leads')).to_a
+    def intercom_segment_users(segment)
+      @intercom_client.users.find_all(segment_id: segment_id(segment)).to_a
     end
 
     def segment_id(segment_name)
-      @intercom_client.segments.all.find { |segment| segment.name == segment_name }&.id
+      @segment_id = Hash.new do
+        @intercom_segments ||= @intercom_client.segments.all
+        @intercom_segments.find { |segment| segment.name == segment_name }&.id
+      end
+      @segment_id[segment_name]
+    end
+
+    def list_id(list_name)
+      @list_id = Hash.new do
+        @sendinblue_lists = @sendinblue_client.get_lists({})
+        @sendinblue_lists['data'].detect { |list| list['name'] == list_name }['id']
+      end
+      @list_id[list_name]
     end
   end
 end

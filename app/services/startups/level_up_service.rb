d3d3e0717bd6c04414e84f5a671c6147ddd68875
Hari Gopal <hari@sv.co>
2017-03-23 13:21:36 +0530

    Spec LevelUpEligibilityService and LevelUpService
    Also fix some mistakes and add ability for LevelUpService to detect
    maximum level.

--- app/services/startups/level_up_service.rb
+++ app/services/startups/level_up_service.rb
@@ -1,17 +1,22 @@
 module Startups
+  # This service should be used to upgrade a startup to the next level.
   class LevelUpService
     def initialize(startup)
       @startup = startup
     end
 
     def execute
-      @startup.update!(level: next_level)
+      if next_level.present?
+        @startup.update!(level: next_level)
+      else
+        raise 'Maximum level reached - cannot level up.'
+      end
     end
 
     private
 
     def next_level
-      Level.find_by(number: @startup.level.number + 1)
+      @next_level ||= Level.find_by(number: @startup.level.number + 1)
     end
   end
 end
---
+++ spec/support/spec_helpers/founder_spec_helper.rb
@@ -0,0 +1,27 @@
+# Some helpers to deal with founders in specs.
+module FounderSpecHelper
+  # This 'completes' a target for a founder - both startup and founder role targets.
+  def complete_target(founder, target)
+    startup = founder.startup
+
+    if target.founder_role?
+      startup.founders.each do |startup_founder|
+        create_verified_timeline_event(startup_founder, target)
+      end
+    else
+      create_verified_timeline_event(founder, target)
+    end
+  end
+
+  # This creates a verified timeline event for a target, attributed to supplied founder.
+  def create_verified_timeline_event(founder, target)
+    FactoryGirl.create(
+      :timeline_event,
+      :verified,
+      founder: founder,
+      target: target,
+      startup: founder.startup,
+      timeline_event_type: target.timeline_event_type
+    )
+  end
+end

c956803bf9a4a8dbc145d44bb9e4701e39462fde
Abdul Jaleel <jaleel@sv.co>
2017-03-24 13:55:44 +0530

    GithubStatsService: expose fetch + add code_frequency
    The #fetch method is exposed to easily call any API end point.
    The #code_frequency method returns total additions and
    deletions in code per week for last 10 weeks.

--- app/services/engineering_metrics/github_stats_service.rb
+++ app/services/engineering_metrics/github_stats_service.rb
@@ -4,27 +4,34 @@ module EngineeringMetrics
       @token = ENV.fetch('GITHUB_ACCESS_TOKEN')
     end
 
+    # fetch data for any Github API end-point.
+    def fetch(path)
+      url = 'https://api.github.com/' + path
+      JSON.parse(RestClient.get(url, Authorization: "token #{@token}"))
+    end
+
     AUTHORS = %w(harigopal ajaleelp mahesh-sv vinutv).freeze
 
+    # contribution details per founder per week - including additions, deletions and commits
     def contributions
       contributions = fetch('repos/SVdotCO/sv.co/stats/contributors')
       contributions.reject! { |c| !c['author']['login'].in?(AUTHORS) }
       contributions.map { |c| pretty_contribution(c) }
     end
 
-    # returns commits per founder per week for last 10 weeks
+    # commits per founder per week for last 10 weeks
     def commits_trend
       contributions.each_with_object({}) do |developer_stats, result|
         result[developer_stats[:name].to_sym] = developer_stats[:recent_weeks].map { |_k, a| a[:commits] }
       end
     end
 
-    private
-
-    def fetch(path)
-      url = 'https://api.github.com/' + path
-      JSON.parse(RestClient.get(url, Authorization: "token #{@token}"))
+    # additions and deletions per week for last 10 weeks
+    def code_frequency
+      fetch('repos/SVdotCO/sv.co/stats/code_frequency')[-10..-1]
     end
+
+    private
 
     def pretty_contribution(user_contribution)
       {

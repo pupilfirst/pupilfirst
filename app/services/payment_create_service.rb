07e8c373932cd0f01b4181bce0957fdb38223e4d
Abdul Jaleel <jaleel@sv.co>
2017-04-04 16:19:33 +0530

    Update PaymentCreateService to use startups

--- app/services/payment_create_service.rb
+++ app/services/payment_create_service.rb
@@ -2,22 +2,23 @@
 #
 # Specifics of how this works can be controlled using options.
 class PaymentCreateService
-  def initialize(batch_application, skip_instamojo: false, skip_payment: false)
-    @batch_application = batch_application
+  def initialize(founder, skip_instamojo: false, skip_payment: false)
+    @founder = founder
+    @startup = founder.startup
     @skip_instamojo = skip_instamojo
     @skip_payment = skip_payment
   end
 
   def execute
     payment = Payment.new(
-      batch_application: @batch_application,
-      batch_applicant: @batch_application.team_lead
+      startup: @startup,
+      founder: @founder
     )
 
     unless @skip_payment || @skip_instamojo
       response = create_instamojo_payment_request
 
-      payment.amount = @batch_application.fee
+      payment.amount = @startup.fee
       payment.instamojo_payment_request_id = response[:id]
       payment.instamojo_payment_request_status = response[:status]
       payment.short_url = response[:short_url]
@@ -43,9 +44,9 @@ class PaymentCreateService
 
   def create_instamojo_payment_request
     instamojo.create_payment_request(
-      amount: @batch_application.fee,
-      buyer_name: @batch_application.team_lead.name,
-      email: @batch_application.team_lead.email
+      amount: @startup.fee,
+      buyer_name: @founder.name,
+      email: @founder.email
     )
   end
 end

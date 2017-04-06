ea90f8f212a1599d03dc6c9149e60d06f27cda0c
Abdul Jaleel <jaleel@sv.co>
2017-04-06 17:18:14 +0530

    Add seeds for chores and sessions [skip ci]

--- db/seeds/development/targets.seeds.rb
+++ db/seeds/development/targets.seeds.rb
@@ -10,7 +10,7 @@ after 'development:target_groups', 'development:timeline_event_types' do
   founder_update = TimelineEventType.find_by(key: 'founder_update')
   team_update = TimelineEventType.find_by(key: 'team_update')
 
-  # Level 0 targets.
+  # Compulsory Level 0 targets.
   level_0_milestone_group = Level.find_by(number: 0).target_groups.find_by(milestone: true)
 
   screening_target = Target.create!(days_to_complete: 1, title: 'Go through Screening', role: Target::ROLE_TEAM, timeline_event_type: team_update, submittability: Target::SUBMITTABILITY_SUBMITTABLE_ONCE, link_to_complete: '/apply/screening', key: Target::KEY_ADMISSIONS_SCREENING, target_group: level_0_milestone_group, description: paragraph)
@@ -29,13 +29,30 @@ after 'development:target_groups', 'development:timeline_event_types' do
 
   Target.create!(days_to_complete: 15, title: 'Pre-selection', role: Target::ROLE_TEAM, timeline_event_type: team_update, key: Target::KEY_ADMISSIONS_PRE_SELECTION, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [interview_target])
 
-  [1, 2, 3, 4].each do |level_number|
+  # Random targets, session and chores for every level
+  (0..4).each do |level_number|
     level = Level.find_by(number: level_number)
 
+    # 3 normal targets per target_group
     level.target_groups.each do |target_group|
       3.times do
         target_group.targets.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, target_group: target_group, description: paragraph)
       end
+    end
+
+    # 3 chores per level
+    3.times do
+      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, chore: true, level: level, description: paragraph)
+    end
+
+    # 3 upcoming sessions per level
+    3.times do
+      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, session_at: rand(4.weeks).seconds.from_now, level: level, description: paragraph)
+    end
+
+    # 3 past sessions per level
+    3.times do
+      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, session_at: rand(4.weeks).seconds.ago, level: level, description: paragraph)
     end
   end
 end

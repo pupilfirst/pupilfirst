81cf2708630360d8e535de81a323a6bd26b87f32
Mahesh Krishna Kumar <mahesh@sv.co>
2017-04-10 13:42:28 +0530

    Update seeds: Add key to interview target

--- db/seeds/development/targets.seeds.rb
+++ db/seeds/development/targets.seeds.rb
@@ -25,7 +25,7 @@ after 'development:target_groups', 'development:timeline_event_types' do
 
   video_target = Target.create!(days_to_complete: 15, title: 'Submit video task', role: Target::ROLE_TEAM, timeline_event_type: team_update, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [fee_target])
 
-  interview_target = Target.create!(days_to_complete: 30, title: 'Attend Interview', role: Target::ROLE_TEAM, timeline_event_type: team_update, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [email_verification_target, coding_target, video_target])
+  interview_target = Target.create!(days_to_complete: 30, title: 'Attend Interview', role: Target::ROLE_TEAM, timeline_event_type: team_update, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [email_verification_target, coding_target, video_target], key: Target::KEY_ADMISSIONS_ATTEND_INTERVIEW)
 
   Target.create!(days_to_complete: 15, title: 'Pre-selection', role: Target::ROLE_TEAM, timeline_event_type: team_update, key: Target::KEY_ADMISSIONS_PRE_SELECTION, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [interview_target])
 

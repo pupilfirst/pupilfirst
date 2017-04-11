00700e2025a9ebe6670ece1734ebed8d5d3cb4fe
Hari Gopal <hari@sv.co>
2017-04-11 17:01:35 +0530

    Clean up after accepting invitation
    This updates the invitation acceptance process to clean up existing
    startup, if any. If the old startup has other founders, one of them
    is set as the new team lead. Otherwise, outstanding invitations for
    the startup are cleared, and the startup is deleted.

--- app/services/founders/accept_invitation_service.rb
+++ app/services/founders/accept_invitation_service.rb
@@ -9,13 +9,52 @@ module Founders
     end
 
     def execute
-      raise CannotAcceptInvitationException if @founder.startup&.level&.number&.positive?
+      raise CannotAcceptInvitationException if original_startup&.level&.number&.positive?
 
+      Founder.transaction do
+        accept_invitation
+        clean_up
+      end
+    end
+
+    private
+
+    def accept_invitation
       @founder.update!(
         startup: @founder.invited_startup,
         invited_startup: nil,
-        invitation_token: nil
+        invitation_token: nil,
+        startup_admin: false
       )
+    end
+
+    def clean_up
+      return if original_startup.blank?
+
+      if original_startup.founders.any?
+        # Make another founder the team lead.
+        another_founder = original_startup.founders.where.not(id: @founder.id).first
+        Founders::BecomeTeamLeadService.new(another_founder).execute
+      else
+        # There are no founders, so cancel all invitations, if any.
+        if original_startup.invited_founders.any?
+          original_startup.invited_founders.each do |invited_founder|
+            invited_founder.update!(
+              invited_startup: nil,
+              invitation_token: nil
+            )
+          end
+        end
+
+        # And delete the startup.
+        original_startup.destroy!
+      end
+    end
+
+    def original_startup
+      # Memoize nil startup as well.
+      return @original_startup if defined?(@original_startup)
+      @original_startup = @founder.startup
     end
   end
 end

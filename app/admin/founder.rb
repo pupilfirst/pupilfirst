18aeb96c20a323337f307202dd0553b9a0950212
Mahesh Krishna Kumar <mahesh@sv.co>
2017-04-13 22:04:14 +0530

    AA: Fix Founder edit form

--- app/admin/founder.rb
+++ app/admin/founder.rb
@@ -35,7 +35,9 @@ ActiveAdmin.register Founder do
   permit_params :name, :email, :remote_avatar_url, :avatar, :startup_id, :slug, :about, :slack_username, :born_on,
     :startup_admin, :communication_address, :identification_proof, :phone, :invitation_token, :college_id, :roll_number,
     :course, :semester, :year_of_graduation, :twitter_url, :linkedin_url, :personal_website_url, :blog_url,
-    :angel_co_url, :github_url, :behance_url, :gender, :skype_id, :exited, roles: [], tag_list: []
+    :angel_co_url, :github_url, :behance_url, :gender, :skype_id, :exited, :id_proof_number,
+    :id_proof_type, :parent_name, :permanent_address, :address_proof, :income_proof, :fee_payment_method,
+    :letter_from_parent, roles: [], tag_list: []
 
   batch_action :tag, form: proc { { tag: Founder.tag_counts_on(:tags).pluck(:name) } } do |ids, inputs|
     Founder.where(id: ids).each do |founder|
@@ -201,6 +203,7 @@ ActiveAdmin.register Founder do
       row :startup_admin
       row :about
       row :born_on
+      row :parent_name
       row :slack_username
       row :slack_user_id
       row 'Skype Id' do

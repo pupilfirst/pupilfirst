0204bf997aa66c6b9e44706cb12626da474d8746
Mahesh Krishna Kumar <mahesh@sv.co>
2017-04-17 12:36:30 +0530

    AA: Update Startup permit_params

--- app/admin/startup.rb
+++ app/admin/startup.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Startup do
   permit_params :product_name, :product_description, :legal_registered_name, :website, :email, :logo, :facebook_link,
     :twitter_link, :created_at, :updated_at, :dropped_out, :registration_type, :agreement_signed_at,
     :presentation_link, :product_video_link, :wireframe_link, :prototype_link, :slug, :batch_id, :level_id,
-    startup_category_ids: [], founder_ids: [], tag_list: []
+    :partnership_deed, :payment_reference, :agreements_verified, startup_category_ids: [], founder_ids: [], tag_list: []
 
   filter :product_name, as: :select
   filter :level
@@ -321,6 +321,7 @@ ActiveAdmin.register Startup do
 
       row :courier_name
       row :courier_number
+      row :payment_reference
     end
   end
 

bff89316092af4e0f00d1f67214c046658bb2d41
Mahesh Krishna Kumar <mahesh@sv.co>
2017-04-24 13:30:19 +0530

    Refactor WeeklyKarmaPointsCreateService

--- app/services/weekly_karma_points/create_service.rb
+++ app/services/weekly_karma_points/create_service.rb
@@ -3,47 +3,45 @@ module WeeklyKarmaPoints
   # leaderboards level-wise. The service will be executed by a task scheduled to run every Monday at 5:55 PM IST
   class CreateService
     def execute
-      create_weekly_karma_points
+      startups_with_points_last_week.each do |startup_id, points|
+        WeeklyKarmaPoint.create!(
+          week_starting_at: last_week_start_date,
+          startup_id: startup_id,
+          level_id: startup_levels[startup_id],
+          points: points
+        )
+      end
+
+      startups_inactive_last_week.each do |startup_id|
+        WeeklyKarmaPoint.create!(
+          week_starting_at: last_week_start_date,
+          startup_id: startup_id,
+          level_id: startup_levels[startup_id],
+          points: 0
+        )
+      end
     end
 
     private
 
-    def create_weekly_karma_points
-      if startups_with_points_last_week.present?
-
-        startups_with_points_last_week.each do |startup_points|
-          WeeklyKarmaPoint.create!(
-            week_starting_at: last_week_start_date,
-            startup_id: startup_points.first,
-            level_id: startup_levels[startup_points.first],
-            points: startup_points.second
-          )
-        end
-      end
-
-      if startups_inactive_last_week.present?
-
-        startups_inactive_last_week.each do |startup_id|
-          WeeklyKarmaPoint.create!(
-            week_starting_at: last_week_start_date,
-            startup_id: startup_id,
-            level_id: startup_levels[startup_id],
-            points: 0
-          )
-        end
-      end
-    end
-
     # Returns startups active last week along with total karma points earned for the week
     def startups_with_points_last_week
-      Startup.admitted.not_dropped_out.joins(:karma_points)
-        .where('karma_points.created_at > ?', last_week_start_date)
-        .where('karma_points.created_at < ?', last_week_end_date).group(:startup_id).sum(:points)
+      @startups_with_points_last_week ||= begin
+
+        Startup.admitted.not_dropped_out.joins(:karma_points)
+          .where('karma_points.created_at > ?', last_week_start_date)
+          .where('karma_points.created_at < ?', last_week_end_date).group(:startup_id).sum(:points)
+
+      end
     end
 
     # Returns id's of all startups that had some activity in the last 2 months
     def active_startups
-      Startup.admitted.not_dropped_out.joins(:karma_points).where('karma_points.created_at > ?', 2.months.ago).distinct.pluck(:id)
+      @active_startups ||= begin
+
+        Startup.admitted.not_dropped_out.joins(:karma_points).where('karma_points.created_at > ?', 2.months.ago).distinct.pluck(:id)
+
+      end
     end
 
     # Returns ids of startups that had no activity last week but are active startups in the platform
@@ -63,13 +61,13 @@ module WeeklyKarmaPoints
 
     # Returns a hash that maps active startups to its level.
     def startup_levels
-      @startup_levels = {}
+      @startup_levels ||= begin
 
-      active_startups.each do |startup_id|
-        @startup_levels[startup_id] = Startup.find(startup_id).level_id
+        active_startups.each_with_object({}) do |startup_id, hash|
+          hash[startup_id] = Startup.find(startup_id).level_id
+        end
+
       end
-
-      @startup_levels
     end
   end
 end

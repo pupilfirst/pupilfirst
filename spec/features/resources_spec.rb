b9f90f84a04dbe196614ac25603d3123a6166023
Abdul Jaleel <jaleel@sv.co>
2017-04-25 13:04:20 +0530

    Update resource related spec

--- spec/features/resources_spec.rb
+++ spec/features/resources_spec.rb
@@ -3,18 +3,17 @@ require 'rails_helper'
 # WARNING: The following tests run with Webmock disabled - i.e., URL calls are let through. Make sure you mock possible
 # requests unless you want to let them through. This is required for JS tests to work.
 feature 'Resources' do
+  let(:level_0) { create :level, :zero }
+  let(:level_1) { create :level, :one }
+  let(:level_2) { create :level, :two }
+
   let(:founder) { create :founder }
-  let(:startup) { create :startup }
+  let(:startup) { create :startup, level: level_1 }
 
-  let!(:public_resource_1) { create :resource }
-  let!(:public_resource_2) { create :resource }
-
-  let(:batch_1) { create :batch }
-  let(:batch_2) { create :batch }
-
-  let!(:approved_resource_for_all) { create :resource, share_status: Resource::SHARE_STATUS_APPROVED }
-  let!(:approved_resource_for_batch_1) { create :resource, share_status: Resource::SHARE_STATUS_APPROVED, batch: batch_1 }
-  let!(:approved_resource_for_batch_2) { create :resource, share_status: Resource::SHARE_STATUS_APPROVED, batch: batch_2 }
+  let!(:public_resource) { create :resource }
+  let!(:approved_resource) { create :resource, level: level_0 }
+  let!(:level_1_resource) { create :resource, level: level_1 }
+  let!(:level_2_resource) { create :resource, level: level_2 }
 
   before :all do
     PublicSlackTalk.mock = true
@@ -27,21 +26,21 @@ feature 'Resources' do
   scenario 'user visits resources page' do
     visit resources_path
 
-    expect(page).to have_selector('.resource', count: 2)
-    expect(page).to have_text(public_resource_1.title[0..10])
-    expect(page).to have_text(public_resource_2.title[0..10])
+    # user only sees the public resource
+    expect(page).to have_selector('.resource', count: 1)
+    expect(page).to have_text(public_resource.title[0..10])
   end
 
   scenario 'user visits restricted resource page' do
-    visit resource_path(approved_resource_for_all)
+    visit resource_path(approved_resource)
     expect(page).to have_text('Approved Startups get access to exclusive content produced by Faculty')
   end
 
   scenario 'user can download public resource' do
-    visit resource_path(public_resource_1)
+    visit resource_path(public_resource)
 
-    expect(page).to have_text(public_resource_1.title)
-    expect(page).to have_link('Download', download_resource_url(public_resource_1))
+    expect(page).to have_text(public_resource.title)
+    expect(page).to have_link('Download', download_resource_url(public_resource))
   end
 
   context 'With a video resource' do
@@ -79,16 +78,19 @@ feature 'Resources' do
     end
 
     context "Founder's startup is not approved" do
-      let(:startup) { create :startup, dropped_out: true }
+      before do
+        startup.update!(dropped_out: true)
+      end
 
       scenario 'Founder visits resources page' do
-        expect(page).to have_selector('.resource', count: 2)
-        expect(page).to have_text(public_resource_1.title[0..10])
-        expect(page).to have_text(public_resource_2.title[0..10])
+        visit resources_path
+        # only public resource is visible
+        expect(page).to have_selector('.resource', count: 1)
+        expect(page).to have_text(public_resource.title[0..10])
       end
 
       scenario 'Founder visits approved resource page' do
-        visit resource_path(approved_resource_for_all)
+        visit resource_path(approved_resource)
         # should be redirected to the index page
         expect(page).to have_text('This is just a small sample of resources available in the SV.CO Startup Library')
       end
@@ -97,38 +99,12 @@ feature 'Resources' do
     context "Founder's startup is approved" do
       scenario 'Founder visits resources page' do
         expect(page).to have_text('Please do not share these resources outside your founding team')
+
+        # public resources + resources upto his level should be shown
         expect(page).to have_selector('.resource', count: 3)
-        expect(page).to have_text(public_resource_1.title[0..10])
-        expect(page).to have_text(public_resource_2.title[0..10])
-        expect(page).to have_text(approved_resource_for_all.title[0..10])
-      end
-
-      context "Founder's startup is from batch 1" do
-        let(:startup) { create :startup, batch: batch_1 }
-
-        scenario 'Founder visits resources page' do
-          expect(page).to have_selector('.resource', count: 4)
-          expect(page).to have_text(public_resource_1.title[0..10])
-          expect(page).to have_text(public_resource_2.title[0..10])
-          expect(page).to have_text(approved_resource_for_all.title[0..10])
-          expect(page).to have_text(approved_resource_for_batch_1.title[0..10])
-        end
-
-        scenario 'Founder visits approved resource for batch 1 page' do
-          visit resource_path(approved_resource_for_batch_1)
-          expect(page).to have_text(approved_resource_for_batch_1.title)
-        end
-
-        scenario 'Founder visits approved resource for batch 2 page' do
-          visit resource_path(approved_resource_for_batch_2)
-          # should be redirected to the index page
-          expect(page).to have_text('You currently have access to exclusive resources')
-        end
-      end
-
-      scenario 'Founder visits approved resource page' do
-        visit resource_path(approved_resource_for_all)
-        expect(page).to have_text(approved_resource_for_all.title)
+        expect(page).to have_text(public_resource.title[0..10])
+        expect(page).to have_text(approved_resource.title[0..10])
+        expect(page).to have_text(level_1_resource.title[0..10])
       end
     end
   end

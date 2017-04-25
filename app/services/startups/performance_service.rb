0fca80cdf82c6a3175b7478c8c27432091637d32
Mahesh Krishna Kumar <mahesh@sv.co>
2017-04-25 16:15:40 +0530

    CleanUp Startups::Performanceservice

--- app/services/startups/performance_service.rb
+++ app/services/startups/performance_service.rb
@@ -1,127 +1,42 @@
 module Startups
   class PerformanceService
-    # returns leaderboard array of [startup, rank, points]
-    def leaderboard(batch, start_date: nil, end_date: nil)
-      @start_date = start_date || last_week_start_date
-      @end_date = end_date || last_week_end_date
-      @batch = batch
-
-      @leaderboard = Hash.new do |hash, key|
-        hash[key] = key.first.present_week_number.in?(1..24) ? rank_list : nil
-      end
-
-      @leaderboard[[@batch, @start_date, @end_date]]
-    end
-
-    # returns leaderboard array of [startup, rank, points, change_in_rank]
-    def leaderboard_with_change_in_rank(batch)
-      return nil unless batch.present_week_number.in?(1..24)
-
-      current_leaderboard = leaderboard(batch)
-
-      change_in_rank = if batch.present_week_number == 1
-        Array.new(batch.startups.count, 0)
-      else
-        start_date_last_week = @start_date - 7.days
-        end_date_last_week = @end_date - 7.days
-        previous_leaderboard = leaderboard(batch, start_date: start_date_last_week, end_date: end_date_last_week)
-
-        current_leaderboard.map do |startup_rank_points|
-          previous_rank = previous_leaderboard.detect { |startup, _rank, _points| startup == startup_rank_points[0] }&.second
-          previous_rank.present? ? (previous_rank - startup_rank_points[1]) : 0
-        end
-      end
-
-      current_leaderboard.each_with_index.map { |startup_rank_points, index| startup_rank_points + [change_in_rank[index]] }
-    end
-
     def leaderboard_rank(specified_startup)
-      leaderboard_for_run = leaderboard(specified_startup.batch)
+      leaderboard_for_run = Startups::LeaderboardService.new.leaderboard(specified_startup.level)
       return if leaderboard_for_run.nil?
       leaderboard_for_run.detect { |startup, _rank, _points| startup == specified_startup }&.second
     end
 
     def last_week_karma(specified_startup)
-      leaderboard_for_run = leaderboard(specified_startup.batch)
-      return if leaderboard_for_run.nil?
-      leaderboard_for_run.detect { |startup, _rank, _points| startup == specified_startup }&.third
+      weekly_karma_point = WeeklyKarmaPoint.where(startup_id: specified_startup.id, week_starting_at: last_week_start_date).first
+      return if weekly_karma_point.nil?
+      weekly_karma_point.points
     end
 
     # returns a relative performance measure for a startup as a %
     def relative_performance(startup)
-      @batch = startup.batch
+      @level = startup.level
       karma = last_week_karma(startup)
       relative_measure(karma)
     end
 
     private
 
-    def rank_list
-      ranks_with_points_for_run = ranks_with_points
-      ranks_with_points_for_run + ranks_without_points(ranks_with_points_for_run)
-    end
-
-    def ranks_with_points
-      @last_points = nil
-      @last_rank = nil
-      startups_sorted_by_points.each_with_index.map { |startup_points, index| rank(startup_points, index) }
-    end
-
-    def ranks_without_points(rank_with_points_for_run)
-      # counts the startups which has the same last rank in the rank-list with points
-      last_rank_count = rank_with_points_for_run.count { |x| x[1] == last_rank_with_points(rank_with_points_for_run) }
-      startups_without_points.each.map { |startup| [startup, last_rank_with_points(rank_with_points_for_run) + last_rank_count, 0] }
-    end
-
-    def last_rank_with_points(rank_with_points_for_run)
-      rank_with_points_for_run.present? ? rank_with_points_for_run[-1][1] : 0
-    end
-
-    def rank(startup_points, index)
-      startup_id, points = startup_points
-
-      if @last_points == points
-        rank = @last_rank
-      else
-        rank = index + 1
-        @last_rank = rank
-      end
-
-      @last_points = points
-
-      [Startup.find_by(id: startup_id), rank, points]
-    end
-
-    def startups_sorted_by_points
-      startups_with_points.group(:startup_id).sum(:points).sort_by { |_startup_id, points| points }.reverse
-    end
-
-    def startups_with_points
-      startups_in_batch.joins(:karma_points)
-        .where('karma_points.created_at > ?', @start_date)
-        .where('karma_points.created_at < ?', @end_date)
-    end
-
-    def startups_without_points
-      startups_in_batch.where.not(id: startups_with_points.select(:startup_id).distinct(:startup_id))
-    end
-
-    def startups_in_batch
-      Startup.not_dropped_out.where(batch: @batch)
-    end
-
     # Starts on the week before last's Monday 6 PM IST.
     def last_week_start_date
       DatesService.last_week_start_date
     end
 
-    # Ends on last week's Monday 6 PM IST.
-    def last_week_end_date
-      DatesService.last_week_end_date
+    def startup_points_hash
+      @startup_points_hash ||= startup_with_points.each_with_object({}) do |startup, startup_points|
+        startup_points[startup] = startup.weekly_karma_points.first.points
+      end.to_h.extend(DescriptiveStatistics)
     end
 
-    def startup_points_hash
-      @startup_points_hash ||= startups_sorted_by_points.to_h.extend(DescriptiveStatistics)
+    def startup_with_points
+      Startup.not_dropped_out.where(level: @level)
+        .includes(:weekly_karma_points)
+        .where(weekly_karma_points: { week_starting_at: last_week_start_date })
+        .where('weekly_karma_points.points > ?', 0)
     end
 
     def mean_karma

f3b44145a358d4c38cc9114fae777fcba281f4d0
Hari Gopal <hari@sv.co>
2017-05-04 12:05:32 +0530

    Cleanup / reorganize images in apply folder

--- config/initializers/app_config.rb
+++
@@ -1,3 +0,0 @@
-APP_CONSTANTS = {
-  certificate_background_base64: Base64.strict_encode64(open(File.expand_path(Rails.root.join('app', 'assets', 'images', 'apply', 'batch_application', 'coding-video-certificate.png'))).read)
-}.with_indifferent_access

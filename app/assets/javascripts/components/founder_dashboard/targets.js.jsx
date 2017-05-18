a2263051e12a0e4558fddb8ea1be16823cc9e780
Abdul Jaleel <jaleel@sv.co>
2017-05-18 14:16:01 +0530

    Bring back the action bar for all levels

--- app/assets/javascripts/components/founder_dashboard/targets.js.jsx
+++ app/assets/javascripts/components/founder_dashboard/targets.js.jsx
@@ -52,10 +52,8 @@ class FounderDashboardTargets extends React.Component {
                                              maxLevelNumber={ this.props.maxLevelNumber }/>
         }
 
-        { this.props.currentLevel !== 0 &&
         <FounderDashboardActionBar filter='targets' filterData={ this.filterData() } pickFilterCB={ this.pickFilter }
-          openTimelineBuilderCB={ this.props.openTimelineBuilderCB }/>
-        }
+          openTimelineBuilderCB={ this.props.openTimelineBuilderCB } currentLevel={ this.props.currentLevel }/>
 
         { this.targetCollections() }
       </div>

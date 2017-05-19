260d6d86c6c623808046b597a8a751180ae36464
Abdul Jaleel <jaleel@sv.co>
2017-05-19 11:28:56 +0530

    Add mocking ability for LevelZeroStageUpdateJob

--- app/jobs/intercom/level_zero_stage_update_job.rb
+++ app/jobs/intercom/level_zero_stage_update_job.rb
@@ -2,7 +2,17 @@ module Intercom
   class LevelZeroStageUpdateJob < ApplicationJob
     queue_as :default
 
+    class << self
+      attr_writer :mock
+
+      def mock?
+        defined?(@mock) ? @mock : Rails.env.test?
+      end
+    end
+
     def perform(founder, stage)
+      return if self.class.mock?
+
       intercom = IntercomClient.new
       user = intercom.find_or_create_user(email: founder.email, name: founder.name)
       intercom.update_user(user, stage: stage)

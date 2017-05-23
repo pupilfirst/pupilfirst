5f34a12c2dd609b6231e7f51c4ceccdbfb19f754
Hari Gopal <hari@sv.co>
2017-05-23 13:20:24 +0530

    DashboardDataService spec: remove debug code [skip ci]

--- spec/services/founders/dashboard_data_service_spec.rb
+++ spec/services/founders/dashboard_data_service_spec.rb
@@ -56,29 +56,20 @@ describe Founders::DashboardDataService do
   describe '#sessions' do
     context 'when the startup is in level 0' do
       it 'responds with all sessions in level 0' do
-        @expected_session_details = level_0_sessions.map { |session| session_details(session.reload) }
-        @expected_session_details = @expected_session_details.sort_by { |e| e['id'] }
-        @actual_sessions = subject.sessions.sort_by { |s| s['id'] }
-        expect(@actual_sessions).to eq(@expected_session_details)
+        expected_session_details = level_0_sessions.map { |session| session_details(session.reload) }
+        expected_session_details = expected_session_details.sort_by { |e| e['id'] }
+        actual_sessions = subject.sessions.sort_by { |s| s['id'] }
+        expect(actual_sessions).to eq(expected_session_details)
       end
     end
 
     context 'when the startup is in a level n > 1' do
       it 'responds with all sessions in level 1 to n' do
         startup.update!(level: level_2)
-        @expected_session_details = (level_1_sessions + level_2_sessions).map { |session| session_details(session.reload) }
-        @expected_session_details = @expected_session_details.sort_by { |e| e['id'] }
-        @actual_sessions = subject.sessions.sort_by { |s| s['id'] }
-        expect(@actual_sessions).to eq(@expected_session_details)
-      end
-    end
-
-    after do |example|
-      if example.exception
-        puts 'Excepted session details:'
-        p @expected_session_details.to_json
-        puts "\nActual session details:"
-        p @actual_sessions.to_json
+        expected_session_details = (level_1_sessions + level_2_sessions).map { |session| session_details(session.reload) }
+        expected_session_details = expected_session_details.sort_by { |e| e['id'] }
+        actual_sessions = subject.sessions.sort_by { |s| s['id'] }
+        expect(actual_sessions).to eq(expected_session_details)
       end
     end
   end

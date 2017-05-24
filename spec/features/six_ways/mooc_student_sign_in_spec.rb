5d7e59c95a8828f978251062a88cfc0ed5b983ef
Abdul Jaleel <jaleel@sv.co>
2017-05-24 13:18:58 +0530

    Update the MOOC student specs to meet college validations

--- spec/features/six_ways/mooc_student_sign_in_spec.rb
+++ spec/features/six_ways/mooc_student_sign_in_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 feature 'MoocStudent Sign In' do
   let!(:first_module) { create :course_module, :with_2_chapters, module_number: 1 }
   let!(:second_module) { create :course_module, :with_2_chapters, module_number: 2 }
-  let(:mooc_student) { create :mooc_student }
+  let(:college) { create :college }
+  let(:mooc_student) { create :mooc_student, college: college }
   let(:first_chapter_name) { first_module.module_chapters.find_by(chapter_number: 1).name }
 
   context 'User visits the sixways start page' do

4ae74bc492c78f2046436d0fd319de0f44af48c4
Hari Gopal <hari@sv.co>
2017-05-26 11:22:46 +0530

    Restrict Google Tag Manager to production

--- app/assets/javascripts/google_tag_manager.js
+++ app/assets/javascripts/google_tag_manager.js
@@ -1,26 +1,32 @@
-// if(/sv\.co/.test(window.location.hostname)) {
+if (/sv\.co/.test(window.location.hostname)) {
+  // Google Tag Manager container snippet.
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(), event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-N2LS39W');
 
-// Google Tag Manager container snippet.
-(function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({
-    'gtm.start': new Date().getTime(), event: 'gtm.js'
+  // Custom pageview event for turbolinks.
+  document.addEventListener('turbolinks:load', function (event) {
+    let url = event.data.url;
+
+    dataLayer.push({
+      'event': 'pageView',
+      'virtualUrl': url
+    });
   });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-  j.async = true;
-  j.src =
-    'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-  f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-N2LS39W');
-
-// Custom pageview event for turbolinks.
-document.addEventListener('turbolinks:load', function (event) {
-  let url = event.data.url;
-
-  dataLayer.push({
-    'event': 'pageView',
-    'virtualUrl': url
-  });
-});
-// }
+} else {
+  // Set up dataLayer for development / test.
+  window.dataLayer = {
+    push: function(pushContents) {
+      console.log(pushContents)
+    }
+  }
+}

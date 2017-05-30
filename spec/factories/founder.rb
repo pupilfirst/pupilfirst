8311ee84cb4a4344daa1f172c411e252c5722a6e
Hari Gopal <hari@sv.co>
2017-05-30 12:53:04 +0530

    Create Users::AnalyticsStateService to load user state
    This service can generate the data to be sent to other services for
    analytics requirements.

--- spec/factories/founder.rb
+++ spec/factories/founder.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :founder do
-    user
+    user { create :user, email: email }
     name { Faker::Name.name }
     email { Faker::Internet.email(name) }
     sequence(:phone) { |n| (9_876_543_210 + n).to_s }

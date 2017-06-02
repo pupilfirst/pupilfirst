899ce4579c2bcf6e2bb0424cc7b671f19cb2539f
Hari Gopal <hari@sv.co>
2017-06-02 11:38:32 +0530

    Specify class name as a string instead of passing class

--- app/models/public_slack_message.rb
+++ app/models/public_slack_message.rb
@@ -2,8 +2,8 @@ class PublicSlackMessage < ApplicationRecord
   belongs_to :founder
   has_one :karma_point, as: :source
 
-  has_many :reactions, class_name: PublicSlackMessage, foreign_key: 'reaction_to_id'
-  belongs_to :reaction_to, class_name: PublicSlackMessage
+  has_many :reactions, class_name: 'PublicSlackMessage', foreign_key: 'reaction_to_id'
+  belongs_to :reaction_to, class_name: 'PublicSlackMessage'
 
   scope :from_batch, ->(batch) { where(founder_id: Founder.in_batch(batch)) }
   scope :last_week, -> { where('created_at > ?', 1.week.ago.beginning_of_day) }

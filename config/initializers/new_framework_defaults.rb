d7f8653f02c542a53118c315cbcad078ce0009ff
Hari Gopal <hari@sv.co>
2017-06-02 16:54:41 +0530

    Remove halt_callback_chains_on_return_false setting
    This updates new_framework_defaults.rb, removing one deprecated setting
    and upgrading three. The only one that remains to be upgraded to
    Rails 5 standards is active_record.belongs_to_required_by_default

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -6,18 +6,15 @@
 #
 # Read the Rails 5.0 release notes for more info on each option.
 
-# Enable per-form CSRF tokens. Previous versions had false.
-Rails.application.config.action_controller.per_form_csrf_tokens = false
+# Enable per-form CSRF tokens. Previous versions had false. [UPGRADED]
+Rails.application.config.action_controller.per_form_csrf_tokens = true
 
-# Enable origin-checking CSRF mitigation. Previous versions had false.
-Rails.application.config.action_controller.forgery_protection_origin_check = false
+# Enable origin-checking CSRF mitigation. Previous versions had false. [UPGRADED]
+Rails.application.config.action_controller.forgery_protection_origin_check = true
 
 # Make Ruby 2.4 preserve the timezone of the receiver when calling `to_time`.
-# Previous versions had false.
-ActiveSupport.to_time_preserves_timezone = false
+# Previous versions had false. [UPGRADED]
+ActiveSupport.to_time_preserves_timezone = true
 
-# Require `belongs_to` associations by default. Previous versions had false.
+# Require `belongs_to` associations by default. Previous versions had false. [PENDING]
 Rails.application.config.active_record.belongs_to_required_by_default = false
-
-# Do not halt callback chains when a callback returns false. Previous versions had true.
-ActiveSupport.halt_callback_chains_on_return_false = true

53ae9ebeccedf44ec9ddf032d50126d76d062ab5
Hari Gopal <hari@sv.co>
2017-06-06 16:58:53 +0530

    Set carrierwave storage after setting fog credentials
    Attempting to fix deployment failure on Heroku using a comment from
    Github issues: https://goo.gl/v4l3rE

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS', # required
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # required
@@ -11,6 +10,7 @@ CarrierWave.configure do |config|
       path_style: true
     }
 
+    config.storage = :fog
     config.fog_public = true # optional, defaults to true
     config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' } # optional, defaults to {}
     config.cache_dir = Rails.root.join('tmp', 'uploads') # To let CarrierWave work on Heroku.

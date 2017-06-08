2768c35eef8d4c39b4eefc4f5b1852345644bfd2
Hari Gopal <hari@sv.co>
2017-06-08 11:34:45 +0530

    Fetch cookie domain from environment configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_sv_dot_co_session', domain: {
-  production: '.sv.co'
+  production: Rails.application.secrets.cookie_domain
 }.fetch(Rails.env.to_sym, :all)

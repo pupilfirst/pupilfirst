0f9b02d61cc653e5fa79e7aa7215c7e7ea1f24d2
Mahesh Krishna Kumar <mahesh@sv.co>
2017-06-12 15:19:34 +0530

    OneOff service to update timeline_event in StartupFeedback [skip ci]

--- app/services/one_off/startup_feedback_timeline_event_update_service.rb
+++ app/services/one_off/startup_feedback_timeline_event_update_service.rb
@@ -1,11 +1,28 @@
 module OneOff
   class StartupFeedbackTimelineEventUpdateService
+    include Loggable
+
+    REGEX_TIMELINE_EVENT_URL = %r{startups/.*event-(?<event_id>[\d]+)}
+
     def execute
       all_startup_feedback = StartupFeedback.all
+      updated_records_count = 0
       all_startup_feedback.map do |startup_feedback|
-        next if startup_feedback.timeline_event.blank?
-        startup_feedback.update!(timeline_event_id: startup_feedback.timeline_event.id)
+        next if startup_feedback.timeline_event.present?
+        next if startup_feedback.reference_url.match(REGEX_TIMELINE_EVENT_URL).blank?
+        timeline_event_id = timeline_event_id(startup_feedback.reference_url)
+        next if TimelineEvent.where(id: timeline_event_id).blank?
+
+        startup_feedback.update!(timeline_event_id: timeline_event_id)
+        updated_records_count += 1
+        log "#{updated_records_count} records updated!"
       end
+    end
+
+    private
+
+    def timeline_event_id(reference_url)
+      reference_url.match(REGEX_TIMELINE_EVENT_URL)[:event_id]
     end
   end
 end

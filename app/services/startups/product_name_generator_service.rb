5ce5891ee1a5fe204df604d40aec9887c8e4ebfb
Abdul Jaleel <jaleel@sv.co>
2017-06-13 13:49:56 +0530

    Extract the pool for startup names into a yml

--- app/services/startups/product_name_generator_service.rb
+++ app/services/startups/product_name_generator_service.rb
@@ -1,13 +1,17 @@
 module Startups
   class ProductNameGeneratorService
-    COLORS = %w[blue brown crimson gold gray green indigo lilac magenta maroon orange pink purple red saffron silver turquoise violet yellow].freeze
-
-    SCIENTISTS = %w[ali aryabhata ashtekar bhabha bell bohr bose copernicus curie darwin dhawan dirac edison einstein faraday fleming galilei
-                    gauss halayudha hawking hertz hubble kalam kepler lovelace madhava maxwell mendeleev newton pasteur pauling planck raman
-                    ramachandran ramakrishnan ramanujan sagan sarabhai tesla volta watt].freeze
-
     def fun_name
-      "#{COLORS.sample} #{SCIENTISTS.sample}".titleize
+      "#{pool['color'].sample} #{pool['scientist'].sample}".titleize
+    end
+
+    def colors
+      pool['color']
+    end
+
+    private
+
+    def pool
+      @pool ||= YAML.load_file('startup_name_pool.yml')
     end
   end
 end

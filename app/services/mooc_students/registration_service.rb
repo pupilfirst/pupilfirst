69543fd42bd143c69d4bd188d20736ed4d9898ee
Mahesh Krishna Kumar <mahesh@sv.co>
2017-07-05 11:27:23 +0530

    Minor refactor to MoocStudentsRegistrationService

--- app/services/mooc_students/registration_service.rb
+++ app/services/mooc_students/registration_service.rb
@@ -16,14 +16,7 @@ module MoocStudents
 
         # Find or initialize the user entry.
         mooc_student = MoocStudent.where(user_id: user.id).first_or_create!(
-          name: @attributes[:name],
-          email: @attributes[:email],
-          phone: @attributes[:phone],
-          gender: @attributes[:gender],
-          college_id: @attributes[:college_id],
-          college_text: @attributes[:college_text],
-          state: @attributes[:state],
-          semester: @attributes[:semester]
+          @attributes.slice(:name, :email, :phone, :gender, :college_id, :college_text, :state, :semester)
         )
 
         # Send the user a login email, welcoming him / her to SixWays.

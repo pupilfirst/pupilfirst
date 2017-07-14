87691ce08fad8e58ce294bb27ec2aa14fcabd1b9
Abdul Jaleel <jaleel@sv.co>
2017-07-14 11:41:54 +0530

    Limit the number of founders addable to 4

--- app/assets/javascripts/components/founders_form.jsx
+++ app/assets/javascripts/components/founders_form.jsx
@@ -70,7 +70,7 @@ class FoundersForm extends React.Component {
   }
 
   addFounderAllowed() {
-    return this.state.founders.length < 6
+    return this.state.founders.length < 4
   }
 
   hasErrors() {

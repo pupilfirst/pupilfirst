69a0c10a8b77da6e9f082b19058fa5ce1472b8ff
Abdul Jaleel <jaleel@sv.co>
2017-08-01 13:04:03 +0530

    Fix target seeds to have assigner

--- db/seeds/development/targets.seeds.rb
+++ db/seeds/development/targets.seeds.rb
@@ -1,6 +1,6 @@
 require_relative 'helper'
 
-after 'development:target_groups', 'development:timeline_event_types' do
+after 'development:target_groups', 'development:timeline_event_types', 'development:faculty' do
   puts 'Seeding targets'
 
   def paragraph
@@ -8,18 +8,20 @@ after 'development:target_groups', 'development:timeline_event_types' do
   end
 
   team_update = TimelineEventType.find_by(key: 'team_update')
+  faculty_1 = Faculty.first
+  faculty_2 = Faculty.second
 
   # Compulsory Level 0 targets.
   level_0_milestone_group = Level.find_by(number: 0).target_groups.find_by(milestone: true)
 
   # Screening target.
-  screening_target = Target.create!(days_to_complete: 1, title: 'Go through Screening', role: Target::ROLE_TEAM, timeline_event_type: team_update, submittability: Target::SUBMITTABILITY_SUBMITTABLE_ONCE, link_to_complete: '/admissions/screening', key: Target::KEY_ADMISSIONS_SCREENING, target_group: level_0_milestone_group, description: paragraph)
+  screening_target = Target.create!(days_to_complete: 1, title: 'Go through Screening', role: Target::ROLE_TEAM, timeline_event_type: team_update, submittability: Target::SUBMITTABILITY_SUBMITTABLE_ONCE, link_to_complete: '/admissions/screening', key: Target::KEY_ADMISSIONS_SCREENING, target_group: level_0_milestone_group, description: paragraph, assigner: faculty_1)
 
   # Cofounder addition target.
-  cofounder_addition_target = Target.create!(days_to_complete: 1, title: 'Add co-founders', role: Target::ROLE_TEAM, timeline_event_type: team_update, link_to_complete: '/admissions/founders', key: Target::KEY_ADMISSIONS_COFOUNDER_ADDITION, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [screening_target])
+  cofounder_addition_target = Target.create!(days_to_complete: 1, title: 'Add co-founders', role: Target::ROLE_TEAM, timeline_event_type: team_update, link_to_complete: '/admissions/founders', key: Target::KEY_ADMISSIONS_COFOUNDER_ADDITION, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [screening_target], assigner: faculty_1)
 
   # Fee payment target.
-  Target.create!(days_to_complete: 1, title: 'Pay Admission Fee', role: Target::ROLE_TEAM, timeline_event_type: team_update, submittability: Target::SUBMITTABILITY_SUBMITTABLE_ONCE, link_to_complete: '/admissions/fee', key: Target::KEY_ADMISSIONS_FEE_PAYMENT, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [cofounder_addition_target])
+  Target.create!(days_to_complete: 1, title: 'Pay Admission Fee', role: Target::ROLE_TEAM, timeline_event_type: team_update, submittability: Target::SUBMITTABILITY_SUBMITTABLE_ONCE, link_to_complete: '/admissions/fee', key: Target::KEY_ADMISSIONS_FEE_PAYMENT, target_group: level_0_milestone_group, description: paragraph, prerequisite_targets: [cofounder_addition_target], assigner: faculty_2)
 
   # Random targets, session and chores for every level
   (1..4).each do |level_number|
@@ -28,23 +30,23 @@ after 'development:target_groups', 'development:timeline_event_types' do
     # 3 normal targets per target_group
     level.target_groups.each do |target_group|
       3.times do
-        target_group.targets.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, target_group: target_group, description: paragraph)
+        target_group.targets.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, target_group: target_group, description: paragraph, assigner: faculty_1)
       end
     end
 
     # 3 chores per level
     3.times do
-      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, chore: true, level: level, description: paragraph)
+      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, chore: true, level: level, description: paragraph, assigner: faculty_2)
     end
 
     # 3 upcoming sessions per level
     3.times do
-      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, session_at: rand(4.weeks).seconds.from_now, level: level, description: paragraph)
+      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, session_at: rand(4.weeks).seconds.from_now, level: level, description: paragraph, assigner: faculty_1)
     end
 
     # 3 past sessions per level
     3.times do
-      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, session_at: rand(4.weeks).seconds.ago, level: level, description: paragraph)
+      Target.create!(days_to_complete: [7, 10, 14].sample, title: Faker::Lorem.sentence, role: Target.valid_roles.sample, timeline_event_type: TimelineEventType.all.sample, session_at: rand(4.weeks).seconds.ago, level: level, description: paragraph, assigner: faculty_2)
     end
   end
 end

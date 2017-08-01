c50297cf1589d286a29095ce650b8735eb5c02c0
Hari Gopal <hari@sv.co>
2017-08-01 13:22:25 +0530

    Fix crash in startup feedback mailer for Level 1+ startups

--- spec/mailers/previews/startup_mailer_preview.rb
+++ spec/mailers/previews/startup_mailer_preview.rb
@@ -1,6 +1,23 @@
 class StartupMailerPreview < ActionMailer::Preview
   def feedback_as_email
-    startup_feedback = StartupFeedback.first
+    startup_feedback = StartupFeedback.new(
+      id: 1,
+      feedback: "This is the feedback text.\nIt is multi-line.",
+      timeline_event: TimelineEvent.new(
+        id: 2,
+        timeline_event_type: TimelineEventType.new(title: 'Timeline Event Type Title'),
+        startup: Startup.new(slug: 'test-startup')
+        # target: Target.new
+      ),
+      faculty: Faculty.new(
+        name: 'C V Raman'
+      ),
+      startup: Startup.new(
+        id: 3,
+        level: Level.new(number: 1)
+      )
+    )
+
     StartupMailer.feedback_as_email(startup_feedback)
   end
 

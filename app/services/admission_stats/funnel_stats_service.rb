dd9a1aa57cba86d0612d3d680e856b54c5ef89ff
Hari Gopal <hari@sv.co>
2017-08-08 12:01:08 +0530

    Add cumulative stats to admissions dashboard and slack notification

--- app/services/admission_stats/funnel_stats_service.rb
+++ app/services/admission_stats/funnel_stats_service.rb
@@ -1,9 +1,11 @@
 module AdmissionStats
   class FunnelStatsService
-    def initialize(start_date, end_date)
-      start_date = start_date.present? ? Date.parse(start_date).beginning_of_day : 1.day.ago.beginning_of_day
-      end_date = end_date.present? ? Date.parse(end_date).end_of_day : 1.day.ago.end_of_day
-      @date_range = start_date..end_date
+    # @param start_time [DateTime, String] (Optional) Default to beginning of yesterday.
+    # @param end_time [DateTime, String] (Optional) Defaults to end of yesterday.
+    def initialize(start_time = 1.day.ago.beginning_of_day, end_time = 1.day.ago.end_of_day)
+      start_time = Date.parse(start_time).beginning_of_day if start_time.is_a?(String)
+      end_time = Date.parse(end_time).end_of_day if end_time.is_a?(String)
+      @date_range = start_time..end_time
     end
 
     def load

fd2b08484102c3c392a272be83b478083bb32128
Hari Gopal <hari@sv.co>
2017-08-10 12:12:22 +0530

    Update seeding of avengers startup to demo iteration-related target statuses [skip ci]

--- db/seeds/development/payments.seeds.rb
+++ db/seeds/development/payments.seeds.rb
@@ -3,26 +3,31 @@ require_relative 'helper'
 after 'development:startups' do
   puts 'Seeding payments'
 
-  # A live subscription for 'Super Startup'
   super_startup = Startup.find_by(legal_registered_name: 'SuperTech Ltd')
-  active_payment = Payment.create!(
-    startup: super_startup,
+  avengers_startup = Startup.find_by(legal_registered_name: 'The Avengers')
+
+  # A live subscription for 'Super Startup' and 'The Avengers'
+  super_startup.payments.create!(
     founder: super_startup.admin,
     amount: super_startup.fee,
     paid_at: 1.week.ago,
     billing_start_at: 1.week.ago,
     billing_end_at: 3.weeks.from_now
   )
-  super_startup.payments << active_payment
 
-  # A pending payment for 'The Avengers'
-  avengers_startup = Startup.find_by(legal_registered_name: 'The Avengers')
-  pending_payment = Payment.create!(
-    startup: avengers_startup,
+  avengers_startup.payments.create!(
     founder: avengers_startup.admin,
     amount: avengers_startup.fee,
-    billing_start_at: 1.day.ago,
-    billing_end_at: 29.days.from_now
+    paid_at: 28.days.ago,
+    billing_start_at: 28.days.ago,
+    billing_end_at: 3.days.from_now
   )
-  avengers_startup.payments << pending_payment
+
+  # ...plus a pending payment for 'The Avengers'
+  avengers_startup.payments.create!(
+    founder: avengers_startup.admin,
+    amount: avengers_startup.fee,
+    billing_start_at: 3.days.from_now,
+    billing_end_at: 33.days.from_now
+  )
 end

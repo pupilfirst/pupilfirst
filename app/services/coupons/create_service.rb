2fef4335cf936d3e18a17d28d278408bceca05c4
Abdul Jaleel <jaleel@sv.co>
2017-08-18 14:41:07 +0530

    Ensure coupon codes generated are unique

--- app/services/coupons/create_service.rb
+++ app/services/coupons/create_service.rb
@@ -6,13 +6,18 @@ module Coupons
 
     def generate_referral(startup)
       Coupon.create!(
-        code: rand(36**6).to_s(36),
+        code: unique_code,
         coupon_type: Coupon::TYPE_REFERRAL,
         user_extension_days: USER_EXTENSION_DEFAULT,
         referrer_extension_days: REFERRER_EXTENSION_DEFAULT,
         redeem_limit: REDEEM_LIMIT_DEFAULT,
         referrer_startup: startup
       )
+    end
+
+    def unique_code
+      code = rand(36**6).to_s(36)
+      Coupon.exists?(code: code) ? unique_code : code
     end
   end
 end

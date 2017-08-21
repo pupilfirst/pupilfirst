add4f0ee116e5562d349a0cae18baf49fc48d776
Abdul Jaleel <jaleel@sv.co>
2017-08-21 15:53:22 +0530

    Admission Dashboard: Pie charts should not be restricted to Level 0 founders

--- app/services/admission_stats/applicants_by_location_service.rb
+++ app/services/admission_stats/applicants_by_location_service.rb
@@ -1,8 +1,7 @@
 module AdmissionStats
   class ApplicantsByLocationService
     def load
-      level_zero_founders = Founder.level_zero
-      location_wise_split = level_zero_founders.joins(:college).select('colleges.state_id').group('colleges.state_id').count
+      location_wise_split = Founder.joins(:college).select('colleges.state_id').group('colleges.state_id').count
       location_wise_split.map { |state_id, count| [State.find(state_id).name, count] }.to_h.to_json
     end
   end
--- app/services/admission_stats/applicants_by_reference_service.rb
+++ app/services/admission_stats/applicants_by_reference_service.rb
@@ -4,8 +4,8 @@ module AdmissionStats
       startups = stage.present? ? Startup.where(admission_stage: stage) : Startup.all
 
       named_references = Founder.reference_sources - ['Other (Please Specify)']
-      founders_with_named_reference = Founder.level_zero.merge(startups).where(reference: named_references).group(:reference).count
-      founders_with_other_reference = Founder.level_zero.merge(startups).where.not(reference: named_references).count
+      founders_with_named_reference = Founder.where(reference: named_references, startup: startups).group(:reference).count
+      founders_with_other_reference = Founder.where(startup: startups).where.not(reference: named_references).count
 
       stats = if founders_with_other_reference.positive?
         founders_with_named_reference.merge('Other' => founders_with_other_reference)

1505cc6e3692301bb821c7f3827ccc1f5a891136
Abdul Jaleel <jaleel@sv.co>
2017-09-19 15:29:50 +0530

    Invite founders back to Slack when subscription is renewed

--- app/services/founders/post_payment_service.rb
+++ app/services/founders/post_payment_service.rb
@@ -2,6 +2,7 @@ module Founders
   class PostPaymentService
     def initialize(payment)
       @payment = payment
+      @startup = payment.startup
     end
 
     def execute
@@ -13,6 +14,10 @@ module Founders
       # Otherwise, the billing period will have to be updated.
       payment_duration = @payment.billing_end_at - @payment.billing_start_at
       @payment.update!(billing_start_at: Time.zone.now, billing_end_at: Time.zone.now + payment_duration)
+
+      # and the founders invited back to all channels on Slack.
+      return if @startup.blank? || @startup.level_zero?
+      @startup.founders.not_exited.each { |founder| Founders::InviteToSlackChannelsJob.perform_later(founder) }
     end
   end
 end
--- spec/services/founders/post_payment_service_spec.rb
+++ spec/services/founders/post_payment_service_spec.rb
@@ -6,8 +6,12 @@ describe Founders::PostPaymentService do
   describe '#execute' do
     context 'when billing start date for the payment has passed' do
       let(:payment) { create :payment, :paid, billing_start_at: 10.days.ago, billing_end_at: 20.days.from_now }
+      let(:mock_invite_service) { instance_double Founders::InviteToSlackChannelsService }
 
-      it 'updates the billing dates for payment' do
+      it 'updates the billing dates for payment and invites the founders back to Slack' do
+        founder_count = payment.startup.founders.count
+        expect(Founders::InviteToSlackChannelsService).to receive(:new).exactly(founder_count).times.and_return(mock_invite_service)
+        expect(mock_invite_service).to receive(:execute).exactly(founder_count).times
         expect do
           subject.execute
         end.to(change { payment.reload.billing_start_at }.and(change { payment.reload.billing_end_at }))

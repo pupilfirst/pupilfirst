35708cd56a973740944d4f26e873ef71f26dd5ad
Hari Gopal <hari@sv.co>
2017-09-22 12:37:13 +0530

    Extract test for founder invitation to own example

--- spec/services/founders/post_payment_service_spec.rb
+++ spec/services/founders/post_payment_service_spec.rb
@@ -3,15 +3,26 @@ require 'rails_helper'
 describe Founders::PostPaymentService do
   subject { described_class.new(payment) }
 
+  let(:mock_invite_service) { instance_double Founders::InviteToSlackChannelsService, execute: nil }
+
+  before do
+    allow(Founders::InviteToSlackChannelsService).to receive(:new).and_return(mock_invite_service)
+  end
+
   describe '#execute' do
+    let(:payment) { create :payment, :paid }
+
+    it 'invites founders back to Slack' do
+      founders_count = payment.startup.founders.count
+      expect(Founders::InviteToSlackChannelsService).to receive(:new).exactly(founders_count).times
+      expect(mock_invite_service).to receive(:execute).exactly(founders_count).times
+      subject.execute
+    end
+
     context 'when billing start date for the payment has passed' do
       let(:payment) { create :payment, :paid, billing_start_at: 10.days.ago }
-      let(:mock_invite_service) { instance_double Founders::InviteToSlackChannelsService }
 
-      it 'updates the billing dates for payment and invites the founders back to Slack' do
-        founder_count = payment.startup.founders.count
-        expect(Founders::InviteToSlackChannelsService).to receive(:new).exactly(founder_count).times.and_return(mock_invite_service)
-        expect(mock_invite_service).to receive(:execute).exactly(founder_count).times
+      it 'updates the billing dates for payment' do
         expect do
           subject.execute
         end.to(change { payment.reload.billing_start_at }.and(change { payment.reload.billing_end_at }))
@@ -21,10 +32,12 @@ describe Founders::PostPaymentService do
     context 'when the billing start date for the payment is in the future' do
       let(:payment) { create :payment, :paid, billing_start_at: 2.days.from_now }
 
-      it 'does nothing' do
+      it 'sets only the end date according to payment period' do
         expect do
           subject.execute
         end.to(not_change { payment.reload.billing_start_at }.and(change { payment.reload.billing_end_at }))
+
+        expect(payment.billing_end_at.beginning_of_minute).to eq((payment.billing_start_at + 1.month).beginning_of_minute)
       end
     end
 

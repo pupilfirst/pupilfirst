dcf55c95d72a966ffeeed7674766c45d066ac768
Abdul Jaleel <jaleel@sv.co>
2017-10-05 21:29:04 +0530

    Prevent players from re-registering

--- app/forms/tech_hunt_sign_up_form.rb
+++ app/forms/tech_hunt_sign_up_form.rb
@@ -3,6 +3,12 @@ class TechHuntSignUpForm < Reform::Form
   property :email, virtual: true, validates: { presence: true, length: { maximum: 250 }, email: true }
   property :showcase_link,  validates: { url: true, allow_blank: true }
 
+  validate :player_must_not_exist
+
+  def player_must_not_exist
+    errors.add(:email, 'player already registered') if User.find_by(email: email)&.player.present?
+  end
+
   def save
     Player.transaction do
       user = User.with_email(email)

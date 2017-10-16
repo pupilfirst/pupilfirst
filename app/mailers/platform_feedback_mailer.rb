a5612772c9b8d94359bea0636387d62639124e51
Hari Gopal <hari@sv.co>
2017-10-16 12:29:27 +0530

    Do not set reply-to for platform feedback notification mail
    This removes the reply-to to avoid sending mails to the founder
    when help@sv.co is the one generating the system mail, meant for
    internal consumption.

--- app/mailers/platform_feedback_mailer.rb
+++ app/mailers/platform_feedback_mailer.rb
@@ -4,8 +4,7 @@ class PlatformFeedbackMailer < ApplicationMailer
 
     mail(
       to: 'help@sv.co',
-      subject: "Plaftorm Feedback (#{@platform_feedback.feedback_type}) by #{@platform_feedback.founder.fullname}",
-      reply_to: @platform_feedback.founder.email
+      subject: "Plaftorm Feedback (#{@platform_feedback.feedback_type}) by #{@platform_feedback.founder.fullname}"
     )
   end
 end

2e093d9f1fb612ef47e81a8df9efe936efaf1e67
Hari Gopal <hari@sv.co>
2017-10-20 13:37:34 +0530

    Organize startups.show strings

--- app/helpers/startups_helper.rb
+++ app/helpers/startups_helper.rb
@@ -29,25 +29,25 @@ module StartupsHelper
 
   def needs_improvement_tooltip_for_founder(event)
     if event.improved_timeline_event.present?
-      I18n.t('startup.show.timeline_cards.improved_later.tooltip_text.founder')
+      I18n.t('startups.show.timeline_cards.improved_later.tooltip_text.founder')
     else
-      I18n.t('startup.show.timeline_cards.needs_imprvement.tooltip_text.founder')
+      I18n.t('startups.show.timeline_cards.needs_improvement.tooltip_text.founder')
     end
   end
 
   def needs_improvement_tooltip_for_public(event)
     if event.improved_timeline_event.present?
-      I18n.t('startup.show.timeline_cards.improved_later.tooltip_text.public')
+      I18n.t('startups.show.timeline_cards.improved_later.tooltip_text.public')
     else
-      I18n.t('startup.show.timeline_cards.needs_imprvement.tooltip_text.public')
+      I18n.t('startups.show.timeline_cards.needs_improvement.tooltip_text.public')
     end
   end
 
   def needs_improvement_status_text(event)
     if event.improved_timeline_event.present?
-      I18n.t('startup.show.timeline_cards.improved_later.status_text')
+      I18n.t('startups.show.timeline_cards.improved_later.status_text')
     else
-      I18n.t('startup.show.timeline_cards.needs_imprvement.status_text')
+      I18n.t('startups.show.timeline_cards.needs_improvement.status_text')
     end
   end
 end

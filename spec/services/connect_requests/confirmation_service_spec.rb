a5a5228b3aadc7fd0c9eb7298d399347919b09c1
Mahesh Krishna Kumar <mahesh@sv.co>
2017-10-24 14:47:26 +0530

    Create spec for ConnectRequests::ConfirmationService

---
+++ spec/services/connect_requests/confirmation_service_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe ConnectRequests::ConfirmationService do
+  subject { described_class.new(connect_request) }
+
+  let!(:connect_request) { create :connect_request, status: ConnectRequest::STATUS_REQUESTED }
+
+  describe '#execute' do
+    let(:faculty_mailer) { double FacultyMailer }
+    let(:startup_mailer) { double StartupMailer }
+    let(:mock_calendar_service) { instance_double ConnectRequests::CreateCalendarEventService, execute: nil }
+
+    it 'sends mail for confirmed, saves confirmation time, sets up google calendar event and creates rating/reminder jobs' do
+      expect(FacultyMailer).to receive(:connect_request_confirmed).with(connect_request).and_return(faculty_mailer)
+      expect(StartupMailer).to receive(:connect_request_confirmed).with(connect_request).and_return(startup_mailer)
+      expect(faculty_mailer).to receive(:deliver_later)
+      expect(startup_mailer).to receive(:deliver_later)
+
+      expect(ConnectRequests::CreateCalendarEventService).to receive(:new).with(connect_request).and_return(mock_calendar_service)
+
+      subject.execute
+
+      expect(FacultyConnectSessionRatingJob).to have_been_enqueued.with(connect_request.id).at(connect_request.connect_slot.slot_at + 45.minutes)
+      expect(FacultyConnectSessionReminderJob).to have_been_enqueued.with(connect_request.id).at(connect_request.connect_slot.slot_at - 30.minutes)
+    end
+  end
+end
+f

43c237c57eb3e01e2cbb016b1e6e4aec31fe0f3e
Abdul Jaleel <jaleel@sv.co>
2017-10-25 16:27:56 +0530

    Remove an insensible validation from AnswerOption

--- app/models/answer_option.rb
+++ app/models/answer_option.rb
@@ -2,5 +2,4 @@ class AnswerOption < ApplicationRecord
   belongs_to :quiz_question, polymorphic: true
 
   validates :value, presence: true
-  validates :quiz_question_id, uniqueness: { scope: [:quiz_question_type] }
 end

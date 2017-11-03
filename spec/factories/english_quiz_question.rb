102260be71bdab8f8d9dc448bc513d2c9c96c33b
Abdul Jaleel <jaleel@sv.co>
2017-11-03 17:12:29 +0530

    Fix the broken EnglishQuizQuestion factory

--- spec/factories/english_quiz_question.rb
+++ spec/factories/english_quiz_question.rb
@@ -2,11 +2,10 @@ FactoryGirl.define do
   factory :english_quiz_question do
     question { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'uploads', 'resources', 'pdf-thumbnail.png')) }
     explanation { Faker::Lorem.words(12).join ' ' }
+    answer_options { build_list :answer_option, 4 }
 
-    # Add four answer options ...
+    # Mark a random answer as the correct one.
     after(:create) do |english_quiz_question|
-      create_list :answer_option, 4, quiz_question: english_quiz_question
-      # ... and mark a random one as the correct answer.
       english_quiz_question.answer_options.sample.update!(correct_answer: true)
     end
   end

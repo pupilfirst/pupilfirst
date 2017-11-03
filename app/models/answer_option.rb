3e7e0e4fa21c2ba8fb3a91a7382a2a51dde42665
Abdul Jaleel <jaleel@sv.co>
2017-11-03 16:23:27 +0530

    Better associations to make nested attributes work

--- app/models/answer_option.rb
+++ app/models/answer_option.rb
@@ -1,5 +1,5 @@
 class AnswerOption < ApplicationRecord
-  belongs_to :quiz_question, polymorphic: true
+  belongs_to :quiz_question, polymorphic: true, inverse_of: :answer_options
 
   validates :value, presence: true
 end

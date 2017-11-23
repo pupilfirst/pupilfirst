043e6526b27e01ee334855d93ce185240365d547
Abdul Jaleel <jaleel@sv.co>
2017-11-23 11:49:58 +0530

    Display a table of previous payments

--- app/presenters/startups/billing_presenter.rb
+++ app/presenters/startups/billing_presenter.rb
@@ -20,6 +20,18 @@ module Startups
       'subscription-notice__text--danger' if subscription_end_date < 5.days.from_now
     end
 
+    def payment_history
+      @startup.payments.paid.order('billing_end_at DESC').map do |payment|
+        {
+          date: payment.paid_at&.strftime('%d/%m/%Y'),
+          start: payment.billing_start_at&.strftime('%b %d, %Y'),
+          end: payment.billing_end_at&.strftime('%b %d, %Y'),
+          amount: (payment.amount.present? ? '&#8377;' + payment.amount.to_s : 'N/A').html_safe,
+          payee: payment.founder&.name
+        }
+      end
+    end
+
     private
 
     def subscription_end_date

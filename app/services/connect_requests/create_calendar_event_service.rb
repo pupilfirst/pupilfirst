e697ee33db50b119eb3dc0513f95be09f732f935
Abdul Jaleel <jaleel@sv.co>
2017-12-06 13:27:57 +0530

    Auto-send invites when creating the Google calendar event

--- app/services/connect_requests/create_calendar_event_service.rb
+++ app/services/connect_requests/create_calendar_event_service.rb
@@ -17,6 +17,7 @@ module ConnectRequests
         e.guests_can_invite_others = false
         e.guests_can_see_other_guests = false
         e.location = @connect_request.meeting_link
+        e.send_notifications = true
 
         # Default visibility should be sufficient since it equals calendar's setting.
         # e.visibility = 'public'

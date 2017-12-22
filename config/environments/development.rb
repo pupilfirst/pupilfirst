d1203f35b74b864e6dbcc7f7db89605361370fe4
Hari Gopal <hari@sv.co>
2017-12-22 13:03:12 +0530

    Add a javascript pack file for all admin pages
    This common pack file enables react-rails' UJS to let its component
    rendering function work in admin pages. This commit also configures
    the setup to load only files in the app/javascripts/admin/components
    path, thus keeping admin and non-admin component autoloading separate.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
--- config/initializers/active_admin_extensions.rb
+++ config/initializers/active_admin_extensions.rb
@@ -1,3 +1,13 @@
 require_relative '../../lib/active_admin/current_admin_user'
 
 ActiveAdmin::BaseController.send(:include, ActiveAdmin::CurrentAdminUser)
+
+# Customize <head> for all active admin pages.
+ActiveAdmin::Views::Pages::Base.class_eval do
+  alias_method :original_build_active_admin_head, :build_active_admin_head
+
+  def build_active_admin_head(*args, &block)
+    original_build_active_admin_head(*args, &block)
+    within(@head) { render '/custom_active_admin_head' }
+  end
+end

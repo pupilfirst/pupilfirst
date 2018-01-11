d2de6013b0e12f74d0c1c3d4aad3e619dff16708
Mahesh Krishna Kumar <mahesh@sv.co>
2018-01-11 16:07:43 +0530

    Create specs for admission screening stage

---
+++ spec/controllers/admissions_controller_spec.rb
@@ -0,0 +1,98 @@
+require 'rails_helper'
+
+describe AdmissionsController do
+  let(:level_0) { create :level, :zero }
+  let(:level_0_targets) { create :target_group, level: level_0 }
+  let!(:screening_target) { create :target, :admissions_screening, target_group: level_0_targets }
+  let!(:tet_team_update) { create :timeline_event_type, :team_update }
+
+  let(:founder) { create :founder }
+  let(:startup) { create :startup, level: level_0 }
+
+  let(:typeform_webhook_response) do
+    {
+      'event_id' => 'hQJi65uTRz',
+      'event_type' => 'form_response',
+      'form_response' => {
+        'form_id' => 'pWmL9d',
+        'token' => '4969bac7b56e83a82ad060f0ae57faed',
+        'submitted_at' => '2017-07-13T10:00:32Z',
+        'calculated' => {
+          'score' => 42
+        },
+        'hidden' => {
+          'user_id' => founder.user.id
+        },
+        'definition' => {
+          'id' => 'pWmL9d',
+          'title' => 'all_questions_test',
+          'fields' => [
+            {
+              'id' => '36754465',
+              'title' => '1. Short text',
+              'type' => 'short_text'
+            },
+            {
+              'id' => '36754478',
+              'title' => '2. Long text',
+              'type' => 'long_text'
+            }
+          ]
+        },
+        'answers' => [
+          {
+            'type' => 'text',
+            'text' => 'Lorem ipsum dolor',
+            'field' => {
+              'id' => '36754465',
+              'type' => 'short_text'
+            }
+          },
+          {
+            'type' => 'text',
+            'text' => 'Lorem ipsum dolor',
+            'field' => {
+              'id' => '36754478',
+              'type' => 'long_text'
+            }
+          }
+        ]
+      }
+    }
+  end
+
+  describe 'GET screening_submit' do
+    before do
+      startup.founders << founder
+      sign_in founder.user
+    end
+
+    it 'redirects to founder dashboard and updates the founder of screening completion ' do
+      get :screening_submit
+      expect(response).to redirect_to(dashboard_founder_path(from: 'screening_submit'))
+    end
+  end
+
+  describe 'POST screening_submit_webhook' do
+    before do
+      startup.founders << founder
+    end
+
+    it 'verifies the screening target, updates founder screening_data and updates stage in intercom' do
+      expected_founder_screening_data = {
+        'score' => 42,
+        'response' => {
+          '1. Short text' => 'Lorem ipsum dolor',
+          '2. Long text' => 'Lorem ipsum dolor'
+        }
+      }
+      expect(Intercom::LevelZeroStageUpdateJob).to receive(:perform_now).with(founder, 'Screening Completed')
+
+      post :screening_submit_webhook, params: typeform_webhook_response, as: :json
+
+      expect(screening_target.status(founder)).to eq(Target::STATUS_COMPLETE)
+      founder.reload
+      expect(founder.screening_data).to eq(expected_founder_screening_data)
+    end
+  end
+end
---
+++ spec/features/admissions/screening_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'Admission Screening' do
+  include UserSpecHelper
+  include FounderSpecHelper
+
+  let(:startup) { create :level_0_startup }
+  let(:founder) { startup.team_lead }
+  let(:level_0) { create :level, :zero }
+  let(:level_0_targets) { create :target_group, milestone: true, level: level_0 }
+  let!(:screening_target) { create :target, :admissions_screening, link_to_complete: '/admissions/screening', target_group: level_0_targets }
+  let!(:fee_payment_target) { create :target, :admissions_fee_payment, target_group: level_0_targets }
+  let!(:cofounder_addition_target) { create :target, :admissions_cofounder_addition, target_group: level_0_targets }
+
+  let(:typeform_url) { Faker::Internet.url }
+
+  # Ensure authorization is in place.
+  context 'Founder attempts to complete screening target' do
+    scenario 'He gets redirected to typeform screening survey' do
+      typeform_url = "http://example.com/typeform?user_id=#{founder.user.id}"
+
+      sign_in_user founder.user, referer: admissions_screening_path
+      expect(page.current_url).to eq(typeform_url)
+    end
+  end
+end

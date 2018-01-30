f34d0181a85e572a427265f18c8900ae84965e21
Hari Gopal <hari@sv.co>
2018-01-30 12:33:40 +0530

    Mark screening target as complete after extracting and storing screening answers

--- app/jobs/admissions/screening_completion_job.rb
+++ app/jobs/admissions/screening_completion_job.rb
@@ -5,12 +5,12 @@ module Admissions
     def perform(founder, screening_response)
       return if founder.screening_data.present?
 
-      # Mark the screening target as complete
-      Admissions::CompleteTargetService.new(founder, Target::KEY_SCREENING).execute
-
       # Store screening response of the founder
       formatted_response = Typeform::AnswersExtractionService.new(screening_response).execute
       founder.update!(screening_data: formatted_response)
+
+      # Mark the screening target as complete
+      Admissions::CompleteTargetService.new(founder, Target::KEY_SCREENING).execute
 
       # Mark as screening completed on Intercom
       Intercom::LevelZeroStageUpdateJob.perform_now(founder, Startup::ADMISSION_STAGE_SELF_EVALUATION_COMPLETED)

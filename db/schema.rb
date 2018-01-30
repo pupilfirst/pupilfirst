7171cea7d3aaf9bb62de710ba476b41d45feb480
Hari Gopal <hari@sv.co>
2018-01-30 14:43:52 +0530

    Convert active admin comments resource_id to an integer to allow join queries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123140219) do
+ActiveRecord::Schema.define(version: 20180130074631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 20180123140219) do
   create_table "active_admin_comments", id: :serial, force: :cascade do |t|
     t.string "namespace"
     t.text "body"
-    t.string "resource_id", null: false
     t.string "resource_type", null: false
     t.integer "author_id"
     t.string "author_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "resource_id", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"

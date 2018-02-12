122cc032a68f0a6f33b209ddc29a3c57a317d7db
Mahesh Krishna Kumar <mahesh@sv.co>
2018-02-12 15:13:22 +0530

    Remove requested_restart_level_id from Startup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208111503) do
+ActiveRecord::Schema.define(version: 20180212094032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -567,7 +567,6 @@ ActiveRecord::Schema.define(version: 20180208111503) do
     t.boolean "dropped_out", default: false
     t.integer "level_id"
     t.integer "iteration", default: 1
-    t.integer "requested_restart_level_id"
     t.date "program_started_on"
     t.boolean "agreements_verified"
     t.string "courier_name"

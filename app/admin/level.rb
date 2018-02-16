0ad11be519b000b386e5f04b46628751df36ca11
Abdul Jaleel <jaleel@sv.co>
2018-02-16 16:11:56 +0530

    Add unlock_on field to Level [skip ci]

--- app/admin/level.rb
+++ app/admin/level.rb
@@ -3,10 +3,32 @@ ActiveAdmin.register Level do
 
   menu parent: 'Startups'
 
-  permit_params :number, :name, :description
+  permit_params :number, :name, :description, :unlock_on
 
   filter :name, as: :string
-  filter :description, as: :string
   filter :number
-  filter :created_at
+  filter :unlock_on, label: 'Unlock Date'
+
+  index do
+    selectable_column
+
+    column :number
+    column :name
+    column :unlock_on
+
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors(*f.object.errors.keys)
+
+    f.inputs do
+      f.input :number
+      f.input :name
+      f.input :description
+      f.input :unlock_on, as: :datepicker
+    end
+
+    f.actions
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213080954) do
+ActiveRecord::Schema.define(version: 20180216102921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -321,6 +321,7 @@ ActiveRecord::Schema.define(version: 20180213080954) do
     t.integer "number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "unlock_on"
     t.index ["number"], name: "index_levels_on_number"
   end
 

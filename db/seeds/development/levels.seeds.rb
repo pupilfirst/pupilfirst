fcecd2420de8e5e9e0fc4f91852784dc841f29b7
Abdul Jaleel <jaleel@sv.co>
2018-02-19 11:56:54 +0530

    Add unlock_on dates to level seeds [skip ci]

--- db/seeds/development/levels.seeds.rb
+++ db/seeds/development/levels.seeds.rb
@@ -4,6 +4,6 @@ puts 'Seeding levels'
 
 Level.create!(number: 0, name: 'Admissions')
 Level.create!(number: 1, name: 'Select Idea')
-Level.create!(number: 2, name: 'Decide What to Build')
-Level.create!(number: 3, name: 'Develop an Alpha Product')
-Level.create!(number: 4, name: 'Find the First Real Customer')
+Level.create!(number: 2, name: 'Decide What to Build', unlock_on: 1.month.from_now)
+Level.create!(number: 3, name: 'Develop an Alpha Product', unlock_on: 2.month.from_now)
+Level.create!(number: 4, name: 'Find the First Real Customer', unlock_on: 3.month.from_now)

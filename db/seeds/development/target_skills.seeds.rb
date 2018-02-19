c4d6ccde81970ca60f51074e256e9604edc49b54
Mahesh Krishna Kumar <mahesh@sv.co>
2018-02-19 12:49:44 +0530

    Create seeds for target_skills (rubric)

---
+++ db/seeds/development/target_skills.seeds.rb
@@ -0,0 +1,24 @@
+require_relative 'helper'
+
+after 'development:targets', 'development:skills' do
+  puts 'Seeding target rubric'
+
+  target = Target.last
+  TargetSkill.create!(
+    target: target,
+    skill: Skill.first,
+    rubric_good: 'Some articulation of why a user will find a solution to this problem valuable.',
+    rubric_great: 'A clear articulation of why a user will find a solution to this problem valuable.',
+    rubric_wow: 'Clear user needs identified, and a user persona described that has this problem',
+    base_karma_points: 20
+  )
+
+  TargetSkill.create!(
+    target: target,
+    skill: Skill.last,
+    rubric_good: 'Some analysis of quantitative methods such as market size, or qualitative methods such as interviews and surveys to justify decision.',
+    rubric_great: 'Quantitative methods such as market size, competitor traction, or other public research data used to justify decision.',
+    rubric_wow: 'Users in the target segment identified & detailed qualitative methods such as interviews and surveys used to justify decisions. Insights from interviews and surveys developed and recorded.',
+    base_karma_points: 30
+  )
+end

20e8093372cfaf6b3fb5090663bacc94929c228e
Abdul Jaleel <jaleel@sv.co>
2018-06-13 15:53:38 +0530

    WIP: Wiring up ReasonReact components

--- app/javascript/packs/coaches/dashboard/index.js
+++ app/javascript/packs/coaches/dashboard/index.js
@@ -1,11 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { make, jsComponent } from "../../../coaches/index.bs";
+import { make, jsComponent } from "../../../coaches/dashboard/index.bs";
 
 $(document).on("turbolinks:load", () => {
   const root = document.getElementById("coaches-dashboard");
+  const props = {coachName: 'Jaleel'};
   // const props = $(root).data("props");
-  const props = { name: "Hari", age: "30" };
-  // ReactDOM.render(element(undefined, undefined, make('Mahesh', '30')), root);
   ReactDOM.render(React.createElement(jsComponent, props), root);
 });

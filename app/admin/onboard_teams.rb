da03cbec890700571bdde856f6193603cfed4d52
Mahesh Krishna Kumar <mahesh@sv.co>
2018-07-11 10:17:50 +0530

    AA: Update onboard team form to have field for each member

--- app/admin/onboard_teams.rb
+++ app/admin/onboard_teams.rb
@@ -11,7 +11,7 @@ ActiveAdmin.register_page 'Onboard Teams' do
 
   page_action :onboard, method: :post do
     form = Admin::OnboardTeamForm.new(Reform::OpenForm.new)
-    if form.validate(params[:admin_onboard_team].merge(members: params[:members].values))
+    if form.validate(params[:admin_onboard_team])
       team = form.save
       redirect_to admin_startup_path(team)
     else
--- app/forms/admin/onboard_team_form.rb
+++ app/forms/admin/onboard_team_form.rb
@@ -1,16 +1,23 @@
 module Admin
   class OnboardTeamForm < Reform::Form
-    property :members, validates: { presence: true }
+    property :member_1_name, validates: { presence: true }
+    property :member_1_email, validates: { presence: true }
+    property :member_2_name
+    property :member_2_email
+    property :member_3_name
+    property :member_3_email
+    property :member_4_name
+    property :member_4_email
     property :team_name, validates: { presence: true }
     property :school_id, validates: { presence: true }
 
     def save
       Founder.transaction do
         team = Startup.create!(product_name: team_name, level: level)
-        members.each do |member|
-          next if member['email'].blank?
-          founder = Founder.where(email: member['email']).first_or_create!(user: user(member['email']))
-          founder.update!(name: member['name'], startup: team)
+        member_details.each do |name, email|
+          next if name.blank? || email.blank?
+          founder = Founder.where(email: email).first_or_create!(user: user(email))
+          founder.update!(name: name, startup: team)
         end
         team
       end
@@ -26,6 +33,15 @@ module Admin
       u = User.with_email(email) || User.create!(email: email)
       u.regenerate_login_token if u.login_token.blank?
       u
+    end
+
+    def member_details
+      {
+        member_1_name => member_1_email,
+        member_2_name => member_2_email,
+        member_3_name => member_3_email,
+        member_4_name => member_4_email
+      }
     end
   end
 end

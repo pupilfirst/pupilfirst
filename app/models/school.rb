4154aad3c83e37a4eb4a1da8327373e7f0669098
Abdul Jaleel <jaleel@sv.co>
2018-11-06 16:33:41 +0530

    Ensure grade_labels match the grading [skip ci]

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,6 +2,7 @@ class School < ApplicationRecord
   validates :name, presence: true
   validates :max_grade,  numericality: { greater_than: 0 }
   validates :pass_grade, numericality: { greater_than: 0, less_than_or_equal_to: :max_grade }
+  validate :grade_labels_must_match_grades
 
   has_many :levels, dependent: :restrict_with_error
   has_many :target_groups, through: :levels
@@ -19,5 +20,13 @@ class School < ApplicationRecord
   # Hack to enable editing grade_labels as an activeadmin text field
   def grade_labels=(labels)
     labels.is_a?(String) ? super(JSON.parse(labels)) : super(labels)
+  end
+
+  private
+
+  def grade_labels_must_match_grades
+    return if grade_labels.keys.map(&:to_i) == [*1..max_grade]
+
+    errors[:grade_labels] << 'do not match available grades'
   end
 end

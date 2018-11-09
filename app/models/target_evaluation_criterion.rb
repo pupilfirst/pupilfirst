e3e0aa656817998cac2fc0b6fc3ff812abcfd036
Abdul Jaleel <jaleel@sv.co>
2018-11-09 16:16:11 +0530

    Fix a few validations

--- app/models/target_evaluation_criterion.rb
+++ app/models/target_evaluation_criterion.rb
@@ -1,7 +1,5 @@
 class TargetEvaluationCriterion < ApplicationRecord
   belongs_to :target
   belongs_to :evaluation_criterion
-
-  validates :rubric, presence: true
   validates :base_karma_points, presence: true
 end

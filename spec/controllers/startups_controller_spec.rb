1de4b5188e34d36034f903ed7853d0bc92b169c0
Bodhish Thomas <bodhish@gmail.com>
2018-11-20 18:46:16 +0530

    Fix a bug in startup controller spec

--- spec/controllers/startups_controller_spec.rb
+++ spec/controllers/startups_controller_spec.rb
@@ -8,10 +8,10 @@ describe StartupsController do
   end
 
   describe 'GET /:id/:event_title/:event_id' do
-    let(:target) { create :target, role: Target::ROLE_FOUNDER }
-    let(:founder) { create :founder, startup: startup }
-    let(:startup) { create :startup }
-    let(:timeline_event) { create :timeline_event, :verified, target: target, founder: founder }
+    let!(:target) { create :target, role: Target::ROLE_TEAM }
+    let!(:founder) { create :founder, startup: startup }
+    let!(:startup) { create :startup }
+    let!(:timeline_event) { create :timeline_event, :verified, target: target, founder: founder }
 
     render_views
 

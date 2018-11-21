83b04002627c7422df634a234b137ba7474cd2e6
Abdul Jaleel <jaleel@sv.co>
2018-11-21 16:32:27 +0530

    Update TargetStatusBadge.jsx to work with the new statuses

--- app/javascript/founders/dashboard/dashboard/founderDashboard/TargetStatusBadge.jsx
+++ app/javascript/founders/dashboard/dashboard/founderDashboard/TargetStatusBadge.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import starsForScore from "./shared/starsForScore";
 
 export default class TargetStatusBadge extends React.Component {
   containerClasses() {
@@ -13,57 +12,38 @@ export default class TargetStatusBadge extends React.Component {
 
   statusIconClasses() {
     return {
-      complete: "fa fa-thumbs-o-up",
-      needs_improvement: "fa fa-line-chart",
+      passed: "fa fa-thumbs-o-up",
+      failed: "fa fa-thumbs-o-down",
       submitted: "fa fa-hourglass-half",
       pending: "fa fa-clock-o",
-      unavailable: "fa fa-lock",
-      not_accepted: "fa fa-thumbs-o-down",
       level_locked: "fa fa-eye",
-      pending_milestone: "fa fa-lock"
+      milestone_locked: "fa fa-lock",
+      prerequisite_locked: "fa fa-lock"
     }[this.props.target.status];
   }
 
   statusString() {
     return {
-      complete: "Completed",
-      needs_improvement: "Needs Improvement",
+      passed: "Passed",
+      failed: "Failed",
       submitted: "Submitted",
       pending: "Pending",
-      unavailable: "Locked",
-      not_accepted: "Not Accepted",
       level_locked: "Preview",
-      pending_milestone: "Locked"
+      milestone_locked: "Locked",
+      prerequisite_locked: "Locked"
     }[this.props.target.status];
   }
 
   statusContents() {
-    let grade = ["good", "great", "wow"].indexOf(this.props.target.grade) + 1;
-    let score = parseFloat(this.props.target.score);
-
-    if (this.props.target.status !== "complete" || grade === 0) {
-      return (
-        <span>
+    return (
+      <span>
           <span className="founder-dashboard-target-header__status-badge-icon">
             <i className={this.statusIconClasses()} />
           </span>
 
           <span>{this.statusString()}</span>
         </span>
-      );
-    } else {
-      const stars = starsForScore(score, this.props.target.id);
-
-      let gradeString =
-        this.props.target.grade.charAt(0).toUpperCase() +
-        this.props.target.grade.slice(1);
-
-      return (
-        <span>
-          {stars} {gradeString}!
-        </span>
-      );
-    }
+    );
   }
 
   render() {

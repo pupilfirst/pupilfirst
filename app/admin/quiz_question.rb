b64f2c7a7c3eafe1eaa629a09854f9d255c70d82
Bodhish Thomas <bodhish@gmail.com>
2018-11-27 14:08:06 +0530

    AA add quiz question

---
+++ app/admin/quiz_question.rb
@@ -0,0 +1,49 @@
+ActiveAdmin.register QuizQuestion do
+  include DisableIntercom
+
+  permit_params :question, :description, answer_options_attributes: %i[id value correct_answer hint _destroy]
+
+  index do
+    selectable_column
+    column :question
+    column :correct_answer do |question|
+      question.correct_answer.value if question.correct_answer.present?
+    end
+    actions
+  end
+
+  form do |f|
+    f.semantic_errors
+
+    # TODO: Reduce height of the text field below. 'input_html' seems to be malfunctioning
+    f.inputs 'Question Details' do
+      f.input :question
+      f.input :description
+    end
+
+    f.inputs 'Answer Options' do
+      f.has_many :answer_options, heading: false, allow_destroy: true, new_record: 'Add Option' do |o|
+        o.input :value
+        o.input :correct_answer
+        o.input :hint
+      end
+    end
+    f.actions
+  end
+
+  show do
+    attributes_table do
+      row :question
+      row :correct_answer do |question|
+        question.correct_answer.value if question.correct_answer.present?
+      end
+      row :answer_options do |question|
+        ul do
+          question.answer_options.pluck(:value).each do |answer|
+            li answer
+          end
+        end
+      end
+    end
+  end
+end

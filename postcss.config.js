79e4fdcb0c2cf6f1d33d258662789f062a30726e
Hari Gopal <mail@harigopal.in>
2018-12-11 17:06:05 +0530

    Add tailwind and set up a demo page

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-flexbugs-fixes"),
+    require("tailwindcss")("./tailwind.js"),
     require("postcss-preset-env")({
       autoprefixer: {
         flexbox: "no-2009"

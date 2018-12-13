ebb4fd7a7de40cedfc6b4c13ad71fc1db9c78069
vinu tv <vinu@sv.co>
2018-12-13 18:02:23 +0530

    Embed sastra admission quiz typeform

---
+++ app/javascript/packs/home/sastra.js
@@ -0,0 +1,5 @@
+$(document).on("turbolinks:load", () => {
+  // Typeform embed for Sastra admission.
+  (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
+
+});

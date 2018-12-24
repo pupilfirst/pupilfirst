a602fea8b043f1367d6e6af1845cd24f56ca9afe
Bodhish Thomas <bodhish@gmail.com>
2018-12-24 15:58:25 +0530

    Add Founders::ProfilePresenter

--- app/presenters/founders/profile_presenter.rb
+++ app/presenters/founders/profile_presenter.rb
@@ -2,21 +2,39 @@ module Founders
   class ProfilePresenter < ApplicationPresenter
     def initialize(founder)
       @founder = founder
-      super(view_context)
     end
 
-    def timeline_events_for_display(viewer)
+    def timeline_events_for_display(timeline_events, viewer)
       events_for_display = timeline_events
 
       # Only display verified of needs-improvement events if 'viewer' is not a member of this startup.
-      if viewer&.startup != self
+      if viewer != @founder
         events_for_display = events_for_display.verified_or_needs_improvement
       end
 
-      decorated_events = events_for_display.includes(:target, :timeline_event_files).order(:event_on, :updated_at).reverse_order
+      events_for_display = events_for_display.includes(:target, :timeline_event_files).order(:event_on, :updated_at).reverse_order
 
       # Hide founder events from everyone other than author of event.
-      decorated_events.reject { |event| event.hidden_from?(viewer) }
+      events_for_display.reject { |event| event.hidden_from?(viewer) }
+    end
+
+    def detailed_description(event)
+      "After #{target_prefix(event.target)} <em>#{event.target.title}:</em>\n #{event.description}"
+    end
+
+    private
+
+    def target_prefix(target)
+      case target.target_action_type
+        when Target::TYPE_TODO
+          'executing'
+        when Target::TYPE_ATTEND
+          'attending'
+        when Target::TYPE_LEARN
+          'watching'
+        when Target::TYPE_READ
+          'reading'
+      end
     end
   end
 end

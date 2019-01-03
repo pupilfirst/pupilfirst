bc31c43617c3dc431c791504b71928c1f6cdc7c4
Abdul Jaleel <jaleel@sv.co>
2019-01-03 13:05:57 +0530

    Partially fix the broken coach dashboard spec

--- spec/factories/evaluation_criterion.rb
+++ spec/factories/evaluation_criterion.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :evaluation_criterion do
-    name { Faker::Lorem.words(2) }
+    name { Faker::Lorem.words(2).join(' ') }
     description { Faker::Lorem.sentence }
   end
 end

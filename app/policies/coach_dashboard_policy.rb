088dfc614d69825553a03086c7263ab71919b4d4
Abdul Jaleel <jaleel@sv.co>
2019-01-15 17:20:45 +0530

    Fix bug in CoachDashboardPolicy

--- app/policies/coach_dashboard_policy.rb
+++ app/policies/coach_dashboard_policy.rb
@@ -1,5 +1,11 @@
 class CoachDashboardPolicy < ApplicationPolicy
   def show?
-    record.present? && record.in?(user&.faculty&.courses_with_dashboard)
+    record.present? && courses_with_dashboard.present? && record.in?(courses_with_dashboard)
+  end
+
+  private
+
+  def courses_with_dashboard
+    @courses_with_dashboard ||= user&.faculty&.courses_with_dashboard
   end
 end

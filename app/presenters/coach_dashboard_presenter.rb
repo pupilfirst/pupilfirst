3a484a1fb858d1dac5ca3c71a703ee6ec0204b28
Abdul Jaleel <jaleel@sv.co>
2019-01-17 16:26:13 +0530

    Write a CoachDashboard::TimelineEventsDataService

--- app/presenters/coach_dashboard_presenter.rb
+++ app/presenters/coach_dashboard_presenter.rb
@@ -9,8 +9,8 @@ class CoachDashboardPresenter < ApplicationPresenter
       coach: { name: current_coach.name, id: current_coach.id, imageUrl: current_coach.image_url },
       founders: founders,
       teams: teams,
-      timelineEvents: timeline_events,
-      moreToLoad: more_to_load?,
+      timelineEvents: timeline_events_service.timeline_events,
+      moreToLoad: timeline_events_service.more_to_load?,
       authenticityToken: view.form_authenticity_token,
       emptyIconUrl: view.image_url('coaches/dashboard/empty_icon.svg'),
       notAcceptedIconUrl: view.image_url('coaches/dashboard/not-accepted-icon.svg'),
@@ -46,60 +46,8 @@ class CoachDashboardPresenter < ApplicationPresenter
     end
   end
 
-  # Only load few latest pending timeline events initially.
-  def timeline_events
-    @timeline_events ||= TimelineEvent.pending_review.from_founders(founders.map { |f| f[:id] })
-      .includes(:timeline_event_owners, :timeline_event_files, :startup_feedback)
-      .includes(target: :level)
-      .includes(:target_evaluation_criteria, :evaluation_criteria)
-      .order(created_at: :DESC).limit(1).map { |timeline_event| timeline_event_fields(timeline_event) }
-  end
-
-  def timeline_event_fields(timeline_event)
-    {
-      id: timeline_event.id,
-      title: title(timeline_event),
-      description: timeline_event.description,
-      eventOn: timeline_event.event_on,
-      founderIds: founder_ids(timeline_event),
-      links: timeline_event.links,
-      files: files(timeline_event),
-      image: timeline_event.image? ? timeline_event.image.url : nil,
-      latestFeedback: timeline_event.startup_feedback&.last&.feedback,
-      evaluation: evaluation(timeline_event),
-      rubric: rubric(timeline_event)
-    }
-  end
-
-  def title(timeline_event)
-    timeline_event.target.level.short_name + ' | ' + timeline_event.target.title
-  end
-
-  def founder_ids(timeline_event)
-    timeline_event.timeline_event_owners.map(&:founder_id)
-  end
-
-  def files(timeline_event)
-    timeline_event.timeline_event_files.map { |file| { title: file.title, id: file.id } }
-  end
-
-  def evaluation(timeline_event)
-    timeline_event.evaluation_criteria.map do |criterion|
-      {
-        criterionId: criterion.id,
-        criterionName: criterion.name,
-        grade: nil
-      }
-    end
-  end
-
-  def rubric(timeline_event)
-    timeline_event.target.rubric_description
-  end
-
-  def more_to_load?
-    TimelineEvent.pending_review.from_founders(founders.map { |f| f[:id] })
-      .where.not(id: timeline_events.map { |te| te[:id] }).exists?
+  def timeline_events_service
+    @timeline_events_service ||= CoachDashboard::TimelineEventsDataService.new(current_coach, @course)
   end
 
   def grade_labels

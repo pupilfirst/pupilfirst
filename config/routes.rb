f63ecca35252a05d0a50355717f7e822c5db8860
Hari Gopal <mail@harigopal.in>
2019-01-18 13:02:39 +0530

    Ensure that OAuth provider route is accessible only via PF

--- config/routes.rb
+++ config/routes.rb
@@ -260,6 +260,6 @@ Rails.application.routes.draw do
 
   get '/school_admin', to: 'school_admins#dashboard'
 
-  get '/oauth/:provider', to: 'home#oauth', as: 'oauth'
+  get '/oauth/:provider', to: 'home#oauth', as: 'oauth', constraints: PupilFirstConstraint.new
   get '/oauth_unknown', to: 'home#oauth_unknown', as: 'oauth_unknown'
 end

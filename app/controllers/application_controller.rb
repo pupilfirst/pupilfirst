dcb7213d6724313f9467164abd295c59f53f7928
Mahesh Krishna Kumar <mahesh@sv.co>
2019-01-22 15:24:33 +0530

    Consider not_exited founders only for dashboard access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -97,10 +97,10 @@ class ApplicationController < ActionController::Base
         founder_id = read_cookie(:founder_id)
 
         # Try to select founder from value stored in cookie.
-        founder = founder_id.present? ? current_user.founders.find_by(id: founder_id) : nil
+        founder = founder_id.present? ? current_user.founders.not_exited.find_by(id: founder_id) : nil
 
         # Return selected founder, if any, or return the first founder (if any).
-        founder.presence || current_user.founders.first
+        founder.presence || current_user.founders.not_exited.first
       end
     end
   end

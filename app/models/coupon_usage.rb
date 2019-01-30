80966b1341104c8739420a4916b107eafba93b1a
Bodhish Thomas <bodhish@gmail.com>
2019-01-30 13:20:11 +0530

    Remove coupon usages

--- app/models/coupon_usage.rb
+++
@@ -1,6 +0,0 @@
-class CouponUsage < ApplicationRecord
-  belongs_to :coupon
-  belongs_to :startup
-
-  scope :redeemed, -> { where.not(redeemed_at: nil) }
-end
--- spec/factories/coupon.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :coupon do
-    code { rand(36**6).to_s(36) }
-    discount_percentage { [10, 25, 50, 75].sample }
-  end
-end

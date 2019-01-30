8bcbd8d7d59750ac8402c7215a743279b76ebdbb
Hari Gopal <mail@harigopal.in>
2019-01-30 17:54:40 +0530

    Minor change to spec

--- spec/services/founders/dashboard_data_service_spec.rb
+++ spec/services/founders/dashboard_data_service_spec.rb
@@ -34,41 +34,39 @@ describe Founders::DashboardDataService do
   let!(:level_4_target) { create :target, target_group: target_group_l4_1 }
 
   describe '#props' do
-    context 'when startup is in level N' do
-      it ' includes data from all open 1+ levels' do
-        expected_target_groups = [
-          hash_including(target_group_l1_1.slice(target_group_fields).merge(track: { id: track_1.id }, level: { id: level_1.id })),
-          hash_including(target_group_l1_2.slice(target_group_fields).merge(track: { id: track_2.id }, level: { id: level_1.id })),
-          hash_including(target_group_l2_1.slice(target_group_fields).merge(track: { id: track_1.id }, level: { id: level_2.id })),
-          hash_including(target_group_l2_2.slice(target_group_fields).merge(track: { id: track_2.id }, level: { id: level_2.id })),
-          hash_including(target_group_l3_1.slice(target_group_fields).merge(track: { id: track_1.id }, level: { id: unlocked_level_3.id }))
-        ]
+    it 'includes data from all open levels' do
+      expected_target_groups = [
+        hash_including(target_group_l1_1.slice(target_group_fields).merge(track: { id: track_1.id }, level: { id: level_1.id })),
+        hash_including(target_group_l1_2.slice(target_group_fields).merge(track: { id: track_2.id }, level: { id: level_1.id })),
+        hash_including(target_group_l2_1.slice(target_group_fields).merge(track: { id: track_1.id }, level: { id: level_2.id })),
+        hash_including(target_group_l2_2.slice(target_group_fields).merge(track: { id: track_2.id }, level: { id: level_2.id })),
+        hash_including(target_group_l3_1.slice(target_group_fields).merge(track: { id: track_1.id }, level: { id: unlocked_level_3.id }))
+      ]
 
-        expected_targets = [
-          hash_including(level_1_target.slice(target_fields).merge(additional_target_fields(level_1_target, target_group_l1_1))),
-          hash_including(level_1_session.slice(target_fields).merge(additional_target_fields(level_1_session, target_group_l1_2))),
-          hash_including(level_1_target_with_prerequisites.slice(target_fields).merge(additional_target_fields(level_1_target_with_prerequisites, target_group_l1_1, :prerequisite_locked))),
-          hash_including(level_2_target.slice(target_fields).merge(additional_target_fields(level_2_target, target_group_l2_2))),
-          hash_including(level_2_session.slice(target_fields).merge(additional_target_fields(level_2_session, target_group_l2_2))),
-          hash_including(level_2_target_with_prerequisites.slice(target_fields).merge(additional_target_fields(level_2_target_with_prerequisites, target_group_l2_1, :milestone_locked))),
-          hash_including(level_3_target.slice(target_fields).merge(additional_target_fields(level_3_target, target_group_l3_1, :level_locked)))
-        ]
+      expected_targets = [
+        hash_including(level_1_target.slice(target_fields).merge(additional_target_fields(level_1_target, target_group_l1_1))),
+        hash_including(level_1_session.slice(target_fields).merge(additional_target_fields(level_1_session, target_group_l1_2))),
+        hash_including(level_1_target_with_prerequisites.slice(target_fields).merge(additional_target_fields(level_1_target_with_prerequisites, target_group_l1_1, :prerequisite_locked))),
+        hash_including(level_2_target.slice(target_fields).merge(additional_target_fields(level_2_target, target_group_l2_2))),
+        hash_including(level_2_session.slice(target_fields).merge(additional_target_fields(level_2_session, target_group_l2_2))),
+        hash_including(level_2_target_with_prerequisites.slice(target_fields).merge(additional_target_fields(level_2_target_with_prerequisites, target_group_l2_1, :milestone_locked))),
+        hash_including(level_3_target.slice(target_fields).merge(additional_target_fields(level_3_target, target_group_l3_1, :level_locked)))
+      ]
 
-        team_members = Faculty.team.all.as_json(only: %i[id name], methods: %i[image_or_avatar_url]).map do |faculty_fields|
-          hash_including(faculty_fields)
-        end
-
-        props = subject.props
-
-        expect(props.keys).to match_array(%i[faculty levels targetGroups targets tracks criteriaNames gradeLabels])
-        expect(props[:faculty]).to match_array(team_members)
-        expect(props[:levels]).to match_array(level_fields(level_1, level_2, unlocked_level_3, locked_level_4))
-        expect(props[:targetGroups]).to match_array(expected_target_groups)
-        expect(props[:targets]).to match_array(expected_targets)
-        expect(props[:tracks]).to match_array(track_fields(track_1, track_2))
-        expect(props[:criteriaNames]).to eq(evaluation_criterion_course_1.id => evaluation_criterion_course_1.name)
-        expect(props[:gradeLabels]).to eq(course_1.grade_labels)
+      team_members = Faculty.team.all.as_json(only: %i[id name], methods: %i[image_or_avatar_url]).map do |faculty_fields|
+        hash_including(faculty_fields)
       end
+
+      props = subject.props
+
+      expect(props.keys).to match_array(%i[faculty levels targetGroups targets tracks criteriaNames gradeLabels])
+      expect(props[:faculty]).to match_array(team_members)
+      expect(props[:levels]).to match_array(level_fields(level_1, level_2, unlocked_level_3, locked_level_4))
+      expect(props[:targetGroups]).to match_array(expected_target_groups)
+      expect(props[:targets]).to match_array(expected_targets)
+      expect(props[:tracks]).to match_array(track_fields(track_1, track_2))
+      expect(props[:criteriaNames]).to eq(evaluation_criterion_course_1.id => evaluation_criterion_course_1.name)
+      expect(props[:gradeLabels]).to eq(course_1.grade_labels)
     end
   end
 

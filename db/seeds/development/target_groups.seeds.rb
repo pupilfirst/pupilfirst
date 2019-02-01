4caeca8f43a557e13929251f45617338cd13b6ae
Abdul Jaleel <ajaleelp@gmail.com>
2019-02-01 14:13:10 +0530

    Bring back level 0 and remove tracks from seeds

--- db/seeds/development/target_groups.seeds.rb
+++ db/seeds/development/target_groups.seeds.rb
@@ -1,13 +1,11 @@
 require_relative 'helper'
 
-after 'development:levels', 'development:tracks' do
+after 'development:levels' do
   puts 'Seeding target_groups'
-
-  product_track = Track.find_by!(name: 'Product')
-  developer_track = Track.find_by!(name: 'Developer')
 
   # TODO: Improve the target group titles here when we have more info on customer-facing program.
   group_data = {
+    0 => ['Sessions'],
     1 => ['Engineering & Design First Steps', 'Level Up'],
     2 => ['Put up a Coming Soon Page', 'Level Up'],
     3 => ['Get First Feedback', 'Level Up'],
@@ -18,15 +16,11 @@ after 'development:levels', 'development:tracks' do
     group_data[level.number].each_with_index do |group_name, index|
       milestone = index == 1 # Second group from data is marked as the milestone group.
 
-      track = milestone ? product_track : developer_track
-
-
       level.target_groups.create!(
         name: group_name,
         sort_index: index + 1,
         description: Faker::Lorem.words(10).join(' '),
-        milestone: milestone,
-        track: track
+        milestone: milestone
       )
     end
   end

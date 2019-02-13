e9bbed613d46df774fd2116581bc78c22b386d04
Abdul Jaleel <ajaleelp@gmail.com>
2019-02-13 21:28:50 +0530

    Fix broken team_up_service_spec

--- spec/services/startups/team_up_service_spec.rb
+++ spec/services/startups/team_up_service_spec.rb
@@ -5,19 +5,18 @@ describe Startups::TeamUpService do
 
   let(:startup_1) { create :startup }
   let(:startup_2) { create :startup }
-  let(:startup_3) { create :startup }
 
   let(:founder_1) { startup_1.founders.first }
-  let(:founder_2) { create :founder, startup: startup_1 }
+  let(:founder_2) { startup_1.founders.second }
   let(:founder_3) { startup_2.founders.first }
-  let(:founder_4) { startup_3.founders.first }
+  let(:founder_4) { startup_2.founders.second }
 
-  let!(:founders) { Founder.where(id: [founder_2.id, founder_3.id, founder_4.id]) }
+  let!(:founders) { Founder.where(id: [founder_1.id, founder_2.id, founder_3.id, founder_4.id]) }
   let(:team_name) { Faker::Lorem.words(2).join(' ') }
 
   describe '#team_up' do
     it 'forms a new team with specified founders' do
-      expect { subject.team_up(team_name) }.to(change { Startup.count }.from(3).to(2))
+      expect { subject.team_up(team_name) }.to(change { Startup.count }.from(2).to(1))
 
       last_startup = Startup.last
 
@@ -26,7 +25,7 @@ describe Startups::TeamUpService do
       expect(last_startup.product_name).to eq(team_name)
       expect(last_startup.name).to eq(team_name)
 
-      expect(founder_1.startup.reload.founders.count).to eq(1)
+      expect(founder_1.reload.startup.founders.count).to eq(4)
     end
   end
 end

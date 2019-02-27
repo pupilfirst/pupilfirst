85fb51f276d63915f75d01f206ab10909ff74b3c
Hari Gopal <mail@harigopal.in>
2019-02-27 13:53:01 +0530

    AA: Show school icon and allow it to be edited

--- app/admin/school.rb
+++ app/admin/school.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register School do
   actions :all, except: %i[new create destroy]
 
-  permit_params :logo
+  permit_params :logo, :icon
 
   controller do
     include DisableIntercom
@@ -23,6 +23,16 @@ ActiveAdmin.register School do
         end
       end
 
+      row :icon do
+        if school.icon.attached?
+          link_to(school.icon) do
+            image_tag(url_for(school.icon_variant(:thumb)))
+          end
+        else
+          em('No icon attached')
+        end
+      end
+
       row :id
       row :created_at
       row :updated_at
@@ -33,6 +43,7 @@ ActiveAdmin.register School do
     f.inputs 'School Details' do
       f.input :name, input_html: { disabled: true }
       f.input :logo, as: :file, hint: f.object.logo.attached? ? "Upload another file to replace #{f.object.logo.filename}" : nil
+      f.input :icon, as: :file, hint: f.object.icon.attached? ? "Upload another file to replace #{f.object.icon.filename}" : nil
     end
 
     f.actions

bf4fbca9878ecd36661866af2b415c18997bdc60
Bodhish Thomas <bodhish@gmail.com>
2019-03-01 15:03:21 +0530

    Add connect link to faculty page

--- app/policies/faculty_policy.rb
+++ app/policies/faculty_policy.rb
@@ -11,11 +11,16 @@ class FacultyPolicy < ApplicationPolicy
     # Cannot connect if user doesn't have a student profile.
     return false if current_founder.blank?
 
-    # Cannot connect if the coach doesn't have appropriate slots available.
-    return false unless record.connect_slots.available_for_founder.exists?
+    # Cannot connect if connect link is blank or doesn't have appropriate slots available.
+    return false unless record.connect_slots.available_for_founder.exists? || record.connect_link?
 
-    # Can connect only if coach been assigned to review the student's team.
-    record.reviewable_startups(current_founder.course).where(id: current_founder.startup).exists?
+    # It should be possible to connect to course-enrolled coaches.
+    return true if record.courses.where(id: current_founder.startup.course).exists?
+
+    # It should be possible to connect to coaches enrolled directly to current team.
+    return true if record.startups.where(id: current_founder.startup).exists?
+
+    false
   end
 
   class Scope < Scope

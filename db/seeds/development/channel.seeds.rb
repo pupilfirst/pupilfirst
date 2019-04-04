7fd4b962c891f74c684d3df23e0b1f6ac698ce2d
Bodhish Thomas <bodhish@gmail.com>
2019-04-04 15:13:42 +0530

    Add community seed

---
+++ db/seeds/development/channel.seeds.rb
@@ -0,0 +1,28 @@
+after 'development:schools', 'development:founders' do
+  puts 'Seeding channel'
+
+  school = School.find_by(name: 'SV.CO')
+  john_doe = User.find_by(email: 'johndoe@example.com')
+  groot = User.find_by(email: 'groot@example.org')
+  rocket = User.find_by(email: 'rocket@example.org')
+  channel = Channel.create!(name: "VR", school: school)
+
+  [john_doe, groot, rocket].each do |user|
+    question = Question.create!(
+      title: Faker::Lorem.sentence,
+      description: Faker::Lorem.sentence,
+      channel: channel,
+      user: user
+    )
+    answer = Answer.create!(
+      description: Faker::Lorem.sentence,
+      question: question,
+      user: [john_doe, groot, rocket].sample
+    )
+    AnswerClap.create!(
+      count: 10,
+      answer: answer,
+      user: [john_doe, groot, rocket].sample
+    )
+  end
+end

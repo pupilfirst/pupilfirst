def56fd254856861c4d3acb5568d08ca2abd0f59
Bodhish Thomas <bodhish@gmail.com>
2019-04-23 16:47:36 +0530

    Add comments to seed and update questions show

--- db/seeds/development/community.seeds.rb
+++ db/seeds/development/community.seeds.rb
@@ -9,27 +9,40 @@ after 'development:schools', 'development:founders', 'development:faculty', 'dev
   donald = User.find_by(email: 'donaldduck@example.com')
   community = Community.create!(name: "VR", school: school)
 
-  [john_doe, mickey, groot, rocket, donald].each do |user_1|
-    question = Question.create!(
-      title: Faker::Lorem.sentence,
-      description: Faker::Lorem.paragraph(sentence_count = 10, supplemental = false, random_sentences_to_add = 5),
-      community: community,
-      user: user_1
-    )
-    [john_doe, mickey, groot, rocket, donald].each do |user_2|
-
-      next if user_1 == user_2
-
-      answer = Answer.create!(
+  3.times do
+    [john_doe, mickey, groot, rocket, donald].each do |user_1|
+      question = Question.create!(
+        title: Faker::Lorem.sentence,
         description: Faker::Lorem.paragraph(sentence_count = 10, supplemental = false, random_sentences_to_add = 5),
-        question: question,
-        user: user_2
+        community: community,
+        user: user_1
       )
-      AnswerClap.create!(
-        count: rand(1..10),
-        answer: answer,
-        user: [john_doe, mickey, groot, rocket, donald].sample
-      )
+      [john_doe, mickey, groot, rocket, donald].each do |user_2|
+
+        next if user_1 == user_2
+
+        answer = Answer.create!(
+          description: Faker::Lorem.paragraph(sentence_count = 10, supplemental = false, random_sentences_to_add = 5),
+          question: question,
+          user: user_2
+        )
+        AnswerClap.create!(
+          count: rand(1..10),
+          answer: answer,
+          user: [john_doe, mickey, groot, rocket, donald].sample
+        )
+        answer.comments.create!(
+          user: [john_doe, mickey, groot, rocket, donald].sample,
+          value: Faker::Lorem.paragraph(sentence_count = 4, supplemental = false, random_sentences_to_add = 5)
+        )
+      end
+
+      [john_doe, mickey, groot, rocket, donald].each do |user_3|
+        question.comments.create!(
+          user: user_3,
+          value: Faker::Lorem.paragraph(sentence_count = 4, supplemental = false, random_sentences_to_add = 5)
+        )
+      end
     end
   end
 

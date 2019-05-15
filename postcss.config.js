a58091686907f7de297113a1aae6082902f980aa
vinu tv <vinu@sv.co>
2019-05-15 17:18:12 +0530

    Update tailwind.js to tailwind.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require("postcss-import"),
     require("postcss-flexbugs-fixes"),
-    require("tailwindcss")("./tailwind.js"),
+    require("tailwindcss")("./tailwind.config.js"),
     require("postcss-preset-env")({
       autoprefixer: {
         flexbox: "no-2009"
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    colors: {
+      primary: {
+        100: '#F0EAFB',
+        200: '#D3BFF3',
+        300: '#B08EE6',
+        400: '#976AE2',
+        500: '#6025C0',
+        600: '#4D1E9A',
+        700: '#380D80',
+        800: '#35156B',
+        900: '#1F0D40',
+      },
+      secondary: {
+        100: '#FEE6EF',
+        200: '#FCB5D0',
+        300: '#FF80B0',
+        400: '#F95392',
+        500: '#F61067',
+        600: '#AC0645',
+        700: '#A61149',
+        800: '#7B0531',
+        900: '#4A031E',
+      },
+    },
+  }
+}

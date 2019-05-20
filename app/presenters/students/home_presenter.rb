df307c2d0055669cff56c3d141f68935267914d4
Bodhish Thomas <bodhish@gmail.com>
2019-05-20 23:38:57 +0530

    Add dynamic links in students home

--- app/presenters/students/home_presenter.rb
+++ app/presenters/students/home_presenter.rb
@@ -7,7 +7,7 @@ module Students
     end
 
     def founders
-      @founders ||= @user.founders
+      @founders ||= @user.founders.where(exited: false)
     end
 
     def user_profile
@@ -25,6 +25,17 @@ module Students
     def communities
       course_ids = founders.joins(:course).pluck(:course_id)
       Community.where(school: current_school).joins(:courses).where(courses: { id: course_ids }).distinct
+    end
+
+    def show_leaderboard_link?(course)
+      lts = LeaderboardTimeService.new
+
+      course_entries_last_week = LeaderboardEntry.joins(:course).where(
+        courses: { id: course },
+        period_from: lts.week_start,
+        period_to: lts.week_end
+      )
+      course_entries_last_week.exists?
     end
   end
 end

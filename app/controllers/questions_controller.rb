72a71e6f6cb068add69edec4a1042497dec37c07
Bodhish Thomas <bodhish@gmail.com>
2019-05-30 13:13:39 +0530

    Fix a bug in questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,7 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :community, except: %i[show versions]
+  before_action :target, except: %i[show versions]
   layout 'student'
 
   def show
@@ -11,5 +13,22 @@ class QuestionsController < ApplicationController
     @question = authorize(Question.find(params[:id]))
 
     raise_not_found if @question.blank?
+  end
+
+  private
+
+  def community
+    @community = Community.find(params[:community_id])
+  end
+
+  def target
+    @target ||= begin
+      if params[:target_id].present?
+        t = Target.find_by(id: params[:target_id])
+
+        # Only return the target if the target is in a course that is linked to this community.
+        @community.courses.where(id: t&.course).exists? ? t : nil
+      end
+    end
   end
 end

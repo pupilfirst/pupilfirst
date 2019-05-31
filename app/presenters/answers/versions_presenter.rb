03e2c90082f39a998fce0505721fd5a35c7e19d9
Bodhish Thomas <bodhish@gmail.com>
2019-05-31 15:37:39 +0530

    Replace markdown view pack with convert markdown pack

--- app/presenters/answers/versions_presenter.rb
+++ app/presenters/answers/versions_presenter.rb
@@ -14,16 +14,14 @@ module Answers
       versions_count + 1
     end
 
+    def markdown_prop(markdown)
+      {
+        markdown: markdown
+      }.to_json
+    end
+
     def version_number(index)
       versions_count - index
-    end
-
-    def markdown_prop(version, index)
-      markdown_props_to_json(version_number(index), version.value)
-    end
-
-    def markdown_props_for_answer
-      markdown_props_to_json(version_number_for_answer, @answer.description)
     end
 
     def editor_name_for_answer
@@ -47,13 +45,6 @@ module Answers
 
     def versions_count
       @versions_count ||= versions.count
-    end
-
-    def markdown_props_to_json(id, text)
-      {
-        id: id.to_s,
-        text: text
-      }.to_json
     end
   end
 end

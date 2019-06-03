716e9245ed7b301b778c4daccc250d7102c00742
Hari Gopal <mail@harigopal.in>
2019-06-03 17:46:03 +0530

    Unify presenters for agreements

---
+++ app/presenters/home/agreement_presenter.rb
@@ -0,0 +1,17 @@
+module Home
+  class AgreementPresenter < ApplicationPresenter
+    def initialize(view_context, agreement)
+      super(view_context)
+
+      @agreement = agreement
+    end
+
+    def page_title
+      current_school.blank? ? 'PupilFirst' : current_school.name
+    end
+
+    def agreement_html
+      Kramdown::Document.new(@agreement).to_html.html_safe
+    end
+  end
+end

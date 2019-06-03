d6bf9fefacc6cb946c54fb90fa2dbce8c25098cd
Bodhish Thomas <bodhish@gmail.com>
2019-06-03 08:49:07 +0530

    Handle coaches without founder profile in home

--- app/presenters/students/home_presenter.rb
+++ app/presenters/students/home_presenter.rb
@@ -1,17 +1,15 @@
 module Students
   class HomePresenter < ApplicationPresenter
-    def initialize(view_context, user)
+    def initialize(view_context)
       super(view_context)
-
-      @user = user
     end
 
     def founders
-      @founders ||= @user.founders.where(exited: false)
+      @founders ||= current_user.founders.where(exited: false)
     end
 
     def user_profile
-      @user_profile ||= UserProfile.where(user: @user, school: current_school).first
+      @user_profile ||= UserProfile.where(user: current_user, school: current_school).first
     end
 
     def avatar
@@ -22,9 +20,32 @@ module Students
       end
     end
 
+    def can_review?(course)
+      return false if current_coach.blank?
+
+      course.in? faculty_course
+    end
+
+    def additional_courses_for_faculty
+      return [] if current_coach.blank?
+
+      (student_courses - faculty_course) | (faculty_course - student_courses)
+    end
+
+    def show_communities?
+      communities.any?
+    end
+
     def communities
-      course_ids = founders.joins(:course).select(:course_id)
-      Community.where(school: current_school).joins(:courses).where(courses: { id: course_ids }).distinct
+      @communities ||= begin
+        communities_in_school = Community.where(school: current_school)
+
+        if current_school_admin.present? || current_coach.present?
+          communities_in_school
+        else
+          communities_in_school.joins(:courses).where(courses: { id: student_courses.pluck(:id) }).distinct
+        end
+      end
     end
 
     def show_leaderboard_link?(course)
@@ -36,6 +57,20 @@ module Students
         period_to: lts.week_end
       )
       course_entries_last_week.exists?
+    end
+
+    def course_cover_button_text(founder)
+      founder.dashboard_toured? ? "Continue Course" : "Start Course"
+    end
+
+    private
+
+    def student_courses
+      @student_courses = current_school&.courses&.where(id: founders.joins(:course).pluck(:course_id))
+    end
+
+    def faculty_course
+      @faculty_course = current_coach&.courses&.where(school: current_school)
     end
   end
 end

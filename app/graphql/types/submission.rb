c4d414417ac418269c8fb48a0a890a142ad88774
Hari Gopal <mail@harigopal.in>
2019-06-13 17:59:15 +0530

    Use underscored names server-side

--- app/graphql/types/submission.rb
+++ app/graphql/types/submission.rb
@@ -2,6 +2,6 @@ module Types
   class Submission < Types::BaseObject
     field :id, ID, null: false
     field :description, String, null: false
-    field :createdAt, String, null: false
+    field :created_at, String, null: false
   end
 end

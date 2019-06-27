8e48035ff69617167d2e7ee8c5d6298c755b459d
Hari Gopal <mail@harigopal.in>
2019-06-27 15:07:17 +0530

    Spec and improve Targets::UpdateVisibilityService

--- app/services/targets/update_visibility_service.rb
+++ app/services/targets/update_visibility_service.rb
@@ -7,16 +7,19 @@ module Targets
     end
 
     def execute
-      if @visibility == Target::VISIBILITY_ARCHIVED || @visibility == Target::VISIBILITY_DRAFT
-        TargetPrerequisite.transaction do
-          target_prerequisites = TargetPrerequisite.where('target_id = ? OR prerequisite_target_id = ?', @target.id, @target.id)
-          target_prerequisites.destroy_all if target_prerequisites.exists?
-          @target.update!(safe_to_change_visibility: true, visibility: @visibility)
-        end
-      elsif @visibility == Target::VISIBILITY_LIVE
-        TargetGroups::ArchivalService.new(@target.target_group).unarchive
-        @target.update!(visibility: Target::VISIBILITY_LIVE)
+      case @visibility
+        when Target::VISIBILITY_ARCHIVED, Target::VISIBILITY_DRAFT
+          TargetPrerequisite.transaction do
+            target_prerequisites = TargetPrerequisite.where('target_id = ? OR prerequisite_target_id = ?', @target.id, @target.id)
+            target_prerequisites.destroy_all if target_prerequisites.exists?
+          end
+        when Target::VISIBILITY_LIVE
+          TargetGroups::ArchivalService.new(@target.target_group).unarchive
+        else
+          raise "Targets::UpdateVisibilityService received unknown visiblity value '#{@visibility}'"
       end
+
+      @target.update!(safe_to_change_visibility: true, visibility: @visibility)
     end
   end
 end
---
+++ spec/services/targets/update_visibility_service_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe Targets::UpdateVisibilityService do
+  subject { described_class }
+
+  let(:target_group) { create :target_group }
+  let(:prerequisite_target) { create :target, target_group: target_group }
+  let(:target) { create :target, target_group: target_group, prerequisite_targets: [prerequisite_target] }
+
+  shared_examples "removes prerequisites and updates non-live visibility" do |requested_visibility|
+    let(:visibility) { requested_visibility }
+
+    it 'removes prerequisites from target' do
+      expect do
+        subject.new(target, visibility).execute
+      end.to change { target.prerequisite_targets.count }.from(1).to(0)
+    end
+
+    it 'updates visibility' do
+      puts "Request visibility is #{visibility}..."
+
+      expect do
+        subject.new(target, visibility).execute
+      end.to change { target.reload.visibility }.from(Target::VISIBILITY_LIVE).to(visibility)
+    end
+  end
+
+  include_examples "removes prerequisites and updates non-live visibility", Target::VISIBILITY_DRAFT
+  include_examples "removes prerequisites and updates non-live visibility", Target::VISIBILITY_ARCHIVED
+
+  context "when the requested visibility is live" do
+    let(:target_group_archival_service) { instance_double(TargetGroups::ArchivalService, unarchive: true) }
+    let(:target) { create :target, :draft, target_group: target_group, prerequisite_targets: [prerequisite_target] }
+
+    it 'uses TargetGroups::ArchivalService to unarchive target group' do
+      expect(TargetGroups::ArchivalService).to receive(:new).with(target_group).and_return(target_group_archival_service)
+      expect(target_group_archival_service).to receive(:unarchive)
+
+      expect do
+        subject.new(target, Target::VISIBILITY_LIVE).execute
+      end.to change { target.reload.visibility }.from(Target::VISIBILITY_DRAFT).to(Target::VISIBILITY_LIVE)
+    end
+  end
+end

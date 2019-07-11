ec960bde953eca89ec9b85090f3e1ea706e40997
Bodhish Thomas <bodhish@gmail.com>
2019-07-11 15:09:00 +0530

    Add migration to merge user_profile with user

---
+++ db/data/20190711070306_populate_user_details_from_user_profiles.rb
@@ -0,0 +1,75 @@
+class PopulateUserDetailsFromUserProfiles < ActiveRecord::Migration[5.2]
+  ATTRIBUTES = %i[
+    name
+    gender
+    phone
+    communication_address
+    title
+    key_skills
+    about
+    resume_url
+    blog_url
+    personal_website_url
+    linkedin_url
+    twitter_url
+    facebook_url
+    angel_co_url
+    github_url
+    behance_url
+    skype_id
+  ]
+
+  def up
+    # Populate data for users in SV School
+    sv_school = School.first
+
+    sv_school.user_profiles.each do |user_profile|
+      attributes = user_profile.attributes.slice(ATTRIBUTES)
+      user = user_profile.user
+
+      user.update!(
+        school_id: sv_school.id,
+        **attributes
+      )
+
+      next unless user_profile.avatar.attached?
+
+      ActiveStorage::Attachment.where(
+        name: 'avatar',
+        record: user
+      ).first_or_create!(
+        blob: user_profile.avatar.blob
+      )
+    end
+
+    # Create new users
+    School.where.not(id: sv_school).each do |school|
+      school.user_profiles.each do |user_profile|
+        old_user = user_profile.user
+        attributes = user_profile.attributes.slice(ATTRIBUTES)
+
+        new_user = school.users.create!(
+          email: old_user.email,
+          **attributes
+        )
+
+        old_user.founders.joins(:school).where(schools: { id: school }).update_all(user: new_user)
+        old_user.faculty.where(school: school).update_all(user: new_user)
+
+        next unless user_profile.avatar.attached?
+
+        ActiveStorage::Attachment.where(
+          name: 'avatar',
+          record: user
+        ).first_or_create!(
+          blob: user_profile.avatar.blob
+        )
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_103138) do
+ActiveRecord::Schema.define(version: 2019_07_11_065042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -724,6 +724,25 @@ ActiveRecord::Schema.define(version: 2019_06_28_103138) do
     t.string "email_bounce_type"
     t.datetime "confirmed_at"
     t.datetime "login_mail_sent_at"
+    t.string "name"
+    t.string "phone"
+    t.string "gender"
+    t.string "communication_address"
+    t.string "title"
+    t.string "key_skills"
+    t.text "about"
+    t.string "resume_url"
+    t.string "blog_url"
+    t.string "personal_website_url"
+    t.string "linkedin_url"
+    t.string "twitter_url"
+    t.string "facebook_url"
+    t.string "angel_co_url"
+    t.string "github_url"
+    t.string "behance_url"
+    t.string "skype_id"
+    t.bigint "school_id"
+    t.index ["school_id"], name: "index_users_on_school_id"
   end
 
   create_table "visits", id: :uuid, default: nil, force: :cascade do |t|
@@ -799,4 +818,5 @@ ActiveRecord::Schema.define(version: 2019_06_28_103138) do
   add_foreign_key "user_activities", "users"
   add_foreign_key "user_profiles", "schools"
   add_foreign_key "user_profiles", "users"
+  add_foreign_key "users", "schools"
 end

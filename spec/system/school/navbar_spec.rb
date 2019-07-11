4fc38626a6f0ab39aeb6c422c2e9e1457ea77d23
Hari Gopal <mail@harigopal.in>
2019-07-11 14:18:57 +0530

    Spec the new SA unified navbar

---
+++ spec/system/school/navbar_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+feature 'School Admin Navbar', js: true do
+  include UserSpecHelper
+
+  let(:school_1) { create :school, :current }
+  let(:school_2) { create :school }
+  let(:school_admin) { create :school_admin, school: school_1 }
+
+  # Let there be two courses in 'this 'school.
+  let!(:course_1) { create :course, school: school_1 }
+  let!(:course_2) { create :course, school: school_1 }
+
+  # And another in a different school.
+  let!(:course_3) { create :course, school: school_2 }
+
+  scenario 'school admin visits the admin interface' do
+    sign_in_user school_admin.user
+
+    # User should be on the school admin overview page.
+    expect(current_path).to eq('/school')
+
+    # There should be additional links on the navbar.
+    expect(page).to have_link('Coaches', href: '/school/coaches')
+    expect(page).to have_link('Settings', href: '/school/customize')
+    expect(page).to have_link('Courses', href: '/school/courses')
+    expect(page).to have_link('Communities', href: '/school/communities')
+
+    # Links to the student page for all courses in school should also be there.
+    expect(page).to have_link(course_1.name, href: "/school/courses/#{course_1.id}/students")
+    expect(page).to have_link(course_2.name, href: "/school/courses/#{course_2.id}/students")
+
+    # Courses from other schools should not be listed.
+    expect(page).not_to have_link(course_3.name)
+
+    # There should also be a link to log out.
+    expect(page).to have_link('Log Out')
+
+    # Check out the settings submenu.
+    click_link('Settings')
+    expect(page).to have_link('Customization', href: "/school/customize")
+
+    # Check out the course submenu.
+    find('a[title="Overview"]').click
+    click_link(course_1.name)
+    expect(page).to have_link('Students', href: "/school/courses/#{course_1.id}/students")
+    expect(page).to have_link('Coaches', href: "/school/courses/#{course_1.id}/coaches")
+    expect(page).to have_link('Curriculum', href: "/school/courses/#{course_1.id}/curriculum")
+
+    # Use the dropdown to navigate to the second course.
+    click_button course_1.name
+    click_link course_2.name
+
+    expect(page).to have_link('Curriculum', href: "/school/courses/#{course_2.id}/curriculum")
+  end
+
+  context 'when the user is also a student and a coach' do
+    let(:level) { create :level, course: course_1 }
+    let(:team) { create :startup, level: level }
+
+    let!(:student) { create :founder, startup: team, user: school_admin.user }
+    let(:coach) { create :faculty, school: school_1, user: school_admin.user }
+    let!(:coach_enrollment) { create :faculty_startup_enrollment, faculty: coach, startup: team }
+
+    scenario 'school admin visits the admin interface' do
+      sign_in_user school_admin.user
+
+      # User should be on the school admin overview page.
+      expect(current_path).to eq('/school')
+
+      # Navbar should also include links to home page and the coach's review dashboard.
+      expect(page).to have_link('Home', href: "/home")
+      expect(page).to have_link('Review Submissions', href: "/courses/#{course_1.id}/coach_dashboard")
+    end
+  end
+end

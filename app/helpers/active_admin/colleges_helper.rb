dd264df52c396469c054aeb0c847cc59e42fa03f
Hari Gopal <mail@harigopal.in>
2019-07-23 17:24:01 +0530

    Remove admissions section from superadmin interface

--- app/helpers/active_admin/colleges_helper.rb
+++
@@ -1,7 +0,0 @@
-module ActiveAdmin
-  module CollegesHelper
-    def admin_create_college_link(college_name)
-      "(#{link_to 'Create', new_admin_college_path(college: { name: college_name })})".html_safe
-    end
-  end
-end

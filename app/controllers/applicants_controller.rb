79fe0e656dc2305a8449843c22f4ee7584314090
Bodhish Thomas <bodhish@gmail.com>
2019-07-30 16:20:37 +0530

    Add create student service to handle signup

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -2,7 +2,14 @@ class ApplicantsController < ApplicationController
   # GET /enroll/:token
   def enroll
     @applicant = authorize(Applicant.find_by(login_token: params[:token]))
-    @course = @applicant.course
-    render 'courses/apply', layout: 'student'
+    if @applicant.present?
+      user = Applicants::CreateStudentService.new(@applicant).execute
+      sign_in user
+      flash[:success] = 'User authentication completed, You can start learning'
+      redirect_to after_sign_in_path_for(user)
+    else
+      flash[:error] = 'User authentication failed. The link you followed appears to be invalid.'
+      redirect_to new_user_session_path
+    end
   end
 end

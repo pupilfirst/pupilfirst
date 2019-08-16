b132ae809b933c8bad169a3c72a0933ce15162e6
Hari Gopal <mail@harigopal.in>
2019-08-16 15:06:46 +0530

    Add a spec that tests the level up button shown to students

--- spec/system/courses/curriculum_spec.rb
+++ spec/system/courses/curriculum_spec.rb
@@ -277,4 +277,33 @@ feature "Student's view of Course Curriculum", js: true do
       end
     end
   end
+
+  context 'when student is in level one with milestone targets complete' do
+    let(:team_l1) { create :team, level: level_1 }
+    let(:student_tl1) { create :student, startup: team_l1 }
+
+    before do
+      # Create the submission...
+      submission = create(:timeline_event, :latest, founders: [student_tl1], target: completed_target_l1, passed_at: 1.day.ago)
+      # ...and grade it.
+      create(:timeline_event_grade, timeline_event: submission, evaluation_criterion: evaluation_criterion, grade: 2)
+    end
+
+    scenario 'student levels up' do
+      sign_in_user student_tl1.user, referer: curriculum_course_path(course)
+
+      # Student should be on shown level 1.
+      expect(page).to have_text(completed_target_l1.title)
+
+      # There should be a level up notice on the page.
+      expect(page).to have_text('You have successfully completed all milestone targets required to level up.')
+      click_button('Level Up')
+
+      # Student should be on level 2 page.
+      expect(page).to have_text(completed_target_l2.title)
+
+      # The team should have leveled up.
+      expect(team_l1.reload.level).to eq(level_2)
+    end
+  end
 end

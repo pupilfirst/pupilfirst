b49d46e51d883ca25e59a7ccbff0ed995d438adb
Bodhish Thomas <bodhish@gmail.com>
2019-09-03 16:12:40 +0530

    Refactor create applicant mutator

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,20 +1,4 @@
 class UserMailer < SchoolMailer
-  # Mails sent directly to students.
-  class FounderMailer < SchoolMailer
-    # Mail sent a little while after the a confirmed connect request meeting occured.
-    #
-    # @param connect_request [ConnectRequest] Request for a meeting which recently occurred.
-    def connect_request_feedback(connect_request)
-      @connect_request = connect_request
-      @faculty = connect_request.faculty
-      # TODO: Do we need to send this to each founder?
-      @founder = connect_request.startup.founders.first
-      @school = @founder.school
-
-      simple_roadie_mail(@founder.email, "Feedback for your recent office hour with coach #{@faculty.name}")
-    end
-  end
-
   # Mail sent to community user when a new answer is posted to one of their questions.
   #
   # @param answer [Answer] The newly posted answer.

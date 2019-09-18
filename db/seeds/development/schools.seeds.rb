51704fcbb0c4bec6501e57183dc89b7e2d64f70f
Hari Gopal <mail@harigopal.in>
2019-09-18 14:23:33 +0530

    Seed generic logos instead of using the SV brand [skip ci]

--- db/seeds/development/schools.seeds.rb
+++ db/seeds/development/schools.seeds.rb
@@ -6,24 +6,24 @@ after 'schools' do
 # Attach a logo (on light) for school.
   unless school.logo_on_light_bg.attached?
     school.logo_on_light_bg.attach(
-      io: File.open(Rails.root.join('spec', 'support', 'uploads', 'files', 'logo_sv_on_light_bg.png')),
-      filename: 'logo_sv_on_light_bg.png'
+      io: File.open(Rails.root.join('spec', 'support', 'uploads', 'files', 'logo_lipsum_on_light_bg.png')),
+      filename: 'logo_lipsum_on_light_bg.png'
     )
   end
 
 # Attach a logo (on dark) for school.
   unless school.logo_on_dark_bg.attached?
     school.logo_on_dark_bg.attach(
-      io: File.open(Rails.root.join('spec', 'support', 'uploads', 'files', 'logo_sv_on_dark_bg.png')),
-      filename: 'logo_sv_on_dark_bg.png'
+      io: File.open(Rails.root.join('spec', 'support', 'uploads', 'files', 'logo_lipsum_on_dark_bg.png')),
+      filename: 'logo_lipsum_on_dark_bg.png'
     )
   end
 
 # Attach an icon for school.
   unless school.icon.attached?
     school.icon.attach(
-      io: File.open(Rails.root.join('spec', 'support', 'uploads', 'files', 'icon_sv.png')),
-      filename: 'icon_sv.png'
+      io: File.open(Rails.root.join('spec', 'support', 'uploads', 'files', 'icon_pupilfirst.png')),
+      filename: 'icon_pupilfirst.png'
     )
   end
 

d8d2c5060ba7373fa156dfa5ea504d0a259d9bcc
Hari Gopal <mail@harigopal.in>
2019-10-04 12:15:21 +0530

    Make markdown attachments readable by the public
    Also spec the policy to cover cases not covered by integration tests.

---
+++ spec/factories/markdown_attachment.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :markdown_attachment do
+    file { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'uploads', 'resources', 'pdf-sample.pdf')) }
+    token { SecureRandom.urlsafe_base64 }
+    user
+  end
+end

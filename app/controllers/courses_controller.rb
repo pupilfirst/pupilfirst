43267864ad1e5c3724ded08ce1d38beb79943b45
Hari Gopal <mail@harigopal.in>
2019-10-08 16:36:29 +0530

    Minor DRY in CoursesController

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,32 +3,39 @@ class CoursesController < ApplicationController
 
   # GET /courses/:id/curriculum
   def curriculum
-    @course = authorize(policy_scope(Course).find(params[:id]))
+    @course = find_course
     @presenter = Courses::CurriculumPresenter.new(view_context, @course)
     render layout: 'student_course'
   end
 
   # GET /courses/:id/leaderboard?weeks_before=
   def leaderboard
-    @course = authorize(policy_scope(Course).find(params[:id]))
+    @course = find_course
     @on = params[:on]
     render layout: 'student_course'
   end
 
   # GET /courses/:id/apply
   def apply
-    @course = authorize(policy_scope(Course).find(params[:id]))
+    @course = find_course
     render layout: 'student'
   end
 
   # GET /courses/:id/(:slug)
   def show
-    @course = authorize(policy_scope(Course).find(params[:id]))
+    @course = find_course
     render layout: 'student'
   end
 
+  # GET /courses/:id/review
   def review
-    @course = authorize(policy_scope(Course).find(params[:id]))
+    @course = find_course
     render layout: 'student_course'
+  end
+
+  private
+
+  def find_course
+    authorize(policy_scope(Course).find(params[:id]))
   end
 end

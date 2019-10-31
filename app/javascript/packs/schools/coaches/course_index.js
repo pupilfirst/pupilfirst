9e211c2794c51ea604181a7c94a0c00c0a1244cc
Hari Gopal <mail@harigopal.in>
2019-10-31 14:37:24 +0530

    Remove all JS pack files that use ReasonReact components
    This replaces them with Reason pack files to help with #104. I've also
    added titles to school admin pages where they were missing.

--- app/javascript/packs/schools/coaches/course_index.js
+++
@@ -1,9 +0,0 @@
-import "schools/coaches/index.css";
-import "schools/shared/shared.css";
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { jsComponent } from "../../../schools/coaches/components/SA_Coaches_CourseIndex.bs";
-
-const root = document.getElementById("sa-coaches-enrollment-panel");
-const props = JSON.parse(root.dataset.props);
-ReactDOM.render(React.createElement(jsComponent, props), root);

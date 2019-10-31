c9803ff985ee6355a6fd92169edca63757430202
Bodhish Thomas <bodhish@gmail.com>
2019-10-31 18:05:49 +0530

    Fix a bug in create grading mutator

--- app/queries/create_grading_mutator.rb
+++ app/queries/create_grading_mutator.rb
@@ -11,7 +11,7 @@ class CreateGradingMutator < ApplicationQuery
   validate :should_not_be_graded
   validate :valid_evaluation_criteria
   validate :valid_grading
-  validates :feedback, presence: true, length: { maximum: 10_000 }
+  validates :feedback, length: { maximum: 10_000 }
 
   def grade
     TimelineEvent.transaction do

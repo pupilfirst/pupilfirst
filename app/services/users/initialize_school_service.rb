a688d7c46e07fc86761431347295e674030c587e
Bodhish Thomas <bodhish@gmail.com>
2019-11-19 15:10:48 +0530

    Create users initialize school service #123

---
+++ app/services/users/initialize_school_service.rb
@@ -0,0 +1,46 @@
+module Users
+  class InitializeSchoolService
+    def initialize(user_id, course_id)
+      @user = User.find(user_id)
+      @course = Course.find(course_id)
+    end
+
+    def execute
+      User.transaction do
+        new_course = Courses::CloneService.new(@course).clone(@course.name, @user.school)
+        create_and_assign_coach(new_course)
+        create_student(new_course)
+        create_community(new_course)
+      end
+    end
+
+    private
+
+    def create_and_assign_coach(course)
+      coach = Faculty.create!(
+        user: @user,
+        school: @user.school,
+        category: Faculty::CATEGORY_VISITING_COACHES
+      )
+      Courses::AssignReviewerService.new(course).assign(coach)
+    end
+
+    def create_team(course)
+      team = Startup.create!(
+        name: @user.name,
+        level: course.levels.find_by(number: 1)
+      )
+      team
+    end
+
+    def create_student(course)
+      team = create_team(course)
+      Founder.create!(user: @user, startup: team)
+    end
+
+    def create_community(course)
+      community = @user.school.communities.create!(name: "Demo Community", target_linkable: true)
+      CommunityCourseConnection.create!(course: course, community: community)
+    end
+  end
+end

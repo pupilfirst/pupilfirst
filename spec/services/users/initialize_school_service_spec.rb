0f0676fd3d70a4f6a69c61c30c96c7653efde83e
Bodhish Thomas <bodhish@gmail.com>
2019-11-19 15:11:03 +0530

    Add spec for initialize school service #123

---
+++ spec/services/users/initialize_school_service_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe Users::InitializeSchoolService do
+  subject { described_class.new(user.id, course.id) }
+
+  let(:school) { create :school }
+  let(:new_school) { create :school }
+
+  let!(:user) { create :user, school: new_school }
+  let(:course) { create :course, school: school }
+
+  let(:level_one) { create :level, :one, course: course }
+  let(:target_group_l1_1) { create :target_group, level: level_one, milestone: true }
+  let!(:target_l1_1_1) { create :target, :with_content, :for_startup, target_group: target_group_l1_1 }
+
+  let(:new_name) { Faker::Lorem.words(2).join(' ') }
+
+  describe '#execute' do
+    it 'create initializes a school with a new course, coach, student and community' do
+      # user should not have a student or coach profile
+      expect(user.founders).to eq([])
+      expect(user.faculty).to eq(nil)
+      expect(new_school.courses.count).to eq(0)
+
+      subject.execute
+
+      # should create a student profile
+      expect(user.reload.founders.count).to eq(1)
+      # should create a coach profile
+      expect(user.faculty).not_to eq(nil)
+      # should clone the course to new school
+      expect(new_school.courses.count).to eq(1)
+      expect(new_school.courses.first.slice(:name, :description)).to match_array(course.slice(:name, :description))
+      # coach should be assigned to the new course
+      expect(user.faculty.courses.first).to eq(new_school.courses.first)
+      # new school should have a community
+      expect(new_school.communities.count).to eq(1)
+      # community should be linked to the new course
+      expect(new_school.communities.first.courses).to eq(new_school.courses)
+    end
+  end
+end

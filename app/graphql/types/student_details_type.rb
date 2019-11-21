c85b3d628f09e13b87e7bd6a3d28fb52363a62e8
Mahesh Krishna Kumar <mahesh@sv.co>
2019-11-21 17:12:25 +0530

    Modify student details type to load only required info

--- app/graphql/types/student_details_type.rb
+++ app/graphql/types/student_details_type.rb
@@ -1,13 +1,16 @@
 module Types
   class StudentDetailsType < Types::BaseObject
+    field :name, String, null: false
     field :title, String, null: false
     field :email, String, null: false
     field :phone, String, null: true
-    field :coach_notes, [Types::CoachNoteType], null: true
-    field :submissions, [Types::StudentSubmissionType], null: true
+    field :coach_notes, [Types::CoachNoteType], null: false
     field :level_id, ID, null: false
-    field :social_links, [String], null: true
+    field :social_links, [String], null: false
     field :evaluation_criteria, [Types::EvaluationCriteriaType], null: false
-    field :grades, [Types::GradeType], null: false
+    field :targets_completed, Integer, null: false
+    field :total_targets, Integer, null: false
+    field :quiz_scores, [String], null: false
+    field :average_grades, [Types::EvaluationCriterionAverageType], null: false
   end
 end

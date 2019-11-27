b9ac1bcd3dafc0acc0677d09ab6f1eafc030f788
Mahesh Krishna Kumar <mahesh@sv.co>
2019-11-27 12:21:37 +0530

    Avoid N+1 in student submissions query

--- app/queries/student_submissions_resolver.rb
+++ app/queries/student_submissions_resolver.rb
@@ -2,7 +2,7 @@ class StudentSubmissionsResolver < ApplicationQuery
   property :student_id
 
   def student_submissions
-    student.timeline_events.evaluated_by_faculty.includes(:timeline_event_grades)
+    student.timeline_events.evaluated_by_faculty.includes(target: :level)
   end
 
   def authorized?

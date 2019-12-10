c28f2ab366a102ebf36e0b835d67c7759a47f788
Mahesh Krishna Kumar <mahesh@sv.co>
2019-12-10 12:07:31 +0530

    Precompile graphiql assets in development

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile graphiql assets in development. Needed for graphiql to work with Sprockets 4.0
+Rails.application.config.assets.precompile += %w[graphiql/rails/application.js graphiql/rails/application.css] if Rails.env.development?

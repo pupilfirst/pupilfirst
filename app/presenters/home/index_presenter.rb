292412df08d781beedded2d25eeb9ca0bb7f3610
Bodhish Thomas <bodhish@gmail.com>
2019-12-12 13:15:10 +0530

    Implement the home index design

---
+++ app/presenters/home/index_presenter.rb
@@ -0,0 +1,31 @@
+module Home
+  class IndexPresenter < ApplicationPresenter
+    def initialize(view_context, school)
+      super(view_context)
+
+      @school = school
+    end
+
+    def page_title
+      @school.name
+    end
+
+    alias school_name page_title
+
+    def about
+      @school.about.presence || "Sample about"
+    end
+
+    def courses
+      @school.courses.where(featured: true)
+    end
+
+    def cover_image
+      view.url_for(@school.cover_image) if @school.cover_image.attached?
+    end
+
+    def course_thumbnail(course)
+      view.url_for(course.thumbnail) if course.thumbnail.attached?
+    end
+  end
+end

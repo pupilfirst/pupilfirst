a698649f484fb624abaa10f19d798db2c7452753
Mahesh Krishna Kumar <mahesh@sv.co>
2019-12-18 14:28:09 +0530

    Refactor migration for adding grade fields to criterion

--- db/migrate/20191204105550_add_grade_columns_to_evaluation_criteria.rb
+++ db/migrate/20191204105550_add_grade_columns_to_evaluation_criteria.rb
@@ -3,15 +3,18 @@ class AddGradeColumnsToEvaluationCriteria < ActiveRecord::Migration[6.0]
   end
 
   class EvaluationCriterion < ActiveRecord::Base
+    belongs_to :course
   end
 
   def change
     add_column :evaluation_criteria, :max_grade, :integer
     add_column :evaluation_criteria, :pass_grade, :integer
     add_column :evaluation_criteria, :grade_labels, :json
+
     EvaluationCriterion.reset_column_information
-    EvaluationCriterion.all.each do |ec|
-      ec.update_attributes!(Course.find(ec.course_id).slice(:max_grade, :pass_grade, :grade_labels))
+
+    EvaluationCriterion.includes(:course).all.each do |ec|
+      ec.update!(ec.course.slice(:max_grade, :pass_grade, :grade_labels))
     end
   end
 end

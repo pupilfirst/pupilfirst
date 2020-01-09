c37746bed10058984b29e64aa78b822dd1bf8dc4
Mahesh Krishna Kumar <mahesh@sv.co>
2020-01-09 16:44:21 +0530

    Remove data_migrate gem and migration files

--- db/data/20181120103330_remove_unused_target_roles.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUnusedTargetRoles < ActiveRecord::Migration[5.2]
-  def up
-    Target.where.not(role: Target::ROLE_FOUNDER).update_all(role: Target::ROLE_TEAM)
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end

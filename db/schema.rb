b76f8fc5782cb2eff630229a53098f2aafde0cce
Mahesh Krishna Kumar <mahesh@sv.co>
2020-01-10 12:45:15 +0530

    Add index to CoachNote#archived_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_111011) do
+ActiveRecord::Schema.define(version: 2020_01_10_071219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(version: 2020_01_09_111011) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "archived_at"
+    t.index ["archived_at"], name: "index_coach_notes_on_archived_at"
     t.index ["author_id"], name: "index_coach_notes_on_author_id"
     t.index ["student_id"], name: "index_coach_notes_on_student_id"
   end

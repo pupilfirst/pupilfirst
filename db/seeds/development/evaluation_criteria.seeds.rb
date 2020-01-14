43f00ea5e3a2aefe322344fc32746970be06f6de
Mahesh Krishna Kumar <mahesh@sv.co>
2020-01-14 23:12:41 +0530

    Remove description from evaluation criterion seeds [skip ci]

--- db/seeds/development/evaluation_criteria.seeds.rb
+++ db/seeds/development/evaluation_criteria.seeds.rb
@@ -6,6 +6,6 @@ after 'development:courses' do
   grade_labels = [{ 'grade' => 1, 'label' => 'Not Accepted' }, { 'grade' => 2, 'label' => 'Needs Improvement' }, { 'grade' => 3, 'label' => 'Meets Expectations' }, { 'grade' => 4, 'label' => 'Exceeds Expectations' }]
 
   Course.all.each do |course|
-    EvaluationCriterion.create!(name: 'Quality', description: 'The default evaluation criteria', max_grade: 4, pass_grade: 2, grade_labels: grade_labels, course: course)
+    EvaluationCriterion.create!(name: 'Quality', max_grade: 4, pass_grade: 2, grade_labels: grade_labels, course: course)
   end
 end

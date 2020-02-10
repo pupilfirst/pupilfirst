c3d8f836782e5973fde50c55535b4613e198a261
Bodhish Thomas <bodhish@gmail.com>
2020-02-10 15:06:47 +0530

    Remove latest_version from content block

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -6,7 +6,7 @@ class ContentBlock < ApplicationRecord
 
   has_one_attached :file
   has_many :content_versions, dependent: :restrict_with_error
-  belongs_to :target_version, optional: true
+  belongs_to :target_version
 
   def self.valid_block_types
     [BLOCK_TYPE_MARKDOWN, BLOCK_TYPE_IMAGE, BLOCK_TYPE_EMBED, BLOCK_TYPE_FILE]
@@ -14,11 +14,6 @@ class ContentBlock < ApplicationRecord
 
   validates :block_type, inclusion: { in: valid_block_types }
   validates :content, presence: true
-
-  # TODO: Fix this when new target_versions table is introduced.
-  def latest_version
-    content_versions.order(version_on: :DESC).first
-  end
 
   def file?
     BLOCK_TYPE_FILE == block_type

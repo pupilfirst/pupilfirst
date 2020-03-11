d1c3e4ffe56cffb54348322078d7d8e39b9bf9ed
Hari Gopal <mail@harigopal.in>
2020-03-11 17:41:09 +0530

    Update the school admin course students index spec

--- spec/system/school/courses/students_index_spec.rb
+++ spec/system/school/courses/students_index_spec.rb
@@ -18,6 +18,7 @@ feature 'School students index', js: true do
 
   let!(:startup_1) { create :startup, level: level_1 }
   let!(:startup_2) { create :startup, level: level_2 }
+  let(:team_in_different_course) { create :team }
 
   let(:team_with_lone_student) { create :team, level: level_2 }
   let!(:lone_student) { create :founder, startup: team_with_lone_student, tag_list: tags }
@@ -33,11 +34,11 @@ feature 'School students index', js: true do
   let!(:new_team_name) { (Faker::Lorem.words(number: 4).join ' ').titleize }
 
   let!(:course_coach) { create :faculty, school: school }
-  let!(:coach) { create :faculty, school: school }
-  let!(:exited_coach) { create :faculty, school: school, exited: true }
+  let!(:coach_in_different_course) { create :faculty, school: school }
 
   before do
-    FacultyCourseEnrollment.create(faculty: course_coach, course: course)
+    create :faculty_course_enrollment, faculty: course_coach, course: course
+    create :faculty_course_enrollment, faculty: coach_in_different_course, course: team_in_different_course.course
   end
 
   scenario 'School admin adds new students and a team' do
@@ -306,18 +307,21 @@ feature 'School students index', js: true do
       # Assign a coach to a team
       founder = startup_2.founders.last
       find("a", text: founder.user.name).click
-      expect(page).to have_text('Team Coaches')
 
-      within '.select-list__group' do
-        expect(page).to_not have_text(exited_coach.name)
-        find('.px-3', text: coach.name).click
-      end
+      # Coach in a different course must not be listed.
+      expect(page).to have_text('Team Coaches')
+      expect(page).not_to have_text(coach_in_different_course.name)
+
+      # But it should be possible to assign a coach in 'this' course.
+      find("div[title='Select #{course_coach.name}']").click
 
       click_button 'Update Student'
+
       expect(page).to have_text("Student updated successfully")
+
       dismiss_notification
-      founder.reload
-      expect(founder.startup.faculty.last).to eq(coach)
+
+      expect(founder.reload.startup.faculty.find_by(id: course_coach)).to be_present
     end
   end
 
@@ -358,9 +362,9 @@ feature 'School students index', js: true do
 
   scenario 'school admin marks students as dropped out' do
     # Enroll the coach as a team coach in all three teams.
-    create :faculty_startup_enrollment, :with_course_enrollment, faculty: coach, startup: startup_1
-    create :faculty_startup_enrollment, :with_course_enrollment, faculty: coach, startup: startup_2
-    create :faculty_startup_enrollment, :with_course_enrollment, faculty: coach, startup: team_with_lone_student
+    create :faculty_startup_enrollment, :with_course_enrollment, faculty: course_coach, startup: startup_1
+    create :faculty_startup_enrollment, :with_course_enrollment, faculty: course_coach, startup: startup_2
+    create :faculty_startup_enrollment, :with_course_enrollment, faculty: course_coach, startup: team_with_lone_student
 
     sign_in_user school_admin.user, referer: school_course_students_path(course)
 
@@ -372,7 +376,7 @@ feature 'School students index', js: true do
 
     expect(page).to have_text(founder_user.name)
     expect(page).to have_text(founder.startup.name)
-    expect(coach.startups.count).to eq(3)
+    expect(course_coach.startups.count).to eq(3)
 
     click_button 'Actions'
     click_button 'Dropout Student'
@@ -391,7 +395,7 @@ feature 'School students index', js: true do
     expect(founder.startup.faculty.count).to eq(0)
 
     # However the coach should still be linked to the same number of teams.
-    expect(coach.startups.count).to eq(3)
+    expect(course_coach.startups.count).to eq(3)
 
     # Mark a student who is alone in a team as dropped out.
     expect(team_with_lone_student.faculty.count).to eq(1)
@@ -410,7 +414,7 @@ feature 'School students index', js: true do
 
     # All coaches should have been removed from the team.
     expect(lone_user_team.faculty.count).to eq(0)
-    expect(coach.startups.count).to eq(2)
+    expect(course_coach.startups.count).to eq(2)
   end
 
   scenario 'user who is not logged in gets redirected to sign in page' do

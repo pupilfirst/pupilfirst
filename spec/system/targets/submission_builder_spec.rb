48b3927170cd53ce0f51219a1babe985b1967014
Hari Gopal <mail@harigopal.in>
2020-04-20 16:19:15 +0530

    Reduce the likelihood that test will generate answers with same phrase

--- spec/system/targets/submission_builder_spec.rb
+++ spec/system/targets/submission_builder_spec.rb
@@ -188,7 +188,7 @@ feature 'Submission Builder', js: true do
 
   scenario 'student submits a target with an MCQ' do
     question = Faker::Lorem.sentence
-    choices = Faker::Lorem.words
+    choices = Faker::Lorem.sentences(number: 4)
     target.update!(checklist: [{ title: question, kind: Target::CHECKLIST_KIND_MULTI_CHOICE, optional: false, metadata: { choices: choices } }])
     answer = choices.last
 

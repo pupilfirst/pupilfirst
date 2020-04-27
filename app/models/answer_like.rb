74a6b32b9b68082051d35d9063b454acde0da773
Mahesh Krishna Kumar <mahesh@sv.co>
2020-04-27 14:31:15 +0530

    Cleanup few unused files from old community

--- app/models/answer_like.rb
+++
@@ -1,6 +0,0 @@
-class AnswerLike < ApplicationRecord
-  belongs_to :user
-  belongs_to :answer
-
-  validates :answer_id, uniqueness: { scope: :user_id }
-end
--- spec/factories/question.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :question do
-    title { Faker::Lorem.sentence }
-    description { Faker::Lorem.paragraph }
-    community
-  end
-end

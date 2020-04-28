8a5da9a93152833d20b899a29dde838022b83b83
Hari Gopal <mail@harigopal.in>
2020-04-28 17:01:18 +0530

    Account for new course columns in clone service

--- spec/factories/course.rb
+++ spec/factories/course.rb
@@ -3,5 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| [Faker::Lorem.word, n.to_s].join(' ') }
     description { Faker::Lorem.sentence }
     school { School.find_by(name: 'test') || create(:school, :current) }
+    progression_behavior { Course::PROGRESSION_BEHAVIOR_LIMITED }
+    progression_limit { 1 }
   end
 end

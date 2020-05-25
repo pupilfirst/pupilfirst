4b2c33b5012747d44fd9e067893b5cb74777bd6b
Hari Gopal <mail@harigopal.in>
2020-05-25 17:23:32 +0530

    Remove usages of key_skills

--- app/admin/faculty.rb
+++ app/admin/faculty.rb
@@ -1,7 +1,4 @@
 ActiveAdmin.register Faculty do
-  permit_params :name, :title, :key_skills, :category, :image, :sort_index, :self_service,
-    :current_commitment, :notify_for_submission, :about, :commitment, :compensation, :slack_username, :public, :connect_link
-
   actions :index, :show
 
   controller do
@@ -15,7 +12,6 @@ ActiveAdmin.register Faculty do
   filter :public
   filter :user_email, as: :string
   filter :title
-  filter :key_skills
 
   scope :all
 
@@ -38,7 +34,6 @@ ActiveAdmin.register Faculty do
       row :title
       row :email
       row :about
-      row :key_skills
       row :category
 
       row :image do
@@ -68,31 +63,5 @@ ActiveAdmin.register Faculty do
 
       row :user
     end
-  end
-
-  form do |f|
-    div id: 'admin-faculty__form'
-
-    f.semantic_errors(*f.object.errors.keys)
-
-    f.inputs 'Faculty Details' do
-      f.input :category, as: :select, collection: Faculty.valid_categories
-      f.input :name
-      f.input :title
-      f.input :about
-      f.input :image, as: :file
-      f.input :key_skills
-      f.input :sort_index
-      f.input :public
-      f.input :notify_for_submission
-      f.input :self_service
-      f.input :commitment, as: :select, collection: commitment_options, label_method: :first, value_method: :last
-      f.input :current_commitment
-      f.input :compensation, as: :select, collection: Faculty.valid_compensation_values
-      f.input :slack_username
-      f.input :connect_link
-    end
-
-    f.actions
   end
 end

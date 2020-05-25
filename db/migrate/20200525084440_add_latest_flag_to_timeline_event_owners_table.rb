0537df6940d5fa6083011f0ea8b8801b636f199b
Bodhish Thomas <bodhish@gmail.com>
2020-05-25 18:24:57 +0530

    Update data migration

--- db/migrate/20200525084440_add_latest_flag_to_timeline_event_owners_table.rb
+++ db/migrate/20200525084440_add_latest_flag_to_timeline_event_owners_table.rb
@@ -1,5 +1,19 @@
 class AddLatestFlagToTimelineEventOwnersTable < ActiveRecord::Migration[6.0]
+  class TimelineEvent < ApplicationRecord
+    has_many :timeline_event_owners
+  end
+
+  class TimelineEventOwner < ApplicationRecord
+    belongs_to :timeline_event
+  end
+
   def change
     add_column :timeline_event_owners, :latest, :boolean, default: false
+
+    TimelineEventOwner.reset_column_information
+
+    TimelineEvent.where(latest: true).map do |submission|
+      submission.timeline_event_owners.update_all(latest: true)
+    end
   end
 end

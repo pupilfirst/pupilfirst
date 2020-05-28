94c8abea2a8993c8ca77594a21d52e81bfbf444f
Hari Gopal <mail@harigopal.in>
2020-05-28 23:53:29 +0530

    Begin by porting students' tags to their team

---
+++ db/migrate/20200527131110_move_founder_tags_to_startup.rb
@@ -0,0 +1,41 @@
+class MoveFounderTagsToStartup < ActiveRecord::Migration[6.0]
+  class Startup < ApplicationRecord
+    acts_as_taggable
+
+    def self.name
+      'Startup'
+    end
+  end
+
+  class Founder < ApplicationRecord
+    acts_as_taggable
+
+    belongs_to :startup
+
+    def self.name
+      'Founder'
+    end
+  end
+
+  def up
+    Founder.joins(:taggings).includes(:tags, startup: :tags).each do |student|
+      team = student.startup
+      student_tags = student.tags.pluck(:name)
+      team_tags = team.tags.pluck(:name)
+
+      # If this student has any tag that the team doesn't have, add it to the team.
+      if (student_tags - team_tags).any?
+        team.tag_list.add(*student_tags)
+        team.save!
+      end
+    end
+
+    # ############################## #
+    # TODO: Remove all founder tags. #
+    # ############################## #
+  end
+
+  def down
+    # noop
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_120747) do
+ActiveRecord::Schema.define(version: 2020_05_27_131110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"

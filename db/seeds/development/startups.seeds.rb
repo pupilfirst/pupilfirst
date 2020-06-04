9203b9dea5f19b8cbcd42335a8df1f6cf48e0bc7
Hari Gopal <mail@harigopal.in>
2020-06-04 17:37:15 +0530

    Seed some team tags in dev environment

--- db/seeds/development/startups.seeds.rb
+++ db/seeds/development/startups.seeds.rb
@@ -4,6 +4,7 @@ after 'development:levels', 'development:users' do
   puts 'Seeding startups'
 
   admin_user = User.find_by(email: 'admin@example.com')
+  available_tags = admin_user.school.founder_tag_list.to_a
 
   admin_user.school.courses.each do |course|
     Startup.create!(name: admin_user.name, level: course.levels.first)
@@ -12,6 +13,6 @@ after 'development:levels', 'development:users' do
   (1..3).each do |index|
     student_user = User.find_by(email: "student#{index}@example.com")
     level = student_user.school.courses.first.levels.first
-    Startup.create!(name: student_user.name, level: level)
+    Startup.create!(name: student_user.name, level: level, tag_list: available_tags.sample(2))
   end
 end

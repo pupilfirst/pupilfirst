288b82d4a47f3e78d6645095f93b451d105fc07e
Mahesh Krishna Kumar <maheshcet@gmail.com>
2020-06-11 00:30:41 +0530

    Allow null for user in issued certificates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_110209) do
+ActiveRecord::Schema.define(version: 2020_06_10_184033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -350,7 +350,7 @@ ActiveRecord::Schema.define(version: 2020_06_07_110209) do
 
   create_table "issued_certificates", force: :cascade do |t|
     t.bigint "certificate_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.string "name", null: false
     t.citext "serial_number", null: false
     t.datetime "created_at", precision: 6, null: false

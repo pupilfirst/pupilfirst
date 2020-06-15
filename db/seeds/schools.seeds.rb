5fbce156140c545d00e3faf177c59aaccc3e8121
Hari Gopal <mail@harigopal.in>
2020-06-15 22:19:22 +0530

    Seed student tags only in development environment [skip ci]

--- db/seeds/schools.seeds.rb
+++ db/seeds/schools.seeds.rb
@@ -1,5 +1,3 @@
 puts 'Seeding schools (production, idempotent)'
 
-tags = (1..10).map { Faker::Lorem.words(number: 2).join(' ') }
-
-school = School.where(name: 'Test School').first_or_create!(founder_tag_list: tags)
+School.where(name: 'Test School').first_or_create!

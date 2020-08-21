c354828149f88b4eeb5da6b209b6756027aa1421
Bodhish Thomas <bodhish@gmail.com>
2020-08-21 14:00:28 +0530

    Create a webhook event for student submission

--- app/models/webhook_entry.rb
+++ app/models/webhook_entry.rb
@@ -2,4 +2,10 @@ class WebhookEntry < ApplicationRecord
   belongs_to :school
 
   scope :pending, -> { where.not(sent_at: nil) }
+
+  SUBMISSION_CREATED_EVENT = 'submission.created'
+
+  def self.valid_event_types
+    [SUBMISSION_CREATED_EVENT].freeze
+  end
 end

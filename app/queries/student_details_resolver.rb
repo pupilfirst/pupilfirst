728e5e22335d3c52a1c6279b66a7f4f06cd9e8dc
Mahesh Krishna Kumar <maheshcet@gmail.com>
2020-09-02 23:28:31 +0530

    Remove redundant check from query

--- app/queries/student_details_resolver.rb
+++ app/queries/student_details_resolver.rb
@@ -78,7 +78,7 @@ class StudentDetailsResolver < ApplicationQuery
   end
 
   def submissions
-    @submissions ||= student.timeline_events.joins(target: :level).where(targets: { id: current_course_targets }).where.not(levels: { number: 0 })
+    @submissions ||= student.timeline_events.joins(target: :level).where(targets: { id: current_course_targets })
   end
 
   def submissions_for_grades

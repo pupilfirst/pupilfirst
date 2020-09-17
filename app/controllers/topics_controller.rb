69e0bd5f5603734af3dedd810acbbd61f9b81ba0
Hari Gopal <mail@harigopal.in>
2020-09-17 15:52:33 +0530

    Count and show topic views

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,5 +4,17 @@ class TopicsController < ApplicationController
 
   def show
     @topic = authorize(Topic.live.find(params[:id]))
+
+    key = views_cache_key(@topic)
+
+    # Increment views only once per hour per user.
+    return if Rails.cache.exist?(key)
+
+    @topic.increment!(:views) # rubocop:disable Rails/SkipsModelValidations
+    Rails.cache.write(key, 1, expires_in: 1.hour)
+  end
+
+  def views_cache_key(topic)
+    "topics/#{topic.id}/views/#{current_user.id}"
   end
 end

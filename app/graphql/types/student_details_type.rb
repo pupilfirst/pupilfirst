15ff53ed5659fe9b6ec96e8170be7cae81611361
Bodhish Thomas <bodhish@gmail.com>
2020-09-24 17:44:36 +0530

    Expand target overview stats in students report (#482)

--- app/graphql/types/student_details_type.rb
+++ app/graphql/types/student_details_type.rb
@@ -3,6 +3,7 @@ module Types
     field :email, String, null: false
     field :evaluation_criteria, [Types::EvaluationCriterionType], null: false
     field :targets_completed, Integer, null: false
+    field :targets_pending_review, Integer, null: false
     field :total_targets, Integer, null: false
     field :quiz_scores, [String], null: false
     field :average_grades, [Types::EvaluationCriterionAverageType], null: false

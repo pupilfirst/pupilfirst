051926348952a36387584b98db338e5092e72cb4
stomp <arthurstomp@gmail.com>
2020-10-29 13:57:42 +0100

    feat: Docker compose with postgresql, redis, keycloak

---
+++ docker_postgres_init.sql
@@ -0,0 +1,10 @@
+CREATE USER keycloak WITH PASSWORD 'password' CREATEDB;
+CREATE DATABASE keycloak
+    WITH
+    OWNER = keycloak
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+

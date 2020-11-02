fa63b4ffad235ea3d77618d9008b13440a83888a
Hari Gopal <mail@harigopal.in>
2020-11-02 19:49:34 +0530

    Validate presence of certificate before attempting to issue it to student

--- app/queries/issue_certificate_mutator.rb
+++ app/queries/issue_certificate_mutator.rb
@@ -3,7 +3,8 @@ class IssueCertificateMutator < ApplicationQuery
   property :student_id, validates: { presence: true }
   property :certificate_id, validates: { presence: true }
 
-  validate :issued_certificate_not_present
+  validate :student_must_not_have_issued_certificate
+  validate :certificate_must_be_present
 
   def execute
     Certificate.transaction do
@@ -13,10 +14,16 @@ class IssueCertificateMutator < ApplicationQuery
 
   private
 
-  def issued_certificate_not_present
+  def student_must_not_have_issued_certificate
     return if student.user.issued_certificates.where(certificate: course.certificates, revoked_at: nil).empty?
 
     errors[:base] << I18n.t('queries.issue_certificate_mutator.issued_error')
+  end
+
+  def certificate_must_be_present
+    return if certificate.present?
+
+    errors[:base] << I18n.t('queries.issue_certificate_mutator.certificate_error')
   end
 
   def resource_school

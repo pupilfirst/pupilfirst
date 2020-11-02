7feea06747985994afbdb7fde91f136feea75f20
Hari Gopal <mail@harigopal.in>
2020-11-02 17:48:32 +0530

    Check revoked_at in the query

--- app/controllers/issued_certificates_controller.rb
+++ app/controllers/issued_certificates_controller.rb
@@ -1,8 +1,8 @@
 class IssuedCertificatesController < ApplicationController
   # GET /c/:serial_number
   def verify
-    @issued_certificate = IssuedCertificate.find_by(serial_number: params[:serial_number])
-    raise_not_found if @issued_certificate.blank? || @issued_certificate.revoked_at.present?
+    @issued_certificate = IssuedCertificate.find_by(serial_number: params[:serial_number], revoked_at: nil)
+    raise_not_found if @issued_certificate.blank?
     render layout: 'student'
   end
 end

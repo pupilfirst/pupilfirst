42de2f3f8860526dbf235add235684392994067a
stomp <arthurstomp@gmail.com>
2020-11-11 22:46:55 +0100

    feat: Keycloak helper to create users

---
+++ app/helpers/keycloak_helper.rb
@@ -0,0 +1,14 @@
+require 'keycloak'
+
+module KeycloakHelper
+    def create_keycloak_user(email, name)
+      names = name.split(' ')
+      first_name = names.pop
+      last_name = names.join(' ') || ''
+      keycloak_client.create_user(email, first_name, last_name)
+    end
+
+    def keycloak_client
+      @keycloak_client ||= Keycloak::Client.new
+    end
+end

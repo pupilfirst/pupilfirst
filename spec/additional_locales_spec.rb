b02fd3c0d87aec3730397e8bc4968114ebf49bac
Mirosław Pragłowski <m@praglowski.com>
2021-01-07 16:05:29 +0100

    Setup expectations how transalations should behave

---
+++ spec/additional_locales_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'locales' do
+  it 'use our locale if defined' do
+    expect(I18n.t('components.CurriculumEditor.level')).to eq('Chapter')
+  end
+
+  it 'use original locale if our is not defined' do
+    expect(I18n.t('shared.cancel')).to eq('Cancel')
+  end
+end

bbf98e41d33c7c99a534147ea1d57949c8cc9119
Mirosław Pragłowski <m@praglowski.com>
2021-01-28 17:45:11 +0100

    Level up -> Next chapter rename

--- spec/system/courses/curriculum_spec.rb
+++ spec/system/courses/curriculum_spec.rb
@@ -194,7 +194,7 @@ feature "Student's view of Course Curriculum", js: true do
     # Reviewed targets, even those without prerequisites, must be locked.
     click_link l5_reviewed_target.title
 
-    expect(page).to have_content('You must level up to complete this lesson')
+    expect(page).to have_content('You must finish the previous chapter to access this lesson')
 
     click_button 'Close'
   end

34e1d82f025519064aa2835b6c9999e015ab34ea
Mirosław Pragłowski <m@praglowski.com>
2021-02-05 11:34:04 +0100

    Fix mistakes from merge conflict resolutions

--- spec/additional_locales_spec.rb
+++ spec/additional_locales_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'locales' do
   it 'use our locale if defined' do
-    expect(I18n.t('shared.level.label')).to eq('Chapter')
+    expect(I18n.t('shared.level')).to eq('Chapter')
   end
 
   it 'use original locale if our is not defined' do

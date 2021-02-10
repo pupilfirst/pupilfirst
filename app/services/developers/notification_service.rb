0f2089594cbf52101004bd21c5600b23add3ef25
Mirosław Pragłowski <m@praglowski.com>
2021-02-10 14:22:27 +0100

    Allow to compose services here

--- app/services/developers/notification_service.rb
+++ app/services/developers/notification_service.rb
@@ -1,7 +1,11 @@
 module Developers
   class NotificationService
+    def initialize(webhook_service: WebhookDeliveries::CreateService.new)
+      @webhook_service = webhook_service
+    end
+
     def execute(context, event_type, _actor, resource)
-      WebhookDeliveries::CreateService.new.execute(context, event_type, resource)
+      @webhook_service.execute(context, event_type, resource)
     end
   end
 end

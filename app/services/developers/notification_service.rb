57a455c3517e03997d931e8b3b920079c85fff6c
Mirosław Pragłowski <m@praglowski.com>
2021-02-10 14:07:57 +0100

    Course is the context here, in which event is published.
    I could imagine having different contexts here

--- app/services/developers/notification_service.rb
+++ app/services/developers/notification_service.rb
@@ -1,7 +1,7 @@
 module Developers
   class NotificationService
-    def execute(course, event_type, _actor, resource)
-      WebhookDeliveries::CreateService.new.execute(course, event_type, resource)
+    def execute(context, event_type, _actor, resource)
+      WebhookDeliveries::CreateService.new.execute(context, event_type, resource)
     end
   end
 end
--- app/services/webhook_deliveries/create_service.rb
+++ app/services/webhook_deliveries/create_service.rb
@@ -1,13 +1,13 @@
 module WebhookDeliveries
   class CreateService
-    def execute(course, event_type, resource)
-      webhook_endpoint = course.webhook_endpoint
+    def execute(context, event_type, resource)
+      webhook_endpoint = context.webhook_endpoint
 
       return unless webhook_endpoint&.active?
 
       return unless event_type.in? webhook_endpoint.events
 
-      WebhookDeliveries::DeliverJob.perform_later(event_type, course, resource)
+      WebhookDeliveries::DeliverJob.perform_later(event_type, context, resource)
     end
   end
 end

356e553fcb6bfc7f81a59bf7b5cad15b6c61b47d
Hari Gopal <mail@harigopal.in>
2021-02-17 20:00:50 +0530

    Begin

--- app/services/timeline_events/create_service.rb
+++ app/services/timeline_events/create_service.rb
@@ -1,6 +1,10 @@
 module TimelineEvents
   class CreateService
-    def initialize(params, founder, notification_service: Developers::NotificationService.new)
+    def initialize(
+      params,
+      founder,
+      notification_service: Developers::NotificationService.new
+    )
       @params = params
       @founder = founder
       @target = params[:target]
@@ -8,43 +12,57 @@ module TimelineEvents
     end
 
     def execute
-      TimelineEvent.transaction do
-        TimelineEvent.create!(@params).tap do |te|
-          @founder.timeline_event_owners.create!(timeline_event: te, latest: true)
-          create_team_entries(te) if @params[:target].team_target?
-          @notification_service.execute(
-            @founder.course,
-            WebhookDelivery.events[:submission_created],
-            @founder.user,
-            te
-          )
-          update_latest_flag(te)
+      submission =
+        TimelineEvent.transaction do
+          TimelineEvent
+            .create!(@params)
+            .tap do |s|
+              @founder.timeline_event_owners.create!(
+                timeline_event: s,
+                latest: true
+              )
+
+              create_team_entries(s) if @params[:target].team_target?
+
+              update_latest_flag(s)
+            end
         end
-      end
+
+      @notification_service.execute(
+        @founder.course,
+        :submission_created,
+        @founder.user,
+        submission
+      )
     end
 
     private
 
     def create_team_entries(timeline_event)
       team_members.each do |member|
-        member.timeline_event_owners.create!(timeline_event: timeline_event, latest: true)
+        member.timeline_event_owners.create!(
+          timeline_event: timeline_event,
+          latest: true
+        )
       end
     end
 
     def update_latest_flag(timeline_event)
-      old_events = @target.timeline_events.joins(:timeline_event_owners)
-        .where(timeline_event_owners: { founder: owners })
-        .where.not(id: timeline_event)
+      old_events =
+        @target
+          .timeline_events
+          .joins(:timeline_event_owners)
+          .where(timeline_event_owners: { founder: owners })
+          .where
+          .not(id: timeline_event)
 
-      TimelineEventOwner.where(timeline_event_id: old_events, founder: owners).update_all(latest: false) # rubocop:disable Rails/SkipsModelValidations
+      TimelineEventOwner
+        .where(timeline_event_id: old_events, founder: owners)
+        .update_all(latest: false) # rubocop:disable Rails/SkipsModelValidations
     end
 
     def owners
-      if @target.team_target?
-        @founder.startup.founders
-      else
-        @founder
-      end
+      @target.team_target? ? @founder.startup.founders : @founder
     end
 
     def team_members
--- app/services/webhook_deliveries/create_service.rb
+++ app/services/webhook_deliveries/create_service.rb
@@ -1,12 +1,15 @@
 module WebhookDeliveries
   class CreateService
     def execute(course, event_type, resource)
+      event_value = WebhookDelivery.events[event_type]
+
+      raise "Invalid event_type #{event_type} encountered" if event_value.blank?
+
       webhook_endpoint = course.webhook_endpoint
 
       return unless webhook_endpoint&.active?
-      return unless event_type.in? webhook_endpoint.events
 
-      WebhookDeliveries::DeliverJob.perform_later(event_type, course, resource)
+      WebhookDeliveries::DeliverJob.perform_later(event_value, course, resource)
     end
   end
 end

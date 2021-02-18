7ea4554fdff7f614239419206491dbc7ce014f60
Hari Gopal <mail@harigopal.in>
2021-02-18 20:54:13 +0530

    Dev notifications (refactor) (#637)
    This enables use of Active Support's Instrumentation feature to notify native
    extensions of the LMS to be notified of, and react to "events" within the Rails
    application.
    
    Co-authored-by: Mirosław Pragłowski <m@praglowski.com>

---
+++ app/services/developers/notification_service.rb
@@ -0,0 +1,16 @@
+module Developers
+  class NotificationService
+    def initialize(
+      webhook_service: WebhookDeliveries::CreateService.new,
+      event_publisher: Developers::EventPublisher.new
+    )
+      @webhook_service = webhook_service
+      @event_publisher = event_publisher
+    end
+
+    def execute(course, event_type, actor, resource)
+      @event_publisher.execute(event_type, actor, resource)
+      @webhook_service.execute(course, event_type, resource)
+    end
+  end
+end

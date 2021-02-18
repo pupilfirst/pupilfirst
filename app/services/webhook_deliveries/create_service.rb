3c64ce1d9d5ed605d12dde953df60c5d6eda6cfd
Hari Gopal <mail@harigopal.in>
2021-02-18 19:58:06 +0530

    Avoid enqueuing webhook delivery job is event isn't a part of the webhook's list
    This also updates the related spec to confine itself to checking the
    enqueuing of jobs, and not its end result.

--- app/services/webhook_deliveries/create_service.rb
+++ app/services/webhook_deliveries/create_service.rb
@@ -1,13 +1,14 @@
 module WebhookDeliveries
   class CreateService
-    def execute(course, event_type, resource)
-      event_value = WebhookDelivery.events[event_type]
+    def execute(course, event_key, resource)
+      event_value = WebhookDelivery.events[event_key]
 
-      raise "Invalid event_type #{event_type} encountered" if event_value.blank?
+      raise "Invalid event_type #{event_key} encountered" if event_value.blank?
 
       webhook_endpoint = course.webhook_endpoint
 
       return unless webhook_endpoint&.active?
+      return unless event_value.in?(webhook_endpoint.events)
 
       WebhookDeliveries::DeliverJob.perform_later(event_value, course, resource)
     end

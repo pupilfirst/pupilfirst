a339b3496ce13810a4d1bdb41daf60ff7aefdb4b
Hari Gopal <mail@harigopal.in>
2021-02-23 13:35:29 +0530

    Do not deliver webhooks for unregistered events (#640)

--- app/jobs/webhook_deliveries/deliver_job.rb
+++ app/jobs/webhook_deliveries/deliver_job.rb
@@ -5,17 +5,19 @@ module WebhookDeliveries
     def perform(event_type, course, resource)
       webhook_endpoint = course.webhook_endpoint
 
-      payload = {
-        data: data(event_type, resource),
-        event: event_type
-      }
+      unless event_type.in?(webhook_endpoint.events)
+        raise "#{event_type} was not one of the requested events in WebhookEndpoint##{webhook_endpoint.id}"
+      end
+
+      payload = { data: data(event_type, resource), event: event_type }
 
       uri = URI.parse(webhook_endpoint.webhook_url)
 
       request = Net::HTTP::Post.new(uri.request_uri)
       request['Content-Type'] = 'application/json'
       request.body = payload.to_json
-      request['Authorization'] = "PF-HMAC-SHA256 #{hmac(webhook_endpoint.hmac_key, request.body)}"
+      request['Authorization'] =
+        "PF-HMAC-SHA256 #{hmac(webhook_endpoint.hmac_key, request.body)}"
 
       http = Net::HTTP.new(uri.host, uri.port)
       http.read_timeout = Rails.application.secrets.webhook_read_timeout
@@ -33,26 +35,23 @@ module WebhookDeliveries
         response_headers: response.header,
         response_body: response.body
       )
-
     rescue Net::OpenTimeout => e
-      if event_type.in? course.webhook_endpoint.events
-        WebhookDelivery.create!(
-          error_class: e.class.name,
-          event: event_type,
-          payload: payload,
-          course: course,
-          webhook_url: webhook_endpoint.webhook_url,
-          sent_at: Time.zone.now,
-        )
-      end
+      WebhookDelivery.create!(
+        error_class: e.class.name,
+        event: event_type,
+        payload: payload,
+        course: course,
+        webhook_url: webhook_endpoint.webhook_url,
+        sent_at: Time.zone.now
+      )
     end
 
     def data(event_type, resource)
       case event_type
-        when WebhookDelivery.events[:submission_created]
-          TimelineEvents::CreateWebhookDataService.new(resource).data
-        else
-          raise "Unknown webhook event type: #{event_type}"
+      when WebhookDelivery.events[:submission_created]
+        TimelineEvents::CreateWebhookDataService.new(resource).data
+      else
+        raise "Unknown webhook event type: #{event_type}"
       end
     end
 

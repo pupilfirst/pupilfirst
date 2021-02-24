573f3dda1d210f5b1e87e90fc4b942631178a962
Mirosław Pragłowski <m@praglowski.com>
2021-02-24 17:59:50 +0100

    Stick to a resource.event naming scheme
    Co-authored-by: Hari Gopal <mail@harigopal.in>

--- app/models/webhook_delivery.rb
+++ app/models/webhook_delivery.rb
@@ -6,7 +6,7 @@ class WebhookDelivery < ApplicationRecord
          submission_graded: 'submission.graded',
          student_added: 'student.added',
          submission_verified: 'submission.verified',
-         submission_automatically_verified: 'submission.automatically.verified',
+         submission_automatically_verified: 'submission.automatically_verified',
          noop: 'noop' # special event, which does not require webhook delivery
        }
 end

f1623af5b4fa9300d395a7aa04ca86c0a3a8d3cf
Mirosław Pragłowski <m@praglowski.com>
2021-02-25 15:49:36 +0100

    Create keycloak account in event subscrber

---
+++ lib/keycloak/setup_student_account.rb
@@ -0,0 +1,21 @@
+module Keycloak
+  class SetupStudentAccount
+    def initialize(keycloak_client = Rails.configuration.keycloak_client)
+      @keycloak_client = keycloak_client
+    end
+
+    def call(actor_id:, **_)
+      student = User.find(actor_id)
+      create_keycloak_user(student.email, student.name)
+    end
+
+    private
+
+    def create_keycloak_user(email, name)
+      names = name.split(' ')
+      first_name = names.pop
+      last_name = names.join(' ') || ''
+      @keycloak_client.create_user(email, first_name, last_name)
+    end
+  end
+end

c5494cafb39e059c76ba6b5362a544b461430d57
Bodhish Thomas <bodhish@gmail.com>
2021-02-25 15:52:14 +0530

    Update courses show and add seed for course highlights

--- db/seeds/development/courses.seeds.rb
+++ db/seeds/development/courses.seeds.rb
@@ -8,7 +8,16 @@ after 'development:schools' do
       name: Faker::Lorem.words(number: 2).join(' ').titleize,
       description: Faker::Lorem.paragraph,
       progression_behavior: Course::PROGRESSION_BEHAVIOR_LIMITED,
-      progression_limit: 1
+      progression_limit: 1,
+      highlights: [highlights, highlights, highlights, highlights]
     )
+  end
+
+  def highlights
+    {
+      icon: %w[clock-solid comment-alt-solid badge-check-solid].sample,
+      title: Faker::Lorem.words(number: 2).join(' ').titleize,
+      description: Faker::Lorem.paragraph
+    }
   end
 end

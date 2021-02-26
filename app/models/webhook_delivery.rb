fbb3c843a226a1fd2bd850cee2d34065f27cefb2
Mirosław Pragłowski <m@praglowski.com>
2021-02-26 14:06:37 +0100

    Add dev's notification for course completion

--- app/models/webhook_delivery.rb
+++ app/models/webhook_delivery.rb
@@ -2,6 +2,7 @@ class WebhookDelivery < ApplicationRecord
   belongs_to :course
 
   enum event: {
+         course_completed: 'course.completed',
          submission_created: 'submission.created',
          submission_graded: 'submission.graded',
          student_added: 'student.added',

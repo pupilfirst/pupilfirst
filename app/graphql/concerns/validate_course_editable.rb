fa92ddd9718376bf5e1faaf0f021c4ec94d4a183
Bodhish Thomas <bodhish@gmail.com>
2021-03-04 20:17:00 +0530

    Convert create and update mutation to new format

---
+++ app/graphql/concerns/validate_course_editable.rb
@@ -0,0 +1,93 @@
+module ValidateCourseEditable
+  extend ActiveSupport::Concern
+
+  class LimitedProgressionRequiresDetails < GraphQL::Schema::Validator
+    def validate(_object, _context, value)
+      unless value[:progression_behavior] ==
+               Course::PROGRESSION_BEHAVIOR_LIMITED
+        return
+      end
+
+      if value[:progression_limit].blank?
+        return(
+          'Progression limit must be specified when the course progression is limited'
+        )
+      end
+    end
+  end
+
+  included do
+    argument :name,
+             String,
+             required: true,
+             validates: {
+               length: {
+                 minimum: 1,
+                 maximum: 50
+               }
+             }
+    argument :description,
+             String,
+             required: true,
+             validates: {
+               length: {
+                 minimum: 1,
+                 maximum: 150
+               }
+             }
+    argument :ends_at, GraphQL::Types::ISO8601DateTime, required: false
+    argument :about,
+             String,
+             required: true,
+             validates: {
+               length: {
+                 maximum: 10_000
+               }
+             }
+
+    argument :progression_behavior,
+             Types::ProgressionBehaviorType,
+             required: true,
+             validates: {
+               inclusion: {
+                 in: Course::VALID_PROGRESSION_BEHAVIORS
+               }
+             }
+    argument :progression_limit,
+             Integer,
+             required: false,
+             validates: {
+               numericality: {
+                 greater_than_or_equal_to: 1,
+                 less_than_or_equal_to: 3
+               }
+             }
+    argument :highlights, [Types::CourseHighlightInputType], required: true
+    argument :processing_url, String, required: false
+
+    validates LimitedProgressionRequiresDetails => {}
+  end
+
+  def sanitized_progression_limit
+    if @params[:progression_behavior] == Course::PROGRESSION_BEHAVIOR_LIMITED
+      @params[:progression_limit]
+    else
+      nil
+    end
+  end
+
+  def course_data
+    {
+      name: @params[:name],
+      description: @params[:description],
+      ends_at: @params[:ends_at],
+      public_signup: @params[:public_signup],
+      about: @params[:about],
+      featured: @params[:featured],
+      progression_behavior: @params[:progression_behavior],
+      progression_limit: sanitized_progression_limit,
+      highlights: @params[:highlights],
+      processing_url: @params[:processing_ur]
+    }
+  end
+end
--- app/graphql/mutations/update_course.rb
+++ app/graphql/mutations/update_course.rb
@@ -1,36 +1,47 @@
 module Mutations
-  class UpdateCourse < GraphQL::Schema::Mutation
+  class UpdateCourse < ApplicationQuery
+    class CourseMustBePresent < GraphQL::Schema::Validator
+      def validate(_object, _context, value)
+        course = Course.find_by(id: value[:id])
+
+        return "Unable to find course with id: #{value[:id]}" if course.blank?
+      end
+    end
+    include QueryAuthorizeSchoolAdmin
+    include ValidateCourseEditable
+
     argument :id, ID, required: true
-    argument :name, String, required: true
-    argument :description, String, required: true
-    argument :ends_at, GraphQL::Types::ISO8601DateTime, required: false
-    argument :about, String, required: true
     argument :public_signup, Boolean, required: true
     argument :featured, Boolean, required: true
-    argument :progression_behavior,
-             Types::ProgressionBehaviorType,
-             required: true
-    argument :progression_limit, Integer, required: false
-    argument :highlights, [Types::CourseHighlightInputType], required: true
-    argument :processing_url, String, required: false
+
+    validates CourseMustBePresent => {}
 
     description 'Update a course.'
 
     field :course, Types::CourseType, null: true
 
-    def resolve(params)
-      mutator = UpdateCourseMutator.new(context, params)
+    def resolve(_params)
+      notify(
+        :success,
+        I18n.t('shared.done_exclamation'),
+        I18n.t('mutations.update_course.success_notification')
+      )
 
-      course =
-        if mutator.valid?
-          mutator.notify(:success, 'Done!', 'Course updated successfully!')
-          mutator.update_course
-        else
-          mutator.notify_errors
-          nil
-        end
+      { course: update_course }
+    end
 
-      { course: course }
+    def update_course
+      course.update!(course_data)
+
+      course
+    end
+
+    def resource_school
+      course.school
+    end
+
+    def course
+      Course.find_by(id: @params[:id])
     end
   end
 end

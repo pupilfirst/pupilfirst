392a65637cb070042044d6a5e63d36f09a80054c
Mirosław Pragłowski <m@praglowski.com>
2021-03-12 12:29:12 +0100

    GTM helper class

---
+++ lib/google_tag_manager.rb
@@ -0,0 +1,61 @@
+class GoogleTagManager
+  def initialize(school, user, tracker: ENV['GTM_TRACKER'])
+    @school = school
+    @user = user
+    @tracker = tracker
+  end
+
+  def with
+    yield self if block_given? && tracker_defined?
+  end
+
+  def setup_data_layer
+    return unless tracker_defined?
+    data = {
+      schoolId: school.id,
+      userId: user&.id.presence || "N/A",
+      userEmail: user&.email
+    }
+    <<~HTML
+    <script>
+      window.dataLayer || (window.dataLayer = []);
+      window.dataLayer.push(#{JSON.dump(data)});
+      window.addEventListener("load", function() {
+        if (window.performance && window.performance.timeOrigin && window.performance.now) {
+          window.dataLayer.push({"pageLoadTime": Math.round(performance.now())});
+        }
+      });
+    </script>
+    HTML
+  end
+
+  def setup_gtm_head
+    return unless tracker_defined?
+    <<-HTML
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','#{tracker}');</script>
+    <!-- End Google Tag Manager -->
+    HTML
+  end
+
+  def setup_gtm_body
+    return unless tracker_defined?
+    <<-HTML
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=#{tracker}"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    HTML
+  end
+
+  private
+  attr_reader :school, :user, :tracker
+
+  def tracker_defined?
+    tracker.present?
+  end
+end
---
+++ spec/google_tag_manager_spec.rb
@@ -0,0 +1,96 @@
+require 'rails_helper'
+
+RSpec.describe GoogleTagManager do
+  let(:school) { create :school }
+
+  describe "#setup_data_layer" do
+    specify "user missing" do
+      gtm = GoogleTagManager.new(school, nil, tracker: 'TRACKER-CODE')
+
+      expect(gtm.setup_data_layer).to eq(
+        <<~HTML
+        <script>
+          window.dataLayer || (window.dataLayer = []);
+          window.dataLayer.push({"schoolId":#{school.id},"userId":"N/A","userEmail":null});
+          window.addEventListener("load", function() {
+            if (window.performance && window.performance.timeOrigin && window.performance.now) {
+              window.dataLayer.push({"pageLoadTime": Math.round(performance.now())});
+            }
+          });
+        </script>
+        HTML
+      )
+    end
+
+    specify "user present" do
+      user = create :user, email: 'user@example.com'
+
+      gtm = GoogleTagManager.new(school, user, tracker: 'TRACKER-CODE')
+
+      expect(gtm.setup_data_layer).to eq(
+        <<~HTML
+        <script>
+          window.dataLayer || (window.dataLayer = []);
+          window.dataLayer.push({"schoolId":#{school.id},"userId":#{user.id},"userEmail":"user@example.com"});
+          window.addEventListener("load", function() {
+            if (window.performance && window.performance.timeOrigin && window.performance.now) {
+              window.dataLayer.push({"pageLoadTime": Math.round(performance.now())});
+            }
+          });
+        </script>
+        HTML
+      )
+    end
+
+    specify do
+      gtm = GoogleTagManager.new(school, nil, tracker: nil)
+
+      expect(gtm.setup_data_layer).to be_nil
+    end
+  end
+
+  describe "#setup_gtm_head" do
+    specify do
+      gtm = GoogleTagManager.new(school, nil, tracker: 'TRACKER-CODE')
+
+      expect(gtm.setup_gtm_head).to include('TRACKER-CODE')
+    end
+
+    specify do
+      gtm = GoogleTagManager.new(school, nil, tracker: nil)
+
+      expect(gtm.setup_gtm_head).to be_nil
+    end
+  end
+
+  describe "#setup_gtm_body" do
+    specify do
+      gtm = GoogleTagManager.new(school, nil, tracker: 'TRACKER-CODE')
+
+      expect(gtm.setup_gtm_body).to include('TRACKER-CODE')
+    end
+
+    specify do
+      gtm = GoogleTagManager.new(school, nil, tracker: nil)
+
+      expect(gtm.setup_gtm_body).to be_nil
+    end
+  end
+
+  it do
+    allow(ENV).to receive(:[]).with('GTM_TRACKER').and_return('TRACKER-CODE')
+    gtm = GoogleTagManager.new(school, nil)
+    expect(gtm.setup_gtm_head).to include('TRACKER-CODE')
+    expect(gtm.setup_gtm_body).to include('TRACKER-CODE')
+  end
+
+  it do
+    gtm = GoogleTagManager.new(school, nil, tracker: 'TRACKER-CODE')
+    expect { |b| gtm.with(&b) }.to yield_with_args(gtm)
+  end
+
+  it do
+    gtm = GoogleTagManager.new(school, nil, tracker: nil)
+    expect { |b| gtm.with(&b) }.not_to yield_control
+  end
+end

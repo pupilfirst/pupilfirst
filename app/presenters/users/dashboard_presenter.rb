3aca5fcdeb203fb8369758a2e5fd617acf9eeb9d
Hari Gopal <mail@harigopal.in>
2021-03-29 13:17:53 +0530

    Read access ends at info from memoized store

--- app/presenters/users/dashboard_presenter.rb
+++ app/presenters/users/dashboard_presenter.rb
@@ -16,7 +16,8 @@ module Users
       }
 
       if current_user.avatar.attached?
-        dashboard_props[:avatar_url] = view.url_for(current_user.avatar_variant(:thumb))
+        dashboard_props[:avatar_url] =
+          view.url_for(current_user.avatar_variant(:thumb))
       end
 
       dashboard_props
@@ -25,95 +26,137 @@ module Users
     private
 
     def issued_certificate_details
-      current_user.issued_certificates.live.includes(:course).map do |issued_certificate|
-        issued_certificate.attributes.slice('id', 'serial_number', 'created_at').merge(
-          course_name: issued_certificate.course.name
-        )
-      end
+      current_user
+        .issued_certificates
+        .live
+        .includes(:course)
+        .map do |issued_certificate|
+          issued_certificate
+            .attributes
+            .slice('id', 'serial_number', 'created_at')
+            .merge(course_name: issued_certificate.course.name)
+        end
     end
 
     def courses
-      @courses ||= begin
-        if current_school_admin.present?
-          current_school.courses.active.or(current_school.courses.live.where(id: courses_with_student_profile.pluck(:course_id)))
-        else
-          current_school.courses.live.where(id: (courses_with_student_profile.pluck(:course_id) + courses_with_review_access + courses_with_author_access).uniq)
-        end.with_attached_thumbnail
-      end
+      @courses ||=
+        begin
+          if current_school_admin.present?
+            current_school.courses.active.or(
+              current_school.courses.live.where(
+                id: courses_with_student_profile.pluck(:course_id)
+              )
+            )
+          else
+            current_school.courses.live.where(
+              id:
+                (
+                  courses_with_student_profile.pluck(:course_id) +
+                    courses_with_review_access + courses_with_author_access
+                ).uniq
+            )
+          end.with_attached_thumbnail
+        end
     end
 
     def courses_with_student_profile
-      @courses_with_student_profile ||= begin
-        current_user.founders.joins(:course).pluck(:course_id, :dropped_out_at).map do |course_id, dropped_out_at|
-          {
-            course_id: course_id,
-            dropped_out_at: dropped_out_at
-          }
+      @courses_with_student_profile ||=
+        begin
+          current_user
+            .founders
+            .joins(:course)
+            .pluck(:course_id, :dropped_out_at, :access_ends_at)
+            .map do |course_id, dropped_out_at, access_ends_at|
+              {
+                course_id: course_id,
+                dropped_out_at: dropped_out_at,
+                access_ends_at: access_ends_at
+              }
+            end
         end
-      end
     end
 
     def courses_with_review_access
-      @courses_with_review_access ||= begin
-        current_user.faculty.present? ? current_user.faculty.courses.pluck(:id) : []
-      end
+      @courses_with_review_access ||=
+        begin
+          if current_user.faculty.present?
+            current_user.faculty.courses.pluck(:id)
+          else
+            []
+          end
+        end
     end
 
     def courses_with_author_access
-      @courses_with_author_access ||= current_user.course_authors.pluck(:course_id)
+      @courses_with_author_access ||=
+        current_user.course_authors.pluck(:course_id)
     end
 
     def communities
-      @communities ||= begin
-        # All communities in school.
-        communities_in_school = Community.where(school: current_school)
+      @communities ||=
+        begin
+          # All communities in school.
+          communities_in_school = Community.where(school: current_school)
 
-        if current_school_admin.present? || current_coach.present?
-          # Coaches and school admins can access all communities in a school.
-          communities_in_school
-        else
-          # Students can access communities linked to their courses, as long as they haven't dropped out.
-          active_courses = Course.live.joins(startups: [founders: :user]).where(users: { id: current_user }).where(startups: { dropped_out_at: nil })
-          communities_in_school.joins(:courses).where(courses: { id: active_courses }).distinct
+          if current_school_admin.present? || current_coach.present?
+            # Coaches and school admins can access all communities in a school.
+            communities_in_school
+          else
+            # Students can access communities linked to their courses, as long as they haven't dropped out.
+            active_courses =
+              Course
+                .live
+                .joins(startups: [founders: :user])
+                .where(users: { id: current_user })
+                .where(startups: { dropped_out_at: nil })
+            communities_in_school
+              .joins(:courses)
+              .where(courses: { id: active_courses })
+              .distinct
+          end
         end
-      end
     end
 
     def community_details_array
-      communities.map do |community|
-        {
-          id: community.id,
-          name: community.name
-        }
-      end
+      communities.map { |community| { id: community.id, name: community.name } }
     end
 
     def course_details_array
-      courses.includes(:communities).map do |course|
-        {
-          id: course.id,
-          name: course.name,
-          review: course.id.in?(courses_with_review_access),
-          author: course.id.in?(courses_with_author_access),
-          enable_leaderboard: course.enable_leaderboard?,
-          description: course.description,
-          exited: student_dropped_out(course.id),
-          thumbnail_url: course.thumbnail_url,
-          linked_communities: course.communities.pluck(:id).map(&:to_s),
-          access_ended: course_access_end(course),
-          ended: course.ended?
-        }
-      end
+      courses
+        .includes(:communities)
+        .map do |course|
+          {
+            id: course.id,
+            name: course.name,
+            review: course.id.in?(courses_with_review_access),
+            author: course.id.in?(courses_with_author_access),
+            enable_leaderboard: course.enable_leaderboard?,
+            description: course.description,
+            exited: student_dropped_out(course.id),
+            thumbnail_url: course.thumbnail_url,
+            linked_communities: course.communities.pluck(:id).map(&:to_s),
+            access_ended: student_access_end(course.id),
+            ended: course.ended?
+          }
+        end
     end
-    
-    def course_access_end(course) 
-      course_founder = course.founders.not_dropped_out.find_by(user_id: current_user.id)
-      course_founder.present? && course_founder.access_ended?
+
+    def student_access_end(course_id)
+      course_with_student =
+        courses_with_student_profile.detect { |c| c[:course_id] == course_id }
+
+      return false if course_with_student.blank?
+
+      course_with_student[:access_ends_at].present?
     end
 
     def student_dropped_out(course_id)
-      course_with_founder = courses_with_student_profile.detect { |c| c[:course_id] == course_id }
-      course_with_founder.present? ? course_with_founder[:dropped_out_at].present? : false
+      course_with_student =
+        courses_with_student_profile.detect { |c| c[:course_id] == course_id }
+
+      return false if course_with_student.blank?
+
+      course_with_student[:dropped_out_at].present?
     end
 
     def show_user_edit?

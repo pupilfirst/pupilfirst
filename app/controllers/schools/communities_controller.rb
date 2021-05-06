e0f1a61e0c96322f41e77f698268c0c7201af545
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2021-05-06 20:36:33 +0200

    access to communities page behind toggle

--- app/controllers/schools/communities_controller.rb
+++ app/controllers/schools/communities_controller.rb
@@ -2,7 +2,11 @@ module Schools
   class CommunitiesController < SchoolsController
     def index
       @school = authorize(current_school, policy_class: Schools::CommunityPolicy)
-      render layout: 'school'
+      if !Feature.active?('communities', current_user)
+        redirect_to root_path
+      else
+        render layout: 'school'
+      end
     end
   end
 end

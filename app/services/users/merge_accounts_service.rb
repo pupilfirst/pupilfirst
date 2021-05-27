e904d43b6a9e3776d51ce504418af7c2386d7000
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-05-27 22:50:49 +0530

    Update service and add spec

--- app/services/users/merge_accounts_service.rb
+++ app/services/users/merge_accounts_service.rb
@@ -1,8 +1,9 @@
 module Users
   class MergeAccountsService
-    def initialize(old_user, new_user)
+    def initialize(old_user, new_user, student_profile_ids: [])
       @old_user = old_user
       @new_user = new_user
+      @student_profile_ids = student_profile_ids.uniq
     end
 
     def execute
@@ -11,15 +12,16 @@ module Users
       end
 
       User.transaction do
+        merge_student_profiles
+        merge_coach_profiles
+        merge_course_authors
+        handle_admin_profile
         merge_community_data
         merge_coach_notes
         merge_markdown_attachments
         merge_issued_certificates
         merge_course_exports
-        merge_coach_profiles
-        merge_student_profiles
-        merge_course_authors
-        handle_admin_profile
+        delete_old_account
       end
 
       Rails.logger.info('Accounts merged successfully!')
@@ -43,11 +45,13 @@ module Users
 
     def merge_coach_notes
       Rails.logger.info('Merging coach notes...')
+
       CoachNote.where(author_id: @old_user).update_all(author_id: @new_user.id) # rubocop:disable Rails/SkipsModelValidations
     end
 
     def merge_markdown_attachments
       Rails.logger.info('Merging markdown attachments...')
+
       MarkdownAttachment
         .where(user_id: @old_user)
         .update_all(user_id: @new_user.id) # rubocop:disable Rails/SkipsModelValidations
@@ -55,6 +59,7 @@ module Users
 
     def merge_issued_certificates
       Rails.logger.info('Merging issued certificates...')
+
       IssuedCertificate
         .where(user_id: @old_user)
         .update_all(user_id: @new_user.id) # rubocop:disable Rails/SkipsModelValidations
@@ -62,6 +67,7 @@ module Users
 
     def merge_course_exports
       Rails.logger.info('Merging course exports...')
+
       CourseExport.where(user_id: @old_user).update_all(user_id: @new_user.id) # rubocop:disable Rails/SkipsModelValidations
     end
 
@@ -112,35 +118,41 @@ module Users
           next
         end
 
-        user_input =
-          prompt_confirmation_message "Do you want to transfer course author rights for #{course_author.course.name} to the new account?\n1 - Yes\n0 - No"
-
-        next if user_input == '0'
-
         course_author.update!(user_id: @new_user.id)
       end
     end
 
     def handle_two_student_profiles_for_same_course(course)
-      user_input =
-        prompt_confirmation_message(
-          "Both users have student profiles in course: #{course.name}. Choose the profile that you would like keep. The other would be removed: \n1. #{@new_user.email}\n0. #{@old_user.email}"
-        )
-      return if user_input == '1'
-
       old_student_profile =
         @old_user.founders.joins(:course).where(courses: { id: course }).first
       new_student_profile =
         @new_user.founders.joins(:course).where(courses: { id: course }).first
 
-      new_student_profile.update!(user_id: @old_user.id)
-      old_student_profile.update!(user_id: @new_user.id)
+      if @student_profile_ids.include?(old_student_profile.id) ^
+           @student_profile_ids.include?(new_student_profile.id)
+        return if @student_profile_ids.include?(new_student_profile.id)
+
+        old_student_profile.update!(user_id: @new_user.id)
+        new_student_profile.destroy!
+      else
+        raise "Only one student profile IDs must be supplied for course: #{course.id}"
+      end
     end
 
     def merge_student_profiles
       return if @old_user.founders.blank?
 
       Rails.logger.info('Merging student profiles...')
+
+      old_user_course_ids = @old_user.courses.pluck(:id)
+
+      new_user_course_ids = @new_user.courses.pluck(:id)
+
+      common_courses = old_user_course_ids && new_user_course_ids
+
+      if common_courses.present? && @student_profile_ids.empty?
+        raise "Both users have student profiles in courses with ids: #{common_courses}. Select one student profile for these courses, and pass the IDs of the selected student profiles as an array to student_profile_ids"
+      end
 
       @old_user.founders.each do |founder|
         course = founder.course
@@ -159,33 +171,18 @@ module Users
     def handle_admin_profile
       return if @old_user.school_admin.blank?
 
-      return if @new_user.school_admin.present?
-
       Rails.logger.info('Merging school admin...')
 
-      user_input =
-        prompt_confirmation_message "Do you want to transfer school admin rights to the new account?\n1: Yes \n0: No"
-
-      return if user_input == '0'
+      if @new_user.school_admin.present?
+        @old_user.school_admin.destroy!
+        return
+      end
 
       @old_user.school_admin.update!(user_id: @new_user.id)
     end
 
-    def prompt_confirmation_message(message)
-      input = ''
-      attempt_counter = 0
-      while input != '1' && input != '0'
-        question =
-          attempt_counter == 0 ? message : 'Retry again with a valid choice'
-        Rails.logger.info(question)
-        input = gets.chomp!
-        attempt_counter += 1
-      end
-      input
-    end
-
     def delete_old_account
-      Users::DeleteAccountService.new(@old_user).execute
+      Users::DeleteAccountService.new(@old_user.reload).execute
     end
   end
 end

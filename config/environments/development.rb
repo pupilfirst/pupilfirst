5f4b42f90a8431caa925e5422f6219a7f01baaea
Mirosław Pragłowski <m@praglowski.com>
2021-06-01 13:13:42 +0200

    Use delayed job queue adapter in development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Let's use delayed job in development to process background jobs.
+  config.active_job.queue_adapter = :delayed_job
+
   # Show full error reports.
   config.consider_all_requests_local = true
 

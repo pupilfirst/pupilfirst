3aa5ea1eb60afaf3ae2b2b4158c34ed100948e98
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-06-02 00:29:32 +0530

    Fix bug

--- app/services/target_groups/archival_service.rb
+++ app/services/target_groups/archival_service.rb
@@ -7,7 +7,12 @@ module TargetGroups
 
     def archive
       TargetGroup.transaction do
-        @target_group.targets.live.each { |target| Targets::UpdateVisibilityService.new(target, Target::VISIBILITY_ARCHIVED).execute }
+        @target_group.targets.each do |target|
+          Targets::UpdateVisibilityService.new(
+            target,
+            Target::VISIBILITY_ARCHIVED
+          ).execute
+        end
         @target_group.update!(safe_to_archive: true, archived: true)
       end
     end

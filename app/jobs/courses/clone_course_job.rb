1e4db08792f3f8856e6ac40099ea35649c36bc73
Mirosław Pragłowski <m@praglowski.com>
2021-06-02 13:53:08 +0200

    Use translation instead of hardcoded strings

--- app/jobs/courses/clone_course_job.rb
+++ app/jobs/courses/clone_course_job.rb
@@ -4,7 +4,7 @@ module Courses
 
     def perform(source_course_id)
       course = Course.find(source_course_id)
-      new_name = [course.name, "copy"].join(" - ")
+      new_name = I18n.t('jobs.courses.clone_course.new_name', name: course.name)
       ::Courses::CloneService.new(course).clone(new_name, course.school)
     end
   end

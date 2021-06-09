6a153e4ab54984c8bd7a6e21551554347c490ba2
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-06-09 20:09:32 +0530

    Fix error in courses seed

--- db/seeds/development/courses.seeds.rb
+++ db/seeds/development/courses.seeds.rb
@@ -2,6 +2,14 @@ after 'development:schools' do
   puts 'Seeding courses'
 
   school = School.first
+  
+  def highlights
+    {
+      icon: Types::CourseHighlightInputType.allowed_icons.sample,
+      title: Faker::Lorem.words(number: 2).join(' ').titleize,
+      description: Faker::Lorem.paragraph
+    }
+  end
 
   2.times do
     school.courses.create!(
@@ -11,13 +19,5 @@ after 'development:schools' do
       progression_limit: 1,
       highlights: [highlights, highlights, highlights, highlights]
     )
-  end
-
-  def highlights
-    {
-      icon: Types::CourseHighlightInputType.allowed_icons.sample,
-      title: Faker::Lorem.words(number: 2).join(' ').titleize,
-      description: Faker::Lorem.paragraph
-    }
   end
 end

6cc9a3ce7f2e36b745bcf7746e56e3d81df5b34e
Mirosław Pragłowski <m@praglowski.com>
2021-06-15 09:11:11 +0200

    Convert the mutation to the new format that as used now
    Co-authored-by: Bodhish Thomas <bodhish@gmail.com>

--- app/graphql/mutations/clone_level.rb
+++ app/graphql/mutations/clone_level.rb
@@ -1,26 +1,39 @@
 module Mutations
-  class CloneLevel < GraphQL::Schema::Mutation
+  class CloneLevel < ApplicationQuery
+    include QueryAuthorizeAuthor
     argument :level_id, ID, required: true
     argument :clone_into_course_id, ID, required: true
 
-    description "Clone level into given course"
+    description 'Clone level into given course'
 
     field :success, Boolean, null: false
 
-    def resolve(params)
-      mutator = CloneLevelMutator.new(context, params)
+    def resolve(_params)
+      clone_level
+      notify(
+        :success,
+        I18n.t('shared.done_exclamation'),
+        I18n.t('mutations.clone_level.success_notification')
+      )
+      { success: true }
+    end
 
-      if mutator.valid?
-        mutator.clone_level
-        mutator.notify(:success,
-          I18n.t('shared.done_exclamation'),
-          I18n.t('mutations.clone_level.success_notification')
-        )
-        { success: true }
-      else
-        mutator.notify_errors
-        { success: false }
-      end
+    def clone_level
+      ::Levels::CloneLevelJob.perform_later(level.id, target_course.id)
+    end
+
+    def resource_school
+      return unless level&.course&.school === target_course&.school
+
+      target_course&.school
+    end
+
+    def target_course
+      @target_course ||= Course.find_by(id: @params[:clone_into_course_id])
+    end
+
+    def level
+      @level ||= Level.find_by(id: @params[:level_id])
     end
   end
 end

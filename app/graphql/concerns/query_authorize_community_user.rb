0ab178228f2431777876b97ea13572930b27a6ba
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-06-16 12:48:20 +0530

    Set last activity for a new topic #733

---
+++ app/graphql/concerns/query_authorize_community_user.rb
@@ -0,0 +1,48 @@
+module QueryAuthorizeCommunityUser
+  include ActiveSupport::Concern
+
+  def authorized_create?
+    # Current user must exist, and the community in question must be in the 'current' school.
+    unless current_user.present? && (community&.school == current_school)
+      return false
+    end
+
+    # Coach has access to all communities
+    return true if moderator?
+
+    # User should have access to the community
+    current_user
+      .founders
+      .includes(:course)
+      .where(courses: { id: community.courses })
+      .any?
+  end
+
+  def authorized_archive?
+    if post.post_number != 1
+      authorized_update?
+    else
+      authorized_create? &&
+        (
+          ((creator == current_user) && !topic.replies.live.exists?) ||
+            moderator?
+        )
+    end
+  end
+
+  def authorized_update?
+    authorized_create? && (creator == current_user || moderator?)
+  end
+
+  def authorized_moderate?
+    moderator? && community&.school == current_school
+  end
+
+  def current_coach
+    current_user.faculty
+  end
+
+  def moderator?
+    @moderator ||= current_coach.present? || current_school_admin.present?
+  end
+end

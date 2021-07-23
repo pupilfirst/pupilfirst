3537c800a9713a9e569eaa5bcb5231bc5e156506
Hari Gopal <mail@harigopal.in>
2021-07-23 13:26:44 +0530

    Move i18n-js initialization to environment-specific files

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -7,8 +7,3 @@ end
 
 I18n.available_locales = ENV['I18N_AVAILABLE_LOCALES'].split(',')
 I18n.default_locale = ENV['I18N_DEFAULT_LOCALE']
-I18n.load_path += Dir[Rails.root.join('config/locales/**/*.yml').to_s]
-
-require 'i18n-js/listen'
-
-I18nJS.listen

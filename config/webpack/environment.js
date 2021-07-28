c939bdc5757f3df885cb55228dd8f89469aab36c
Hari Gopal <mail@harigopal.in>
2021-07-28 17:14:41 +0530

    Install Tailwind v2.2

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,5 @@
 const { environment } = require("@rails/webpacker");
-const erb = require('./loaders/erb')
+const erb = require("./loaders/erb");
 const webpack = require("webpack");
 const dotenv = require("dotenv");
 
@@ -7,10 +7,10 @@ const dotenvFiles = [
   `.env.${process.env.NODE_ENV}.local`,
   ".env.local",
   `.env.${process.env.NODE_ENV}`,
-  ".env"
+  ".env",
 ];
 
-dotenvFiles.forEach(dotenvFile => {
+dotenvFiles.forEach((dotenvFile) => {
   dotenv.config({ path: dotenvFile, silent: true });
 });
 
@@ -19,5 +19,29 @@ environment.plugins.prepend(
   new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env)))
 );
 
-environment.loaders.prepend('erb', erb)
+environment.loaders.prepend("erb", erb);
+
+// Workaround for webpacker + postcss-loader v4 incompatibility.
+// Source: https://github.com/rails/webpacker/issues/2784
+const hotfixPostcssLoaderConfig = (subloader) => {
+  const subloaderName = subloader.loader;
+
+  if (subloaderName === "postcss-loader") {
+    if (subloader.options.postcssOptions) {
+      console.log(
+        "\x1b[31m%s\x1b[0m",
+        "Remove postcssOptions workaround in config/webpack/environment.js"
+      );
+    } else {
+      subloader.options.postcssOptions = subloader.options.config;
+      delete subloader.options.config;
+    }
+  }
+};
+
+environment.loaders.keys().forEach((loaderName) => {
+  const loader = environment.loaders.get(loaderName);
+  loader.use.forEach(hotfixPostcssLoaderConfig);
+});
+
 module.exports = environment;

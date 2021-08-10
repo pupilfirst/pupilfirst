d1da4b7dfd992d12768f28630670e494cde87db4
Your Name <prashanth726@gmail.com>
2021-08-10 21:12:03 +0530

    Remove extra block from Refresh page button

--- app/javascript/shared/notifier.js
+++ app/javascript/shared/notifier.js
@@ -11,7 +11,7 @@ const notify = (title, text, type) => {
     notificationText =
       "<div>" +
       text +
-      "<div><button class='btn btn-secondary mt-4 block'>Refresh Page</button></div></div>";
+      "<div><button class='btn btn-secondary mt-4'>Refresh Page</button></div></div>";
     textTrusted = true;
     reloadRequired = true;
   }

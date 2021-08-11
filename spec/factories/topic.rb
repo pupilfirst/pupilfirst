a622249eceb69312adf3990649bb90e8276241ba
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-08-11 15:51:47 +0530

    Add last activity to topic factories

--- spec/factories/topic.rb
+++ spec/factories/topic.rb
@@ -2,11 +2,10 @@ FactoryBot.define do
   factory :topic do
     title { Faker::Lorem.sentence }
     community
+    last_activity_at { Time.zone.now }
 
     trait :with_first_post do
-      transient do
-        creator { create :user }
-      end
+      transient { creator { create :user } }
 
       after(:create) do |topic, evaluator|
         create :post, :first_post, topic: topic, creator: evaluator.creator

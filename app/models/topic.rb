f7bd8795b5afd94fd026362533462784f477731d
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-08-11 11:39:35 +0530

    Add migration to update missing last activity in topics #733

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,26 +7,48 @@ class Topic < ApplicationRecord
   belongs_to :locked_by, class_name: 'User', optional: true
 
   has_many :posts, dependent: :restrict_with_error
-  has_one :first_post, -> { where post_number: 1 }, class_name: 'Post', inverse_of: :topic
+  has_one :first_post,
+          -> { where post_number: 1 },
+          class_name: 'Post',
+          inverse_of: :topic
   has_one :creator, through: :first_post
-  has_many :replies, -> { where('post_number > ?', 1) }, class_name: 'Post', inverse_of: :topic
-  has_many :live_replies, -> { where('post_number > ?', 1).merge(Post.live) }, class_name: 'Post', inverse_of: :topic
+  has_many :replies,
+           -> { where('post_number > ?', 1) },
+           class_name: 'Post',
+           inverse_of: :topic
+  has_many :live_replies,
+           -> { where('post_number > ?', 1).merge(Post.live) },
+           class_name: 'Post',
+           inverse_of: :topic
   has_many :post_likes, through: :posts
   has_many :topic_subscription, dependent: :destroy
   has_many :subscribers, through: :topic_subscription, source: :user
   has_many :users, through: :topic_subscription
 
+  validates :last_activity_at, presence: true
+
   scope :live, -> { where(archived: false) }
 
-  pg_search_scope :search_by_title, against: :title, using: {
-    tsearch: { prefix: true, any_word: true },
-  }
+  pg_search_scope :search_by_title,
+                  against: :title,
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                      any_word: true
+                    }
+                  }
 
-  pg_search_scope :search_by_title_and_post_body, against: :title, using: {
-    tsearch: { prefix: true, any_word: true },
-  }, associated_against: {
-    posts: :body
-  }
+  pg_search_scope :search_by_title_and_post_body,
+                  against: :title,
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                      any_word: true
+                    }
+                  },
+                  associated_against: {
+                    posts: :body
+                  }
 
   def solution
     replies.find_by(solution: true)

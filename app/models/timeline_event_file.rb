b3105a5031324a1b9441e92e89f35c53aee9b73f
Bodhish Thomas <bodhish@gmail.com>
2021-08-16 17:38:52 +0530

    Add migration and documentation

--- app/models/timeline_event_file.rb
+++ app/models/timeline_event_file.rb
@@ -1,6 +1,6 @@
 class TimelineEventFile < ApplicationRecord
   belongs_to :timeline_event, optional: true
-  has_one_attached :file
+  has_one_attached :file, service: :amazon_private
 
   validates :file, attached: true
 

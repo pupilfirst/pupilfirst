81756a337c4f2c039f23cf4698c977b04445b975
Hari Gopal <mail@harigopal.in>
2021-08-23 16:24:02 +0530

    Fix seeding of topic.last_activity_at for development DB

--- db/seeds/development/posts.seeds.rb
+++ db/seeds/development/posts.seeds.rb
@@ -1,4 +1,6 @@
-after 'development:communities', 'development:topic_categories', 'development:users' do
+after 'development:communities',
+      'development:topic_categories',
+      'development:users' do
   puts 'Seeding topics and posts...'
 
   community = Community.first
@@ -8,15 +10,22 @@ after 'development:communities', 'development:topic_categories', 'development:us
   # Let's add a few topics and posts so that the resulting topics paginates the community index.
   3.times do
     community_users.each do |topic_author|
-      topic = Topic.create!(
-        title: Faker::Lorem.sentence,
-        community: community,
-        topic_category: topic_categories.sample
-      )
+      topic =
+        Topic.create!(
+          title: Faker::Lorem.sentence,
+          community: community,
+          topic_category: topic_categories.sample,
+          last_activity_at: Time.zone.now
+        )
 
       topic.posts.create!(
         post_number: 1,
-        body: Faker::Lorem.paragraph(sentence_count: 10, supplemental: false, random_sentences_to_add: 5),
+        body:
+          Faker::Lorem.paragraph(
+            sentence_count: 10,
+            supplemental: false,
+            random_sentences_to_add: 5
+          ),
         creator: topic_author
       )
 
@@ -25,11 +34,17 @@ after 'development:communities', 'development:topic_categories', 'development:us
       community_users.each_with_index do |reply_author, i|
         next if topic_author == reply_author
 
-        post = topic.posts.create!(
-          creator: reply_author,
-          post_number: post_number,
-          body: Faker::Lorem.paragraph(sentence_count: 10, supplemental: false, random_sentences_to_add: 5)
-        )
+        post =
+          topic.posts.create!(
+            creator: reply_author,
+            post_number: post_number,
+            body:
+              Faker::Lorem.paragraph(
+                sentence_count: 10,
+                supplemental: false,
+                random_sentences_to_add: 5
+              )
+          )
 
         post_number += 1
 
@@ -40,12 +55,15 @@ after 'development:communities', 'development:topic_categories', 'development:us
           topic: topic,
           creator: community_users.sample,
           post_number: post_number,
-          body: Faker::Lorem.paragraph(sentence_count: 4, supplemental: false, random_sentences_to_add: 5)
+          body:
+            Faker::Lorem.paragraph(
+              sentence_count: 4,
+              supplemental: false,
+              random_sentences_to_add: 5
+            )
         )
 
         post_number += 1
-
-        topic.touch(:last_activity_at)
       end
     end
   end

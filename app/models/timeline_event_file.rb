a3628b47867de85492abea4895c525b491ae2c2a
Prashanth <prashanth726@gmail.com>
2021-09-08 11:37:00 +0530

    Rebase changes to master - merge conflict resolved

--- app/models/timeline_event_file.rb
+++ app/models/timeline_event_file.rb
@@ -1,6 +1,6 @@
 class TimelineEventFile < ApplicationRecord
   belongs_to :timeline_event, optional: true
-  has_one_attached :file, service: :amazon_private
+  has_one_attached :file
 
   validates :file, attached: true
 

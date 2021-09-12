b121899569fae7f5f10ab42c23fc8e99dbe4e50d
Prashanth <prashanth726@gmail.com>
2021-09-12 12:13:14 +0530

    Update manual_sign_out_spec to use login_token_digest

--- spec/system/users/manual_sign_out_spec.rb
+++ spec/system/users/manual_sign_out_spec.rb
@@ -8,7 +8,9 @@ feature 'Manual User Sign Out', js: true do
 
   scenario 'active user session is interrupted by the setting of the flag' do
     # Log in the user.
-    visit user_token_path(token: user.login_token_digest, referrer: edit_user_path)
+    user.regenerate_login_token
+
+    visit user_token_path(token: user.original_login_token, referrer: edit_user_path)
 
     expect(page).to have_content('profile')
 
@@ -24,7 +26,7 @@ feature 'Manual User Sign Out', js: true do
       # Log the user in again.
       user.regenerate_login_token
 
-      visit user_token_path(token: user.login_token_digest, referrer: edit_user_path)
+      visit user_token_path(token: user.original_login_token, referrer: edit_user_path)
 
       expect(page).to have_content('profile')
     end
@@ -44,7 +46,9 @@ feature 'Manual User Sign Out', js: true do
     end
 
     scenario 'user signs in as usual' do
-      visit user_token_path(token: user.login_token_digest, referrer: edit_user_path)
+      user.regenerate_login_token
+
+      visit user_token_path(token: user.original_login_token, referrer: edit_user_path)
 
       expect(page).to have_content('profile')
 

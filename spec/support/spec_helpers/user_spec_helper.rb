2a09be33a68a47b303002d97c36a9b7b49fba3b3
Prashanth <prashanth726@gmail.com>
2021-09-22 03:07:56 +0530

    Update UserSpecHelper to use login_token_sent_at

--- spec/support/spec_helpers/user_spec_helper.rb
+++ spec/support/spec_helpers/user_spec_helper.rb
@@ -3,6 +3,7 @@ module UserSpecHelper
   # @param referrer [String] (Optional) path to which user should be redirected after signing in.
   def sign_in_user(user, referrer: nil)
     user.regenerate_login_token
+    user.update!(login_mail_sent_at: Time.zone.now)
     login_token = user.original_login_token
     sign_in_path = user_token_path(token: login_token, referrer: referrer)
     visit(sign_in_path)

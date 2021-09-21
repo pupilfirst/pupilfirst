c290751a3caf90bfdcb7c357f5bfc52ceb90334c
Bodhish Thomas <bodhish@gmail.com>
2021-09-22 02:13:08 +0530

    Update webpack deps

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,4 +2,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const environment = require("./environment");
 
-module.exports = environment.toWebpackConfig();
+const config = environment.toWebpackConfig();
+
+config.output.filename = "js/[name]-[hash].js";
+
+module.exports = config;

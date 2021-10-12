320c46a502bc1ab9f17320ccefea188208965d6d
a-c-sreedhar-reddy <acreddysreedharreddy@gmail.com>
2021-10-13 00:50:09 +0530

    fix(post-version): store editor in the user field

--- app/models/text_version.rb
+++ app/models/text_version.rb
@@ -1,5 +1,4 @@
 class TextVersion < ApplicationRecord
   belongs_to :versionable, polymorphic: true
   belongs_to :user, optional: true
-  belongs_to :editor, class_name: 'User', optional: true
 end

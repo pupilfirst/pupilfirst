4b4006e3601b202ce936aa0b601945f78aba7499
Prashanth <prashanth726@gmail.com>
2022-01-17 11:12:55 +0530

    Update mail templates specs

--- spec/services/mail_login_token_service_spec.rb
+++ spec/services/mail_login_token_service_spec.rb
@@ -27,6 +27,7 @@ describe MailLoginTokenService do
         expect(current_email.subject).to eq("Log in to #{school.name}")
         expect(current_email.body).to include("http://#{domain.fqdn}/users/token?")
         expect(current_email.body).to include("referrer=#{CGI.escape(referrer)}")
+        expect(current_email.body).to include("#{user.login_token_expiration_time}")
         expect(current_email.body).to include("token=#{user.original_login_token}")
       end
     end

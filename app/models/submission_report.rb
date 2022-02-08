f33e557506de9859c95dbb82d073c99884f44d58
Mahesh Krishna Kumar <maheshcet@gmail.com>
2022-02-08 13:28:34 +0530

    Add validations for submission report

--- app/models/submission_report.rb
+++ app/models/submission_report.rb
@@ -10,4 +10,34 @@ class SubmissionReport < ApplicationRecord
        }
 
   enum conclusion: { success: 'success', failure: 'failure', error: 'error' }
+
+  validate :queued_state_is_valid
+
+  def queued_state_is_valid
+    return unless queued?
+
+    return if [started_at, completed_at, conclusion].all?(&:blank?)
+
+    errors[:status] << 'invalid queued report status'
+  end
+
+  validate :in_progress_state_is_valid
+
+  def in_progress_state_is_valid
+    return unless in_progress?
+
+    return if started_at.present? && [completed_at, conclusion].all?(&:blank?)
+
+    errors[:status] << 'invalid in progress report status'
+  end
+
+  validate :completed_state_is_valid
+
+  def completed_state_is_valid
+    return unless completed?
+
+    return if [started_at, completed_at, conclusion].all?(&:present?)
+
+    errors[:status] << 'invalid completed report status'
+  end
 end

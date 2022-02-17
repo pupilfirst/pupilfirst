36d3c360f761de66cf0b0b0dc3256ff8d986928a
Mahesh Krishna Kumar <maheshcet@gmail.com>
2022-02-17 18:28:29 +0530

    Add audit record always when user is deleted

--- app/queries/delete_account_mutator.rb
+++ app/queries/delete_account_mutator.rb
@@ -2,10 +2,7 @@ class DeleteAccountMutator < ApplicationQuery
   property :token, validates: { presence: true }
 
   def execute
-    User.transaction do
-      create_audit_record(user)
-      Users::DeleteAccountJob.perform_later(current_user)
-    end
+    User.transaction { Users::DeleteAccountJob.perform_later(current_user) }
   end
 
   private
@@ -16,9 +13,5 @@ class DeleteAccountMutator < ApplicationQuery
 
   def user
     @user ||= User.find_by_hashed_delete_account_token(token) # rubocop:disable Rails/DynamicFindBy
-  end
-
-  def create_audit_record(user)
-    AuditRecord.create!(audit_type: AuditRecord::TYPE_DELETE_ACCOUNT, school_id: current_school.id, metadata: { email: user.email })
   end
 end

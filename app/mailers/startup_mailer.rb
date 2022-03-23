77254dca2e5c62bd158ef2f28534cb59e0dd35a8
indicozy <berkutkarlibai@gmail.com>
2022-03-23 22:06:33 +0600

    changed saluation from group name to username

--- app/mailers/startup_mailer.rb
+++ app/mailers/startup_mailer.rb
@@ -2,8 +2,11 @@
 class StartupMailer < SchoolMailer
   def feedback_as_email(startup_feedback)
     @startup_feedback = startup_feedback
+
+    # it's intended to send only to one recipient
     send_to = startup_feedback.timeline_event.founders.map { |e| "#{e.fullname} <#{e.email}>" }
     @school = startup_feedback.startup.school
+    @student_fullname = startup_feedback.timeline_event.founders.first.fullname
 
     subject = "New feedback from #{startup_feedback.faculty.name} on your submission"
     simple_mail(send_to, subject)

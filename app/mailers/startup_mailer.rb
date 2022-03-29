501d3268a3b5d7831e85655b6ca946c59a3521e1
indicozy <berkutkarlibai@gmail.com>
2022-03-29 20:24:10 +0600

    Minor improvement

--- app/mailers/startup_mailer.rb
+++ app/mailers/startup_mailer.rb
@@ -1,18 +1,17 @@
 # Mails sent out to teams, as a whole.
 class StartupMailer < SchoolMailer
   def feedback_as_email(startup_feedback)
-    emails_fullnames = startup_feedback.timeline_event.founders.map { |e| {"#{e.fullname} <#{e.email}>" => {"fullname" => e.fullname, "email" => e.email}} }
-    emails_fullnames.each do | student |
-        student.each do |send_to, values|
-          send_single_email(send_to, values, startup_feedback)
-        end
+    emails_fullnames = startup_feedback.timeline_event.founders.map { |e| [e.fullname, e.email] }
+    emails_fullnames.each do | fullname, email |
+      send_to = "#{fullname} <#{email}>"
+      send_single_email(send_to, fullname, email, startup_feedback)
     end
   end
 
-  def send_single_email(send_to, values, startup_feedback)
+  def send_single_email(send_to, fullname, email, startup_feedback)
     @startup_feedback = startup_feedback
-    @student_fullname = values["fullname"]
-    @email = values["email"]
+    @student_fullname = fullname
+    @email = email
     @school = startup_feedback.startup.school
     subject = "New feedback from #{startup_feedback.faculty.name} on your submission"
     simple_mail(send_to, subject)

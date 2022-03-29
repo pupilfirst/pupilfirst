b8d9ae48cd4b990ae3cfa8e23d8053ee0fe015a2
indicozy <berkutkarlibai@gmail.com>
2022-03-29 17:38:21 +0600

    Made mailer able to send several students

--- app/mailers/startup_mailer.rb
+++ app/mailers/startup_mailer.rb
@@ -1,14 +1,21 @@
 # Mails sent out to teams, as a whole.
 class StartupMailer < SchoolMailer
   def feedback_as_email(startup_feedback)
+    emails_fullnames = startup_feedback.timeline_event.founders.map { |e| {"#{e.fullname} <#{e.email}>" => {"fullname" => e.fullname, "email" => e.email}} }
+    emails_fullnames.each do | student |
+        student.each do |send_to, values|
+          send_single_email(send_to, values, startup_feedback)
+        end
+    end
+  end
+
+  def send_single_email(send_to, values, startup_feedback)
     @startup_feedback = startup_feedback
-
-    # it's intended to send only to one recipient
-    send_to = startup_feedback.timeline_event.founders.map { |e| "#{e.fullname} <#{e.email}>" }
+    @student_fullname = values["fullname"]
+    @email = values["email"]
     @school = startup_feedback.startup.school
-    @student_fullname = startup_feedback.timeline_event.founders.first.fullname
-
     subject = "New feedback from #{startup_feedback.faculty.name} on your submission"
     simple_mail(send_to, subject)
   end
+
 end

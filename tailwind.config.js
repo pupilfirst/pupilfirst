071b7510d68547134254c85626d84c5997f85bb4
Raj Suhail <rajsuhailmalik@gmail.com>
2022-04-26 11:40:46 +0530

    Eliminate rerely used colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,6 @@ module.exports = {
       orange: colors.orange,
       blue: colors.blue,
       green: colors.green,
-      teal: colors.teal,
-      purple: colors.purple,
-      pink: colors.pink,
       primary: {
         100: "var(--color-primary-100)",
         200: "var(--color-primary-200)",
@@ -28,17 +25,6 @@ module.exports = {
         700: "var(--color-primary-700)",
         800: "var(--color-primary-800)",
         900: "var(--color-primary-900)",
-      },
-      secondary: {
-        100: "var(--color-secondary-100)",
-        200: "var(--color-secondary-200)",
-        300: "var(--color-secondary-300)",
-        400: "var(--color-secondary-400)",
-        500: "var(--color-secondary-500)",
-        600: "var(--color-secondary-600)",
-        700: "var(--color-secondary-700)",
-        800: "var(--color-secondary-800)",
-        900: "var(--color-secondary-900)",
       },
       gray: {
         50: "var(--color-gray-50)",

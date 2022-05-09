eebdc4979b86fb5dbd2157347859695ea0e3d661
Prashanth Reddy <prashanth726@gmail.com>
2022-05-09 09:34:36 +0530

    Allow coaches to review submissions of inactive students for a configurable period of time

--- app/graphql/concerns/validate_submission_gradable.rb
+++ app/graphql/concerns/validate_submission_gradable.rb
@@ -5,7 +5,16 @@ module ValidateSubmissionGradable
     def validate(_object, _context, value)
       submission = TimelineEvent.find(value[:submission_id])
 
-      if submission.founders.active.empty?
+      #days since submission
+      days_since_submission =
+        (Time.zone.now - submission.created_at) / (3600 * 24)
+      submission_review_allowed_days =
+        Rails.application.secrets.inactive_submission_review_allowed_days
+
+      submission_review_allowed =
+        (days_since_submission < submission_review_allowed_days)
+
+      if (submission.founders.active.empty? && !submission_review_allowed)
         return(
           I18n.t(
             'graphql.concerns.validate_submission_gradable.owners_should_be_active_error'

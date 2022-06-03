e1b5dacdefe919742f0ea413de698d47aebd739f
Bodhish Thomas <bodhish@gmail.com>
2022-06-03 05:37:06 +0530

    Add more resources in courses type

--- app/graphql/types/course_type.rb
+++ app/graphql/types/course_type.rb
@@ -17,29 +17,28 @@ module Types
     field :archived_at, GraphQL::Types::ISO8601DateTime, null: true
     field :highlights, [Types::CourseHighlightType], null: false
     field :processing_url, String, null: true
-    field :certificates, [Types::CertificateType], null: false do
+
+    field :student_tags, [String], null: false do
       def authorized?(_object, _args, context)
         context[:current_school_admin].present?
       end
     end
 
-    def cover
-      image_details(object.cover)
+    field :coaches, [Types::UserProxyType], null: false do
+      def authorized?(_object, _args, context)
+        context[:current_school_admin].present?
+      end
     end
 
-    def thumbnail
-      image_details(object.thumbnail)
+    field :cohorts, [Types::CohortType], null: false do
+      def authorized?(_object, _args, context)
+        context[:current_school_admin].present?
+      end
     end
 
-    private
-
-    def image_details(image)
-      if image.attached?
-        {
-          url:
-            Rails.application.routes.url_helpers.rails_public_blob_url(image),
-          filename: image.filename
-        }
+    field :certificates, [Types::CertificateType], null: false do
+      def authorized?(_object, _args, context)
+        context[:current_school_admin].present?
       end
     end
 
@@ -55,6 +54,77 @@ module Types
               end
             end
         end
+    end
+
+    def cohorts
+      BatchLoader::GraphQL
+        .for(object.id)
+        .batch(default_value: []) do |course_ids, loader|
+          Cohort
+            .where(course_id: course_ids)
+            .each do |cohort|
+              loader.call(cohort.course_id) do |memo|
+                memo |= [cohort].compact # rubocop:disable Lint/UselessAssignment
+              end
+            end
+        end
+    end
+
+    def coaches
+      BatchLoader::GraphQL
+        .for(object.id)
+        .batch(default_value: []) do |course_ids, loader|
+          FacultyCohortEnrollment
+            .includes(%i[cohort faculty])
+            .where(cohort: { course_id: course_ids })
+            .each do |enrollment|
+              loader.call(enrollment.cohort.course_id) do |memo|
+                memo |= [enrollment.faculty].compact # rubocop:disable Lint/UselessAssignment
+              end
+            end
+        end
+    end
+
+    def thumbnail
+      BatchLoader::GraphQL
+        .for(object.id)
+        .batch do |course_ids, loader|
+          Course
+            .includes(thumbnail_attachment: :blob)
+            .where(id: course_ids)
+            .each do |course|
+              if course.cover.attached?
+                loader.call(course.id, image_details(course.thumbnail))
+              end
+            end
+        end
+    end
+
+    def cover
+      BatchLoader::GraphQL
+        .for(object.id)
+        .batch do |course_ids, loader|
+          Course
+            .includes(cover_attachment: :blob)
+            .where(id: course_ids)
+            .each do |course|
+              if course.cover.attached?
+                loader.call(course.id, image_details(course.cover))
+              end
+            end
+        end
+    end
+
+    private
+
+    def image_details(image)
+      if image.attached?
+        {
+          url:
+            Rails.application.routes.url_helpers.rails_public_blob_url(image),
+          filename: image.filename
+        }
+      end
     end
   end
 end

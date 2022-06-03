cff1bb2be127bcf9441963176f430c3c5beed99a
Bodhish Thomas <bodhish@gmail.com>
2022-06-03 05:36:44 +0530

    Optimize userproxy type to avoid eger loading

--- app/graphql/types/user_proxy_type.rb
+++ app/graphql/types/user_proxy_type.rb
@@ -11,11 +11,42 @@ module Types
     field :avatar_url, String, null: true
 
     def avatar_url
-      object.user.avatar_url(variant: :thumb)
+      BatchLoader::GraphQL
+        .for(object.user_id)
+        .batch do |user_ids, loader|
+          User
+            .includes(avatar_attachment: :blob)
+            .where(id: user_ids)
+            .each do |user|
+              if user.avatar.attached?
+                url =
+                  Rails.application.routes.url_helpers.rails_public_blob_url(
+                    user.avatar_variant(:thumb)
+                  )
+                loader.call(user.id, url)
+              end
+            end
+        end
+    end
+
+    def name
+      BatchLoader::GraphQL
+        .for(object.user_id)
+        .batch do |user_ids, loader|
+          User
+            .where(id: user_ids)
+            .each { |user| loader.call(user.id, user.name) }
+        end
     end
 
     def full_title
-      object.user.full_title
+      BatchLoader::GraphQL
+        .for(object.user_id)
+        .batch do |user_ids, loader|
+          User
+            .where(id: user_ids)
+            .each { |user| loader.call(user.id, user.full_title) }
+        end
     end
   end
 end

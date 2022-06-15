39990a8a526efd06363ed1604a5690e3ee0a84dc
developedbysj <developedbysj@gmail.com>
2022-06-15 13:51:38 +0530

    Change `title type` to `not null`

--- app/graphql/types/school_link.rb
+++ app/graphql/types/school_link.rb
@@ -1,7 +1,7 @@
 module Types
   class SchoolLink < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: true
+    field :title, String, null: false
     field :url, String, null: false
     field :kind, String, null: false
     field :sort_index, Integer, null: false

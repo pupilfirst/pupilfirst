85861423c01f9f859979d079147a1a64dc907538
Hari Gopal <mail@harigopal.in>
2022-06-24 19:09:27 +0530

    Remove files related to webpacker

--- config/webpack/development.js
+++
@@ -1,9 +0,0 @@
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
-const environment = require("./environment");
-
-const config = environment.toWebpackConfig();
-
-config.output.filename = "js/[name]-[hash].js";
-
-module.exports = config;

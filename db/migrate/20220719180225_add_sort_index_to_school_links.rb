59f834315dd0131c7dc70415fb3c92f49b346b20
Mahesh Krishna Kumar <maheshcet@gmail.com>
2022-07-19 23:36:10 +0530

    Fix missing version in database migration

--- db/migrate/20220719180225_add_sort_index_to_school_links.rb
+++ db/migrate/20220719180225_add_sort_index_to_school_links.rb
@@ -1,4 +1,4 @@
-class AddSortIndexToSchoolLink < ActiveRecord::Migration[6.1]
+class AddSortIndexToSchoolLinks < ActiveRecord::Migration[6.1]
   def change
     add_column :school_links, :sort_index, :integer, default: 0, null: false
   end

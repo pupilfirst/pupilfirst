c7070a32b58d8ddc9ded0254b11ea9a9d66ac5ea
Swapnil J <76519654+developedBySJ@users.noreply.github.com>
2022-08-01 18:20:01 +0530

    Allow reordering and editing school links in admin interface (#1052)
    Co-authored-by: Mahesh Krishna Kumar <maheshcet@gmail.com>
    Co-authored-by: Raj Suhail <rajsuhailmalik@gmail.com>
    Co-authored-by: Bodhish Thomas <bodhish@gmail.com>

--- app/graphql/mutations/create_school_link.rb
+++ app/graphql/mutations/create_school_link.rb
@@ -1,22 +1,58 @@
 module Mutations
-  class CreateSchoolLink < GraphQL::Schema::Mutation
+  class CreateSchoolLink < ApplicationQuery
+    class TitleConditionallyRequired < GraphQL::Schema::Validator
+      def validate(_object, _context, value)
+        title = value[:title]
+        kind = value[:kind]
+        if title.blank? && kind != SchoolLink::KIND_SOCIAL
+          return I18n.t('mutations.create_school_link.blank_title_error')
+        end
+      end
+    end
+
+    include QueryAuthorizeSchoolAdmin
+
     argument :kind, String, required: true
     argument :title, String, required: false
     argument :url, String, required: true
 
-    description "Create a school link."
+    description 'Create a school link.'
 
     field :school_link, Types::SchoolLink, null: true
-    field :errors, [Types::CreateSchoolLinkError], null: true
 
-    def resolve(params)
-      mutator = CreateSchoolLinkMutator.new(context, params)
+    validates TitleConditionallyRequired => {}
 
-      if mutator.valid?
-        { school_link: mutator.create_school_link, errors: nil }
-      else
-        { school_link: nil, errors: mutator.error_messages }
-      end
+    def resolve(_params)
+      notify(
+        :success,
+        I18n.t('shared.notifications.done_exclamation'),
+        I18n.t('mutations.create_school_link.success_notification')
+      )
+      { school_link: create_school_link }
+    end
+
+    def create_school_link
+      data =
+        case @params[:kind]
+        when SchoolLink::KIND_HEADER, SchoolLink::KIND_FOOTER
+          { title: @params[:title], url: @params[:url] }
+        when SchoolLink::KIND_SOCIAL
+          { url: @params[:url] }
+        else
+          raise "Unknown kind '#{@params[:kind]}' encountered!"
+        end
+
+      data[:kind] = @params[:kind]
+      data[:sort_index] =
+        SchoolLink.where(kind: @params[:kind], school: resource_school).maximum(:sort_index).to_i + 1
+
+      current_school.school_links.create!(data)
+    end
+
+    private
+
+    def resource_school
+      current_school
     end
   end
 end

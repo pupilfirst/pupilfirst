aae13c549b4809308d41d2f5161271e38216828a
Bodhish Thomas <bodhish@gmail.com>
2022-09-27 03:48:05 -0700

    Fix migration

--- db/migrate/20220425082348_add_cohorts.rb
+++ db/migrate/20220425082348_add_cohorts.rb
@@ -11,6 +11,7 @@ class AddCohorts < ActiveRecord::Migration[6.1]
 
   class Tagging < ApplicationRecord
     belongs_to :tag, class_name: '::ActsAsTaggableOn::Tag'
+    belongs_to :taggable, polymorphic: true
   end
 
   class Tags < ApplicationRecord
@@ -124,75 +125,76 @@ class AddCohorts < ActiveRecord::Migration[6.1]
     courses_count = Course.all.count
     total_startups = Startup.count
     n_updated_startups = 0
-    Course
-      .where(school_id: 1)
-      .each_with_index do |course, index|
-        puts "Setting up cohorts #{index + 1}/#{courses_count} :#{course.name}"
-        default_cohort =
-          Cohort.create!(
-            name: 'Purple (Auto-generated)',
-            description:
-              "Auto generated cohort for active students in #{course.name}",
-            ends_at: course.ends_at,
-            course_id: course.id
-          )
+    Course.all.each_with_index do |course, index|
+      puts "Setting up cohorts #{index + 1}/#{courses_count} :#{course.name}"
+      default_cohort =
+        Cohort.create!(
+          name: 'Purple (Auto-generated)',
+          description:
+            "Auto generated cohort for active students in #{course.name}",
+          ends_at: course.ends_at,
+          course_id: course.id
+        )
 
-        course
-          .startups
-          .includes(:founders, :taggings)
-          .group_by { |x| x.access_ends_at&.to_date }
-          .each do |ends_at, startups|
-            cohort =
-              if ends_at.nil?
-                default_cohort
-              else
-                Cohort.create!(
-                  name: "Batch ended on #{ends_at}",
-                  description:
-                    "Cohort created automatically for students whose access_ends_at was #{ends_at}",
-                  ends_at: ends_at,
-                  course_id: course.id
+      course
+        .startups
+        .includes(:founders, :taggings)
+        .group_by { |x| x.access_ends_at&.to_date }
+        .each do |ends_at, startups|
+          cohort =
+            if ends_at.nil?
+              default_cohort
+            else
+              Cohort.create!(
+                name: "Batch ended on #{ends_at}",
+                description:
+                  "Cohort created automatically for students whose access_ends_at was #{ends_at}",
+                ends_at: ends_at,
+                course_id: course.id
+              )
+            end
+          startup_count = startups.count
+
+          startups.each_with_index do |startup, i_s|
+            n_updated_startups += 1
+            puts "Setting up cohorts #{index + 1}/#{courses_count} :#{course.name} | #{i_s + 1}/#{startup_count} | Total: #{n_updated_startups * 100 / total_startups}%"
+
+            if startup.founders.count > 1
+              team = Team.create!(name: startup.name, cohort_id: cohort.id)
+            end
+
+            taggings =
+              Tagging.where(taggable_id: startup.id, taggable_type: 'Startup')
+
+            startup.founders.each do |student|
+              student.update!(
+                level_id: startup.level_id,
+                cohort_id: cohort.id,
+                dropped_out_at: startup.dropped_out_at,
+                team_id: team&.id
+              )
+
+              taggings.each do |tagging|
+                ActsAsTaggableOn::Tagging.create!(
+                  tag_id: tagging.tag_id,
+                  taggable_id: student.id,
+                  taggable_type: 'Founder',
+                  context: tagging.context
                 )
-              end
-            startup_count = startups.count
-
-            startups.each_with_index do |startup, i_s|
-              n_updated_startups += 1
-              puts "Setting up cohorts #{index + 1}/#{courses_count} :#{course.name} | #{i_s + 1}/#{startup_count} | Total: #{n_updated_startups * 100 / total_startups}%"
-
-              if startup.founders.count > 1
-                team = Team.create!(name: startup.name, cohort_id: cohort.id)
-              end
-
-              startup.founders.each do |student|
-                student.update!(
-                  level_id: startup.level_id,
-                  cohort_id: cohort.id,
-                  dropped_out_at: startup.dropped_out_at,
-                  team_id: team&.id
-                )
-
-                startup.taggings.each do |tagging|
-                  ActsAsTaggableOn::Tagging.create!(
-                    tag_id: tagging.tag_id,
-                    taggable_id: student.id,
-                    taggable_type: 'Founder',
-                    context: tagging.context
-                  )
-                end
               end
             end
           end
-
-        course.faculty_course_enrollments.each do |enrollment|
-          FacultyCohortEnrollment.create!(
-            faculty_id: enrollment.faculty_id,
-            cohort_id: default_cohort.id
-          )
         end
 
-        course.update!(default_cohort_id: default_cohort.id)
+      course.faculty_course_enrollments.each do |enrollment|
+        FacultyCohortEnrollment.create!(
+          faculty_id: enrollment.faculty_id,
+          cohort_id: default_cohort.id
+        )
       end
+
+      course.update!(default_cohort_id: default_cohort.id)
+    end
 
     FacultyStartupEnrollment.all.each do |enrollment|
       enrollment
@@ -208,6 +210,14 @@ class AddCohorts < ActiveRecord::Migration[6.1]
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    drop_table :faculty_founder_enrollments
+    drop_table :faculty_cohort_enrollments
+    remove_column :founders, :team_id
+    remove_column :founders, :level_id
+    remove_column :founders, :cohort_id
+    remove_column :founders, :dropped_out_at
+    remove_column :courses, :default_cohort_id
+    drop_table :teams
+    drop_table :cohorts
   end
 end

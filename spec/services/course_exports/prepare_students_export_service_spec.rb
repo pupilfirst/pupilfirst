4d37b02f9f7d0157ce6f90a22e0c9ede2ebc90a9
developedbysj <developedbysj@gmail.com>
2022-10-14 12:35:02 +0530

    Fix tests

--- spec/services/course_exports/prepare_students_export_service_spec.rb
+++ spec/services/course_exports/prepare_students_export_service_spec.rb
@@ -228,7 +228,7 @@ describe CourseExports::PrepareStudentsExportService do
             student_1.affiliation,
             'tag 1, tag 2',
             last_seen_at(student_1),
-            student_1.completed_at&.iso8601,
+            student_1.completed_at&.iso8601 || '',
             student_1_reviewed_submission
               .timeline_event_grades
               .find_by(evaluation_criterion: evaluation_criterion_1)
@@ -252,7 +252,7 @@ describe CourseExports::PrepareStudentsExportService do
             student_2.affiliation,
             '',
             last_seen_at(student_2),
-            student_2.completed_at&.iso8601,
+            student_2.completed_at&.iso8601 || '',
             student_2_reviewed_submission
               .timeline_event_grades
               .find_by(evaluation_criterion: evaluation_criterion_1)
@@ -400,7 +400,7 @@ describe CourseExports::PrepareStudentsExportService do
                 student_1.affiliation,
                 'tag 1, tag 2',
                 last_seen_at(student_1),
-                student_1.completed_at&.iso8601,
+                student_1.completed_at&.iso8601 || '',
                 student_1_reviewed_submission
                   .timeline_event_grades
                   .find_by(evaluation_criterion: evaluation_criterion_1)
@@ -424,7 +424,7 @@ describe CourseExports::PrepareStudentsExportService do
                 student_3_access_ended.affiliation,
                 'tag 2',
                 last_seen_at(student_3_access_ended),
-                student_3_access_ended.completed_at&.iso8601,
+                student_3_access_ended.completed_at&.iso8601 || '',
                 nil,
                 nil
               ],
@@ -438,7 +438,7 @@ describe CourseExports::PrepareStudentsExportService do
                 student_4_dropped_out.affiliation,
                 'tag 3',
                 last_seen_at(student_4_dropped_out),
-                student_4_dropped_out.completed_at&.iso8601,
+                student_4_dropped_out.completed_at&.iso8601 || '',
                 nil,
                 nil
               ]

944896f16584f960c41b340a727d69c735309b48
Hari Gopal <mail@harigopal.in>
2022-10-17 19:23:07 +0530

    Set distribution in Cohorts#students

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -13,23 +13,33 @@ class CohortsController < ApplicationController
   def students
     @organisation = policy_scope(Organisation).find(params[:organisation_id])
     @cohort = authorize current_school.cohorts.find(params[:id])
+    @distribution = distribution
   end
 
   private
 
-  def prepare_counts
-    scope = @organisation.founders.not_dropped_out.where(cohort_id: @cohort.id)
+  def scope
+    @scope ||=
+      @organisation.founders.not_dropped_out.where(cohort_id: @cohort.id)
+  end
 
+  def distribution
+    scope
+      .joins(:level)
+      .group('levels.number')
+      .count
+      .tap do |dist|
+        max_level_number = @cohort.course.levels.where.not(number: 0).count
+
+        (1..max_level_number).each { |level_number| dist[level_number] ||= 0 }
+      end
+  end
+
+  def prepare_counts
     {
       total: scope.count,
       # completed: scope.where.not(completed_at: nil).count,
-      distribution: scope.joins(:level).group('levels.number').count
-    }.tap do |counts|
-      max_level_number = @cohort.course.levels.where.not(number: 0).count
-
-      (1..max_level_number).each do |level_number|
-        counts[:distribution][level_number] ||= 0
-      end
-    end
+      distribution: distribution
+    }
   end
 end

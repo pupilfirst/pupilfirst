af506f4c401076426901e0d38c99dd9e426cb5bc
Hari Gopal <mail@harigopal.in>
2022-10-25 01:53:53 +0530

    students and show actions are identical

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -11,10 +11,5 @@ class CohortsController < ApplicationController
   end
 
   # GET /organisations/:organisation_id/cohorts/:id/students
-  def students
-    @organisation = policy_scope(Organisation).find(params[:organisation_id])
-    @cohort = authorize current_school.cohorts.find(params[:id])
-    @presenter =
-      Cohorts::StudentsPresenter.new(view_context, @organisation, @cohort)
-  end
+  alias students show
 end

a13390eb7b2b67523aee91631005016be32c8a1b
Mohamad Abras <mohamad.abras@gmail.com>
2022-11-02 18:09:11 +0300

    fix file limit on backend

--- app/forms/markdown_attachments/create_form.rb
+++ app/forms/markdown_attachments/create_form.rb
@@ -7,7 +7,7 @@ module MarkdownAttachments
              validates: {
                presence: true,
                file_size: {
-                 less_than: 5.megabytes
+                 less_than: ENV['MAX_UPLOAD_FILE_SIZE'].to_i
                }
              }
 

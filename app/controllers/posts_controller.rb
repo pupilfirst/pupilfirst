de5bad590f4c5173ad1e22a6e862b7dc72782eb9
yash-learner <yash.topaz@gmail.com>
2022-11-08 02:35:47 +0530

    Edit Posts controllers => Removing the code added when tried to implement authorization using Pundit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   layout 'student'
-  after_action :verify_authorized
 
   def versions
     @post = authorize(Post.live.find(params[:id]))

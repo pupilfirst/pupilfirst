13fdd90176f82e4b7584e3f5ec75ab225ce66050
Hari Gopal <mail@harigopal.in>
2022-11-15 13:36:00 +0530

    Make the last user of each org its org admin

--- db/seeds/development/organisation_admins.seeds.rb
+++ db/seeds/development/organisation_admins.seeds.rb
@@ -1,9 +1,9 @@
 after 'development:organisations' do
   puts 'Seeding organisation_admins'
 
-  # Make the first user of each org its org admin.
+  # Make the last user of each org its org admin.
   Organisation.all.each do |organisation|
-    user = organisation.users.first
+    user = organisation.users.last
     organisation.organisation_admins.create!(user: user)
   end
 end

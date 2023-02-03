72a79a250c39377b92057186a48df12d12c59813
Mahesh Krishna Kumar <maheshcet@gmail.com>
2023-02-03 15:50:29 +0530

    Fix UJS issue in calendars page

--- spec/system/school/courses/calendars_spec.rb
+++ spec/system/school/courses/calendars_spec.rb
@@ -212,9 +212,7 @@ feature 'Calendars', js: true do
         expect(page).to have_text(calendar_event_1.title)
 
         # Delete event
-        # TODO: The delete button has a confirmation dialog, but it does not trigger in test similar to production because of missing UJS in test build.
-        # This is a workaround for now and will be fixed once we remove the hack for rendering calendar pages in admin with a server rendered layout.
-        click_button('Delete')
+        accept_confirm { click_button('Delete') }
 
         dismiss_notification
 

6af1d0eb5f72e8fed3849fe6e555ec9facb747f4
yash-learner <yash.topaz@gmail.com>
2023-03-01 18:03:45 +0530

    Modified the mailer preview to pick the lastest timeline_grade and feedback

--- spec/mailers/previews/startup_mailer_preview.rb
+++ spec/mailers/previews/startup_mailer_preview.rb
@@ -1,7 +1,7 @@
 class StartupMailerPreview < ActionMailer::Preview
   def feedback_as_email_with_grade
     last_timeline_event_grade = TimelineEventGrade.last
-    startup_feedback = StartupFeedback.where(timeline_event_id: last_timeline_event_grade.timeline_event_id).last
+    startup_feedback = StartupFeedback.where(timeline_event_id: last_timeline_event_grade.timeline_event_id).first
 
     StartupMailer.feedback_as_email(startup_feedback, true)
   end

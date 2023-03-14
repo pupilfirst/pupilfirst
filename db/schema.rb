bbe9264bb6a11787c4eefa7cb39efb8ee45dba54
Mahesh Krishna Kumar <maheshcet@gmail.com>
2023-03-14 15:17:24 +0100

    Add a data migration to update checklist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_05_094729) do
+ActiveRecord::Schema.define(version: 2023_03_14_123822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"

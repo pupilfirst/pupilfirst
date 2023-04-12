0829b343187c7e11772942f708e6f6cfcca6f16e
yash-learner <yash.topaz@gmail.com>
2023-04-12 19:17:39 +0530

    Remove postcss dev dependency

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     require("postcss-import"),
-    require("postcss-logical"),
     require("postcss-flexbugs-fixes"),
     require("tailwindcss"),
     require("postcss-preset-env")({

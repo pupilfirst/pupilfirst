2898c090b6691a576e28476e56d6ad243ff88171
yash-learner <yash.topaz@gmail.com>
2023-04-18 02:40:50 +0530

    Updated specs

--- spec/system/courses/organisation_course_show_spec.rb
+++ spec/system/courses/organisation_course_show_spec.rb
@@ -62,19 +62,16 @@ feature 'Organisation show' do
   end
 
   context 'when the user is an organisation admin' do
-    scenario 'user can see overview of all student activity in their org' do
-      sign_in_user(org_admin_user, referrer: organisation_path(organisation_1))
+    scenario 'user can see all the active and inactive cohorts' do
+      sign_in_user(org_admin_user, referrer: organisation_course_path(organisation_1, course_1))
 
-      expect(page).to have_text("Total Students\n8")
-      expect(page).to have_text("Active Students\n6")
+      expect(page).to have_text("#{course_1.name}")
 
-      expect(page).to have_text(
-        "#{course_1.name}\n4 students enrolled in 2 active cohorts"
-      )
+      expect(page).to have_text("Active Cohorts\n2")
+      expect(page).to have_text("Ended Cohorts\n1")
 
-      expect(page).to have_text(
-        "#{course_2.name}\n2 students enrolled in 1 active cohort"
-      )
+      expect(page).to have_text("Active Cohorts")
+      expect(page).to have_text("Ended Cohorts")
 
       # There should be links to three active cohorts...
       expect(page).to have_link(
@@ -86,52 +83,6 @@ feature 'Organisation show' do
         cohort_2.name,
         href: organisation_cohort_path(organisation_1, cohort_2)
       )
-
-      expect(page).to have_link(
-        cohort_4.name,
-        href: organisation_cohort_path(organisation_1, cohort_4)
-      )
-
-      # ...but not to the inactive cohort.
-      expect(page).not_to have_link(cohort_3.name)
-
-      # There should be a link to view all cohorts.
-      expect(page).to have_link("View All Cohorts", href: organisation_course_path(organisation_1, course_1))
-    end
-  end
-
-  context 'when the user is a school admin' do
-    scenario 'user can access the organisation page' do
-      sign_in_user(
-        school_admin_user,
-        referrer: organisation_path(organisation_1)
-      )
-
-      expect(page).to have_text("Total Students\n8")
-      expect(page).to have_text("Active Students\n6")
-
-      # There should be a link to view all cohorts.
-      expect(page).to have_link("View All Cohorts", href: organisation_course_path(organisation_1, course_1))
-
-      # Both orgs should be accessible.
-      sign_in_user(
-        school_admin_user,
-        referrer: organisation_path(organisation_2)
-      )
-
-      expect(page).to have_text("Total Students\n2")
-      expect(page).to have_text("Active Students\n2")
-
-      # There should be a link to view all cohorts.
-      expect(page).to have_link("View All Cohorts", href: organisation_course_path(organisation_2, course_1))
-    end
-  end
-
-  context 'when the user is a non-admin' do
-    scenario 'user cannot see the organisation page' do
-      sign_in_user(regular_user, referrer: organisation_path(organisation_1))
-
-      expect(page).to have_http_status(:not_found)
     end
   end
 end

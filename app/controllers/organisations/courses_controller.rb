56bb75561acf7aea95f9e605ab9d07847276bb5b
yash-learner <yash.topaz@gmail.com>
2023-04-24 13:03:54 +0530

    Refactored the active/inactive views into single partial

--- app/controllers/organisations/courses_controller.rb
+++ app/controllers/organisations/courses_controller.rb
@@ -8,8 +8,9 @@ module Organisations
       @course = authorize current_school.courses.find(params[:id]), policy_class: Organisations::CoursePolicy
 
       @cohorts = @organisation.cohorts.where(course_id: @course.id).distinct
-      @ended_cohorts = @cohorts.ended.page(params[:ended_cohort_page]).per(10)
       @active_cohorts = @cohorts.active.page(params[:active_cohort_page]).per(10)
+
+      render '_cohorts', locals: { cohorts: @active_cohorts, organisation: @organisation, active: true }
     end
 
     def inactive_cohorts
@@ -18,6 +19,8 @@ module Organisations
 
       @cohorts = @organisation.cohorts.where(course_id: @course.id).distinct
       @ended_cohorts = @cohorts.ended.page(params[:ended_cohort_page]).per(10)
+
+      render '_cohorts', locals: { cohorts: @ended_cohorts, organisation: @organisation, active: false }
     end
   end
 end

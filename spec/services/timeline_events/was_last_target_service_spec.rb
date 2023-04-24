6fda4aa1519a2b4638ef35bc5f0a9e8a55e50d25
Kaiser Sakhi <kaisersakhi@gmail.com>
2023-04-24 20:21:04 +0530

    Refactor: modify specs of was_last_target_service

--- spec/services/timeline_events/was_last_target_service_spec.rb
+++ spec/services/timeline_events/was_last_target_service_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 describe TimelineEvents::WasLastTargetService do
+  include SubmissionsHelper
+
   let(:school) { create :school, :current }
   let(:course) { create :course, :with_cohort, school: school }
   let(:cohort) { course.cohorts.first }
@@ -9,93 +11,40 @@ describe TimelineEvents::WasLastTargetService do
 
   let(:target_group) { create :target_group, level: level, milestone: true }
   let(:target) do
-    create :target, role: Target::ROLE_STUDENT, target_group: target_group
-  end
-  let(:target_group_1) { create :target_group, level: level, milestone: true }
-  let(:target_1) do
-    create :target, role: Target::ROLE_STUDENT, target_group: target_group_1
+    create :target, :with_evaluation_criterion, role: Target::ROLE_STUDENT, target_group: target_group
   end
 
-  let!(:submission_1) do
-    create(
-      :timeline_event,
-      :with_owners,
-      owners: [student],
-      latest: true,
-      target: target
-    )
-  end
-
-  let!(:submission_2) do
-    create(
-      :timeline_event,
-      :with_owners,
-      owners: [student],
-      target: target_1,
-      latest: true
-    )
-  end
-
-  def mark_submission_as_reviewed(submission)
-    submission.evaluated_at = 1.day.ago
-    submission.evaluator_id = create(:faculty).id
-    submission.passed_at = 1.day.ago
-    submission.save
-  end
-
-  def mark_submission_as_not_reviewed(submission)
-    submission.evaluated_at = nil
-    submission.evaluator_id = nil
-    submission.passed_at = nil
-    submission.save
-  end
-
-  context "when cohort is not ended" do
-    describe "#was_last_target?" do
-      before do
-        cohort.ends_at = 1.day.from_now
-        cohort.save
-      end
-
-      it "returns false as submission is not reviewed" do
-        expect(described_class.new(submission_1).was_last_target?).to eq(false)
-      end
+  describe "#was_last_target?" do
+    let(:submission) { fail_target(target, student) }
+    it "returns false as submission is not reviewed" do
+      expect(described_class.new(submission).was_last_target?).to eq(false)
     end
+  end
 
-    describe "#was_last_target?" do
-      before do
-        mark_submission_as_reviewed(submission_1)
-        mark_submission_as_reviewed(submission_2)
-      end
-
-      it "returns true as submission is reviewed" do
-        expect(described_class.new(submission_2).was_last_target?).to eq(true)
-      end
+  describe "#was_last_target?" do
+    let(:submission) { complete_target(target, student) }
+    it "returns true as submission is reviewed" do
+      expect(described_class.new(submission).was_last_target?).to eq(true)
     end
   end
 
   context "when cohort is ended" do
-    describe "#was_last_target?" do
-      before do
-        cohort.ends_at = 1.day.ago
-        cohort.save
-        # keep submission_1 as reviewed
-        mark_submission_as_not_reviewed(submission_2)
-      end
+    before do
+      cohort.ends_at = 1.day.ago
+      cohort.save
+    end
 
+    describe "#was_last_target?" do
+      let(:submission) { fail_target(target, student) }
       it "returns false as submission is not reviewed" do
-        expect(described_class.new(submission_1).was_last_target?).to eq(false)
+        expect(described_class.new(submission).was_last_target?).to eq(false)
       end
     end
 
     describe "#was_last_target?" do
-      before do
-        mark_submission_as_reviewed(submission_1)
-        mark_submission_as_reviewed(submission_2)
-      end
-
+      let(:submission) { complete_target(target, student) }
       it "returns true as submission is reviewed" do
-        expect(described_class.new(submission_2).was_last_target?).to eq(true)
+        expect(described_class.new(submission).was_last_target?).to eq(true)
       end
     end
   end

4816c9e8cdcca0f7e5f1a71fc505a6249505e837
yash-learner <yash.topaz@gmail.com>
2023-04-24 17:37:36 +0530

    Refactor and clean up

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -7,16 +7,6 @@ class CohortsController < ApplicationController
     @organisation = policy_scope(Organisation).find(params[:organisation_id])
     @cohort = authorize current_school.cohorts.find(params[:id])
 
-    if request.referer.present? &&
-      URI(request.referer).path == "/organisations/#{@organisation.id}"
-        @back_link_path = organisation_path(@organisation)
-    elsif request.referer.present? &&
-      URI(request.referer).path == "/organisations/#{@organisation.id}/cohorts/#{params[:id]}"
-        @back_link_path = organisation_cohort_path(@organisation, @cohort)
-    else
-      @back_link_path = organisation_course_path(@organisation, @cohort.course)
-    end
-
     @presenter =
       Cohorts::StudentsPresenter.new(view_context, @organisation, @cohort)
   end
--- app/controllers/organisations/courses_controller.rb
+++ app/controllers/organisations/courses_controller.rb
@@ -4,23 +4,29 @@ module Organisations
     layout 'student'
 
     def active_cohorts
-      @organisation = policy_scope(Organisation).find(params[:organisation_id])
-      @course = authorize current_school.courses.find(params[:id]), policy_class: Organisations::CoursePolicy
-
-      @cohorts = @organisation.cohorts.where(course_id: @course.id).distinct
-      @active_cohorts = @cohorts.active.page(params[:active_cohort_page]).per(10)
-
+      find_organisation_and_course
+      @active_cohorts = find_cohorts(:active)
       render '_cohorts', locals: { cohorts: @active_cohorts, organisation: @organisation, active: true }
     end
 
     def inactive_cohorts
+      find_organisation_and_course
+      @ended_cohorts = find_cohorts(:inactive)
+      render '_cohorts', locals: { cohorts: @ended_cohorts, organisation: @organisation, active: false }
+    end
+
+    private
+
+    def find_organisation_and_course
       @organisation = policy_scope(Organisation).find(params[:organisation_id])
       @course = authorize current_school.courses.find(params[:id]), policy_class: Organisations::CoursePolicy
+      @cohorts = @organisation.cohorts.where(course_id: @course.id)
+    end
 
-      @cohorts = @organisation.cohorts.where(course_id: @course.id).distinct
-      @ended_cohorts = @cohorts.ended.page(params[:ended_cohort_page]).per(10)
-
-      render '_cohorts', locals: { cohorts: @ended_cohorts, organisation: @organisation, active: false }
+    def find_cohorts(status)
+      cohorts = status == :active ? @cohorts.active : @cohorts.ended
+      paged_cohorts = cohorts.page(params["#{status}_cohort_page"]).per(10)
+      paged_cohorts.count.zero? ? paged_cohorts.page(paged_cohorts.total_pages) : paged_cohorts
     end
   end
 end

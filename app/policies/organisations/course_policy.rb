bba9b62d738bf87b8bc801107bf6fed69ba289da
Yaswanth Kumar Bethu <53794102+yash-learner@users.noreply.github.com>
2023-05-03 11:12:19 +0530

    Organisation admins and school admins can see the inactive cohorts (#1278)
    Co-authored-by: Hari Gopal <mail@harigopal.in>
    Co-authored-by: vinu tv <vinutv91@gmail.com>

---
+++ app/policies/organisations/course_policy.rb
@@ -0,0 +1,18 @@
+module Organisations
+  class CoursePolicy < ApplicationPolicy
+    def active_cohorts?
+      return true if user.school_admin.present?
+
+      organisation_ids = user.organisations.pluck(:id)
+
+      return false if organisation_ids.blank?
+
+      record
+        .founders
+        .joins(:user)
+        .exists?(users: { organisation_id: organisation_ids })
+    end
+
+    alias inactive_cohorts? active_cohorts?
+  end
+end

927996c5a18c78afcec7174e57c8a932d275f517
yash-learner <yash.topaz@gmail.com>
2023-06-06 23:03:50 +0530

    Update the scenario name

--- spec/system/organisations/show_spec.rb
+++ spec/system/organisations/show_spec.rb
@@ -127,7 +127,7 @@ feature "Organisation show" do
       )
     end
 
-    scenario "check for view ended cohorts link when a course has only inactive cohorts" do
+    scenario "check for view all cohorts link when a course has only inactive cohorts" do
       sign_in_user(org_admin_user, referrer: organisation_path(organisation_1))
 
       # There should be two view all cohorts links.
@@ -203,7 +203,7 @@ feature "Organisation show" do
       )
     end
 
-    scenario "check for view ended cohorts link when a course has only inactive cohorts" do
+    scenario "check for view all cohorts link when a course has only inactive cohorts" do
       sign_in_user(
         school_admin_user,
         referrer: organisation_path(organisation_2)

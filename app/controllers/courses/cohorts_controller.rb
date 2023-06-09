b8da79a5f17266f78b57dc943e83d7df4814e316
Mahesh Krishna Kumar <maheshcet@gmail.com>
2023-06-09 19:00:43 +0530

    New students page and progress overview

--- app/controllers/courses/cohorts_controller.rb
+++ app/controllers/courses/cohorts_controller.rb
@@ -1,10 +1,15 @@
 class Courses::CohortsController < ApplicationController
   before_action :authenticate_user!
+  layout "student_course_v2"
 
   def show
     @course = policy_scope(Course).find(params[:course_id])
-    @cohort = authorize current_school.cohorts.find(params[:id])
-
-    render html: "twaing", layout: "student_course_v2"
+    @cohort =
+      authorize current_school.cohorts.find(params[:id]),
+                policy_class: Courses::CohortPolicy
+    @presenter =
+      Courses::Cohorts::StudentsPresenter.new(view_context, @course, @cohort)
   end
+
+  alias students show
 end
---
+++ app/presenters/courses/cohorts/students_presenter.rb
@@ -0,0 +1,114 @@
+class Courses::Cohorts::StudentsPresenter < ApplicationPresenter
+  def initialize(view_context, course, cohort)
+    @course = course
+    @cohort = cohort
+    super(view_context)
+  end
+
+  def filter
+    @filter ||= {
+      id: "course-cohort-students-filter",
+      filters: [
+        { key: "name", label: "Name", filterType: "Search", color: "red" },
+        { key: "email", label: "Email", filterType: "Search", color: "yellow" }
+      ],
+      placeholder: "Search by name or email",
+      hint: "...or start typing to search by student's name of email",
+      sorter: {
+        key: "sort_by",
+        default: "Recently Seen",
+        options: [
+          "Recently Seen",
+          "Name",
+          "First Created",
+          "Last Created",
+          "Earliest Seen"
+        ]
+      }
+    }
+  end
+
+  def counts
+    @counts = {
+      total: scope.count,
+      completed: scope.where.not(completed_at: nil).count
+    }
+  end
+
+  def students
+    @students ||=
+      begin
+        filter_1 = filter_students_by_name(scope)
+        filter_2 = filter_students_by_email(filter_1)
+        sorted = sort_students(filter_2)
+        included = sorted.includes(:user, :level)
+        paged = included.page(params[:page]).per(24)
+        paged.count.zero? ? paged.page(paged.total_pages) : paged
+      end
+  end
+
+  def milestone_completion_status
+    milestone_targets =
+      @course.targets.where(milestone: true).order(:milestone_number)
+
+    status = {}
+
+    milestone_targets.each do |target|
+      submissions =
+        TimelineEvent
+          .from_founders(@cohort.founders)
+          .where(target: target)
+          .passed
+      students_count = submissions.map(&:founders).flatten.uniq.count
+      status[target] = (
+        (students_count / total_students_count.to_f) * 100
+      ).round
+    end
+
+    status
+  end
+
+  def total_students_count
+    @total_students_count ||= scope.count
+  end
+
+  private
+
+  def filter_students_by_name(scope)
+    if params[:name].present?
+      scope.joins(:user).where("users.name ILIKE ?", "%#{params[:name]}%")
+    else
+      scope
+    end
+  end
+
+  def filter_students_by_email(scope)
+    if params[:email].present?
+      scope.joins(:user).where(
+        "lower(users.email) ILIKE ?",
+        "%#{params[:email].downcase}%"
+      )
+    else
+      scope
+    end
+  end
+
+  def sort_students(scope)
+    case params[:sort_by]
+    when "Name"
+      scope.joins(:user).order("users.name")
+    when "First Created"
+      scope.order(created_at: :asc)
+    when "Earliest Seen"
+      scope.joins(:user).order("users.last_seen_at ASC NULLS FIRST")
+    when "Last Created"
+      scope.order(created_at: :desc)
+    else
+      scope.joins(:user).order("users.last_seen_at DESC NULLS LAST")
+    end
+  end
+
+  def scope
+    @scope ||= @cohort.founders.not_dropped_out
+  end
+end

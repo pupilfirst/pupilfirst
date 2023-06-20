ca943ca1b80ca8330139b1f4eaeac23e7d882009
yash-learner <yash.topaz@gmail.com>
2023-06-20 16:43:17 +0530

    Remove the auto verify event type for auto accepted submission

--- app/services/timeline_events/create_service.rb
+++ app/services/timeline_events/create_service.rb
@@ -36,17 +36,13 @@ module TimelineEvents
             end
         end
 
-      submission_event_type =
-        if @target.evaluation_criteria.blank?
-          TimelineEvents::AfterMarkingAsCompleteJob.perform_later(submission)
-          :submission_automatically_verified
-        else
-          :submission_created
-        end
+      if @target.evaluation_criteria.blank?
+        TimelineEvents::AfterMarkingAsCompleteJob.perform_later(submission)
+      end
 
       @notification_service.execute(
         @founder.course,
-        submission_event_type,
+        :submission_created,
         @founder.user,
         submission
       )

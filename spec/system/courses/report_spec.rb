d526d3a025007b3720dd773c1dfb55b0122a058b
Mahesh Krishna Kumar <maheshcet@gmail.com>
2023-06-26 15:59:31 +0530

    Fix student level association in reports spec

--- spec/system/courses/report_spec.rb
+++ spec/system/courses/report_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
-feature 'Students view performance report and submissions overview', js: true do
+feature "Students view performance report and submissions overview", js: true do
   include UserSpecHelper
   include NotificationHelper
 
@@ -17,10 +17,8 @@ feature 'Students view performance report and submissions overview', js: true do
   # Create a team
   let!(:team) { create :team, cohort: cohort }
 
-  let!(:student) { create :founder, team: team, level: level_3, cohort: cohort }
-  let!(:another_student) do
-    create :founder, team: team, level: level_3, cohort: cohort
-  end
+  let!(:student) { create :founder, team: team, cohort: cohort }
+  let!(:another_student) { create :founder, team: team, cohort: cohort }
 
   # Create few targets for the student
   let(:target_group_l1) do
@@ -118,7 +116,7 @@ feature 'Students view performance report and submissions overview', js: true do
       owners: [student],
       target: quiz_target_1,
       passed_at: 1.day.ago,
-      quiz_score: '1/3'
+      quiz_score: "1/3"
     )
   end
   let!(:submission_quiz_target_2) do
@@ -129,7 +127,7 @@ feature 'Students view performance report and submissions overview', js: true do
       owners: [student],
       target: quiz_target_2,
       passed_at: 1.day.ago,
-      quiz_score: '3/5'
+      quiz_score: "3/5"
     )
   end
   let!(:coach_note_1) do
@@ -179,61 +177,61 @@ feature 'Students view performance report and submissions overview', js: true do
 
   around { |example| Time.use_zone(student.user.time_zone) { example.run } }
 
-  scenario 'student visits course report link' do
+  scenario "student visits course report link" do
     sign_in_user student.user, referrer: report_course_path(course)
 
-    expect(page).to have_text('Level Progress')
+    expect(page).to have_text("Level Progress")
     expect(page).to have_selector(
-      '.courses-report-overview__student-level',
+      ".courses-report-overview__student-level",
       count: course.levels.where.not(number: 0).count
     )
 
     # Targets Overview
-    expect(page).to have_text('Targets Overview')
+    expect(page).to have_text("Targets Overview")
 
     within("div[aria-label='target-completion-status']") do
-      expect(page).to have_content('Incomplete: 1')
-      expect(page).to have_content('Pending Review: 1')
-      expect(page).to have_content('Completed: 4')
-      expect(page).to have_content('66%')
+      expect(page).to have_content("Incomplete: 1")
+      expect(page).to have_content("Pending Review: 1")
+      expect(page).to have_content("Completed: 4")
+      expect(page).to have_content("66%")
     end
 
     within("div[aria-label='quiz-performance-chart']") do
-      expect(page).to have_content('Average Quiz Score')
-      expect(page).to have_content('46%')
-      expect(page).to have_content('2 Quizzes Attempted')
+      expect(page).to have_content("Average Quiz Score")
+      expect(page).to have_content("46%")
+      expect(page).to have_content("2 Quizzes Attempted")
     end
 
     # Average Grades
-    expect(page).to have_text('Average Grades')
+    expect(page).to have_text("Average Grades")
 
     within(
       "div[aria-label='average-grade-for-criterion-#{evaluation_criterion_1.id}']"
     ) do
       expect(page).to have_content(evaluation_criterion_1.name)
-      expect(page).to have_content('2.5/3')
+      expect(page).to have_content("2.5/3")
     end
 
     within(
       "div[aria-label='average-grade-for-criterion-#{evaluation_criterion_2.id}']"
     ) do
       expect(page).to have_content(evaluation_criterion_2.name)
-      expect(page).to have_content('2/3')
+      expect(page).to have_content("2/3")
     end
 
     # Coaches assigned to student
-    expect(page).to have_content('Personal Coaches')
+    expect(page).to have_content("Personal Coaches")
     expect(page).to have_content(team_coach.name)
     expect(page).to_not have_content(course_coach.name)
 
     # Checks submissions
-    click_button 'Submissions'
+    click_button "Submissions"
 
     expect(page).to have_link(target_l1.title, href: "/targets/#{target_l1.id}")
     expect(page).to_not have_content(target_4.title)
 
     # Filter submissions by level
-    fill_in 'filter', with: 'level'
+    fill_in "filter", with: "level"
     click_button "Level 2: #{level_2.name}"
 
     expect(page).not_to have_text(target_l1.title)
@@ -243,14 +241,14 @@ feature 'Students view performance report and submissions overview', js: true do
     find("button[title='Remove selection: #{level_2.name}']").click
 
     # Filter submissions by target status
-    fill_in 'filter', with: 'status'
-    click_button 'Status: Pending Review'
+    fill_in "filter", with: "status"
+    click_button "Status: Pending Review"
 
     expect(page).not_to have_text(target_l1.title)
     expect(page).to have_link(target_l3.title, href: "/targets/#{target_l3.id}")
   end
 
-  scenario 'student loads more submissions' do
+  scenario "student loads more submissions" do
     # Create over 20 reviewed submissions
     20.times do
       submission =
@@ -271,25 +269,25 @@ feature 'Students view performance report and submissions overview', js: true do
     end
 
     sign_in_user student.user, referrer: report_course_path(course)
-    expect(page).to have_text('Targets Overview')
-    click_button 'Submissions'
-    expect(page).to have_button('Load More...')
-    click_button('Load More...')
+    expect(page).to have_text("Targets Overview")
+    click_button "Submissions"
+    expect(page).to have_button("Load More...")
+    click_button("Load More...")
 
     total_submissions =
       student.timeline_events.evaluated_by_faculty.count +
         student.timeline_events.pending_review.count
 
     within("div[aria-label='Student submissions']") do
-      expect(page).to have_selector('a', count: total_submissions)
+      expect(page).to have_selector("a", count: total_submissions)
     end
 
     # Switching tabs should preserve already loaded submissions
-    click_button 'Overview'
-    click_button 'Submissions'
+    click_button "Overview"
+    click_button "Submissions"
 
     within("div[aria-label='Student submissions']") do
-      expect(page).to have_selector('a', count: total_submissions)
+      expect(page).to have_selector("a", count: total_submissions)
     end
   end
 
@@ -310,14 +308,14 @@ feature 'Students view performance report and submissions overview', js: true do
 
     before do
       # Add a team member to student's team
-      create :student, team: team, level: level_3, cohort: cohort
+      create :student, team: team, cohort: cohort
     end
 
-    scenario 'submissions by old team has info on effect of team change' do
+    scenario "submissions by old team has info on effect of team change" do
       sign_in_user student.user, referrer: report_course_path(course)
 
       # Switch to submissions tab
-      click_button 'Submissions'
+      click_button "Submissions"
 
       # The main link should point to the "backup" submission page.
       expect(page).to have_link(
@@ -334,34 +332,34 @@ feature 'Students view performance report and submissions overview', js: true do
 
         # There should be an additional link to the target as well.
         expect(page).to have_link(
-          'View Target',
+          "View Target",
           href: "/targets/#{target_l1.id}"
         )
       end
     end
   end
 
-  context 'course has targets in level zero' do
+  context "course has targets in level zero" do
     let!(:level_0) { create :level, :zero, course: course }
     let!(:target_group_l0) { create :target_group, level: level_0 }
     let!(:target_l0) do
       create :target, :for_founders, target_group: target_group_l0
     end
 
-    scenario 'checks status of total targets completed in report' do
+    scenario "checks status of total targets completed in report" do
       sign_in_user student.user, referrer: report_course_path(course)
 
       # Check that level zero targets are not counted in the targets overview
       within("div[aria-label='target-completion-status']") do
-        expect(page).to have_content('66%')
-        expect(page).to have_content('Incomplete: 1')
-        expect(page).to have_content('Pending Review: 1')
-        expect(page).to have_content('Completed: 4')
+        expect(page).to have_content("66%")
+        expect(page).to have_content("Incomplete: 1")
+        expect(page).to have_content("Pending Review: 1")
+        expect(page).to have_content("Completed: 4")
       end
     end
   end
 
-  context 'course has archived targets' do
+  context "course has archived targets" do
     let!(:target_4) do
       create :target, :for_founders, :archived, target_group: target_group_l3
     end
@@ -371,15 +369,15 @@ feature 'Students view performance report and submissions overview', js: true do
       create :target, :for_founders, :archived, target_group: target_group_l3
     end
 
-    scenario 'checks status of total targets completed in report' do
+    scenario "checks status of total targets completed in report" do
       sign_in_user student.user, referrer: report_course_path(course)
 
       # Check that level zero targets are not counted in the targets overview
       within("div[aria-label='target-completion-status']") do
-        expect(page).to have_content('100%')
-        expect(page).to have_content('Incomplete: 0')
-        expect(page).to have_content('Pending Review: 0')
-        expect(page).to have_content('Completed: 4')
+        expect(page).to have_content("100%")
+        expect(page).to have_content("Incomplete: 0")
+        expect(page).to have_content("Pending Review: 0")
+        expect(page).to have_content("Completed: 4")
       end
     end
   end

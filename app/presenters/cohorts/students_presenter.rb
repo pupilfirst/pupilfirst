9133cb6268473b7e4bc372b77eba6af85eec66e3
yash-learner <yash.topaz@gmail.com>
2023-06-30 21:55:03 +0530

    Add the milestone filter in org and UI wiring

--- app/presenters/cohorts/students_presenter.rb
+++ app/presenters/cohorts/students_presenter.rb
@@ -3,6 +3,7 @@ module Cohorts
     def initialize(view_context, organisation, cohort)
       @organisation = organisation
       @cohort = cohort
+      @course = cohort.course
       super(view_context)
     end
 
@@ -16,6 +17,20 @@ module Cohorts
             label: "Email",
             filterType: "Search",
             color: "yellow"
+          },
+          {
+            key: "milestone",
+            label: "Milestone",
+            filterType: "MultiSelect",
+            values:
+              @course
+                .targets
+                .where(milestone: true)
+                .order(:milestone_number)
+                .map do |target|
+                  "M#{target.milestone_number}: #{target.title}"
+                end,
+            color: "blue"
           }
         ],
         placeholder: "Search by name or email",
@@ -44,9 +59,10 @@ module Cohorts
     def students
       @students ||=
         begin
-          filter_1 = filter_students_by_name(scope)
-          filter_2 = filter_students_by_email(filter_1)
-          sorted = sort_students(filter_2)
+          filter_1 = filter_students_by_milestone(scope)
+          filter_2 = filter_students_by_name(filter_1)
+          filter_3 = filter_students_by_email(filter_2)
+          sorted = sort_students(filter_3)
           included = sorted.includes(:user)
           paged = included.page(params[:page]).per(24)
           paged.count.zero? ? paged.page(paged.total_pages) : paged
@@ -61,17 +77,20 @@ module Cohorts
 
       milestone_targets.each do |target|
         submissions =
-          TimelineEvent
-            .from_founders(@cohort.founders)
-            .where(target: target)
-            .passed
+          TimelineEvent.from_founders(scope).where(target: target).passed
         students_count = submissions.map(&:founders).flatten.uniq.count
-        status[target] = (
-          (students_count / total_students_count.to_f) * 100
-        ).round
+        percentage = ((students_count / total_students_count.to_f) * 100).round
+        status[target] = {
+          percentage: percentage,
+          students_count: students_count
+        }
       end
 
       status
+    end
+
+    def total_students_count
+      @total_students_count ||= scope.count
     end
 
     private
@@ -95,6 +114,23 @@ module Cohorts
       end
     end
 
+    def filter_students_by_milestone(scope)
+      if params[:milestone].present?
+        milestone_number = params[:milestone].split(":").first[1..-1]
+        scope
+          .joins(timeline_events: :target)
+          .where(
+            targets: {
+              milestone_number: milestone_number,
+              milestone: true
+            }
+          )
+          .where.not(timeline_events: { passed_at: nil })
+      else
+        scope
+      end
+    end
+
     def sort_students(scope)
       case params[:sort_by]
       when "Name"
@@ -113,10 +149,6 @@ module Cohorts
     def scope
       @scope ||=
         @organisation.founders.not_dropped_out.where(cohort_id: @cohort.id)
-    end
-
-    def total_students_count
-      @total_students_count ||= scope.count
     end
   end
 end

d83763a7f110edd68c60a32b6c4fb1637a457f61
yash-learner <yash.topaz@gmail.com>
2023-07-31 01:36:28 +0530

    Update cohort policy

--- app/policies/cohort_policy.rb
+++ app/policies/cohort_policy.rb
@@ -2,7 +2,7 @@ class CohortPolicy < ApplicationPolicy
   def show?
     return false if user.faculty.blank?
 
-    user.faculty.cohorts.exists?(id: record.id)
+    user.faculty.cohorts.exists?(id: record.id) || current_school_admin.present?
   end
 
   alias students? show?

6a2b9f929e13236784df727160dbfdb5e08a7150
yash-learner <yash.topaz@gmail.com>
2023-08-04 23:49:03 +0530

    Update the view submissions authorizer

--- app/queries/concerns/authorize_view_submissions.rb
+++ app/queries/concerns/authorize_view_submissions.rb
@@ -2,11 +2,11 @@ module AuthorizeViewSubmissions
   include ActiveSupport::Concern
 
   def authorized?
-    return false if current_user&.faculty.blank?
+    return false if current_user&.faculty.blank? && current_school_admin.blank?
 
     return false if course&.school != current_school
 
-    current_user.faculty.courses.exists?(id: course) ||
-      current_school_admin.present?
+    current_school_admin.present? ||
+      current_user.faculty.courses.exists?(id: course)
   end
 end

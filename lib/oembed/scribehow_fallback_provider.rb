51f77f51418af7a1be903df2377734717f726776
yash-learner <yash.topaz@gmail.com>
2023-08-22 00:21:26 +0530

    Allow links with shared in url path

--- lib/oembed/scribehow_fallback_provider.rb
+++ lib/oembed/scribehow_fallback_provider.rb
@@ -5,7 +5,11 @@ module Oembed
     end
 
     def self.paths
-      [%r{^/embed/.*}]
+      [%r{^/embed/.*}, %r{^/shared/.*}]
+    end
+
+    def resource_url
+      @resource_url.gsub("/shared", "/embed")
     end
   end
 end

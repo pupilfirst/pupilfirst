9c1ddadba6f342a47411fb571621bc7134e7e633
Hari Gopal <mail@harigopal.in>
2023-08-24 19:14:28 +0530

    Add :en to fallbacks when en isn't the default locale
    Also set "en" as the frontend's `defaultLocale`, since it appears that
    the only purpose of `defaultLocale` for `i18n-js` is to act as a
    fallback for missing translations. The locale used in the frontend is
    anyway dictated by the locale decided on by the server, which takes
    default locale and user's choice (if any) into consideration.

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -8,3 +8,10 @@ end
 
 I18n.available_locales = Rails.application.secrets.locale[:available]
 I18n.default_locale = Rails.application.secrets.locale[:default]
+
+if Rails.application.secrets.locale[:default] != "en"
+  Rails.application.config.i18n.fallbacks = [
+    Rails.application.secrets.locale[:default].to_sym,
+    :en
+  ]
+end

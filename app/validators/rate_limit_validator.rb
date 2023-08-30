8517325d57d6e91adac8bb3c77f9dfaeae7869c1
Bodhish Thomas <bodhish@gmail.com>
2023-08-30 16:09:27 +0530

    Add rate limit validator

---
+++ app/validators/rate_limit_validator.rb
@@ -0,0 +1,12 @@
+class RateLimitValidator < ActiveModel::Validator
+  def validate(record)
+    limit = options[:limit]
+    scope = options[:scope]
+    time_frame = options[:time_frame]
+    query = record.class.where(scope => record.send(scope))
+    query = query.where("created_at >= ?", Time.now - time_frame) if time_frame
+    count = query.count
+
+    record.errors.add(:base, "Rate limit exceeded: #{limit}") if count >= limit
+  end
+end
---
+++ spec/validators/rate_limit_validator_spec.rb
@@ -0,0 +1,65 @@
+require "rails_helper"
+
+RSpec.describe RateLimitValidator do
+  describe "#validate" do
+    let(:creator) { create(:user) }
+
+    context "with time_frame" do
+      before do
+        Post.clear_validators!
+        Post.validates_with RateLimitValidator,
+                            limit: 10,
+                            scope: :creator_id,
+                            time_frame: 1.day
+      end
+
+      after do
+        Post.clear_validators!
+        Post.validates_with RateLimitValidator, limit: 10, scope: :creator_id
+      end
+
+      it "does not add any error if the rate limit is not exceeded" do
+        create_list(:post, 9, creator: creator)
+        post = build(:post, creator: creator)
+        post.validate
+        expect(post.errors).to be_empty
+      end
+
+      it "adds an error if the rate limit is exceeded" do
+        create_list(:post, 10, creator: creator)
+        post = build(:post, creator: creator)
+        post.validate
+        expect(post.errors[:base]).to include("Rate limit exceeded: 10")
+      end
+    end
+
+    context "without time_frame" do
+      before do
+        Post.clear_validators!
+        Post.validates_with RateLimitValidator, limit: 10, scope: :creator_id
+      end
+
+      after do
+        Post.clear_validators!
+        Post.validates_with RateLimitValidator,
+                            limit: 10,
+                            scope: :creator_id,
+                            time_frame: 1.day
+      end
+
+      it "does not add any error if the rate limit is not exceeded" do
+        create_list(:post, 9, creator: creator)
+        post = build(:post, creator: creator)
+        post.validate
+        expect(post.errors).to be_empty
+      end
+
+      it "adds an error if the rate limit is exceeded" do
+        create_list(:post, 10, creator: creator)
+        post = build(:post, creator: creator)
+        post.validate
+        expect(post.errors[:base]).to include("Rate limit exceeded: 10")
+      end
+    end
+  end
+end

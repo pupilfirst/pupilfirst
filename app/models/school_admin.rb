2dc0a1078eaca762b62fc3d4882b18a9ad122fa0
Kaiser Sakhi <kaisersakhi@gmail.com>
2023-09-08 16:14:05 +0530

    remove rate limiting for school_admin

--- app/models/school_admin.rb
+++ app/models/school_admin.rb
@@ -1,6 +1,4 @@
 class SchoolAdmin < ApplicationRecord
   belongs_to :user
   delegate :school, :name, :email, :title, to: :user
-  
-  validates_with RateLimitValidator, limit: 100, scope: :school_id
 end

d8f9c0949acbe00edc4f264ee56827c52507cb29
yash-learner <yash.topaz@gmail.com>
2023-09-12 18:48:31 +0530

    Remove the redudant admin check

--- app/queries/concerns/authorize_view_submissions.rb
+++ app/queries/concerns/authorize_view_submissions.rb
@@ -2,11 +2,12 @@ module AuthorizeViewSubmissions
   include ActiveSupport::Concern
 
   def authorized?
-    return false if current_user&.faculty.blank? && current_school_admin.blank?
+    return true if current_school_admin.present?
+
+    return false if current_user&.faculty.blank?
 
     return false if course&.school != current_school
 
-    current_school_admin.present? ||
-      current_user.faculty.courses.exists?(id: course)
+    current_user.faculty.courses.exists?(id: course)
   end
 end

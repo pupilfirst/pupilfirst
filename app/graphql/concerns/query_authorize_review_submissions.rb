1595d4d6b8599a6014c9a702bb1788b5ab848a85
yash-learner <yash.topaz@gmail.com>
2023-09-12 18:59:25 +0530

    Reorder the checks

--- app/graphql/concerns/query_authorize_review_submissions.rb
+++ app/graphql/concerns/query_authorize_review_submissions.rb
@@ -2,9 +2,9 @@ module QueryAuthorizeReviewSubmissions
   include ActiveSupport::Concern
 
   def query_authorized?
-    return false if current_user&.faculty.blank?
-
     return false if course&.school != current_school
+
+    return false if current_user&.faculty.blank?
 
     current_user.faculty.cohorts.exists?(id: submission.students.first.cohort)
   end
--- app/queries/concerns/authorize_view_submissions.rb
+++ app/queries/concerns/authorize_view_submissions.rb
@@ -2,11 +2,11 @@ module AuthorizeViewSubmissions
   include ActiveSupport::Concern
 
   def authorized?
+    return false if course&.school != current_school
+
     return true if current_school_admin.present?
 
     return false if current_user&.faculty.blank?
-
-    return false if course&.school != current_school
 
     current_user.faculty.courses.exists?(id: course)
   end

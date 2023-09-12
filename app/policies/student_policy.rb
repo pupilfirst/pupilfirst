0ff5bdc96501ee1cda77754f990042908a53649a
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2023-09-13 01:23:17 +0530

    Update app/policies/student_policy.rb
    Co-authored-by: Hari Gopal <mail@harigopal.in>

--- app/policies/student_policy.rb
+++ app/policies/student_policy.rb
@@ -2,7 +2,7 @@ class StudentPolicy < ApplicationPolicy
   def report?
     return false if user.blank?
 
-    # Coaches who review submissions from this student can view their profile.
+    # School admins and coaches assigned to student's cohort can view their profile.
     faculty = user.faculty
     current_school_admin.present? ||
       (faculty.present? && faculty.cohorts.exists?(id: record.cohort))

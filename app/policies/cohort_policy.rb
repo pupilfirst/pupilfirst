247b5050947387778a17606150d8695eecbdae75
yash-learner <yash.topaz@gmail.com>
2023-09-12 17:13:08 +0530

    Remove the redundant check

--- app/policies/cohort_policy.rb
+++ app/policies/cohort_policy.rb
@@ -1,7 +1,5 @@
 class CohortPolicy < ApplicationPolicy
   def show?
-    return false if record.school != current_school
-
     return true if current_school_admin.present?
 
     return false if user.faculty.blank?

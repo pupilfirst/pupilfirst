014ab8dcef880902efcdb2cd01ba6a7c033bcb2c
yash-learner <yash.topaz@gmail.com>
2023-09-12 20:46:05 +0530

    Scope the query with current_school

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,7 @@
 class StudentsController < ApplicationController
   # GET /students/:id/report
   def report
-    student = authorize(Student.find(params[:id]))
+    student = authorize current_school.students.find(params[:id])
     @course = student.course
     render html: "", layout: "app_router"
   end

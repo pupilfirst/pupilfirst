ec1f716ca1b95251a16b4375143f1b6a40453789
yash-learner <yash.topaz@gmail.com>
2023-11-20 01:34:22 +0530

    Update the allowed keys

--- app/queries/update_school_string_mutator.rb
+++ app/queries/update_school_string_mutator.rb
@@ -1,18 +1,44 @@
 class UpdateSchoolStringMutator < ApplicationQuery
   include AuthorizeSchoolAdmin
 
-  property :key, validates: { inclusion: { in: SchoolString::VALID_KEYS, message: 'InvalidKey' } }
+  property :key,
+           validates: {
+             inclusion: {
+               in: SchoolString::VALID_KEYS,
+               message: "InvalidKey"
+             }
+           }
   property :value
 
   # 'value' has different validations for different school string types.
-  validates :value, length: { maximum: 20_000, message: 'InvalidLengthValue' }, allow_blank: true, if: :agreement?
-  validates :value, length: { maximum: 1000, message: 'InvalidLengthValue' }, allow_blank: true, if: :address?
-  validates :value, email: { message: 'InvalidValue' }, allow_blank: true, if: :email_address?
+  validates :value,
+            length: {
+              maximum: 20_000,
+              message: "InvalidLengthValue"
+            },
+            allow_blank: true,
+            if: :agreement?
+
+  validates :value,
+            length: {
+              maximum: 1000,
+              message: "InvalidLengthValue"
+            },
+            allow_blank: true,
+            if: :address?
+
+  validates :value,
+            email: {
+              message: "InvalidValue"
+            },
+            allow_blank: true,
+            if: :email_address?
 
   def update_school_string
     SchoolString.transaction do
       if value.present?
-        school_string = current_school.school_strings.where(key: key).first_or_initialize
+        school_string =
+          current_school.school_strings.where(key: key).first_or_initialize
         school_string.value = value.strip
         school_string.save!
       else
@@ -28,7 +54,13 @@ class UpdateSchoolStringMutator < ApplicationQuery
   end
 
   def agreement?
-    key.in?([SchoolString::PrivacyPolicy.key, SchoolString::TermsAndConditions.key])
+    key.in?(
+      [
+        SchoolString::PrivacyPolicy.key,
+        SchoolString::TermsAndConditions.key,
+        SchoolString::CodeOfConduct.key
+      ]
+    )
   end
 
   def address?

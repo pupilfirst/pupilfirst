47a4e80254f4b929812d9287dfb1230af7a9f698
yash-learner <yash.topaz@gmail.com>
2023-11-29 16:55:44 +0530

    Merge master and exlude the yarn-4.0.0.cjs in file size overcommit

--- app/controllers/users/postmark_webhook_controller.rb
+++ app/controllers/users/postmark_webhook_controller.rb
@@ -1,22 +1,37 @@
 module Users
   class PostmarkWebhookController < ApplicationController
     skip_before_action :verify_authenticity_token
-    http_basic_authenticate_with name: ENV['POSTMARK_HOOK_ID'], password: ENV['POSTMARK_HOOK_SECRET']
+    before_action :authenticate_request_from_postmark
 
     # POST /users/email_bounce
     def email_bounce
-      mark_email_bounced if params[:Email].present? && params[:Type].in?(accepted_webhook_types)
+      if params[:Email].present? && params[:Type].in?(accepted_webhook_types)
+        mark_email_bounced
+      end
       head :ok
     end
 
     private
+
+    def authenticate_request_from_postmark
+      if authenticate_with_http_basic { |username, password|
+           username == ENV["POSTMARK_HOOK_ID"] &&
+             password == ENV["POSTMARK_HOOK_SECRET"]
+         }
+        true
+      else
+        head :unauthorized
+      end
+    end
 
     def accepted_webhook_types
       %w[HardBounce SpamComplaint]
     end
 
     def mark_email_bounced
-      BounceReport.where(email: params[:Email]).first_or_create!(bounce_type: params[:Type])
+      BounceReport.where(email: params[:Email]).first_or_create!(
+        bounce_type: params[:Type]
+      )
     end
   end
 end

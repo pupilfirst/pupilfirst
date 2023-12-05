fabb2e2168bafb4e06be4a69a51b5c91ccbc4887
Ramzi Hemadou <ramzihemadou@gmail.com>
2023-12-05 10:41:25 +0100

    fix show flash message overlap

--- app/frontend/shared/notifier.js
+++ app/frontend/shared/notifier.js
@@ -1,4 +1,4 @@
-import { alert, defaultModules } from "@pnotify/core";
+import { alert, defaultModules, Stack } from "@pnotify/core";
 import "@pnotify/core/dist/PNotify.css";
 import * as PNotifyMobile from "@pnotify/mobile";
 import "@pnotify/mobile/dist/PNotifyMobile.css";
@@ -19,6 +19,9 @@ const notify = (title, text, type) => {
     reloadRequired = true;
   }
 
+  const notificationContainer = document.getElementById("notifications");
+  document.body.appendChild(notificationContainer);
+
   const notification = alert({
     type: type,
     title: title,
@@ -27,10 +30,24 @@ const notify = (title, text, type) => {
     styling: "custom",
     icons: "custom",
     mode: "light",
+    addClass: "mb-4",
     maxTextHeight: null,
     closer: true,
     sticker: false,
     delay: 6000,
+    stack: new Stack({
+      context: notificationContainer,
+      dir1: "down",
+      dir2: "left",
+      firstpos1: 0,
+      firstpos2: 0,
+      push: "down",
+      spacing1: 0,
+      spacing2: 0,
+      maxStrategy: 'wait',
+      maxOpen: 6,
+      modal: false,
+    }),
     modules: new Map([...defaultModules, [PNotifyMobile, {}]]),
   });
 

6265e095e6c303e33832fc9a33e09676940aa114
yash-learner <yash.topaz@gmail.com>
2023-12-09 00:31:58 +0530

    Update the migration

--- db/migrate/20231205143719_rename_icon_to_icon_on_light_bg.rb
+++
@@ -1,7 +0,0 @@
-class RenameIconToIconOnLightBg < ActiveRecord::Migration[6.1]
-  def change
-    ActiveStorage::Attachment.where(name: "icon").update(
-      name: "icon_on_light_bg"
-    )
-  end
-end

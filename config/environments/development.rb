6b2d29c40baf373c7754bc3496e9f968aa7299da
yash-learner <yash.topaz@gmail.com>
2023-12-22 03:05:44 +0530

    Set the requests to false in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
 
   # Enable server timing
   config.server_timing = true

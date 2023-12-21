4c2b09d35cb311837f8056b52a3ceff85d760d06
yash-learner <yash.topaz@gmail.com>
2023-12-22 04:19:34 +0530

    Update the mailer previews and feedback mailer template

--- spec/mailers/previews/startup_mailer_preview.rb
+++ spec/mailers/previews/startup_mailer_preview.rb
@@ -1,12 +1,46 @@
 class StartupMailerPreview < ActionMailer::Preview
   def feedback_as_email_with_grade
-    startup_feedback = TimelineEventGrade.last.timeline_event.startup_feedback.first
+    startup_feedback =
+      TimelineEventGrade.last.timeline_event.startup_feedback.first
 
     StartupMailer.feedback_as_email(startup_feedback, true)
   end
 
   def feedback_as_email
-    startup_feedback = StartupFeedback.last
+    timeline_event = TimelineEventGrade.last.timeline_event
+
+    startup_feedback =
+      StartupFeedback.create!(
+        timeline_event: timeline_event,
+        faculty: timeline_event.startup_feedback.first.faculty,
+        feedback: "This is additional feedback"
+      )
+
+    StartupMailer.feedback_as_email(startup_feedback, false)
+  end
+
+  def feedback_as_email_when_submission_is_rejected
+    startup_feedback =
+      TimelineEvent
+        .where
+        .missing(:timeline_event_grades)
+        .first
+        .startup_feedback
+        .first
+
+    StartupMailer.feedback_as_email(startup_feedback, false)
+  end
+
+  def feedback_as_email_for_form_response
+    startup_feedback =
+      Assignment
+        .includes(:evaluation_criteria, :quiz)
+        .where(evaluation_criteria: { id: nil }, quizzes: { id: nil })
+        .first
+        .timeline_events
+        .first
+        .startup_feedback
+        .first
 
     StartupMailer.feedback_as_email(startup_feedback, false)
   end

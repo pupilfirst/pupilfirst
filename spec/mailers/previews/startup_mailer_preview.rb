eedfafdd14738362c0dd166de6aa5089570cf1aa
Yaswanth Kumar Bethu <yash.topaz@gmail.com>
2023-12-22 16:47:49 +0530

    Fix incorrect message in submission feedback email (#1518)
    Co-authored-by: Hari Gopal <mail@harigopal.in>

--- spec/mailers/previews/startup_mailer_preview.rb
+++ spec/mailers/previews/startup_mailer_preview.rb
@@ -1,12 +1,46 @@
 class StartupMailerPreview < ActionMailer::Preview
   def feedback_as_email_with_grade
-    startup_feedback = TimelineEventGrade.last.timeline_event.startup_feedback.first
+    startup_feedback =
+      TimelineEventGrade.last.timeline_event.startup_feedback.first
 
     StartupMailer.feedback_as_email(startup_feedback, true)
   end
 
-  def feedback_as_email
-    startup_feedback = StartupFeedback.last
+  def additional_feedback_as_email
+    timeline_event =
+      TimelineEvent
+        .joins(:startup_feedback)
+        .group(:id)
+        .having("count(startup_feedback.id) > 1")
+        .first
+
+    startup_feedback = timeline_event.startup_feedback.last
+
+    StartupMailer.feedback_as_email(startup_feedback, false)
+  end
+
+  def feedback_as_email_when_submission_is_rejected
+    startup_feedback =
+      TimelineEvent
+        .where
+        .missing(:timeline_event_grades)
+        .first
+        .startup_feedback
+        .first
+
+    StartupMailer.feedback_as_email(startup_feedback, false)
+  end
+
+  def feedback_as_email_for_form_response
+    startup_feedback =
+      Assignment
+        .includes(:evaluation_criteria, :quiz)
+        .where(evaluation_criteria: { id: nil }, quizzes: { id: nil })
+        .first
+        .timeline_events
+        .first
+        .startup_feedback
+        .first
 
     StartupMailer.feedback_as_email(startup_feedback, false)
   end

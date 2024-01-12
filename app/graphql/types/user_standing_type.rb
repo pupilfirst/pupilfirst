bf5feaff657147725804160e61d325d50e606e8f
yash-learner <yash.topaz@gmail.com>
2024-01-13 02:30:29 +0530

    Use batch loader

--- app/graphql/types/user_standing_type.rb
+++ app/graphql/types/user_standing_type.rb
@@ -8,15 +8,36 @@ module Types
     field :reason, String, null: false
 
     def creator_name
-      object.creator.name
+      BatchLoader::GraphQL
+        .for(object.creator_id)
+        .batch do |creator_ids, loader|
+          User
+            .where(id: creator_ids)
+            .each { |user| loader.call(user.id, user.name) }
+        end
+      # object.creator.name
     end
 
     def standing_name
-      object.standing.name
+      BatchLoader::GraphQL
+        .for(object.standing_id)
+        .batch do |standing_ids, loader|
+          Standing
+            .where(id: standing_ids)
+            .each { |standing| loader.call(standing.id, standing.name) }
+        end
+      # object.standing.name
     end
 
     def standing_color
-      object.standing.color
+      BatchLoader::GraphQL
+        .for(object.standing_id)
+        .batch do |standing_ids, loader|
+          Standing
+            .where(id: standing_ids)
+            .each { |standing| loader.call(standing.id, standing.color) }
+        end
+      # object.standing.color
     end
   end
 end

8a3f4ee9d485f7d099b2f06c7fc13dfa600a708b
shilendras <shilendrasoman@gmail.com>
2024-01-18 16:43:23 +0530

    remove primary key

--- app/models/assignments_prerequisite_assignment.rb
+++ app/models/assignments_prerequisite_assignment.rb
@@ -1,5 +1,4 @@
 class AssignmentsPrerequisiteAssignment < ApplicationRecord
-  self.primary_key = :assignment_id
   belongs_to :assignment
   belongs_to :prerequisite_assignment, class_name: "Assignment"
   validates_with RateLimitValidator, limit: 25, scope: :assignment_id

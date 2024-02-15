b25fff0d1e39200c804c450a2eaaf63221e02f0b
Mohamad Abras <mohamad.abras@gmail.com>
2024-02-15 23:40:12 +0400

    run `rails app:update`

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,13 +8,15 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Turn false under Spring and add config.action_view.cache_template_loading = true.
-  config.cache_classes = true
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = false
+
   config.action_view.cache_template_loading = true
 
-  # Eager loading loads your whole application. When running a single test locally,
-  # this probably isn't necessary. It's a good idea to do in a continuous integration
-  # system, or in some way before deploying your code.
+  # Eager loading loads your entire application. When running a single test locally,
+  # this is usually not necessary, and can slow down your test suite. However, it's
+  # recommended that you enable it in continuous integration systems to ensure eager
+  # loading is working properly before deploying your code.
   config.eager_load = ENV["CI"].present?
 
   # Configure public file server for tests with Cache-Control for performance.
@@ -31,8 +33,8 @@ Rails.application.configure do
   # Request timeout using Slowpoke.
   config.slowpoke.timeout = ENV.fetch("SLOWPOKE_TIMEOUT", "120").to_i
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = true
+  # Render exception templates for rescuable exceptions and raise for other exceptions.
+  config.action_dispatch.show_exceptions = :rescuable
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = true
@@ -65,6 +67,9 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
 
   # Override Rails 5's default of :async, and force jobs to run inline.
   test_adapter = ActiveJob::QueueAdapters::TestAdapter.new

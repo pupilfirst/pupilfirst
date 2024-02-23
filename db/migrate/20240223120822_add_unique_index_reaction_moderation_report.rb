ba7df96b6296c4f9a261871ad94eacfcbba37419
shilendras <shilendrasoman@gmail.com>
2024-02-23 19:35:11 +0530

    Remove duplicates before adding unique index

--- db/migrate/20240223120822_add_unique_index_reaction_moderation_report.rb
+++ db/migrate/20240223120822_add_unique_index_reaction_moderation_report.rb
@@ -1,5 +1,23 @@
 class AddUniqueIndexReactionModerationReport < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    duplicates =
+      Reaction
+        .select(:user_id, :reactionable_type, :reactionable_id, :reaction_value)
+        .group(:user_id, :reactionable_type, :reactionable_id, :reaction_value)
+        .having("COUNT(*) > 1")
+    duplicates.each do |duplicate|
+      Reaction
+        .where(
+          user_id: duplicate.user_id,
+          reactionable_type: duplicate.reactionable_type,
+          reactionable_id: duplicate.reactionable_id,
+          reaction_value: duplicate.reaction_value
+        )
+        .order(created_at: :desc)
+        .offset(1)
+        .destroy_all
+    end
+
     add_index :moderation_reports,
               %i[user_id reportable_type reportable_id],
               unique: true,
@@ -8,5 +26,9 @@ class AddUniqueIndexReactionModerationReport < ActiveRecord::Migration[7.0]
               %i[user_id reactionable_type reactionable_id reaction_value],
               unique: true,
               name: "index_reactions_on_user_and_reactionable"
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
   end
 end

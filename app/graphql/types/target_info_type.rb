f797f6f1cba781e0e6a9cd9bb39567baba432b49
Hari Gopal <mail@harigopal.in>
2024-02-28 00:49:29 +0530

    Prep the batch loader for milestone number

--- app/graphql/types/target_info_type.rb
+++ app/graphql/types/target_info_type.rb
@@ -3,5 +3,17 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :milestone_number, Integer, null: true
+
+    # def milestone_number
+    #   BatchLoader::GraphQL
+    #     .for(object.id)
+    #     .batch do |target_ids, loader|
+    #       Assignment
+    #         .where(target_id: target_ids)
+    #         .each do |assignment|
+    #           loader.call(assignment.target_id, assignment.milestone_number)
+    #         end
+    #     end
+    # end
   end
 end

93ff8f9b954d7c636136b69650893cff6a38818e
Hari Gopal <mail@harigopal.in>
2024-04-04 21:17:08 +0530

    Add some comments explaining locale assignments

--- app/frontend/shared/i18n.js
+++ app/frontend/shared/i18n.js
@@ -4,8 +4,12 @@ import translations from "../locales.json";
 const i18n = new I18n();
 
 i18n.store(translations);
+
+// i18n-js uses the default locale only as the fallback locale. "en" is the safe choice for this.
 i18n.defaultLocale = "en";
 i18n.enableFallback = true;
+
+// Set the locale to the locale selected by the server - this takes server's choice for default locale into account.
 i18n.locale = window.pupilfirst.locale;
 i18n.missingTranslationPrefix = "Missing translation: ";
 

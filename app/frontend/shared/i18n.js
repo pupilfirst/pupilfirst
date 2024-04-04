219d5fc2cae87777e12faf4c8a25d64eef4edb72
Hari Gopal <mail@harigopal.in>
2024-04-04 21:07:59 +0530

    Minor reorder

--- app/frontend/shared/i18n.js
+++ app/frontend/shared/i18n.js
@@ -1,11 +1,12 @@
 import { I18n } from "i18n-js";
 import translations from "../locales.json";
 
-const i18n = new I18n(translations);
+const i18n = new I18n();
 
-i18n.enableFallback = true;
-i18n.missingTranslationPrefix = "Missing translation: ";
+i18n.store(translations);
 i18n.defaultLocale = "en";
+i18n.enableFallback = true;
 i18n.locale = window.pupilfirst.locale;
+i18n.missingTranslationPrefix = "Missing translation: ";
 
 export default i18n;

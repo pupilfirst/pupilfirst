aabfd9a9b6234daadcf8d0b9c1ae2a3c9da25e08
yash-learner <yash.topaz@gmail.com>
2024-05-27 00:56:26 +0530

    Update resolve path service spec

--- spec/services/notifications/resolve_path_service_spec.rb
+++ spec/services/notifications/resolve_path_service_spec.rb
@@ -32,5 +32,49 @@ describe Notifications::ResolvePathService do
         expect(path).to eq(url_helpers.topic_path(topic))
       end
     end
+
+    context "When its a submission comment created notification" do
+      let(:submission_comment) { create :submission_comment }
+      let!(:notification) do
+        create :notification,
+               event: Notification.events[:submission_comment_created],
+               notifiable: submission_comment
+      end
+
+      it 'resolves target path' do
+        path = subject.resolve
+        expect(path).to eq(url_helpers.target_path(submission_comment.submission.target))
+      end
+    end
+
+    context "When its a reaction created notification" do
+      context "When its a reaction on a submission" do
+        let(:reaction) { create :reaction }
+        let!(:notification) do
+          create :notification,
+                 event: Notification.events[:reaction_created],
+                 notifiable: reaction
+        end
+
+        it 'resolves target path' do
+          path = subject.resolve
+          expect(path).to eq(url_helpers.target_path(reaction.reactionable.target))
+        end
+      end
+
+      context "When its a reaction on a submission comment" do
+        let(:reaction) { create :reaction, reactionable: create(:submission_comment) }
+        let!(:notification) do
+          create :notification,
+                 event: Notification.events[:reaction_created],
+                 notifiable: reaction
+        end
+
+        it 'resolves target path' do
+          path = subject.resolve
+          expect(path).to eq(url_helpers.target_path(reaction.reactionable.submission.target))
+        end
+      end
+    end
   end
 end

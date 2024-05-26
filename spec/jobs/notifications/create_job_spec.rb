470ddc60974292a07eaf926d1f798f12660b1191
yash-learner <yash.topaz@gmail.com>
2024-05-27 00:31:05 +0530

    Update the create_job_spec

--- spec/jobs/notifications/create_job_spec.rb
+++ spec/jobs/notifications/create_job_spec.rb
@@ -62,5 +62,108 @@ describe Notifications::CreateJob do
         expect(notification.recipient).to eq(coach.user)
       end
     end
+
+    shared_context 'discussion assignment setup' do
+      let(:school) { create :school }
+      let(:course) { create :course, school: school }
+      let(:cohort) { create :cohort, course: course }
+      let(:level) { create :level, course: course }
+      let(:target_group) { create :target_group, level: level }
+      let(:student) { create :student, cohort: cohort }
+      let(:coach) { create :faculty }
+      let(:assignment) do
+        create :assignment, :with_default_checklist, discussion: true
+      end
+      let(:submission) do
+        create :timeline_event, :with_owners, target: assignment.target,
+               passed_at: 1.day.ago, owners: [student]
+      end
+      let(:actor) { resource.user }
+    end
+
+    context "When the event is submission_comment_created" do
+      include_context 'discussion assignment setup'
+
+      let(:event) { :submission_comment_created }
+      let(:resource) do
+        create :submission_comment,
+               submission: submission,
+               user: coach.user
+      end
+
+      it "will create notification for students of the submission" do
+        expect { subject.perform_now }.to change { Notification.count }.by(1)
+
+        notification = Notification.last
+        expect(notification.actor).to eq(resource.user)
+        expect(notification.notifiable_type).to eq('SubmissionComment')
+        expect(notification.notifiable_id).to eq(resource.id)
+        expect(notification.read_at).to eq(nil)
+        expect(notification.event).to eq('submission_comment_created')
+        expect(notification.message).to eq(
+          "#{resource.user.name} has commented on your submission for #{submission.target.title} target assignment"
+        )
+        expect(notification.recipient).to eq(student.user)
+      end
+    end
+
+    context 'when the event is reaction_created' do
+      include_context 'discussion assignment setup'
+
+      let(:event) { :reaction_created }
+
+      context 'when the reaction is on a submission' do
+        let(:resource) do
+          create :reaction,
+                 reactionable: submission,
+                 user: coach.user
+        end
+
+        it 'will create notification for students of the submission' do
+          expect { subject.perform_now }.to change { Notification.count }.by(1)
+
+          notification = Notification.last
+          expect(notification.actor).to eq(resource.user)
+          expect(notification.notifiable_type).to eq('Reaction')
+          expect(notification.notifiable_id).to eq(resource.id)
+          expect(notification.read_at).to eq(nil)
+          expect(notification.event).to eq('reaction_created')
+          expect(notification.message).to eq(
+            "#{resource.user.name} has reacted to your submission for #{submission.target.title} target assignment"
+          )
+          expect(notification.recipient).to eq(student.user)
+        end
+      end
+
+      context 'when the reaction is on a submission comment' do
+        let(:submission_comment) do
+          create :submission_comment,
+                 submission: submission,
+                 user: student.user
+        end
+        let(:resource) do
+          create :reaction,
+                 reactionable: submission_comment,
+                 user: coach.user
+        end
+
+        let(:actor) { resource.user }
+
+        it 'will create notification for the user of the comment' do
+          expect { subject.perform_now }.to change { Notification.count }.by(1)
+
+          notification = Notification.last
+          expect(notification.actor).to eq(resource.user)
+          expect(notification.notifiable_type).to eq('Reaction')
+          expect(notification.notifiable_id).to eq(resource.id)
+          expect(notification.read_at).to eq(nil)
+          expect(notification.event).to eq('reaction_created')
+          expect(notification.message).to eq(
+            "#{resource.user.name} has reacted to your comment on the submission for #{submission.target.title} target assignment"
+          )
+          expect(notification.recipient).to eq(student.user)
+        end
+      end
+    end
   end
 end

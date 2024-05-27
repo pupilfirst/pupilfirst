45af47f5c8f1f707ba7b9544e7b7a2ad965031de
yash-learner <yash.topaz@gmail.com>
2024-05-27 17:53:05 +0530

    Update submission_discussion spec

--- spec/system/targets/submission_discussion_spec.rb
+++ spec/system/targets/submission_discussion_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 feature "Assignment Discussion", js: true do
   include UserSpecHelper
   include NotificationHelper
+  include HtmlSanitizerSpecHelper
 
   let(:school) { create :school, :current }
   let(:course) { create :course, school: school, public_preview: true }
@@ -276,6 +277,13 @@ feature "Assignment Discussion", js: true do
         expect(page).to have_text(student.name)
         expect(page).to have_text("Great work!")
       end
+
+      open_email(another_student.email)
+      expect(current_email.subject).to include("New comment on your submission")
+      body = sanitize_html(current_email.body)
+      expect(body).to include("#{student.name} has commented on your submission for a discussion assignment")
+      expect(body).to include("Great work!")
+      expect(body).to include("http://test.host/targets/#{target.id}")
 
       page.refresh
 

1274552f6da9eade2b1f3e802f5f68ca68b04071
Bodhish Thomas <bodhish@gmail.com>
2024-06-04 17:51:41 +0530

    Add OnSupportDataService to handle support data

---
+++ app/services/beckn/api/on_support_data_service.rb
@@ -0,0 +1,25 @@
+module Beckn::Api
+  class OnSupportDataService < Beckn::DataService
+    def execute
+      return error_response("30004", "Order not found") if student.blank?
+
+      { support: { ref_id: order_id, email: school.email } }
+    end
+
+    def school
+      @school ||= student.school
+    end
+
+    def student
+      @student ||= Student.find_by(id: order_data[:student_id])
+    end
+
+    def order_data
+      @order_data ||= EncryptorService.new.decrypt(order_id)
+    end
+
+    def order_id
+      @order_id = @payload["message"]["support"]["ref_id"]
+    end
+  end
+end

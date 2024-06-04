5af21272529d6bd5f6ae99bcd3bc374e17b6ac47
Bodhish Thomas <bodhish@gmail.com>
2024-06-04 18:07:18 +0530

    Add OnRatingDataService to handle rating data

---
+++ app/services/beckn/api/on_rating_data_service.rb
@@ -0,0 +1,30 @@
+module Beckn::Api
+  class OnRatingDataService < Beckn::DataService
+    def execute
+      return error_response("30004", "Order not found") if student.blank?
+
+      create_rating
+
+      { message: {} }
+    end
+
+    def create_rating
+      course
+        .course_ratings
+        .where(user: student.user)
+        .first_or_create!(rating: @payload["value"])
+    end
+
+    def course
+      @course ||= student.course
+    end
+
+    def student
+      @student ||= Student.find_by(id: order_data[:student_id])
+    end
+
+    def order_id
+      @order_id = @payload["message"]["id"]
+    end
+  end
+end

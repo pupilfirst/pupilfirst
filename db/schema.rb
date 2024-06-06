722cb3585775085881d95d2c4bb8f682d00d44cb
Mohamad Abras <mohamad.abras@gmail.com>
2024-06-06 20:22:23 +0400

    Update ActiveRecord schema version to 7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_07_184819) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_07_184819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_stat_statements"
@@ -542,8 +542,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_07_184819) do
 
   create_table "quizzes", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.bigint "assignment_id"
     t.index ["assignment_id"], name: "index_quizzes_on_assignment_id"
   end

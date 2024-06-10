eeaf431f84a885874bdeda0078765b6d13211506
Bodhish Thomas <bodhish@gmail.com>
2024-06-10 21:08:13 +0530

    Rename Beckn::Api::ErrorDataService to Beckn::ErrorDataService

--- app/services/beckn/error_data_service.rb
+++ app/services/beckn/error_data_service.rb
@@ -1,6 +1,6 @@
 # Adapted from: https://github.com/beckn/protocol-specifications/blob/master/docs/BECKN-005-Error-Codes-Draft-01.md
-module Beckn::Api
-  class ErrorDataService < Beckn::DataService
+module Beckn
+  class ErrorDataService
     def data(code, message)
       { error: { type: "DOMAIN-ERROR", code: code, message: message } }
     end

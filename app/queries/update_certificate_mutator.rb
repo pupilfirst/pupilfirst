2e446f00352ad7173cdce030261708a63aba6ca5
Hari Gopal <mail@harigopal.in>
2024-06-14 15:30:11 +0530

    Autoformat

--- app/queries/update_certificate_mutator.rb
+++ app/queries/update_certificate_mutator.rb
@@ -2,11 +2,35 @@ class UpdateCertificateMutator < ApplicationQuery
   include AuthorizeSchoolAdmin
 
   property :id
-  property :margin, validates: { numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 20 } }
-  property :name_offset_top, validates: { numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 95 } }
-  property :font_size, validates: { numericality: { greater_than_or_equal_to: 75, less_than_or_equal_to: 150 } }
+  property :margin,
+           validates: {
+             numericality: {
+               greater_than_or_equal_to: 0,
+               less_than_or_equal_to: 20
+             }
+           }
+  property :name_offset_top,
+           validates: {
+             numericality: {
+               greater_than_or_equal_to: 0,
+               less_than_or_equal_to: 95
+             }
+           }
+  property :font_size,
+           validates: {
+             numericality: {
+               greater_than_or_equal_to: 75,
+               less_than_or_equal_to: 150
+             }
+           }
   property :qr_corner, validates: { presence: true }
-  property :qr_scale, validates: { numericality: { greater_than_or_equal_to: 50, less_than_or_equal_to: 150 } }
+  property :qr_scale,
+           validates: {
+             numericality: {
+               greater_than_or_equal_to: 50,
+               less_than_or_equal_to: 150
+             }
+           }
   property :active
   property :name, validates: { length: { minimum: 1, maximum: 30 } }
 

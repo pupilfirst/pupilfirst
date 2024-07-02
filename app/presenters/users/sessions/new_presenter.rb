668b1644499374a81c3a27c9eae750ff16005b29
Hari Gopal <mail@harigopal.in>
2024-07-02 23:54:13 +0530

    wip: Work on the password-based sign in page

--- app/presenters/users/sessions/new_presenter.rb
+++ app/presenters/users/sessions/new_presenter.rb
@@ -2,7 +2,7 @@ module Users
   module Sessions
     class NewPresenter < ApplicationPresenter
       def page_title
-        "#{I18n.t("presenters.users.sessions_new.page_title.title")} | #{school_name}"
+        "#{I18n.t("presenters.users.sessions.new.page_title")} | #{school_name}"
       end
 
       def props
@@ -132,7 +132,7 @@ module Users
             raise_unexpected_provider(provider)
           end
 
-        I18n.t("presenters.users.sessions_new.button_text.#{key}")
+        I18n.t("presenters.users.sessions.new.button_text.#{key}")
       end
 
       def raise_unexpected_provider(provider)

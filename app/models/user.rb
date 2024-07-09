bcd80028769ce61e36fe938f6246a36f6aa4db3c
Hari Gopal <mail@harigopal.in>
2024-07-09 23:29:06 +0530

    Consider user's time zone when generating token expiration timestamp

--- app/models/user.rb
+++ app/models/user.rb
@@ -164,7 +164,7 @@ class User < ApplicationRecord
     (
       login_token_generated_at +
         Rails.application.secrets.login_token_time_limit
-    ).strftime("%B %-d, %Y at %-l:%M %p")
+    ).in_time_zone(self.time_zone).strftime("%B %-d, %Y at %-l:%M %p")
   end
 
   # True if the user has ever signed in, handled by Users::ConfirmationService.

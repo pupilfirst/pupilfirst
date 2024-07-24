0d43d6ed3d415ff9012a3c83a8284b5b230da537
Kaiser Sakhi <mail@kaisersakhi.com>
2024-07-24 15:57:54 +0530

    Set default Puma worker count to 2

--- config/puma.rb
+++ config/puma.rb
@@ -14,8 +14,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
-  require "concurrent-ruby"
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY", 2))
   workers worker_count if worker_count > 1
 end
 

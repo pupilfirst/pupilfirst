42a2f96eca0b576738e7fa56f8f8e4db403caac3
yash-learner <yash.topaz@gmail.com>
2024-08-10 03:08:15 +0530

    Update system spec for testing the access

--- spec/factories/timeline_event.rb
+++ spec/factories/timeline_event.rb
@@ -44,6 +44,40 @@ FactoryBot.define do
       end
     end
 
+    trait :has_checklist_with_image_file do
+      transient { file_user { create(:user) } }
+
+      timeline_event_files do
+        # We'll set `timeline_event` to `nil` here, but it'll get set to the correct ID
+        # when this `timeline_event_files` assignment is completed.
+        [
+          create(
+            :timeline_event_file,
+            :image,
+            user: file_user,
+            timeline_event: nil
+          )
+        ]
+      end
+
+      checklist do
+        [
+          {
+            "kind" => Assignment::CHECKLIST_KIND_LONG_TEXT,
+            "title" => Faker::Lorem.sentence,
+            "result" => Faker::Lorem.sentence,
+            "status" => TimelineEvent::CHECKLIST_STATUS_NO_ANSWER
+          },
+          {
+            "kind" => Assignment::CHECKLIST_KIND_FILES,
+            "title" => Faker::Lorem.sentence,
+            "result" => [timeline_event_files.first.id.to_s],
+            "status" => TimelineEvent::CHECKLIST_STATUS_NO_ANSWER
+          }
+        ]
+      end
+    end
+
     trait :evaluated do
       evaluated_at { 1.day.ago }
       evaluator { create :faculty }

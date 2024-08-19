016496c0fd5c7b63d23d763274a13294f2b012e4
Mohamad Abras <mabras@users.noreply.github.com>
2024-08-19 10:07:28 +0300

    Upgrade to Rails 7.1 (#1564)
    Co-authored-by: Kaiser Sakhi <kaisersakhi@gmail.com>
    Co-authored-by: Kaiser Sakhi <mail@kaisersakhi.com>
    Co-authored-by: Yaswanth Kumar Bethu <yash.topaz@gmail.com>

--- app/services/encryptor_service.rb
+++ app/services/encryptor_service.rb
@@ -1,8 +1,6 @@
 class EncryptorService
-  def initialize(
-    key = Rails.application.secrets.secret_key_base.bytes[0..31].pack('c' * 32)
-  )
-    @crypt = ActiveSupport::MessageEncryptor.new(key, digest: 'base64_urlsafe')
+  def initialize(key = Settings.secret_key_base.bytes[0..31].pack("c" * 32))
+    @crypt = ActiveSupport::MessageEncryptor.new(key, digest: "base64_urlsafe")
   end
 
   def encrypt(value, options = {})
@@ -10,6 +8,15 @@ class EncryptorService
   end
 
   def decrypt(value, options = {})
-    @crypt.decrypt_and_verify(value, **options)
+    decrypted_value = @crypt.decrypt_and_verify(value, **options)
+
+    # If the decrypted value is a hash, convert its keys to symbols.
+    # This is necessary because the json_allow_marshal serializer primarily
+    # uses JSON, which converts all hash keys to strings during serialization.
+    if decrypted_value.is_a?(Hash)
+      decrypted_value.deep_symbolize_keys
+    else
+      decrypted_value
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   direct :rails_public_blob do |blob|
-    if Rails.env.development? || Rails.env.test? || ENV['CLOUDFRONT_HOST'].blank?
+    if Rails.env.local? || ENV['CLOUDFRONT_HOST'].blank?
       route =
         if blob.is_a?(ActiveStorage::Variant) || blob.is_a?(ActiveStorage::VariantWithRecord)
           :rails_representation
